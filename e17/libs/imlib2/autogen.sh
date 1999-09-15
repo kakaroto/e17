2968fda617e92789defbb7588c66e28fc44e014e
mej <mej@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-09-15 00:16:44 +0000

    *sigh*
    This should fix those weird autogen.sh problems.  Apparently
    automake --copy thinks it fails even though it really doesn't.
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@276 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/imlib2/autogen.sh
+++ e17/libs/imlib2/autogen.sh
@@ -1,9 +1,24 @@
 #! /bin/sh
 
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+THEDIR="`pwd`"
+cd "$srcdir"
+DIE=0
+
 set -x
 autoheader
 libtoolize --ltdl --force --copy
 aclocal
-automake --foreign --add-missing --copy
+automake --foreign --add-missing
 autoconf
 
+if test -z "$*"; then
+        echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+cd "$THEDIR"
+
+$srcdir/configure "$@" && echo && echo "Now type 'make' to compile imlib."
