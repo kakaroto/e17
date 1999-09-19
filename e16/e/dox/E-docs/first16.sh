b782c6c9e45c2cedd4f7a8dfccf82ebd20263e8f
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-09-19 21:28:36 +0000

    fix mistke in docs
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@333 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e16/e/dox/E-docs/first16.sh
+++ e16/e/dox/E-docs/first16.sh
@@ -2,47 +2,18 @@
 cat << __EOF__
 <page columns=1 padding=16 name=front background=Edoc_bg.png linkcolor=#88dddd>
 <font face=aircut3/12 color=#ffffff>
+<p align=50%>
+How old are you?
+<br>
+<br>
+_1-12(INPUT.first16-age-12.sh)<br>
+_13-15(INPUT.first16-age-15.sh)<br>
+_16-18(INPUT.first16-age-18.sh)<br>
+_19-21(INPUT.first16-age-21.sh)<br>
+_22-27(INPUT.first16-age-27.sh)<br>
+_28-35(INPUT.first16-age-35.sh)<br>
+_36-45(INPUT.first16-age-45.sh)<br>
+_46-59(INPUT.first16-age-59.sh)<br>
+_60+(INPUT.first16-age-60+.sh)<br>
 __EOF__
-# find the users .xinitrc, .xsession, .Xclients
-EXISTS="0"
-if [ -e ~/.xsession ]; then
-  EXISTS="1"
-fi
-if [ -e ~/.xinitrc ]; then
-  EXISTS="1"
-fi
-if [ -e ~/.Xclients ]; then
-  EXISTS="1"
-fi
-E_BIN=$E_BINDIR/enlightenment
-E_EESH=$E_BINDIR/eesh
-E_DOX=$E_BINDIR/dox
-E_DATA=$E_DATADIR
-
-
-if [ $EXISTS = "1" ]; then
 cat << __EOF__
-<p align=50%>
-It appears you already have some of your own X Login startup scripts (such as 
-.xsession, .xinitrc and .Xclients). If you have no idea what this is all
-about just click Contiue below and everything will be taken care of for you.
-<p align=50%>
-If you have customised these files and wish to keep your modifications, it is 
-suggested that you review each or all of them and manually ensure they run
-$E_BIN $0 as the window manager on the last line of your script(s).
-<p align=50%>
-If you wish to abort just close this window.
-<p align=50%>
-_Contiune(INPUT.first16-2.sh)
-__EOF__
-else
-cat << __EOF__
-<p align=50%>
-You appear to have no custom X Login setup for your user. If you click on
-Contiue below Enlightenment will set everything up for you
-<p align=50%>
-If you wish to abort just close this window.
-<p align=50%>
-_Contiune(INPUT.first16-2.sh)
-__EOF__
-fi
