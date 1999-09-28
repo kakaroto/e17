32687fb3eb66c87f62fe1c24e9e6f70473767164
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-09-28 23:36:58 +0000

    just a little cruft.  also the .spec file at least looks like it belongs to something now
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@448 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/eConfig/src/eConfig.h
+++ misc/eConfig/src/eConfig.h
@@ -1,3 +1,17 @@
+/*****************************************************************************/
+/* eConfig - the configuration library that just wouldn't die (yet)          */
+/*****************************************************************************/
+/* Copyright (C) 1999 - 1999 Carsten Haitzler (The Rasterman)                */
+/*                       and Geoff Harrison   (Mandrake)                     */
+/*                                                                           */
+/* This program and utilites is free software; you can redistribute it       */
+/* and/or modify it under the terms of the License shown in COPYING          */
+/*                                                                           */
+/* This software is distributed in the hope that it will be useful,          */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                      */
+/*****************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,6 +28,18 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <sys/resource.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
 
+void    econf_set_cache_size(int bytes);
+int     econf_get_cache_size(void);
+
+char    econf_get_bool  (char *type, int instance, char *key, char    def);
+int     econf_get_int   (char *type, int instance, char *key, int     def);
+double  econf_get_float (char *type, int instance, char *key, double  def);
+char   *econf_get_str   (char *type, int instance, char *key, char   *def);
+void   *econf_get_data  (char *type, int instance, char *key, void   *def);
+
+void    econf_set_bool  (char *type, int instance, char *key, char    val);
+void    econf_set_int   (char *type, int instance, char *key, int     val);
+void    econf_set_float (char *type, int instance, char *key, double  val);
+void    econf_set_str   (char *type, int instance, char *key, char   *val);
+void    econf_set_data  (char *type, int instance, char *key, void   *val);
