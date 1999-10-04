9b7ed22495d7441692b35fc757ca49c043f89779
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-10-04 20:54:17 +0000

    add copyrights.......
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@648 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e16/e/sample-scripts/bouncingball.pl
+++ e16/e/sample-scripts/bouncingball.pl
@@ -1,5 +1,26 @@
 #!/usr/bin/perl
 
+# Copyright (C) 1999 Carsten Haitzler, Geoff Harrison and various contributors
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies of the Software, its documentation and marketing & publicity
+# materials, and acknowledgment shall be given in the documentation, materials
+# and software packages that this Software was used.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 # This is a little script that will create a window that says
 # "Follow the Bouncing Ball" and then drops it to the bottom of the
 # screen and slowly bounces it around.
