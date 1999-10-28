b42d39d92afc068ae92c11c63065d69316fc3124
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-10-28 13:43:50 +0000

    better api concept
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1030 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/eConfig/src/eConfig.h
+++ misc/eConfig/src/eConfig.h
@@ -25,6 +25,7 @@ char **eConfigPaths(int *num);
 void   eConfigInit(void);
 
 void  *eConfigGetData(char *loc, int *length);
+void  *eConfigRefreshData(char *loc, int *length);
 void   eConfigUnloadData(char *loc);
 int    eConfigStoreData(char *loc, void *data, int *length, char local);
 
