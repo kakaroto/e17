0dcc4e98e97978a4ebde190d26c3e508bc12bba5
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-10-31 19:29:14 +0000

    comments.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1083 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/eConfig/src/cache.c
+++ misc/eConfig/src/cache.c
@@ -5,6 +5,7 @@ int    eConfigUpdateCache(void) {
 
 	/* this function should force an update of all the cache in memory
 	 * currently it does nothing :)
+	 * This function is exported by eConfig.
 	 */
 
 	return 0;
@@ -12,6 +13,11 @@ int    eConfigUpdateCache(void) {
 }
 
 void   eConfigSetCacheSize(unsigned long newsize) {
+
+	/* This function allows an application to set the cache size
+	 * that eConfig will use.
+	 * This function is exported by eConfig.
+	 */
 
 	eConfig.cachesize = newsize;
 	eConfigUpdateCache();
@@ -22,11 +28,23 @@ void   eConfigSetCacheSize(unsigned long newsize) {
 
 unsigned long  eConfigGetCacheSize(void) {
 
+	/* This function will return the current cachesize that 
+	 * eConfig is using.
+	 * This function is exported by eConfig.
+	 */
+
 	return eConfig.cachesize;
 
 }
 
 unsigned long eConfigGetCacheFilled(void) {
+
+	/* This function will return the current amount of filled data
+	 * that eConfig has used in the cache size.  Useful for displaying
+	 * statistics, as well as trimming the size of the cache using
+	 * eConfigSetCacheSize().
+	 * This function is exported by eConfig.
+	 */
 
 	return eConfig.cachesize;
 
