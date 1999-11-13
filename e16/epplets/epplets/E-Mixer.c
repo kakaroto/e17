7d4ce1392ff8229a9c9a644b684b41d0a464fd10
gilbertt <gilbertt@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-11-13 00:11:47 +0000

    Credit where its due ;-)
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1287 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e16/epplets/epplets/E-Mixer.c
+++ e16/epplets/epplets/E-Mixer.c
@@ -1,6 +1,6 @@
 /* E-Mixer.c
  *
- * Copyright (C) 1999 Tom Gilbert
+ * Copyright (C) 1999 Tom Gilbert, Steve Brunton
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -17,6 +17,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA
  */
+
+/* SGI Stuff skillfully implemented (and supported by) Steve Brunton
+ * <brunton@dweeb.turner.com> */
 
 #include <stdio.h>
 #include <sys/ioctl.h>
