0c47c2e8957f19bb8727ec2966066ae43e8039c1
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-11-23 17:24:20 +0000

    added copyright header to this file, too.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1377 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/eConfig/testapp/main.c
+++ misc/eConfig/testapp/main.c
@@ -1,47 +1,63 @@
+/*****************************************************************************/
+/* eConfig - the configuration library that just wouldn't die (yet)          */
+/*****************************************************************************/
+/* Copyright (C) 1999 - 1999 Carsten Haitzler (The Rasterman)                */
+/*                       and Geoff Harrison   (Mandrake)                     */
+/*                                                                           */
+/* This program and utilites is free software; you can redistribute it       */
+/* and/or modify it under the terms of the License shown in COPYING          */
+/*                                                                           */
+/* This software is distributed in the hope that it will be useful,          */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                      */
+/*****************************************************************************/
+
 #include "../src/eConfig.h"
 #include "../src/eConfig.internal.h"
 
 int
 main(int argc, char **argv)
 {
-	
-	char **stuff;
-	int num;
 
-	argc=0;
-	argv = NULL;
+   char              **stuff;
+   int                 num;
 
-	eConfigInit();
+   argc = 0;
+   argv = NULL;
 
-	eConfigAddPath("/home");
-	eConfigAddPath("/usr");
-	eConfigAddPath("/enlightenment");
+   eConfigInit();
 
-	if((stuff = eConfigPaths(&num))) {
-		int i;
+   eConfigAddPath("/home");
+   eConfigAddPath("/usr");
+   eConfigAddPath("/enlightenment");
 
+   if ((stuff = eConfigPaths(&num)))
+     {
+	int                 i;
 
-		for(i=0;i<num;i++) {
-			printf("path: %s\n",stuff[i]);
-		}
-		free(stuff);
-	
-	}
+	for (i = 0; i < num; i++)
+	  {
+	     printf("path: %s\n", stuff[i]);
+	  }
+	free(stuff);
 
-	printf("/home,/usr,and /enlightenment should have shown up\n");
-	eConfigRemovePath("/usr");
+     }
 
-	if((stuff = eConfigPaths(&num))) {
-		int i;
+   printf("/home,/usr,and /enlightenment should have shown up\n");
+   eConfigRemovePath("/usr");
 
+   if ((stuff = eConfigPaths(&num)))
+     {
+	int                 i;
 
-		for(i=0;i<num;i++) {
-			printf("path: %s\n",stuff[i]);
-		}
-		free(stuff);
-	
-	}
-	printf("now just /home and /enlightenment should have shown up\n");
+	for (i = 0; i < num; i++)
+	  {
+	     printf("path: %s\n", stuff[i]);
+	  }
+	free(stuff);
 
-	return 0;
+     }
+   printf("now just /home and /enlightenment should have shown up\n");
+
+   return 0;
 }
