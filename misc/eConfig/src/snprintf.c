57f915278094a3b34d121680187be696d8a8b5ab
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-11-23 17:45:35 +0000

    copyright notice.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1380 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/eConfig/src/snprintf.c
+++ misc/eConfig/src/snprintf.c
@@ -1,3 +1,17 @@
+/*****************************************************************************/
+/* eConfig - the configuration library that just wouldn't die (yet)          */
+/*****************************************************************************/
+/* Copyright (C) 1999 - 1999 Carsten Haitzler (The Rasterman)                */
+/*                       and Geoff Harrison   (Mandrake)                     */
+/*                                                                           */
+/* This program and utilites is free software; you can redistribute it       */
+/* and/or modify it under the terms of the License shown in COPYING          */
+/*                                                                           */
+/* This software is distributed in the hope that it will be useful,          */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                      */
+/*****************************************************************************/
+
 /* sgi's stdio.h has:
  * 
  * #if _SGIAPI && _NO_ANSIMODE
