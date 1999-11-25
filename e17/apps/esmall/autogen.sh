371776df4d2bb46db0e23863d451079c09e392ff
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-11-25 17:24:33 +0000

    bleh
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1399 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/esmall/autogen.sh
+++ e17/apps/esmall/autogen.sh
@@ -4,46 +4,48 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-THEDIR="`pwd`"
-cd "$srcdir"
+THEDIR=`pwd`
+cd $srcdir
+
 DIE=0
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have autoconf installed to compile Enlightenment."
-        echo "Download the appropriate package for your distribution,"
-        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-        DIE=1
+	echo
+	echo "You must have autoconf installed to compile esmall."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have automake installed to compile Enlightenment."
-        echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
-        echo "(or a newer version if it is available)"
-        DIE=1
+	echo
+	echo "You must have automake installed to compile esmall."
+	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
 }
 
 if test "$DIE" -eq 1; then
-        exit 1
+	exit 1
 fi
 
 if test -z "$*"; then
-        echo "I am going to run ./configure with no arguments - if you wish "
+	echo "I am going to run ./configure with no arguments - if you wish "
         echo "to pass any to it, please specify them on the $0 command line."
 fi
 
-echo "Generating configuration files for esmall, please wait...."
+case $CC in
+xlc )
+    am_opt=--include-deps;;
+esac
 
-echo "  aclocal $ACLOCAL_FLAGS"
 aclocal $ACLOCAL_FLAGS
-echo "  autoheader"
 autoheader
-echo "  automake --add-missing"
-automake --add-missing 
-echo "  autoconf"
+automake --add-missing $am_opt
 autoconf
+cd $THEDIR
 
-cd "$THEDIR"
+$srcdir/configure "$@"
 
-$srcdir/configure "$@" && echo && echo "Now type 'make' to compile esmall."
+echo 
+echo "Now type 'make' to compile esmall."
