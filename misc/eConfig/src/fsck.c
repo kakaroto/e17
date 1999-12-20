16f18b71f687e95f74588687adba597db37affa4
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-12-20 16:39:25 +0000

    Mon Dec 20 16:31:55 PST 1999 (Mandrake)
    about half of fsck is done.
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1697 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/eConfig/src/fsck.c
+++ misc/eConfig/src/fsck.c
@@ -21,13 +21,16 @@ eConfigFsckPath(char *path)
 
    /* This function is supposed to fsck a theme at the specified location
     * *path.  It is exported by the command.  returns a 0 on failure and
-    * a 1 on success.
+    * a 1 on success.  This function will remove all the "dirty" tagged
+    * entries in a fat table, and then it will create a new data table based
+    * off of all the non-dirty data.
     */
 
    char                tablepath[FILEPATH_LEN_MAX];
    char                tablepath2[FILEPATH_LEN_MAX];
    FILE               *FAT_TABLE;
    FILE               *NEW_FAT_TABLE;
+   eConfigFAT          tableentry;
 
    if (!path)
       return 0;
@@ -37,12 +40,33 @@ eConfigFsckPath(char *path)
    _econf_lock_path(path);
    if ((FAT_TABLE = fopen(tablepath, "r")))
      {
-	if ((NEW_FAT_TABLE = fopen(tablepath2, "r")))
+	if ((NEW_FAT_TABLE = fopen(tablepath2, "w")))
 	  {
+	     char               *last_pointer = NULL;
+
 	     while (!feof(FAT_TABLE))
 	       {
+
+		  fread(&tableentry, sizeof(eConfigFAT), 1, FAT_TABLE);
+		  if (strcmp(tableentry.loc, "dirty"))
+		    {
+		       if ((last_pointer && strcmp(tableentry.loc, last_pointer))
+			   || !last_pointer)
+			 {
+			    if (last_pointer)
+			       free(last_pointer);
+			    last_pointer = malloc(strlen(tableentry.loc) + 1);
+			    strcpy(last_pointer, tableentry.loc);
+			    fwrite(&tableentry, sizeof(eConfigFAT), 1,
+				   NEW_FAT_TABLE);
+			 }
+		    }
 	       }
+	     if (last_pointer)
+		free(last_pointer);
 	     fclose(NEW_FAT_TABLE);
+	     unlink(tablepath);
+	     rename(tablepath2, tablepath);
 	  }
 	fclose(FAT_TABLE);
      }
--- misc/eConfig/src/lock.c
+++ misc/eConfig/src/lock.c
@@ -15,7 +15,7 @@
 #include "eConfig.internal.h"
 #include "eConfig.h"
 
-int 
+int
 _econf_lock_path(char *path)
 {
 
