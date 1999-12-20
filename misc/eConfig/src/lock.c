550646ce71f3978655d643e8e5cdf00a51c822bf
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-12-20 17:35:06 +0000

    Mon Dec 20 17:27:23 PST 1999 (Mandrake)
    right.  locking.
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1703 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/eConfig/src/lock.c
+++ misc/eConfig/src/lock.c
@@ -15,7 +15,7 @@
 #include "eConfig.internal.h"
 #include "eConfig.h"
 
-int
+int 
 _econf_lock_path(char *path)
 {
 
@@ -36,41 +36,61 @@ _econf_lock_path(char *path)
    sprintf(lockpath, "%s/lock", path);
    if ((LOCKFILE = fopen(lockpath, "r")))
      {
+
 	while (!feof(LOCKFILE))
 	  {
+
 	  }
 	fclose(LOCKFILE);
+     }
+   else
+     {
+	/* we might have gotten the lock */
+	if ((LOCKFILE = fopen(lockpath, "r+")))
+	  {
+	     fprintf(LOCKFILE, "%i", getpid());
+	     fclose(LOCKFILE);
+	     if ((LOCKFILE = fopen(lockpath, "r")))
+	       {
+		  pid_t               mypid;
+
+		  fscanf(LOCKFILE, "%i", &mypid);
+		  if (mypid == getpid())
+		    {
+		       return 1;
+		    }
+		  else
+		    {
+		       return _econf_lock_path(path);
+		    }
+	       }
+	  }
+	else
+	  {
+	     /* we can't lock there because it probably doesn't exist */
+	     return 0;
+	  }
      }
    return 1;
 
 }
 
-int
+int 
 _econf_unlock_path(char *path)
 {
 
-   /* This function is supposed to lock a theme at the specified location
+   /* This function is supposed to unlock a theme at the specified location
     * *path.  It is not exported by the library.  returns a 0 on failure and
     * a 1 on success.
-    * This function will wait until the lock is attainable, and then lock
-    * the path, and check to make sure he was the one who locked it.  Once
-    * this lock is obtained, the function returns.  until then it sleeps.
     */
 
    char                lockpath[FILEPATH_LEN_MAX];
-   FILE               *LOCKFILE;
 
    if (!path)
       return 0;
 
    sprintf(lockpath, "%s/lock", path);
-   if ((LOCKFILE = fopen(lockpath, "r")))
-     {
-	while (!feof(LOCKFILE))
-	  {
-	  }
-	fclose(LOCKFILE);
-     }
+   unlink(lockpath);
    return 1;
 
 }
