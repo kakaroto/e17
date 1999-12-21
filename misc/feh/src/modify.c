939864df27695c8b13e00c1227ae686820e6a399
gilbertt <gilbertt@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-12-21 10:11:36 +0000

    And now, I've actually *added* it :)
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1719 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ misc/feh/src/modify.c
@@ -0,0 +1,179 @@
+/* modify.c
+ *
+ * Copyright (C) 1999 Tom Gilbert Andrew Glover
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+#include "feh.h"
+#include "modify.h"
+
+void
+feh_set_background (winwidget winwid)
+{
+  Window woot;
+  int screen;
+  screen = DefaultScreen (disp);
+  woot = RootWindow (disp, screen);
+  XSetWindowBackgroundPixmap (disp, woot, winwid->bg_pmap);
+  XClearWindow (disp, woot);
+  XFlush (disp);
+}
+
+void
+feh_blur (winwidget winwid, int radius)
+{
+  imlib_context_set_image (winwid->im);
+  imlib_image_blur (radius);
+  winwidget_rerender_image (winwid);
+}
+
+void
+feh_sharpen (winwidget winwid, int radius)
+{
+  imlib_context_set_image (winwid->im);
+  imlib_image_sharpen (radius);
+  winwidget_rerender_image (winwid);
+}
+
+void
+feh_tile_seamless_v (winwidget winwid)
+{
+  imlib_context_set_image (winwid->im);
+  imlib_image_tile_vertical ();
+  winwidget_rerender_image (winwid);
+}
+
+void
+feh_tile_seamless_h (winwidget winwid)
+{
+  imlib_context_set_image (winwid->im);
+  imlib_image_tile_horizontal ();
+  winwidget_rerender_image (winwid);
+}
+
+void
+feh_tile_seamless (winwidget winwid)
+{
+  imlib_context_set_image (winwid->im);
+  imlib_image_tile ();
+  winwidget_rerender_image (winwid);
+}
+
+void
+feh_rotate_clockwise (winwidget winwid)
+{
+  imlib_context_set_image (winwid->im);
+  imlib_image_flip_diagonal ();
+  winwidget_rerender_image (winwid);
+}
+
+void
+feh_flip_h (winwidget winwid)
+{
+  imlib_context_set_image (winwid->im);
+  imlib_image_flip_horizontal ();
+  winwidget_rerender_image (winwid);
+}
+
+void
+feh_flip_v (winwidget winwid)
+{
+  imlib_context_set_image (winwid->im);
+  imlib_image_flip_vertical ();
+  winwidget_rerender_image (winwid);
+}
+
+void
+feh_modify_brightness (winwidget winwid, double value)
+{
+  Imlib_Color_Modifier color_modifier;
+  imlib_context_set_image (winwid->im);
+  color_modifier = imlib_create_color_modifier ();
+  imlib_context_set_color_modifier (color_modifier);
+  imlib_modify_color_modifier_brightness (value);
+  imlib_apply_color_modifier ();
+  winwidget_rerender_image (winwid);
+  imlib_free_color_modifier ();
+}
+
+void
+feh_modify_brightness_to_rectangle (winwidget winwid, double value, int x0,
+				    int y0, int width, int height)
+{
+  Imlib_Color_Modifier color_modifier;
+  imlib_context_set_image (winwid->im);
+  color_modifier = imlib_create_color_modifier ();
+  imlib_context_set_color_modifier (color_modifier);
+  imlib_modify_color_modifier_brightness (value);
+  imlib_apply_color_modifier_to_rectangle (x0, y0, width, height);
+  winwidget_rerender_image (winwid);
+  imlib_free_color_modifier ();
+}
+
+void
+feh_modify_gamma (winwidget winwid, double value)
+{
+  Imlib_Color_Modifier color_modifier;
+  imlib_context_set_image (winwid->im);
+  color_modifier = imlib_create_color_modifier ();
+  imlib_context_set_color_modifier (color_modifier);
+  imlib_modify_color_modifier_gamma (value);
+  imlib_apply_color_modifier ();
+  winwidget_rerender_image (winwid);
+  imlib_free_color_modifier ();
+}
+
+void
+feh_modify_gamma_to_rectangle (winwidget winwid, double value, int x0, int y0,
+			       int width, int height)
+{
+  Imlib_Color_Modifier color_modifier;
+  imlib_context_set_image (winwid->im);
+  color_modifier = imlib_create_color_modifier ();
+  imlib_context_set_color_modifier (color_modifier);
+  imlib_modify_color_modifier_gamma (value);
+  imlib_apply_color_modifier_to_rectangle (x0, y0, width, height);
+  winwidget_rerender_image (winwid);
+  imlib_free_color_modifier ();
+}
+
+void
+feh_modify_contrast (winwidget winwid, double value)
+{
+  Imlib_Color_Modifier color_modifier;
+  imlib_context_set_image (winwid->im);
+  color_modifier = imlib_create_color_modifier ();
+  imlib_context_set_color_modifier (color_modifier);
+  imlib_modify_color_modifier_contrast (value);
+  imlib_apply_color_modifier ();
+  winwidget_rerender_image (winwid);
+  imlib_free_color_modifier ();
+}
+
+void
+feh_modify_contrast_to_rectangle (winwidget winwid, double value, int x0,
+				  int y0, int width, int height)
+{
+  Imlib_Color_Modifier color_modifier;
+  imlib_context_set_image (winwid->im);
+  color_modifier = imlib_create_color_modifier ();
+  imlib_context_set_color_modifier (color_modifier);
+  imlib_modify_color_modifier_contrast (value);
+  imlib_apply_color_modifier_to_rectangle (x0, y0, width, height);
+  winwidget_rerender_image (winwid);
+  imlib_free_color_modifier ();
+}
---
+++ misc/feh/src/modify.h
@@ -0,0 +1,39 @@
+/* modify.h
+ *
+ * Copyright (C) 1999 Tom Gilbert
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+void feh_set_background (winwidget winwid);
+void feh_blur (winwidget winwid, int radius);
+void feh_sharpen (winwidget winwid, int radius);
+void feh_tile_seamless_v (winwidget winwid);
+void feh_tile_seamless_h (winwidget winwid);
+void feh_tile_seamless (winwidget winwid);
+void feh_rotate_clockwise (winwidget winwid);
+void feh_flip_h (winwidget winwid);
+void feh_flip_v (winwidget winwid);
+void feh_modify_brightness (winwidget winwid, double value);
+void feh_modify_brightness_to_rectangle (winwidget winwid, double value,
+					 int x0, int y0, int width,
+					 int height);
+void feh_modify_gamma (winwidget winwid, double value);
+void feh_modify_gamma_to_rectangle (winwidget winwid, double value, int x0,
+				    int y0, int width, int height);
+void feh_modify_contrast (winwidget winwid, double value);
+void feh_modify_contrast_to_rectangle (winwidget winwid, double value, int x0,
+				       int y0, int width, int height);
