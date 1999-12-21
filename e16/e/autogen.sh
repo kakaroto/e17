c3c204a2c84977148b5c72cf4524df6b51229c6e
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-12-21 15:10:04 +0000

    Tue Dec 21 15:15:53 PST 1999 (Mandrake)
    added gettext check to autogen.sh
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1727 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e16/e/autogen.sh
+++ e16/e/autogen.sh
@@ -12,15 +12,21 @@ DIE=0
         echo
         echo "You must have autoconf installed to compile Enlightenment."
         echo "Download the appropriate package for your distribution,"
-        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/autoconf"
         DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
         echo
         echo "You must have automake installed to compile Enlightenment."
-        echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
-        echo "(or a newer version if it is available)"
+        echo "Get it at ftp://ftp.gnu.org/pub/gnu/automake"
+        DIE=1
+}
+
+(gettext --version) < /dev/null > /dev/null 2>&1 || {
+        echo
+        echo "You must have gettext installed to compile Enlightenment."
+        echo "Get ftp://ftp.gnu.org/pub/gnu/gettext"
         DIE=1
 }
 
