61d04c5e35587d14184b7a68c556f4558097e761
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-12-27 12:13:28 +0000

    forgot to add these files.  menu shortcuts
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1764 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ misc/flipbook/src/menus.h
@@ -0,0 +1,4 @@
+GtkWidget *CreateBarSubMenu(GtkWidget *menu, char *szName);
+GtkWidget * CreateMenuItem(GtkWidget *menu, char *szName, char *szAccel, 
+		char *szTip, GtkSignalFunc func, gpointer data);
+
