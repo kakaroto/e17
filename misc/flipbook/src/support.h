b2f71aa26ff8545bb8c0f8d2a1e3129a7b94629d
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
1999-12-29 13:26:31 +0000

    added Brad's name to all the copyrights
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1794 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/flipbook/src/support.h
+++ misc/flipbook/src/support.h
@@ -2,7 +2,7 @@
 /* VA Linux Systems Flipbook demo                                            */
 /*****************************************************************************/
 /*
- * Copyright (C) 1999 Geoff Harrison and VA Linux Systems
+ * Copyright (C) 1999 Bradtham, Geoff Harrison, and VA Linux Systems
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
  * deal in the Software without restriction, including without limitation the
