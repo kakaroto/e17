6eb3b9610c49153f6c75c4e3cb9f40bc9be2afc6
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-01-06 09:10:15 +0000

    updated copyright to 2000
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1867 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e16/e/eesh/lists.c
+++ e16/e/eesh/lists.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 1999 Carsten Haitzler, Geoff Harrison and various contributors
+ *  Copyright (C) 2000 Carsten Haitzler, Geoff Harrison and various contributors
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to
@@ -20,6 +20,7 @@
  *  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  *  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 #include "E.h"
 
 void               *
