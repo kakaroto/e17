6f4796532881bdc87fa31bcc7437453266b7de1e
gilbertt <gilbertt@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-01-20 20:17:11 +0000

    Thu Jan 20 20:15:26 GMT 2000 (gilbertt)
    Argh. Finally got bugged by E-Exec's size not tesselating properly, and so I
    added a --proper-size commandline option to fix that right up =)
    
    Sorry StriderZ ;)
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@1954 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e16/epplets/epplets/E-Exec.c
+++ e16/epplets/epplets/E-Exec.c
@@ -6,145 +6,159 @@
 
 #define MAX_HIST_LEN	15
 
-Epplet_gadget       textbox = NULL;
-Epplet_gadget       history_popup = NULL;
-char               *command_history[MAX_HIST_LEN];
-int                 current_command = 0;
-int                 num_commands = 0;
+Epplet_gadget textbox = NULL;
+Epplet_gadget history_popup = NULL;
+char *command_history[MAX_HIST_LEN];
+int current_command = 0;
+int num_commands = 0;
 
-static void         cb_close(void *data);
-static void         run_contents(void *data);
-static void         change_textbox(void *data);
+static void cb_close (void *data);
+static void run_contents (void *data);
+static void change_textbox (void *data);
 
 static void
-cb_close(void *data)
+cb_close (void *data)
 {
-   Epplet_cleanup();
-   Epplet_unremember();
-   exit(0);
-   data = NULL;
+  Epplet_cleanup ();
+  Epplet_unremember ();
+  exit (0);
+  data = NULL;
 }
 
 static void
-run_contents(void *data)
+run_contents (void *data)
 {
-   char               *command = Epplet_textbox_contents(textbox);
-   int                 i;
+  char *command = Epplet_textbox_contents (textbox);
+  int i;
 
-   if (command && strlen(command))
-     {
+  if (command && strlen (command))
+    {
 
-	if (num_commands < MAX_HIST_LEN)
-	   command_history[num_commands++] = strdup(command);
-	else
-	  {
-	     free(command_history[0]);
-	     Epplet_remove_popup_entry(history_popup, 1);
+      if (num_commands < MAX_HIST_LEN)
+	command_history[num_commands++] = strdup (command);
+      else
+	{
+	  free (command_history[0]);
+	  Epplet_remove_popup_entry (history_popup, 1);
 
-	     for (i = 0; i < MAX_HIST_LEN; i++)
-		command_history[i] = command_history[i + 1];
+	  for (i = 0; i < MAX_HIST_LEN; i++)
+	    command_history[i] = command_history[i + 1];
 
-	     command_history[MAX_HIST_LEN - 1] = strdup(command);
-	  }
+	  command_history[MAX_HIST_LEN - 1] = strdup (command);
+	}
 
-	current_command = num_commands;
+      current_command = num_commands;
 
-	Epplet_add_popup_entry(history_popup, command, NULL, change_textbox,
-			       strdup(command));
-	Epplet_spawn_command(command);
-	Epplet_reset_textbox(textbox);
-     }
+      Epplet_add_popup_entry (history_popup, command, NULL, change_textbox,
+			      strdup (command));
+      Epplet_spawn_command (command);
+      Epplet_reset_textbox (textbox);
+    }
 
-   return;
-   data = NULL;
+  return;
+  data = NULL;
 }
 
 static void
-change_textbox(void *data)
+change_textbox (void *data)
 {
-   char               *s = (char *)data;
+  char *s = (char *) data;
 
-   Epplet_change_textbox(textbox, s);
+  Epplet_change_textbox (textbox, s);
 }
 
 static void
-hist_last(void *data)
+hist_last (void *data)
 {
-   if (current_command == 0)
-     {
-	current_command = num_commands;
-	Epplet_reset_textbox(textbox);
-     }
-   else
-     {
-	Epplet_change_textbox(textbox, command_history[--current_command]);
-     }
+  if (current_command == 0)
+    {
+      current_command = num_commands;
+      Epplet_reset_textbox (textbox);
+    }
+  else
+    {
+      Epplet_change_textbox (textbox, command_history[--current_command]);
+    }
 
-   return;
-   data = NULL;
+  return;
+  data = NULL;
 }
 
 static void
-hist_next(void *data)
+hist_next (void *data)
 {
-   if (current_command == num_commands - 1)
-     {
-	current_command++;
-	Epplet_reset_textbox(textbox);
-	return;
-     }
-   else if (current_command >= num_commands)
-     {
-	current_command = 0;
-     }
-   else
-     {
-	current_command++;
-     }
-   Epplet_change_textbox(textbox, command_history[current_command]);
+  if (current_command == num_commands - 1)
+    {
+      current_command++;
+      Epplet_reset_textbox (textbox);
+      return;
+    }
+  else if (current_command >= num_commands)
+    {
+      current_command = 0;
+    }
+  else
+    {
+      current_command++;
+    }
+  Epplet_change_textbox (textbox, command_history[current_command]);
 
-   return;
-   data = NULL;
+  return;
+  data = NULL;
 }
 
 int
-main(int argc, char *argv[])
+main (int argc, char *argv[])
 {
-   atexit(Epplet_cleanup);
+  atexit (Epplet_cleanup);
 
-   Epplet_Init(EPPLET_NAME, EPPLET_VERSION, EPPLET_INFO, 5, 3, argc, argv, 0);
+  if ((argc > 1) && (!strcmp ("--proper-size", argv[1])))
+    {
+      Epplet_Init (EPPLET_NAME, EPPLET_VERSION, EPPLET_INFO, 6, 3, argc, argv,
+		   0);
 
-   Epplet_gadget_show(Epplet_create_button(NULL, NULL, 2, 2,
-					   12, 12, "CLOSE", 0, NULL, cb_close,
-					   NULL));
+      Epplet_gadget_show (Epplet_create_label (-12, 2, "E-Exec", 2));
 
-   Epplet_gadget_show(Epplet_create_label(-12, 2, "E-Exec", 2));
+      textbox =
+	Epplet_create_textbox (NULL, NULL, 2, 32, 92, 14, 2, run_contents,
+			       NULL);
+    }
+  else
+    {
+      Epplet_Init (EPPLET_NAME, EPPLET_VERSION, EPPLET_INFO, 5, 3, argc, argv,
+		   0);
+      Epplet_gadget_show (Epplet_create_label (-12, 2, "E-Exec", 2));
 
-   Epplet_gadget_show(Epplet_create_button(NULL, NULL, 2, 16,
-					   12, 12, "PREVIOUS", 0, NULL,
-					   hist_last, NULL));
+      textbox =
+	Epplet_create_textbox (NULL, NULL, 2, 32, 76, 14, 2, run_contents,
+			       NULL);
+    }
+  Epplet_gadget_show (Epplet_create_button (NULL, NULL, 2, 2,
+					    12, 12, "CLOSE", 0, NULL,
+					    cb_close, NULL));
 
-   Epplet_gadget_show(Epplet_create_button(NULL, NULL, 16, 16,
-					   12, 12, "NEXT", 0, NULL, hist_next,
-					   NULL));
+  Epplet_gadget_show (Epplet_create_button (NULL, NULL, 2, 16,
+					    12, 12, "PREVIOUS", 0, NULL,
+					    hist_last, NULL));
 
-   Epplet_gadget_show(Epplet_create_button(NULL, NULL, 60, 16,
-					   12, 12, "PLAY", 0, NULL,
-					   run_contents, NULL));
+  Epplet_gadget_show (Epplet_create_button (NULL, NULL, 16, 16,
+					    12, 12, "NEXT", 0, NULL,
+					    hist_next, NULL));
 
-   history_popup = Epplet_create_popup();
-   Epplet_add_popup_entry(history_popup, "-NeverMind-", NULL, NULL, NULL);
+  Epplet_gadget_show (Epplet_create_button (NULL, NULL, 60, 16,
+					    12, 12, "PLAY", 0, NULL,
+					    run_contents, NULL));
 
-   Epplet_gadget_show(Epplet_create_popupbutton(NULL, NULL, 30, 16,
-						12, 12, "ARROW_UP",
-						history_popup));
+  history_popup = Epplet_create_popup ();
+  Epplet_add_popup_entry (history_popup, "-NeverMind-", NULL, NULL, NULL);
 
-   textbox =
-      Epplet_create_textbox(NULL, NULL, 2, 32, 76, 14, 2, run_contents, NULL);
+  Epplet_gadget_show (Epplet_create_popupbutton (NULL, NULL, 30, 16,
+						 12, 12, "ARROW_UP",
+						 history_popup));
 
-   Epplet_gadget_show(textbox);
+  Epplet_gadget_show (textbox);
 
-   Epplet_show();
-   Epplet_Loop();
-   return 0;
+  Epplet_show ();
+  Epplet_Loop ();
+  return 0;
 }
