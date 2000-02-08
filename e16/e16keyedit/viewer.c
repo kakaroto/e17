0ea91493765709eaf4aa24941ef411d7f0ccace0
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-02-08 22:49:32 +0000

    still not much here.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@2020 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e16/e16keyedit/viewer.c
+++ e16/e16keyedit/viewer.c
@@ -13,6 +13,45 @@ extern GtkAccelGroup *accel_group;
 GtkWidget *clist;
 
 void
+selection_made(GtkWidget *clist, gint row, gint column, GdkEventButton *event,
+		                    gpointer data)
+{
+
+	return;
+}
+
+void
+on_resort_columns(GtkWidget *widget, gint column, gpointer user_data)
+{
+	static int order=0;
+	static int last_col=0;
+
+	if(user_data) {
+		widget = NULL;
+	}
+	gtk_clist_set_sort_column(GTK_CLIST(clist),column);
+	if(last_col == column) {
+		if(order) {
+			order=0;
+			gtk_clist_set_sort_type(GTK_CLIST(clist),GTK_SORT_DESCENDING);
+		} else {
+			order=1;
+			gtk_clist_set_sort_type(GTK_CLIST(clist),GTK_SORT_ASCENDING);
+		}
+	} else {
+		order=1;
+		gtk_clist_set_sort_type(GTK_CLIST(clist),GTK_SORT_ASCENDING);
+		last_col = column;
+	}
+
+	gtk_clist_sort(GTK_CLIST(clist));
+
+	return;
+
+}
+
+
+void
 on_exit_application(GtkWidget * widget, gpointer user_data)
 {
 
@@ -89,6 +128,16 @@ create_list_window(void)
 	clist = gtk_clist_new(4);
 	gtk_widget_show(clist);
 	gtk_container_add(GTK_CONTAINER(scrollybit), clist);
+
+	gtk_clist_set_column_title(GTK_CLIST(clist), 0, "Modifier");
+	gtk_clist_set_column_title(GTK_CLIST(clist), 1, "Key");
+	gtk_clist_set_column_title(GTK_CLIST(clist), 2, "Action to perform");
+	gtk_clist_set_column_title(GTK_CLIST(clist), 3, "Optional Parameters");
+	gtk_clist_column_titles_show(GTK_CLIST(clist));
+	gtk_signal_connect(GTK_OBJECT(clist), "select_row",
+			GTK_SIGNAL_FUNC(selection_made), NULL);
+	gtk_signal_connect(GTK_OBJECT(clist), "click_column",
+			GTK_SIGNAL_FUNC(on_resort_columns), NULL);
 
 
 	return list_window;
--- e16/e16keyedit/viewer.h
+++ e16/e16keyedit/viewer.h
@@ -1,5 +1,9 @@
 GtkWidget *create_list_window(void);
 void on_exit_application(GtkWidget * widget, gpointer user_data);
+void selection_made(GtkWidget *clist, gint row, gint column,
+	   	GdkEventButton *event, gpointer data);
+void on_resort_columns(GtkWidget *widget, gint column, gpointer user_data);
+
 
 
 
