c5c2c298a44568ea501a7fbb4382953c25567739
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-02-08 23:23:44 +0000

    still nothing here.  adding ipc stuff in
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@2021 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e16/e16keyedit/viewer.c
+++ e16/e16keyedit/viewer.c
@@ -6,11 +6,16 @@
 #include <string.h>
 #include "viewer.h"
 #include "menus.h"
+#include "ipc.h"
 
 extern GtkTooltips *tooltips;
 extern GtkAccelGroup *accel_group;
 
 GtkWidget *clist;
+
+static void receive_ipc_msg(gchar * msg);
+gchar *e_ipc_msg = NULL;
+
 
 void
 selection_made(GtkWidget *clist, gint row, gint column, GdkEventButton *event,
@@ -72,6 +77,9 @@ create_list_window(void)
 	GtkWidget *panes;
 	GtkWidget *scrollybit;
 	GtkWidget *vbox;
+	GtkWidget *frames;
+	GtkWidget *alignment;
+	GtkWidget *frame_vbox;
 
 
 	list_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -139,9 +147,33 @@ create_list_window(void)
 	gtk_signal_connect(GTK_OBJECT(clist), "click_column",
 			GTK_SIGNAL_FUNC(on_resort_columns), NULL);
 
+	vbox = gtk_vbox_new(FALSE,0);
+	gtk_widget_show(vbox);
+	frames = gtk_frame_new("Edit Keybinding Properties");
+	gtk_container_set_border_width(GTK_CONTAINER(frames),2);
+	gtk_widget_show(frames);
+	gtk_paned_pack2(GTK_PANED(panes),vbox,FALSE,TRUE);
+	gtk_box_pack_start(GTK_BOX(vbox), frames, TRUE, TRUE, 0);
+
+
+	frame_vbox = gtk_vbox_new(FALSE,3);
+	gtk_widget_show(frame_vbox);
+
+	gtk_container_set_border_width(GTK_CONTAINER(frame_vbox),4);
+	gtk_container_add(GTK_CONTAINER(frames), frame_vbox);
+			
 
 	return list_window;
 
+}
+
+static void receive_ipc_msg(gchar * msg)
+{
+	gdk_flush();
+	e_ipc_msg = g_strdup(msg);
+	/* */
+	gtk_main_quit();
+	/*  got_e_ipc_msg = 1;*/
 }
 
 int main(int argc, char *argv[])
@@ -153,6 +185,44 @@ int main(int argc, char *argv[])
 
 	tooltips = gtk_tooltips_new();
 	accel_group = gtk_accel_group_new();
+
+	if(!CommsInit(receive_ipc_msg)) {
+		GtkWidget *win, *label, *align, *frame, *button, *vbox;
+
+		win = gtk_window_new(GTK_WINDOW_POPUP);
+		gtk_window_set_policy(GTK_WINDOW(win), 0, 0, 1);
+		gtk_window_set_position(GTK_WINDOW(win), GTK_WIN_POS_CENTER);
+		frame = gtk_frame_new(NULL);
+		gtk_frame_set_shadow_type(GTK_FRAME(frame), GTK_SHADOW_OUT);
+		align = gtk_alignment_new(0.0, 0.0, 0.0, 0.0);
+		gtk_container_set_border_width(GTK_CONTAINER(align), 32);
+		vbox = gtk_vbox_new(FALSE, 5);
+		button = gtk_button_new_with_label("Quit");
+		gtk_signal_connect(GTK_OBJECT(button), "clicked",
+				GTK_SIGNAL_FUNC(on_exit_application), NULL);
+		label = gtk_label_new("You are not running Enlightenment\n"
+					"\n"
+					"This window manager has to be running in order\n"
+					"to configure it.\n" "\n");
+		gtk_container_add(GTK_CONTAINER(win), frame);
+		gtk_container_add(GTK_CONTAINER(frame), align);
+		gtk_container_add(GTK_CONTAINER(align), vbox);
+		gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 0);
+		gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
+		gtk_widget_show_all(win);
+		gtk_main();
+		exit(1);
+	}
+	CommsSend("set clientname Enlightenment Configuration Utility");
+	CommsSend("set version 0.1.0");
+	CommsSend("set author Mandrake (Geoff Harrison)");
+	CommsSend("set email mandrake@mandrake.net");
+	CommsSend("set web http://mandrake.net/");
+	CommsSend("set address C/O VA Linux Systems, USA");
+	CommsSend("set info "
+			"This is the Enlightenemnt KeyBindings Configuration Utility\n"
+			"that uses Enlightenment's IPC mechanism to configure\n"
+			"it remotely.");
 
 	lister = create_list_window();
 
