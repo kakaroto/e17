9b8830a36d25f0f69eec941e73f07e078129aa69
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-02-13 01:41:11 +0000

    nothing here yet.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@2058 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e16/e16menuedit/viewer.c
@@ -0,0 +1,12 @@
+#include <gdk_imlib.h>
+#include <gdk/gdkkeysyms.h>
+#include <gtk/gtk.h>
+#include <glib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+
+
