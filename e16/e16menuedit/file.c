e9c91e30949525f2b2b0204cdc644895d9276855
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-02-14 02:45:06 +0000

    forgot some files
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@2069 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e16/e16menuedit/file.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include "file.h"
+	
+char *
+homedir(int uid)
+{
+	char *s;
+
+	static int usr_uid = -1;
+	static char *usr_s = NULL;
+	struct passwd *pwd;
+
+	if (usr_uid < 0)
+		usr_uid = getuid();
+	if ((uid == usr_uid) && (usr_s)) {
+		return(duplicate(usr_s));
+	}
+	pwd = getpwuid(uid);
+	if (pwd) {
+		s = duplicate(pwd->pw_dir);
+		if (uid == usr_uid)
+			usr_s = duplicate(s);
+		return(s);
+	}
+	return(duplicate((getenv("TMPDIR") == NULL) ? "/tmp"
+				: getenv("TMPDIR")));
+}
+
+char *
+field(char *s, int field)
+{
+	char                buf[4096];
+
+	buf[0] = 0;
+	fword(s, field + 1, buf);
+	if (buf[0]) {
+		if ((!strcmp(buf, "NULL")) || (!strcmp(buf, "(null)")))
+			return(NULL);
+		return(duplicate(buf));
+	}
+	return NULL;
+}
+
+char *
+duplicate(char *s)
+{
+	char               *ss;
+	int                 sz;
+
+	if (!s)
+		return(NULL);
+	sz = strlen(s);
+	ss = malloc(sz + 1);
+	strncpy(ss, s, sz + 1);
+	return(ss);
+}
+
+void fword(char *s, int num, char *wd)
+{
+	char *cur, *start, *end;
+	int count, inword, inquote, len;
+
+	if (!s)
+		return;
+	if (!wd)
+		return;
+	*wd = 0;
+	if (num <= 0)
+		return;
+	cur = s;
+	count = 0;
+	inword = 0;
+	inquote = 0;
+	start = NULL;
+	end = NULL;
+	while ((*cur) && (count < num)) {
+		if (inword) {
+			if (inquote) {
+				if (*cur == '"') {
+					inquote = 0;
+					inword = 0;
+					end = cur;
+					count++;
+				}
+			} else {
+				if (isspace(*cur)) {
+					end = cur;
+					inword = 0;
+					count++;
+				}
+			}
+		} else {
+			if (!isspace(*cur)) {
+				if (*cur == '"') {
+					inquote = 1;
+					start = cur + 1;
+				} else
+					start = cur;
+				inword = 1;
+			}
+		}
+		if (count == num)
+			break;
+		cur++;
+	}
+	if (!start)
+		return;
+	if (!end)
+		end = cur;
+	if (end <= start)
+		return;
+	len = (int) (end - start);
+	if (len > 4000)
+		len = 4000;
+	if (len > 0) {
+		strncpy(wd, start, len);
+		wd[len] = 0;
+	}
+	return;
+}
+
---
+++ e16/e16menuedit/file.h
@@ -0,0 +1,4 @@
+char *field(char *s, int field);
+char *duplicate(char *s);
+void fword(char *s, int num, char *wd);
+char *homedir(int uid);
