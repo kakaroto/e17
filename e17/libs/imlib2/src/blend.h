a3711803b339f28c3a93047bb39e12544840e150
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-03-26 00:35:43 +0000

    ummmm fix dat.....
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@2331 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/imlib2/src/blend.h
+++ e17/libs/imlib2/src/blend.h
@@ -1,12 +1,14 @@
 #ifndef __BLEND
 #define __BLEND 1
 
+#include "../config.h"
+
 #ifndef WORDS_BIGENDIAN
 
-#define A_VAL(p) ((DATA8 *)(p))[0]
-#define R_VAL(p) ((DATA8 *)(p))[1]
-#define G_VAL(p) ((DATA8 *)(p))[2]
-#define B_VAL(p) ((DATA8 *)(p))[3]
+#define A_VAL(p) ((DATA8 *)(p))[3]
+#define R_VAL(p) ((DATA8 *)(p))[2]
+#define G_VAL(p) ((DATA8 *)(p))[1]
+#define B_VAL(p) ((DATA8 *)(p))[0]
 
 #else
 
