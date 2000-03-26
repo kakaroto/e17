7976459af8ff3236db609672d4ad1c2f3c4eff76
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-03-26 00:10:45 +0000

    fix endianes problems..... works now on sparc solaris nicely.. :)
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@2330 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/imlib2/src/blend.h
+++ e17/libs/imlib2/src/blend.h
@@ -3,10 +3,10 @@
 
 #ifndef WORDS_BIGENDIAN
 
-#define A_VAL(p) ((DATA8 *)(p))[3]
-#define R_VAL(p) ((DATA8 *)(p))[2]
-#define G_VAL(p) ((DATA8 *)(p))[1]
-#define B_VAL(p) ((DATA8 *)(p))[0]
+#define A_VAL(p) ((DATA8 *)(p))[0]
+#define R_VAL(p) ((DATA8 *)(p))[1]
+#define G_VAL(p) ((DATA8 *)(p))[2]
+#define B_VAL(p) ((DATA8 *)(p))[3]
 
 #else
 
