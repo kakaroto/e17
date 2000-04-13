9e1b87c26721864ce3b55a68e7ffbdda65bae369
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-04-13 17:10:50 +0000

    oops =- add
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@2483 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/libs/imlib2/src/filter.h
@@ -0,0 +1,39 @@
+#ifndef __FILTER
+#define __FILTER 1
+
+typedef struct _imlib_filter ImlibFilter;
+typedef struct _imlib_filter_color ImlibFilterColor;
+typedef struct _imlib_filter_pixel ImlibFilterPixel;
+
+struct _imlib_filter_pixel
+{
+   int xoff, yoff;
+   int a, r, g, b;
+};
+
+struct _imlib_filter_color
+{
+   int size, entries;
+   int div, cons;
+   ImlibFilterPixel *pixels;
+};
+
+struct _imlib_filter
+{
+   ImlibFilterColor alpha, red, green, blue;
+};
+
+ImlibFilter *
+__imlib_CreateFilter(int size);
+void
+__imlib_FreeFilter(ImlibFilter *fil);
+void
+__imlib_FilterSet(ImlibFilterColor *fil, int x, int y,
+		  int a, int r, int g, int b);
+void
+__imlib_FilterDivisors(ImlibFilter *fil, int a, int r, int g, int b);
+void
+__imlib_FilterConstants(ImlibFilter *fil, int a, int r, int g, int b);
+void
+__imlib_FilterImage(ImlibImage *im, ImlibFilter *fil);
+#endif
