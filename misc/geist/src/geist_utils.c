6b14771df4905bb14bbc801c5870d8a8e60a0b10
gilbertt <gilbertt@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-09-08 19:42:47 +0000

    need these
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@3361 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ misc/geist/src/geist_utils.c
@@ -0,0 +1,49 @@
+/* geist_utils.c */
+
+#include "geist_utils.h"
+#include "geist_list.h"
+
+geist_list *
+geist_string_split(const char *string, const char *delimiter)
+{
+   geist_list *string_list = NULL;
+   char *s;
+   unsigned int n = 1;
+
+   D_ENTER(3);
+
+   if (!string || !delimiter)
+      D_RETURN(3, NULL);
+
+   s = strstr(string, delimiter);
+   if (s)
+   {
+      unsigned int delimiter_len = strlen(delimiter);
+
+      do
+      {
+         unsigned int len;
+         char *new_string;
+
+         len = s - string;
+         new_string = emalloc(sizeof(char) * (len + 1));
+
+         strncpy(new_string, string, len);
+         new_string[len] = 0;
+         string_list = geist_list_add_front(string_list, new_string);
+         n++;
+         string = s + delimiter_len;
+         s = strstr(string, delimiter);
+      }
+      while (s);
+   }
+   if (*string)
+   {
+      n++;
+      string_list = geist_list_add_front(string_list, estrdup((char *)string));
+   }
+
+   string_list = geist_list_reverse(string_list);
+
+   D_RETURN(3, string_list);
+}
---
+++ misc/geist/src/geist_utils.h
@@ -0,0 +1,11 @@
+/* geist_utils.h */
+
+#ifndef GEIST_UTILS_H
+#define GEIST_UTILS_H
+
+#include "geist.h"
+
+geist_list *
+geist_string_split(const char *string, const char *delimiter);
+
+#endif
