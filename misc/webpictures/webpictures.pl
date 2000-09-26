87c4fff636a9f0242acaae486b967962d36aec65
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-09-26 19:02:33 +0000

    small patch from Maher Awamy <muhri@muhri.net>
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@3547 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/webpictures/webpictures.pl
+++ misc/webpictures/webpictures.pl
@@ -42,7 +42,12 @@ sub select_clist {
 	($widget,$row,$col,$ev,$data) = @_;
 
 	$text = $widget->get_text($row,$col);
-#print "$text\n";
+	#check if its a file to beign with and then if its an image.
+	if (!-f $text || $text !~ /(bmp|gif|jpg|jpeg|png)$/i) {
+		return;
+	}	
+	#print "$text\n";
+	
 	$im = load_image Gtk::Gdk::ImlibImage($path . "/" . $text);
 	$im->render(300,300);
 	$p->imlib_free;
