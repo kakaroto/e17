3eda2d29824ec8c69f98507f05b680c4b124eb91
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-10-25 20:49:50 +0000

    added copyright notice to file.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@3718 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/webpictures/webpictures.pl
+++ misc/webpictures/webpictures.pl
@@ -1,6 +1,32 @@
 #!/usr/bin/perl
 
-# Copyright 2000 some people on the net.
+# CVS Commits List Messages
+# Copyright (C) 2000 Geoff Harrison and various contributors
+#
+# For questions, concerns, please consult the e-develop mailing list.
+# please see:
+# http://lists.sourceforge.net/mailman/listinfo/enlightenment-devel
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies of the Software, its documentation and marketing & publicity
+# materials, and acknowledgment shall be given in the documentation, materials
+# and software packages that this Software was used.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 use Imlib2;
 use Gtk;
 use Gtk::Atoms;
