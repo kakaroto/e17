868ba366c3685fd4e2c9a075556f4de5f82c7a6d
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2000-12-28 01:08:10 +0000

    fix that
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@4046 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- CVSROOT/commit.pl
+++ CVSROOT/commit.pl
@@ -28,10 +28,10 @@
 # Here come the bits you'll want to twiddle configuration-wise:
 
 $SENDMAIL    = "/usr/sbin/sendmail";
-$MAILFROM    = "E CVS LIST <enlightenment-cvs\@lists.sourceforge.net>";
-$MAILREPLYTO = "E DEV LIST <enlightenment-devel\@lists.sourceforge.net>";
-$MAILTO      = "enlightenment-cvs\@lists.sourceforge.net";
-$MAILTO      = "raster\@valinux.com";
+$MAILFROM    = "E CVS LIST <enlightenment-cvs\@sourceforge.net>";
+$MAILREPLYTO = "E DEV LIST <enlightenment-devel\@sourceforge.net>";
+$MAILTO      = "enlightenment-cvs\@sourceforge.net";
+#$MAILTO      = "raster\@valinux.com";
 $cvsroot = $ENV{'CVSROOT'};
 $login = $ENV{'LOGNAME'} || getlogin || (getpwuid($<))[0] || "nobody";
 
