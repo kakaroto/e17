7e5d1e4400300eb90f4be5f500c825de0113694f
mandrake <mandrake@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2001-01-10 21:53:53 +0000

    Problems with font_style.c, fixed.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@4110 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/etox/src/font_style.c
+++ e17/libs/etox/src/font_style.c
@@ -1,5 +1,5 @@
-#include "loadfile.h"
 #include "Etox.h"
+#include "loadfile.h"
 
 E_Font_Style **style_list;
 int num_styles=0;
@@ -9,13 +9,13 @@ E_Font_Style *E_load_font_style(char *path) {
 	E_Font_Style *style;
 	FILE *font_file;
 	char s[4096];
-	int i1, i2, i3 fields;
+	int i1, i2, i3, fields;
 	char s2[4096];
 
 	if(!path)
 		return NULL;
 
-	style = malloc(sizeof E_Font_Style);
+	style = malloc(sizeof(E_Font_Style));
 	style->in_use = 0;
 
 	style->bits = NULL;
@@ -27,7 +27,7 @@ E_Font_Style *E_load_font_style(char *path) {
 	while(GetLine(s,4096,font_file)) {
 		i1=i2=i3=0;
 		memset(s2,0,4096);
-		fields = sscanf(s,"%4000[^=] %i %i %i",s2,i1,i2,i3);
+		fields = sscanf(s,"%4000[^=] %i %i %i",s2,&i1,&i2,&i3);
 		if(fields < 3) {
 			fclose(font_file);
 			return style;
@@ -40,14 +40,14 @@ E_Font_Style *E_load_font_style(char *path) {
 			style->bits = malloc(style->num_bits * sizeof(E_Style_Bit) + 1);
 		}
 		if(!strcmp(s2,"sh"))
-			style->bits[style->num_bits - 1]->type = STYLE_TYPE_SHADOW;
+			style->bits[style->num_bits - 1].type = STYLE_TYPE_SHADOW;
 		if(!strcmp(s2,"fg"))
-			style->bits[style->num_bits - 1]->type = STYLE_TYPE_FOREGROUND;
+			style->bits[style->num_bits - 1].type = STYLE_TYPE_FOREGROUND;
 		if(!strcmp(s2,"ol"))
-			style->bits[style->num_bits - 1]->type = STYLE_TYPE_OUTLINE;
-		style->bits[style->num_bits - 1]->x = i1;
-		style->bits[style->num_bits - 1]->y = i2;
-		style->bits[style->num_bits - 1]->alpha = i3;
+			style->bits[style->num_bits - 1].type = STYLE_TYPE_OUTLINE;
+		style->bits[style->num_bits - 1].x = i1;
+		style->bits[style->num_bits - 1].y = i2;
+		style->bits[style->num_bits - 1].alpha = i3;
 	}
 	fclose(font_file);
 
