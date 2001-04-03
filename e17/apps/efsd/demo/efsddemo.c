02af6c5b2550be3be662d5233dd8e5078da21004
cpk <cpk@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2001-04-03 12:44:09 +0000

    Ouch.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@4485 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/efsd/demo/efsddemo.c
+++ e17/apps/efsd/demo/efsddemo.c
@@ -446,7 +446,7 @@ main(int argc, char** argv)
   sleep(2);
 
   /* List contents of a directory */
-  if ((id = efsd_listdir(ec, getenv("HOME"), 3,
+  if ((id = efsd_listdir(ec, getenv("HOME"), 2,
 			 efsd_op_get_stat(),
 			 efsd_op_get_filetype())) >= 0)
     printf("Listing directory, command ID %i\n", id);
