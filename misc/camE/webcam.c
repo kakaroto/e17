3c1631cbf7b3fd59f6e52062d7872dedf06e6825
gilbertt <gilbertt@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2001-04-12 22:54:01 +0000

    scp example
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@4600 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/camE/webcam.c
+++ misc/camE/webcam.c
@@ -762,7 +762,7 @@ main(int argc, char *argv[])
       overlay_y = i;
 
    /* print config */
-   fprintf(stderr, "camE v0.6 - (c) 1999, 2000 Gerd Knorr, Tom Gilbert\n");
+   fprintf(stderr, "camE v0.7 - (c) 1999, 2000 Gerd Knorr, Tom Gilbert\n");
    fprintf(stderr,
            "grabber config: size %dx%d, input %d, norm %d, "
            "jpeg quality %d\n", grab_width, grab_height, grab_input,
