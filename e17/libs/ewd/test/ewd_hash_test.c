ca60f0d5060ea61c38db17029ab3f13b63b2a623
rbdpngn <rbdpngn@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2001-05-01 18:57:45 +0000

    Ok, forgot to add one more file. My brain must be fried today.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@4697 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/libs/ewd/test/ewd_hash_test.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/times.h>
+
+/* #define USE_GLIB */
+
+#ifdef USE_GLIB
+
+#include <glib.h>
+#define HASH_TABLE GHashTable
+#define HASH_NEW(arg1, arg2) g_hash_table_new(arg1, arg2)
+#define HASH_SET(hash, key, value) g_hash_table_insert(hash, key, value)
+#define HASH_GET(hash, key) g_hash_table_lookup(hash, key)
+#define HASH_REMOVE(hash, key) g_hash_table_remove(hash, key)
+#define HASH_DESTROY(hash) g_hash_table_destroy(hash)
+
+#else
+
+#include "Ewd.h"
+#define HASH_TABLE Ewd_Hash
+#define HASH_NEW(arg1, arg2) ewd_hash_new(arg1, arg2)
+#define HASH_SET(hash, key, value) ewd_hash_set(hash, key, value)
+#define HASH_GET(hash, key) ewd_hash_get(hash, key)
+#define HASH_REMOVE(hash, key) ewd_hash_remove(hash, key)
+#define HASH_DESTROY(hash) ewd_hash_destroy(hash)
+
+#endif
+
+#define MAX_VAL 10000
+
+int main()
+{
+        int i = 1;
+        HASH_TABLE *hash;
+	clock_t sys_use, user_use;
+	struct tms start_buf, end_buf;
+
+	times(&start_buf);
+
+        hash = HASH_NEW(NULL, NULL);
+
+        while (i < MAX_VAL) {
+                HASH_SET(hash, (void *)i, (void *)(i + 1));
+                i++;
+        }
+/*	printf("Got through the first loop\n"); */
+
+	times(&end_buf);
+	sys_use = end_buf.tms_stime - start_buf.tms_stime;
+	user_use = end_buf.tms_utime - start_buf.tms_utime;
+
+	printf("Used %ld ms of system time at halfway point\n", sys_use);
+	printf("Used %ld ms of user time at halfway point\n", user_use);
+
+	while (--i) {
+		int value;
+
+		printf("Key: %d\t", i);
+		value = (int)HASH_GET(hash, (void *)i);
+		printf("Value: %d\n", value);
+		fflush(stdout);
+
+	}
+/*	printf("Got through the second loop\n"); */
+
+	HASH_DESTROY(hash);
+
+	times(&end_buf);
+	sys_use = end_buf.tms_stime - start_buf.tms_stime;
+	user_use = end_buf.tms_utime - start_buf.tms_utime;
+
+	printf("Used %ld ms of system time total\n", sys_use);
+	printf("Used %ld ms of user time total\n", user_use);
+
+	return TRUE;
+}
