3b82c52365805cef7a9c1b98d923c0a32046f222
smugg <smugg@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2001-09-14 17:51:34 +0000

    Ewpss.. forgot to add theese.. sorry...
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@5353 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/libs/ewl/src/ewl_cursor.c
@@ -0,0 +1,67 @@
+#include <Ewl.h>
+
+
+void ewl_cursor_init(Ewl_Cursor * c);
+
+
+Ewl_Widget *
+ewl_cursor_new(void)
+{
+	Ewl_Cursor *c;
+
+	DENTER_FUNCTION;
+
+	c = NEW(Ewl_Cursor, 1);
+
+	memset(c, 0, sizeof(Ewl_Cursor));
+	ewl_cursor_init(c);
+
+	DRETURN_PTR(EWL_WIDGET(c));
+}
+
+void
+ewl_cursor_set_position(Ewl_Widget * w, int p)
+{
+	Ewl_Cursor *c;
+
+	DENTER_FUNCTION;
+	DCHECK_PARAM_PTR("w", w);
+
+	c = EWL_CURSOR(w);
+
+	c->position = p;
+
+	ewl_callback_call(w, EWL_CALLBACK_VALUE_CHANGED);
+
+	DLEAVE_FUNCTION;
+}
+
+int
+ewl_cursor_get_position(Ewl_Widget * w)
+{
+	Ewl_Cursor *c;
+
+	DENTER_FUNCTION;
+	DCHECK_PARAM_PTR_RET("w", w, 0);
+
+	c = EWL_CURSOR(w);
+
+	DRETURN_INT(c->position);
+}
+
+void
+ewl_cursor_init(Ewl_Cursor * c)
+{
+	Ewl_Widget *w;
+
+	DENTER_FUNCTION;
+	DCHECK_PARAM_PTR("c", c);
+
+	w = EWL_WIDGET(c);
+
+	ewl_widget_init(w, "/appearance/cursor/default");
+
+	c->position = 1;
+
+	DLEAVE_FUNCTION;
+}
---
+++ e17/libs/ewl/src/ewl_cursor.h
@@ -0,0 +1,21 @@
+
+#ifndef __EWL_CURSOR_H__
+#define __EWL_CURSOR_H__
+
+typedef struct _ewl_cursor Ewl_Cursor;
+
+#define EWL_CURSOR(cursor) ((Ewl_Cursor *) cursor)
+
+struct _ewl_cursor
+{
+	Ewl_Box box;
+
+	int position;
+};
+
+Ewl_Widget *ewl_cursor_new(void);
+void ewl_cursor_set_position(Ewl_Widget * w, int p);
+int ewl_cursor_get_position(Ewl_Widget * w);
+
+
+#endif /* __EWL_CURSOR_H__ */
