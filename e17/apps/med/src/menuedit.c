eeab29c95d1c052a55f3dd837d6cd1e8d484dc89
sleuth <sleuth@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2001-10-10 03:05:58 +0000

    And the rest of the source...
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@5460 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/apps/med/src/menuedit.c
@@ -0,0 +1,26 @@
+
+#include "menu.h"
+
+#if 0
+#include "e.h"
+#endif
+
+#include "menuedit.h"
+
+
+void
+med_edit_menu_item(E_Menu *m, E_Menu_Item *mi, void *data)
+{
+
+#if 0
+  printf( "text: %s, exe: %s, icon: %s, # %d,%d.\n", 
+	  mi->str, 
+	  (char*)data,
+	  mi->icon, mi->db_num1, mi->db_num2);
+
+  med_entry_text( mi->str, (char*)data, mi->icon );
+#endif
+}
+
+
+/*eof*/
---
+++ e17/apps/med/src/menuedit.h
@@ -0,0 +1,8 @@
+
+/* Needs menu.h included */
+
+void
+med_edit_menu_item(E_Menu *m, E_Menu_Item *mi, void *data);
+
+
+/*eof*/
