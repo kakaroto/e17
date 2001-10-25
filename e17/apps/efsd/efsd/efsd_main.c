1bebc0bfb190c7b2dc3e4ebb18d195792df5ff88
cpk <cpk@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2001-10-25 18:59:01 +0000

    #ifdef SIGSYS ...
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@5574 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/efsd/efsd/efsd_main.c
+++ e17/apps/efsd/efsd/efsd_main.c
@@ -1042,8 +1042,9 @@ main_initialize(char *appname)
   signal(SIGSEGV,   main_crash_sighandler);
   signal(SIGFPE,    main_crash_sighandler);
   signal(SIGILL,    main_crash_sighandler);
+#ifdef SIGSYS
   signal(SIGSYS,    main_crash_sighandler);
-
+#endif
   atexit(efsd_misc_remove_socket_file);
   efsd_misc_create_efsd_dir();
   getcwd(app_cmd, MAXPATHLEN);
