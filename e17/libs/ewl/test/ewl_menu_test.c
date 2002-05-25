5035c2ed109737d057917c2977760d3a2a0b4433
rbdpngn <rbdpngn@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2002-05-25 15:39:32 +0000

    Forgot to add the test for the new menu's. This test does not work right at all yet, so don't be surprised if it looks odd.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@6277 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/libs/ewl/test/ewl_menu_test.c
@@ -0,0 +1,117 @@
+#include <Ewl.h>
+
+static Ewl_Widget *menu_button = NULL;
+
+void            __create_menu_test_window(Ewl_Widget * w, void *ev_data,
+					   void *user_data);
+void            __item_down(Ewl_Widget * w, void *ev_data, void *user_data);
+
+void
+__destroy_menu_test_window(Ewl_Widget * w, void *ev_data, void *user_data)
+{
+	ewl_widget_destroy(w);
+
+	ewl_callback_append(menu_button, EWL_CALLBACK_CLICKED,
+			    __create_menu_test_window, NULL);
+
+	return;
+	w = NULL;
+	ev_data = NULL;
+	user_data = NULL;
+}
+
+
+void
+__menu_select(Ewl_Widget * w, void *ev_data, void *user_data)
+{
+	char           *text;
+
+	text = ewl_text_get_text(EWL_TEXT(EWL_MENU_ITEM(w)->text));
+	printf("%s item down\n", text);
+
+	return;
+	w = NULL;
+	ev_data = NULL;
+	user_data = NULL;
+}
+
+void
+__create_menu_test_window(Ewl_Widget * w, void *ev_data, void *user_data)
+{
+	Ewl_Widget     *menu_win;
+	Ewl_Widget     *menu_box;
+	Ewl_Widget     *menu1, *menu2;
+	Ewl_Widget     *item;
+
+
+	ewl_callback_del(w, EWL_CALLBACK_CLICKED, __create_menu_test_window);
+
+	menu_button = w;
+
+	menu_win = ewl_window_new();
+	ewl_window_resize(menu_win, 300, 300);
+	ewl_callback_append(menu_win, EWL_CALLBACK_DELETE_WINDOW,
+			    __destroy_menu_test_window, NULL);
+	ewl_widget_show(menu_win);
+
+	/*
+	 * Create the main box for holding the widgets
+	 */
+	menu_box = ewl_vbox_new();
+	ewl_object_set_fill_policy(EWL_OBJECT(menu_box), EWL_FILL_POLICY_FILL);
+	ewl_container_append_child(EWL_CONTAINER(menu_win), menu_box);
+	ewl_theme_data_set_str(menu_box,
+			       "/appearance/box/vertical/base/visible", "yes");
+	ewl_widget_show(menu_box);
+
+	/*
+	 * Create the menu
+	 */
+	menu1 = ewl_menu_new(NULL, "test menu");
+	ewl_container_append_child(EWL_CONTAINER(menu_box), menu1);
+	ewl_widget_show(menu1);
+
+	/*
+	 * Append some test items
+	 */
+	item = ewl_menu_item_new("/usr/share/pixmaps/dia.xpm", "dia");
+	ewl_container_append_child(EWL_CONTAINER(menu1), item);
+	ewl_callback_append(item, EWL_CALLBACK_SELECT, __menu_select, NULL);
+	ewl_widget_show(item);
+
+	item = ewl_menu_item_new("/usr/share/pixmaps/wilber.xpm", "gimp");
+	ewl_container_append_child(EWL_CONTAINER(menu1), item);
+	ewl_callback_append(item, EWL_CALLBACK_SELECT, __menu_select, NULL);
+	ewl_widget_show(item);
+
+	item = ewl_menu_item_new(NULL, "button");
+	ewl_container_append_child(EWL_CONTAINER(menu1), item);
+	ewl_callback_append(item, EWL_CALLBACK_SELECT, __menu_select, NULL);
+	ewl_widget_show(item);
+
+	/*
+	 * Create a sub-menu
+	 */
+	menu2 = ewl_menu_new(NULL, "Sub menu");
+	ewl_container_append_child(EWL_CONTAINER(menu1), menu2);
+	ewl_widget_show(menu2);
+
+	item = ewl_menu_item_new(NULL, "button");
+	ewl_container_append_child(EWL_CONTAINER(menu2), item);
+	ewl_callback_append(item, EWL_CALLBACK_SELECT, __menu_select, NULL);
+	ewl_widget_show(item);
+
+	item = ewl_menu_item_new(NULL, "button");
+	ewl_container_append_child(EWL_CONTAINER(menu2), item);
+	ewl_callback_append(item, EWL_CALLBACK_SELECT, __menu_select, NULL);
+	ewl_widget_show(item);
+
+	/*
+	 * ewl_menu_set_title_expandable(EWL_MENU(menu));
+	 */
+
+	return;
+	w = NULL;
+	ev_data = NULL;
+	user_data = NULL;
+}
