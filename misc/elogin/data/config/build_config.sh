b65d50fa6685c1eb41482e4f2879aae3f2c6f02e
atmosphere <atmosphere@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2003-01-21 18:28:48 +0000

    If you're using elogin right now you have to update your config.db
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@6613 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/elogin/data/config/build_config.sh
+++ misc/elogin/data/config/build_config.sh
@@ -8,6 +8,10 @@ edb_ed $DB add /elogin/welcome/font/r int 192
 edb_ed $DB add /elogin/welcome/font/g int 192
 edb_ed $DB add /elogin/welcome/font/b int 192
 edb_ed $DB add /elogin/welcome/font/a int 220
+edb_ed $DB add /elogin/welcome/pos/x float 0.5
+edb_ed $DB add /elogin/welcome/pos/y float 0.8
+edb_ed $DB add /elogin/welcome/pos/offset/x int 0
+edb_ed $DB add /elogin/welcome/pos/offset/y int 0
 edb_ed $DB add /elogin/passwd/mess str "Enter Your Password..."
 edb_ed $DB add /elogin/passwd/font/name str "notepad.ttf"
 edb_ed $DB add /elogin/passwd/font/size int 20
@@ -15,6 +19,21 @@ edb_ed $DB add /elogin/passwd/font/r int 192
 edb_ed $DB add /elogin/passwd/font/g int 192
 edb_ed $DB add /elogin/passwd/font/b int 192
 edb_ed $DB add /elogin/passwd/font/a int 220
+edb_ed $DB add /elogin/passwd/pos/x float 0.5
+edb_ed $DB add /elogin/passwd/pos/y float 0.8
+edb_ed $DB add /elogin/passwd/pos/offset/x int 0
+edb_ed $DB add /elogin/passwd/pos/offset/y int 0
+edb_ed $DB add /elogin/greeting/mess str "Welcome to "
+edb_ed $DB add /elogin/greeting/font/name str "orbit.ttf"
+edb_ed $DB add /elogin/greeting/font/size int 18
+edb_ed $DB add /elogin/greeting/font/r int 255
+edb_ed $DB add /elogin/greeting/font/g int 255
+edb_ed $DB add /elogin/greeting/font/b int 255
+edb_ed $DB add /elogin/greeting/font/a int 210
+edb_ed $DB add /elogin/greeting/pos/x float 0.0
+edb_ed $DB add /elogin/greeting/pos/y float 1.0
+edb_ed $DB add /elogin/greeting/pos/offset/x int 30
+edb_ed $DB add /elogin/greeting/pos/offset/y int -60
 edb_ed $DB add /elogin/xinerama/screens/w int 1
 edb_ed $DB add /elogin/xinerama/screens/h int 1
 edb_ed $DB add /elogin/xinerama/on/w int 1
@@ -36,4 +55,5 @@ edb_ed $DB add /elogin/session/6/path str ""
 edb_ed $DB add /elogin/session/7/name str "Failsafe"
 edb_ed $DB add /elogin/session/7/path str "failsafe"
 edb_ed $DB add /elogin/session/8/name str "Default"
+edb_ed $DB add /elogin/session/8/path str "~/.xinitrc"
 edb_ed $DB add /elogin/session/count int 9
