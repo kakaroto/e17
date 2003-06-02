ca1cd583cee59f2fc8d1cad8342a1280619f4b38
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2003-06-02 08:54:01 +0000

    ciabot
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@6955 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ CVSROOT/ciabot.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+message=`cat`
+uname=`id -un`
+lineno=`echo "$message" | grep -n "Log Message:" | awk -F: ' { print $1 } '`
+message=`echo "$message" | sed "1,${lineno}d"`
+
+projectname="e"
+tmpfile="/tmp/$RANDOM-$projectname"
+message=`echo "$message" | head -n 5`
+
+cat <<EOF >$tmpfile
+From: commits@enlightenment.org
+To: cia@users.sf.net
+Content-Type: text/plain;
+Subject: Announce $projectname
+
+commit by $uname: $message
+EOF
+
+if [ -e /tmp/lastlog-$projectname ]; then
+        if ! diff /tmp/lastlog-$projectname $tmpfile &>/dev/null; then
+                # there are differences, so this is a different commit
+                cat $tmpfile | /usr/sbin/sendmail -t
+                mv $tmpfile /tmp/lastlog-$projectname
+        else
+                # it's just cvs spamming us from another directory
+                rm $tmpfile
+        fi
+else
+        cat $tmpfile | /usr/sbin/sendmail -t
+        mv $tmpfile /tmp/lastlog-$projectname
+fi
