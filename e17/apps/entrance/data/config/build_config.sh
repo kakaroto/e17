35237b48bc9381b88aa90534e729c200cfac2843
atmosphere <atmosphere@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2003-07-25 04:54:27 +0000

    New config building script to match the slightly modified keying and config structure.
    JeopardE lemme know what you think.
    
    - greeting has a before/after attribute
    - Since font paths are important now you can append system wide font
    directories
    - Sessions info is stored in a hash now
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@7233 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/entrance/data/config/build_config.sh
+++ e17/apps/entrance/data/config/build_config.sh
@@ -2,32 +2,35 @@
 DB="./entrance_config.db"
 rm -f $DB
 
-edb_ed $DB add /entrance/welcome str "Enter Username:"
-edb_ed $DB add /entrance/passwd str "Enter Password:"
-edb_ed $DB add /entrance/passwd_echo int 1
-edb_ed $DB add /entrance/use_pam_auth int 1
-edb_ed $DB add /entrance/xinerama/screens/w int 1
-edb_ed $DB add /entrance/xinerama/screens/h int 1
-edb_ed $DB add /entrance/xinerama/on/w int 1
-edb_ed $DB add /entrance/xinerama/on/h int 1
-edb_ed $DB add /entrance/theme str "BlueCrystal"
+# set auth to 1 for pam, 2 for shadow
+edb_ed $DB add /entrance/auth int 1
+#edb_ed $DB add /entrance/xinerama/screens/w int 1
+#edb_ed $DB add /entrance/xinerama/screens/h int 1
+#edb_ed $DB add /entrance/xinerama/on/w int 1
+#edb_ed $DB add /entrance/xinerama/on/h int 1
+edb_ed $DB add /entrance/theme str "default.eet"
 edb_ed $DB add /entrance/date_format str "%A %B %e, %Y"
 edb_ed $DB add /entrance/time_format str "%l:%M:%S %p"
-edb_ed $DB add /entrance/session/0/name str "enlightenment"
-edb_ed $DB add /entrance/session/0/path str ""
-edb_ed $DB add /entrance/session/0/icon str "enlightenment.png"
-edb_ed $DB add /entrance/session/1/name str "kde2"
-edb_ed $DB add /entrance/session/1/path str ""
-edb_ed $DB add /entrance/session/1/icon str "kde.png"
-edb_ed $DB add /entrance/session/2/name str "blackbox"
-edb_ed $DB add /entrance/session/2/path str ""
-edb_ed $DB add /entrance/session/2/icon str "blackbox.png"
-edb_ed $DB add /entrance/session/3/name str "fluxbox"
-edb_ed $DB add /entrance/session/3/path str ""
-edb_ed $DB add /entrance/session/3/icon str "default.png"
-edb_ed $DB add /entrance/session/4/name str "Failsafe"
-edb_ed $DB add /entrance/session/4/path str "failsafe"
-edb_ed $DB add /entrance/session/4/icon str "default.png"
-edb_ed $DB add /entrance/session/count int 5
-edb_ed $DB add /entrance/session/default int 0
-
+#edb_ed $DB add /entrance/fonts/count int 2
+#edb_ed $DB add /entrance/fonts/0/str str "/usr/share/fonts/truetype/"
+#edb_ed $DB add /entrance/fonts/1/str str "/usr/X11R6/lib/X11/fonts/Truetype/"
+edb_ed $DB add /entrance/session/count int 9
+edb_ed $DB add /entrance/session/0/value str ""
+edb_ed $DB add /entrance/session/0/key str "Default"
+edb_ed $DB add /entrance/session/1/value str "enlightenment"
+edb_ed $DB add /entrance/session/1/key str "Enlightenment"
+edb_ed $DB add /entrance/session/2/value str "kde"
+edb_ed $DB add /entrance/session/2/key str "KDE"
+edb_ed $DB add /entrance/session/3/value str "gnome"
+edb_ed $DB add /entrance/session/3/key str "Gnome"
+edb_ed $DB add /entrance/session/4/value str "gnome"
+edb_ed $DB add /entrance/session/4/key str "Gnome"
+edb_ed $DB add /entrance/session/5/value str "blackbox"
+edb_ed $DB add /entrance/session/5/key str "Blackbox"
+edb_ed $DB add /entrance/session/6/value str "sawfish"
+edb_ed $DB add /entrance/session/6/key str "Sawfish"
+edb_ed $DB add /entrance/session/7/value str "xfce"
+edb_ed $DB add /entrance/session/7/key str "XFce"
+edb_ed $DB add /entrance/session/8/value str "failsafe"
+edb_ed $DB add /entrance/session/8/key str "Failsafe"
+sudo cp $DB /usr/local/etc/
