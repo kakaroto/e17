4230d0a5b80e05e3b7426b1235b5b1f44fdb651e
atmosphere <atmosphere@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2003-10-21 03:06:29 +0000

    Add config opt to generation script and make this easier for some people.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@7681 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/entrance/data/config/build_config.sh
+++ e17/apps/entrance/data/config/build_config.sh
@@ -35,3 +35,4 @@ edb_ed $DB add /entrance/session/8/value str "failsafe"
 edb_ed $DB add /entrance/session/8/key str "Failsafe"
 edb_ed $DB add /entrance/system/reboot int 1
 edb_ed $DB add /entrance/system/halt int 1
+#edb_ed $DB add /entrance/engine str "gl"
