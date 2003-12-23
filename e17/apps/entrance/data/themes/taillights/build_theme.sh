e0b56b5f04dbf2e02802b25499928cbab29b275e
atmosphere <atmosphere@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2003-12-23 14:07:48 +0000

    I knew there was a reason those system signals were commented out
    How about we try confirmation dialogs :)
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@8201 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/entrance/data/themes/taillights/build_theme.sh
+++ e17/apps/entrance/data/themes/taillights/build_theme.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 THEME="taillights"
-if [ ! -f $THEME.eet ]; then
-	edje_cc -v -id ./images $THEME.edc $THEME.eet
+if [ -f $THEME.eet ]; then
+    rm -f $THEME.eet
 fi
+edje_cc -v -id ./images $THEME.edc $THEME.eet
 #edje $THEME.eet
 #sudo cp -f $THEME.eet /usr/share/entrance/themes/
