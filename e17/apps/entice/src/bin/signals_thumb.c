567a1555c27e35d71ae16b516ced2a3cd783f74e
atmosphere <atmosphere@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-01-08 19:58:16 +0000

    forget manually marshalling signals to edje, passing events elsewhere works fine.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@8307 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/entice/src/bin/signals_thumb.c
+++ e17/apps/entice/src/bin/signals_thumb.c
@@ -36,86 +36,6 @@ _entice_thumb_preview_cb(void *data, Evas_Object * o, const char *emission,
 }
 
 /**
- * The following five functions translate evas mouse events into edje
- * mouse event signal emissions
- */
-static void
-_entice_thumb_mouse_wheel_translate(void *data, Evas * e, Evas_Object * obj,
-                                    void *event_info)
-{
-   Evas_Event_Mouse_Wheel *ev = NULL;
-   Evas_Object *o = NULL;
-
-   if ((ev = (Evas_Event_Mouse_Wheel *) event_info))
-   {
-      if ((o = (Evas_Object *) data))
-      {
-         fprintf(stderr, "Mouse WHEEL: %d,%d\n", ev->direction, ev->z);
-         /* 
-            evas_event_feed_mouse_wheel_data(e, ev->direction, ev->z, o); */
-      }
-   }
-}
-static void
-_entice_thumb_mouse_in_translate(void *data, Evas * e, Evas_Object * obj,
-                                 void *event_info)
-{
-   Evas_Event_Mouse_In *ev = NULL;
-   Evas_Object *o = NULL;
-
-   if ((ev = (Evas_Event_Mouse_In *) event_info))
-      if ((o = (Evas_Object *) data))
-         edje_object_signal_emit(o, "mouse,in", "EnticeThumb");
-}
-static void
-_entice_thumb_mouse_out_translate(void *data, Evas * e, Evas_Object * obj,
-                                  void *event_info)
-{
-   Evas_Event_Mouse_Out *ev = NULL;
-   Evas_Object *o = NULL;
-
-   if ((ev = (Evas_Event_Mouse_Out *) event_info))
-      if ((o = (Evas_Object *) data))
-         edje_object_signal_emit(o, "mouse,out", "EnticeThumb");
-}
-static void
-_entice_thumb_mouse_up_translate(void *data, Evas * e, Evas_Object * obj,
-                                 void *event_info)
-{
-   Evas_Event_Mouse_Up *ev = NULL;
-   Evas_Object *o = NULL;
-
-   if ((ev = (Evas_Event_Mouse_Up *) event_info))
-   {
-      if ((o = (Evas_Object *) data))
-      {
-         char buf[PATH_MAX];
-
-         snprintf(buf, PATH_MAX, "mouse,up,%i", (int) ev->button);
-         edje_object_signal_emit(o, buf, "EnticeThumb");
-      }
-   }
-}
-static void
-_entice_thumb_mouse_down_translate(void *data, Evas * e, Evas_Object * obj,
-                                   void *event_info)
-{
-   Evas_Event_Mouse_Down *ev = NULL;
-   Evas_Object *o = NULL;
-
-   if ((ev = (Evas_Event_Mouse_Down *) event_info))
-   {
-      if ((o = (Evas_Object *) data))
-      {
-         char buf[PATH_MAX];
-
-         snprintf(buf, PATH_MAX, "mouse,down,%i", (int) ev->button);
-         edje_object_signal_emit(o, buf, "EnticeThumb");
-      }
-   }
-}
-
-/**
  * @o - the edje object we're hooking signals into
  * @im - the E_Thumb object want want to translate mouse events for
  * Evil things are done in order to get mouse events through to edje, it
@@ -133,23 +53,4 @@ hookup_entice_thumb_signals(Evas_Object * o, Evas_Object * im)
    count = sizeof(signals) / sizeof(char *);
    for (i = 0; i < count; i++)
       edje_object_signal_callback_add(o, signals[i], "", funcs[i], im);
-
-   /* 
-    *  this is weird, but we intercept callbacks and feed them to edje, for
-    *  some reason i can't get the mouse callbacks to be handled properly
-    *  for a swallowed smart object w/o this
-    */
-   if ((thumb = (E_Thumb *) evas_object_smart_data_get(im)))
-   {
-      evas_object_event_callback_add(thumb->image, EVAS_CALLBACK_MOUSE_IN,
-                                     _entice_thumb_mouse_in_translate, o);
-      evas_object_event_callback_add(thumb->image, EVAS_CALLBACK_MOUSE_OUT,
-                                     _entice_thumb_mouse_out_translate, o);
-      evas_object_event_callback_add(thumb->image, EVAS_CALLBACK_MOUSE_UP,
-                                     _entice_thumb_mouse_up_translate, o);
-      evas_object_event_callback_add(thumb->image, EVAS_CALLBACK_MOUSE_DOWN,
-                                     _entice_thumb_mouse_down_translate, o);
-      evas_object_event_callback_add(thumb->image, EVAS_CALLBACK_MOUSE_WHEEL,
-                                     _entice_thumb_mouse_wheel_translate, o);
-   }
 }
