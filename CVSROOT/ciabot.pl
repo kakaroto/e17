a73943e855ecabf6ae4fc08b8de81b624ddc30de
xcomputerman <xcomputerman@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-01-16 02:16:50 +0000

    Making some progress...
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@8458 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- CVSROOT/ciabot.pl
+++ CVSROOT/ciabot.pl
@@ -117,6 +117,9 @@ $dirfiles[0] = "@files" or die "$0: no files specified\n";
 
 #$module = $dir[0]; $module =~ s#/.*##;
 $_ = $dir[0];
+my $first = "";
+my $second = "";
+my $third = "";
 (my $first, my $second, my $third) = /^((\w*)\/(\w+| )*\/(.*))/;
 if ($first eq 'e17') {
   $module = $first . "/" . $second . "/" . $third;
@@ -161,7 +164,7 @@ while (<STDIN>) {
 
 $dirfiles[0] = join (' ',
   grep {
-    my $f = "$module/$dir[0]/$_";
+    my $f = "$dir[0]/$_";
     $f !~ m/$ignore_regexp/;
   } split (/\s+/, $dirfiles[0])
 ) if ($ignore_regexp);
