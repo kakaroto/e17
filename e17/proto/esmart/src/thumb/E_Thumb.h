cffe7a65840cff15a36fd48c11e949db4a70cd2f
atmosphere <atmosphere@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-02-29 22:23:02 +0000

    add e_thumb_freshen to the header file and ran indent over it
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@9165 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/esmart/src/thumb/E_Thumb.h
+++ e17/proto/esmart/src/thumb/E_Thumb.h
@@ -16,18 +16,18 @@
  */
 struct _E_Thumb
 {
-    Epsilon *e;
-    Epsilon_Info *info;
-    Evas_Object *image;	/* thumb image that's displayed */
-    double x, y, w, h;	/* smart object location/geometry */
-    int tw, th;
+  Epsilon *e;
+  Epsilon_Info *info;
+  Evas_Object *image;		/* thumb image that's displayed */
+  double x, y, w, h;		/* smart object location/geometry */
+  int tw, th;
 };
 typedef struct _E_Thumb E_Thumb;
 
 /**
  * e_thumb_free - free an E_Thumb smart object 
  */
-void e_thumb_free(Evas_Object *o);
+void e_thumb_free (Evas_Object * o);
 /**
  * e_thumb_new - create a new E_Thumb object.  If the freedesktop cached
  * thumbnail exists it loads it, if not it creates it.  There's some
@@ -35,33 +35,41 @@ void e_thumb_free(Evas_Object *o);
  * @evas - the evas we want to add the object to
  * @file - the name of the file we want a thumb of
  */
-Evas_Object * e_thumb_new(Evas *evas, const char *file);
+Evas_Object *e_thumb_new (Evas * evas, const char *file);
 /**
  * e_thumb_file_get - get the full path to the thumbnail's real image
  * @o - The smart object we want the filename for
  * Returns a pointer to the filename, you should NOT free this memory
  */
-const char* e_thumb_file_get(Evas_Object *o);
+const char *e_thumb_file_get (Evas_Object * o);
 /**
  * e_thumb_geometry_get - Get the geometry of the source image
  * @o - The smart object we want the geometry of
  * @w - A pointer to somewhere we can store the width
  * @h - A pointer to somewhere we can store the height
  */
-void e_thumb_geometry_get(Evas_Object *o, int *w, int *h);
+void e_thumb_geometry_get (Evas_Object * o, int *w, int *h);
 
 /**
  * e_thumb_evas_object_get - Load the image file to an evas object
  * @o - The smart object we want the geometry of
  * Returns a valid Evas_Object if imlib2 or evas can load the file
  */
-Evas_Object *e_thumb_evas_object_get(Evas_Object *o);
+Evas_Object *e_thumb_evas_object_get (Evas_Object * o);
 
 /**
  * e_thumb_format_get - get the format of the image this thumb is for
  * @o - The smart object we want the format for
  * NOTE: Don't free this string, dupe it if you wanna keep it around
  */
-const char * e_thumb_format_get(Evas_Object *o);
+const char *e_thumb_format_get (Evas_Object * o);
+
+/** 
+ * e_thumb_freshen - forced reloading of the thumbnail image from disk
+ * @o - The smart object we want the format for
+ * Returns EPSILON_OK if the cache was bad and we regenerated, EPSILON_FAIL
+ * if the cached image is still valid
+ */
+int e_thumb_freshen (Evas_Object * o);
 
 #endif
