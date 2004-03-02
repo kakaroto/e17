2da9fbb8eb910ed5b89db3e4a681fa371fe4d689
rbdpngn <rbdpngn@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-03-02 20:24:24 +0000

    Add another check for the value passed on key events.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@9199 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/ewl_events.c
+++ e17/libs/ewl/src/ewl_events.c
@@ -262,7 +262,7 @@ int ewl_ev_x_key_down(void *data, int type, void *e)
 		key_modifiers |= EWL_KEY_MODIFIER_WIN;
 	else if (strstr(ev->keyname, "Hyper_"))
 		key_modifiers |= EWL_KEY_MODIFIER_WIN;
-	else if (strlen(ev->keyname) > 1)
+	else if (strlen(ev->keyname) > 1 || !ev->key_compose)
 		ewl_embed_feed_key_down(embed, ev->keyname, key_modifiers);
 	else
 		ewl_embed_feed_key_down(embed, ev->key_compose, key_modifiers);
@@ -304,10 +304,10 @@ int ewl_ev_x_key_up(void *data, int type, void *e)
 		key_modifiers &= ~EWL_KEY_MODIFIER_WIN;
 	else if (strstr(ev->keyname, "Hyper_"))
 		key_modifiers &= ~EWL_KEY_MODIFIER_WIN;
-	else if (strlen(ev->keyname) > 1)
+	else if (strlen(ev->keyname) > 1 || !ev->key_compose)
 		ewl_embed_feed_key_up(embed, ev->keyname, key_modifiers);
 	else
-		ewl_embed_feed_key_up(embed, ev->keyname, key_modifiers);
+		ewl_embed_feed_key_up(embed, ev->key_compose, key_modifiers);
 
 	DRETURN_INT(TRUE, DLEVEL_STABLE);
 }
