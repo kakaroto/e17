e52f6ba86aae7d1efcdc3d797bd58cbc41981a21
atmosphere <atmosphere@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-03-24 23:58:53 +0000

    ran indent, use const char* where applicable
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@9451 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/esmart/src/entry/Esmart_Text_Entry.h
+++ e17/proto/esmart/src/entry/Esmart_Text_Entry.h
@@ -8,46 +8,47 @@
 
 struct _Esmart_Text_Entry
 {
-   Evas_Object *clip;
-   Evas_Object *base;
-   struct
-   {
-      int size;
-      char *text;
-      int index;
-   } buf;
-   struct
-   {
-      Evas_Object *o;
-      char *part;
-   } edje;
-   struct
-   {
-      void (*func) (void *data, const char *str);
-      void *arg;
-   } return_key;
-   int passwd;
+  Evas_Object *clip;
+  Evas_Object *base;
+  struct
+  {
+    int size;
+    char *text;
+    int index;
+  } buf;
+  struct
+  {
+    Evas_Object *o;
+    char *part;
+  } edje;
+  struct
+  {
+    void (*func) (void *data, const char *str);
+    void *arg;
+  } return_key;
+  int passwd;
 };
 typedef struct _Esmart_Text_Entry Esmart_Text_Entry;
 
 /* create a new text entry */
-Evas_Object *esmart_text_entry_new(Evas * e);
+Evas_Object *esmart_text_entry_new (Evas * e);
 
-void esmart_text_entry_text_set(Evas_Object * o, const char *str);
-void esmart_text_entry_is_password_set(Evas_Object * o, int val);
-void esmart_text_entry_focus_set(Evas_Object * o, int val);
-int esmart_text_entry_is_focused(Evas_Object * o);
-void esmart_text_entry_max_chars_set(Evas_Object * o, int max);
-void esmart_text_entry_edje_part_set(Evas_Object * o, Evas_Object * edje,
-                                   char *part);
+void esmart_text_entry_text_set (Evas_Object * o, const char *str);
+void esmart_text_entry_is_password_set (Evas_Object * o, int val);
+void esmart_text_entry_focus_set (Evas_Object * o, int val);
+int esmart_text_entry_is_focused (Evas_Object * o);
+void esmart_text_entry_max_chars_set (Evas_Object * o, int max);
+void esmart_text_entry_edje_part_set (Evas_Object * o, Evas_Object * edje,
+				      const char *part);
 
 
 /* you've gotta free this resut */
 //char * esmart_text_entry_string_get(Evas_Object *o)
 
-void esmart_text_entry_return_key_callback_set(Evas_Object * o,
-                                             void (*func) (void *data,
-                                                           const char *str),
-                                             void *data);
+void esmart_text_entry_return_key_callback_set (Evas_Object * o,
+						void (*func) (void *data,
+							      const char
+							      *str),
+						void *data);
 
 #endif
