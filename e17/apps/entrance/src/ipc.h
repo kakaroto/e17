829b30056f3188940f255650dc1a3b5192de3148
xcomputerman <xcomputerman@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-03-25 06:42:38 +0000

    Add global ipc header
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@9467 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/apps/entrance/src/ipc.h
@@ -0,0 +1,11 @@
+#ifndef __E_IPC_H
+#define __E_IPC_H
+
+#define E_XAUTH_REQ  0xA0
+#define E_XAUTH_ACK  0xA1
+#define E_XAUTH_NAK  0xA2
+#define E_UID        0x10
+#define E_GID        0x11
+#define E_HOMEDIR    0x12
+
+#endif
