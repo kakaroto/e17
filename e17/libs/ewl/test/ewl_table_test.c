345b39913d15c1594b290e457efad0e6c1d5cbbd
digitalfallout <digitalfallout@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-06-01 18:13:53 +0000

    another warning fix
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@10408 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/test/ewl_table_test.c
+++ e17/libs/ewl/test/ewl_table_test.c
@@ -69,7 +69,7 @@ __create_table_test_window(Ewl_Widget * w, void *ev_data, void *user_data)
 	cell = ewl_cell_new();
 	button[0] = ewl_button_new("FIRST");
 	ewl_container_append_child(EWL_CONTAINER(cell), button[0]);
-	ewl_table_add(EWL_TABLE(table), cell, 3, 4, 2, 3);
+	ewl_table_add(EWL_TABLE(table), EWL_CELL(cell), 3, 4, 2, 3);
 	ewl_widget_show(button[0]);
 	ewl_widget_show(cell);
 
