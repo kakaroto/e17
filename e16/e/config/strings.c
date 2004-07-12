233cd95ad03afb5efedb5d3e83ea5300227ffa4f
kwo <kwo@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-07-12 23:48:16 +0000

    Stuff to help localizing e.g. menu strings.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@10800 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e16/e/config/strings.c
@@ -0,0 +1,19 @@
+#define _(x) x
+
+char *s[] = {
+_("Focus Settings ..."),
+_("Move & Resize Settings ..."),
+_("Pager Settings ..."),
+_("Window Placement Settings ..."),
+_("Multiple Desktop Settings ..."),
+_("Virtual Desktop Settings ..."),
+_("Autoraise Settings ..."),
+_("Tooltip Settings ..."),
+_("Audio Settings ..."),
+_("Group Settings ..."),
+_("Remember Settings ..."),
+_("Special FX Settings ..."),
+_("Desktop Background Settings ..."),
+_("Miscellaneous Settings ..."),
+_("Legacy E-conf tool ..."),
+};
