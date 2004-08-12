ee839073df49229f4766b1e327cfd34522685eb1
werkt <werkt@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-08-12 13:36:49 +0000

    Added actual min/max gets.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@11204 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/ewl_object.c
+++ e17/libs/ewl/src/ewl_object.c
@@ -756,6 +756,41 @@ inline int ewl_object_get_minimum_h(Ewl_Object * o)
 }
 
 /**
+ * @param o: the object to get the minimum width
+ * @return Returns the minimum width of the object @a o.
+ * @brief Get the minimum width of an object
+ */
+inline int ewl_object_get_actual_minimum_w(Ewl_Object * o)
+{
+	int val;
+
+	DENTER_FUNCTION(DLEVEL_STABLE);
+	DCHECK_PARAM_PTR_RET("o", o, 0);
+
+	val = MINIMUM_W(o);
+
+	DRETURN_INT(val + PADDING_HORIZONTAL(o) + INSET_HORIZONTAL(o),
+			DLEVEL_STABLE);
+}
+
+/**
+ * @param o: the object to get the minimum height
+ * @return Returns the minimum height of the object.
+ * @brief Get the minimum height of an object
+ */
+inline int ewl_object_get_actual_minimum_h(Ewl_Object * o)
+{
+	int val;
+
+	DENTER_FUNCTION(DLEVEL_STABLE);
+	DCHECK_PARAM_PTR_RET("o", o, 0);
+
+	val = MINIMUM_H(o);
+
+	DRETURN_INT(val, DLEVEL_STABLE);
+}
+
+/**
  * @param o: the object to retrieve the minimum dimensions
  * @param w: a pointer to an integer to store the minimum width
  * @param h: a pointer to an integer to store the minimum height
@@ -918,6 +953,42 @@ inline int ewl_object_get_maximum_h(Ewl_Object * o)
 
 	if ((val + PADDING_VERTICAL(o) + INSET_VERTICAL(o)) > val)
 		val += PADDING_VERTICAL(o) + INSET_VERTICAL(o);
+
+	DRETURN_INT(val, DLEVEL_STABLE);
+}
+
+
+/**
+ * @param o: the object to get the maximum width
+ * @return Returns the maximum width of the object.
+ * @brief Get the maximum width of an object
+ */
+inline int ewl_object_get_actual_maximum_w(Ewl_Object * o)
+{
+	int val;
+
+	DENTER_FUNCTION(DLEVEL_STABLE);
+	DCHECK_PARAM_PTR_RET("o", o, 0);
+
+	val = MAXIMUM_W(o);
+
+	DRETURN_INT(val, DLEVEL_STABLE);
+}
+
+
+/**
+ * @param o: the object to get the maximum height
+ * @return Returns the maximum height of the object.
+ * @brief Get the maximum height of an object
+ */
+inline int ewl_object_get_actual_maximum_h(Ewl_Object * o)
+{
+	int val;
+
+	DENTER_FUNCTION(DLEVEL_STABLE);
+	DCHECK_PARAM_PTR_RET("o", o, 0);
+
+	val = MAXIMUM_H(o);
 
 	DRETURN_INT(val, DLEVEL_STABLE);
 }
