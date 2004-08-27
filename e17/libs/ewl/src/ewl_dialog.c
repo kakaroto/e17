b9f613ed274789b580d9ae8d346c7f6a2b231add
rbdpngn <rbdpngn@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-08-27 06:13:18 +0000

    Indent run on the dialog code.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@11398 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/ewl_dialog.c
+++ e17/libs/ewl/src/ewl_dialog.c
@@ -5,20 +5,19 @@
  * @return Returns a pointer to a new dialog on success, NULL on failure.
  * @brief Create a new internal dialog
  */
-Ewl_Widget *
-ewl_dialog_new (Ewl_Position pos)
+Ewl_Widget *ewl_dialog_new(Ewl_Position pos)
 {
-  Ewl_Dialog *d;
-  
-  DENTER_FUNCTION(DLEVEL_STABLE);
-  
-  d = NEW(Ewl_Dialog, 1);
-  if (!d)
-    DRETURN_PTR(NULL, DLEVEL_STABLE);
-  
-  ewl_dialog_init(d, pos);
+	Ewl_Dialog *d;
 
-  DRETURN_PTR(EWL_WIDGET(d), DLEVEL_STABLE);
+	DENTER_FUNCTION(DLEVEL_STABLE);
+
+	d = NEW(Ewl_Dialog, 1);
+	if (!d)
+		DRETURN_PTR(NULL, DLEVEL_STABLE);
+
+	ewl_dialog_init(d, pos);
+
+	DRETURN_PTR(EWL_WIDGET(d), DLEVEL_STABLE);
 }
 
 /**
@@ -27,160 +26,187 @@ ewl_dialog_new (Ewl_Position pos)
  * @return Return TRUE on success, FALSE otherwise.
  * @brief Initialize an internal dialog to starting values
  */
-int
-ewl_dialog_init (Ewl_Dialog *dialog, Ewl_Position pos)
+int ewl_dialog_init(Ewl_Dialog * dialog, Ewl_Position pos)
 {
-  Ewl_Widget *w;
-  Ewl_Widget *box;
-  Ewl_Widget *spacer;
-  
-  DENTER_FUNCTION(DLEVEL_STABLE);
-  DCHECK_PARAM_PTR_RET("d", dialog, 0);
-  
-  w = EWL_WIDGET(dialog);
+	Ewl_Widget *w;
+	Ewl_Widget *box;
+	Ewl_Widget *spacer;
 
-  if (!ewl_window_init(EWL_WINDOW(dialog)))
-    DRETURN_INT(FALSE, DLEVEL_STABLE);
-  
-  ewl_widget_appearance_set(w, "window");
+	DENTER_FUNCTION(DLEVEL_STABLE);
+	DCHECK_PARAM_PTR_RET("d", dialog, 0);
 
-  dialog->position = pos;
-  
-  switch (pos)
-    {
-    case EWL_POSITION_LEFT : box = ewl_hbox_new (); break;
-    case EWL_POSITION_RIGHT: box = ewl_hbox_new (); break;
-    case EWL_POSITION_TOP  : box = ewl_vbox_new (); break;
-    default                : box = ewl_vbox_new (); break;
-    }
+	w = EWL_WIDGET(dialog);
 
-  if (box)
-    {
-      ewl_container_child_append(EWL_CONTAINER(dialog), box);
-      ewl_widget_show(box);
-    }
+	if (!ewl_window_init(EWL_WINDOW(dialog)))
+		DRETURN_INT(FALSE, DLEVEL_STABLE);
 
-  dialog->vbox = ewl_vbox_new ();
-  if (dialog->vbox)
-    {
-      ewl_container_child_append(EWL_CONTAINER(box), dialog->vbox);
-      ewl_box_homogeneous_set (EWL_BOX (dialog->vbox), FALSE);
-      switch (pos)
-	{
-	case EWL_POSITION_LEFT: 
-	  {
-	    dialog->action_area = ewl_vbox_new (); 
-	    dialog->separator = ewl_vseparator_new();
-	    break;
-	  }
-	case EWL_POSITION_RIGHT: 
-	  {
-	    dialog->action_area = ewl_vbox_new (); 
-	    dialog->separator = ewl_vseparator_new();
-	    break;
-	  }
-	case EWL_POSITION_TOP: 
-	  {
-	    dialog->action_area = ewl_hbox_new (); 
-	    dialog->separator = ewl_hseparator_new();
-	    break;
-	  }
-	default: 
-	  {
-	    dialog->action_area = ewl_hbox_new ();
-	    dialog->separator = ewl_hseparator_new();
-	    break;
-	  }
-	}
-      ewl_widget_show(dialog->vbox);
-    }
+	ewl_widget_appearance_set(w, "window");
 
-  if (dialog->separator)
-    {
-      switch (pos)
-	{
-	case EWL_POSITION_LEFT: 
-	  {
-	    ewl_container_child_prepend (EWL_CONTAINER (box),
-					 dialog->separator);
-	    break;
-	  }
-	case EWL_POSITION_TOP:
-	  {
-	    ewl_container_child_prepend (EWL_CONTAINER (box),
-					 dialog->separator);
-	    break;
-	  }
+	dialog->position = pos;
+
+	switch (pos) {
+	case EWL_POSITION_LEFT:
+		box = ewl_hbox_new();
+		break;
 	case EWL_POSITION_RIGHT:
-	  {
-	    ewl_container_child_append (EWL_CONTAINER (box),
-					dialog->separator);
-	    break;
-	  }
-	default: 
-	  {
-	    ewl_container_child_append (EWL_CONTAINER (box),
-					dialog->separator);
-	    break;
-	  }
-	}
-      ewl_object_fill_policy_set(EWL_OBJECT (dialog->separator), 
-				  EWL_FLAG_FILL_SHRINK);
-      ewl_widget_show(dialog->separator);
-    }
-
-  if (dialog->action_area)
-    {
-      switch (pos)
-	{
-	case EWL_POSITION_LEFT: 
-	  {
-	    ewl_container_child_prepend (EWL_CONTAINER (box), 
-					dialog->action_area);
-	    ewl_object_fill_policy_set (EWL_OBJECT (dialog->action_area), 
-					EWL_FLAG_FILL_VFILL);
-	    break;
-	  }
+		box = ewl_hbox_new();
+		break;
 	case EWL_POSITION_TOP:
-	  {
-	    ewl_container_child_prepend (EWL_CONTAINER (box), 
-					dialog->action_area);
-	    ewl_object_fill_policy_set (EWL_OBJECT (dialog->action_area), 
-					EWL_FLAG_FILL_HFILL);
-	    break;
-	  }
-	case EWL_POSITION_RIGHT:
-	  {
-	    ewl_container_child_append (EWL_CONTAINER (box), 
-					 dialog->action_area);
-	    ewl_object_fill_policy_set (EWL_OBJECT (dialog->action_area), 
-					EWL_FLAG_FILL_VFILL);
-	    break;
-	  }
-	default: 
-	  {
-	    ewl_container_child_append (EWL_CONTAINER (box), 
-					dialog->action_area);
-	    ewl_object_fill_policy_set (EWL_OBJECT (dialog->action_area), 
-					EWL_FLAG_FILL_HFILL);
-	    break;
-	  }
+		box = ewl_vbox_new();
+		break;
+	default:
+		box = ewl_vbox_new();
+		break;
 	}
 
-      ewl_box_homogeneous_set (EWL_BOX (dialog->action_area), FALSE);
-      ewl_widget_show (dialog->action_area);
-      
-      spacer = ewl_spacer_new();
-      ewl_container_child_append(EWL_CONTAINER(dialog->action_area),
-				 spacer);
-      ewl_object_fill_policy_set (EWL_OBJECT (spacer), EWL_FLAG_FILL_FILL);
-      ewl_widget_show (spacer);
+	if (box) {
+		ewl_container_child_append(EWL_CONTAINER(dialog), box);
+		ewl_widget_show(box);
+	}
 
-      ewl_container_redirect_set (EWL_CONTAINER (dialog), 
-				  EWL_CONTAINER (dialog->action_area));
-    }
-  
-  DRETURN_INT(TRUE, DLEVEL_STABLE); 
+	dialog->vbox = ewl_vbox_new();
+	if (dialog->vbox) {
+		ewl_container_child_append(EWL_CONTAINER(box),
+					   dialog->vbox);
+		ewl_box_homogeneous_set(EWL_BOX(dialog->vbox), FALSE);
+		switch (pos) {
+		case EWL_POSITION_LEFT:
+			{
+				dialog->action_area = ewl_vbox_new();
+				dialog->separator = ewl_vseparator_new();
+				break;
+			}
+		case EWL_POSITION_RIGHT:
+			{
+				dialog->action_area = ewl_vbox_new();
+				dialog->separator = ewl_vseparator_new();
+				break;
+			}
+		case EWL_POSITION_TOP:
+			{
+				dialog->action_area = ewl_hbox_new();
+				dialog->separator = ewl_hseparator_new();
+				break;
+			}
+		default:
+			{
+				dialog->action_area = ewl_hbox_new();
+				dialog->separator = ewl_hseparator_new();
+				break;
+			}
+		}
+		ewl_widget_show(dialog->vbox);
+	}
+
+	if (dialog->separator) {
+		switch (pos) {
+		case EWL_POSITION_LEFT:
+			{
+				ewl_container_child_prepend(EWL_CONTAINER
+							    (box),
+							    dialog->
+							    separator);
+				break;
+			}
+		case EWL_POSITION_TOP:
+			{
+				ewl_container_child_prepend(EWL_CONTAINER
+							    (box),
+							    dialog->
+							    separator);
+				break;
+			}
+		case EWL_POSITION_RIGHT:
+			{
+				ewl_container_child_append(EWL_CONTAINER
+							   (box),
+							   dialog->
+							   separator);
+				break;
+			}
+		default:
+			{
+				ewl_container_child_append(EWL_CONTAINER
+							   (box),
+							   dialog->
+							   separator);
+				break;
+			}
+		}
+		ewl_object_fill_policy_set(EWL_OBJECT(dialog->separator),
+					   EWL_FLAG_FILL_SHRINK);
+		ewl_widget_show(dialog->separator);
+	}
+
+	if (dialog->action_area) {
+		switch (pos) {
+		case EWL_POSITION_LEFT:
+			{
+				ewl_container_child_prepend(EWL_CONTAINER
+							    (box),
+							    dialog->
+							    action_area);
+				ewl_object_fill_policy_set(EWL_OBJECT
+							   (dialog->
+							    action_area),
+							   EWL_FLAG_FILL_VFILL);
+				break;
+			}
+		case EWL_POSITION_TOP:
+			{
+				ewl_container_child_prepend(EWL_CONTAINER
+							    (box),
+							    dialog->
+							    action_area);
+				ewl_object_fill_policy_set(EWL_OBJECT
+							   (dialog->
+							    action_area),
+							   EWL_FLAG_FILL_HFILL);
+				break;
+			}
+		case EWL_POSITION_RIGHT:
+			{
+				ewl_container_child_append(EWL_CONTAINER
+							   (box),
+							   dialog->
+							   action_area);
+				ewl_object_fill_policy_set(EWL_OBJECT
+							   (dialog->
+							    action_area),
+							   EWL_FLAG_FILL_VFILL);
+				break;
+			}
+		default:
+			{
+				ewl_container_child_append(EWL_CONTAINER
+							   (box),
+							   dialog->
+							   action_area);
+				ewl_object_fill_policy_set(EWL_OBJECT
+							   (dialog->
+							    action_area),
+							   EWL_FLAG_FILL_HFILL);
+				break;
+			}
+		}
+
+		ewl_box_homogeneous_set(EWL_BOX(dialog->action_area),
+					FALSE);
+		ewl_widget_show(dialog->action_area);
+
+		spacer = ewl_spacer_new();
+		ewl_container_child_append(EWL_CONTAINER
+					   (dialog->action_area), spacer);
+		ewl_object_fill_policy_set(EWL_OBJECT(spacer),
+					   EWL_FLAG_FILL_FILL);
+		ewl_widget_show(spacer);
+
+		ewl_container_redirect_set(EWL_CONTAINER(dialog),
+					   EWL_CONTAINER(dialog->
+							 action_area));
+	}
+
+	DRETURN_INT(TRUE, DLEVEL_STABLE);
 }
 
 /**
@@ -189,40 +215,42 @@ ewl_dialog_init (Ewl_Dialog *dialog, Ewl_Position pos)
  * @return Returns no value.
  * @brief Convenient function to add widgets in the vbox.
  */
-void
-ewl_dialog_widget_add (Ewl_Dialog *dialog, Ewl_Widget *w)
+void ewl_dialog_widget_add(Ewl_Dialog * dialog, Ewl_Widget * w)
 {
-  DENTER_FUNCTION(DLEVEL_STABLE);
-  DCHECK_PARAM_PTR("d", dialog);
+	DENTER_FUNCTION(DLEVEL_STABLE);
+	DCHECK_PARAM_PTR("d", dialog);
 
-  if (!dialog)
-    return;
-  
-  switch (dialog->position)
-    {
-    case EWL_POSITION_LEFT: 
-      {
-	ewl_container_child_append (EWL_CONTAINER (dialog->vbox), w);
-	break;
-      }
-    case EWL_POSITION_TOP:
-      {
-	ewl_container_child_append (EWL_CONTAINER (dialog->vbox), w);
-	break;
-      }
-    case EWL_POSITION_RIGHT:
-      {
-	ewl_container_child_prepend (EWL_CONTAINER (dialog->vbox), w);
-	break;
-      }
-    default: 
-      {
-	ewl_container_child_prepend (EWL_CONTAINER (dialog->vbox), w);
-	break;
-      }
-    }
-  
-  DLEAVE_FUNCTION(DLEVEL_STABLE);
+	if (!dialog)
+		return;
+
+	switch (dialog->position) {
+	case EWL_POSITION_LEFT:
+		{
+			ewl_container_child_append(EWL_CONTAINER
+						   (dialog->vbox), w);
+			break;
+		}
+	case EWL_POSITION_TOP:
+		{
+			ewl_container_child_append(EWL_CONTAINER
+						   (dialog->vbox), w);
+			break;
+		}
+	case EWL_POSITION_RIGHT:
+		{
+			ewl_container_child_prepend(EWL_CONTAINER
+						    (dialog->vbox), w);
+			break;
+		}
+	default:
+		{
+			ewl_container_child_prepend(EWL_CONTAINER
+						    (dialog->vbox), w);
+			break;
+		}
+	}
+
+	DLEAVE_FUNCTION(DLEVEL_STABLE);
 }
 
 /**
@@ -232,30 +260,30 @@ ewl_dialog_widget_add (Ewl_Dialog *dialog, Ewl_Widget *w)
  * @return Returns a button, or NULL on failure.
  * @brief Add a (stock) button on the right of the action_area of @a dialog.
  */
-Ewl_Widget *
-ewl_dialog_button_add (Ewl_Dialog *dialog, char *button_text,
-		       int response_id)
+Ewl_Widget *ewl_dialog_button_add(Ewl_Dialog * dialog, char *button_text,
+				  int response_id)
 {
-  Ewl_Widget *button;
-  
-  DENTER_FUNCTION(DLEVEL_STABLE);
-  DCHECK_PARAM_PTR_RET("d", dialog, 0);
+	Ewl_Widget *button;
 
-  if (!dialog)
-    return NULL;
+	DENTER_FUNCTION(DLEVEL_STABLE);
+	DCHECK_PARAM_PTR_RET("d", dialog, 0);
 
-  button = ewl_button_stock_new (button_text);
-  ewl_object_padding_set (EWL_OBJECT (button), 0, 3, 3, 3);
-  ewl_container_child_append (EWL_CONTAINER (dialog->action_area),
-			      button);
-  ewl_object_fill_policy_set (EWL_OBJECT (button),
-			      EWL_FLAG_FILL_VFILL || EWL_FLAG_FILL_SHRINK);
+	if (!dialog)
+		return NULL;
 
-  ewl_widget_data_set(button, "RESPONSE_ID", (void *)response_id);
+	button = ewl_button_stock_new(button_text);
+	ewl_object_padding_set(EWL_OBJECT(button), 0, 3, 3, 3);
+	ewl_container_child_append(EWL_CONTAINER(dialog->action_area),
+				   button);
+	ewl_object_fill_policy_set(EWL_OBJECT(button),
+				   EWL_FLAG_FILL_VFILL
+				   || EWL_FLAG_FILL_SHRINK);
 
-  ewl_widget_show(button);
-  
-  DRETURN_PTR(button, DLEVEL_STABLE);
+	ewl_widget_data_set(button, "RESPONSE_ID", (void *) response_id);
+
+	ewl_widget_show(button);
+
+	DRETURN_PTR(button, DLEVEL_STABLE);
 }
 
 /**
@@ -265,30 +293,30 @@ ewl_dialog_button_add (Ewl_Dialog *dialog, char *button_text,
  * @return Returns a button, or NULL on failure.
  * @brief Same as ewl_dialog_add_button(), but add the button on the left.
  */
-Ewl_Widget *
-ewl_dialog_button_left_add (Ewl_Dialog *dialog, char *button_text,
-			    int response_id)
+Ewl_Widget *ewl_dialog_button_left_add(Ewl_Dialog * dialog,
+				       char *button_text, int response_id)
 {
-  Ewl_Widget *button;
-  
-  DENTER_FUNCTION(DLEVEL_STABLE);
-  DCHECK_PARAM_PTR_RET("d", dialog, 0);
+	Ewl_Widget *button;
 
-  if (!dialog)
-    return NULL;
+	DENTER_FUNCTION(DLEVEL_STABLE);
+	DCHECK_PARAM_PTR_RET("d", dialog, 0);
 
-  button = ewl_button_stock_new (button_text);
-  ewl_object_padding_set (EWL_OBJECT (button), 0, 3, 3, 3);
-  ewl_container_child_prepend (EWL_CONTAINER (dialog->action_area),
-			       button);
-  ewl_object_fill_policy_set (EWL_OBJECT (button),
-			      EWL_FLAG_FILL_VFILL || EWL_FLAG_FILL_SHRINK);
+	if (!dialog)
+		return NULL;
 
-  ewl_widget_data_set (button, "RESPONSE_ID", (void *)response_id);
+	button = ewl_button_stock_new(button_text);
+	ewl_object_padding_set(EWL_OBJECT(button), 0, 3, 3, 3);
+	ewl_container_child_prepend(EWL_CONTAINER(dialog->action_area),
+				    button);
+	ewl_object_fill_policy_set(EWL_OBJECT(button),
+				   EWL_FLAG_FILL_VFILL
+				   || EWL_FLAG_FILL_SHRINK);
 
-  ewl_widget_show (button);
-  
-  DRETURN_PTR(button, DLEVEL_STABLE);
+	ewl_widget_data_set(button, "RESPONSE_ID", (void *) response_id);
+
+	ewl_widget_show(button);
+
+	DRETURN_PTR(button, DLEVEL_STABLE);
 }
 
 /**
@@ -296,16 +324,15 @@ ewl_dialog_button_left_add (Ewl_Dialog *dialog, char *button_text,
  * @return Returns TRUE if @a dialog has a separator.
  * @brief Checks if @a dialog has a separator or not.
  */
-unsigned int
-ewl_dialog_has_separator_get (Ewl_Dialog *dialog)
+unsigned int ewl_dialog_has_separator_get(Ewl_Dialog * dialog)
 {
-  DENTER_FUNCTION(DLEVEL_STABLE);
-  DCHECK_PARAM_PTR_RET("d", dialog, 0);
+	DENTER_FUNCTION(DLEVEL_STABLE);
+	DCHECK_PARAM_PTR_RET("d", dialog, 0);
 
-  if (!dialog)
-    return FALSE;
+	if (!dialog)
+		return FALSE;
 
-  DRETURN_INT(dialog->separator != NULL, DLEVEL_STABLE);
+	DRETURN_INT(dialog->separator != NULL, DLEVEL_STABLE);
 }
 
 /**
@@ -315,45 +342,43 @@ ewl_dialog_has_separator_get (Ewl_Dialog *dialog)
  * @brief Sets the separator of @a dialog.
  */
 void
-ewl_dialog_has_separator_set (Ewl_Dialog *dialog,
-			      unsigned int has_sep)
+ewl_dialog_has_separator_set(Ewl_Dialog * dialog, unsigned int has_sep)
 {
-  Ewl_Widget *child;
-  int         n;
-  
-  DENTER_FUNCTION(DLEVEL_STABLE);
-  DCHECK_PARAM_PTR("d", dialog);
+	Ewl_Widget *child;
+	int n;
 
-  if (!dialog)
-    DLEAVE_FUNCTION(DLEVEL_STABLE);
+	DENTER_FUNCTION(DLEVEL_STABLE);
+	DCHECK_PARAM_PTR("d", dialog);
 
-  if (has_sep && (dialog->separator == NULL))
-    {
-      ewl_container_child_iterate_begin (EWL_CONTAINER (EWL_DIALOG
-							(dialog)->vbox));
-      n = 0;
-      child = ewl_container_child_next (EWL_CONTAINER (EWL_DIALOG
-						       (dialog)->vbox));
-      while (child)
-	{
-	  n++;
-	  child = ewl_container_child_next (EWL_CONTAINER (EWL_DIALOG
-							   (dialog)->vbox));
+	if (!dialog)
+		DLEAVE_FUNCTION(DLEVEL_STABLE);
+
+	if (has_sep && (dialog->separator == NULL)) {
+		ewl_container_child_iterate_begin(EWL_CONTAINER(EWL_DIALOG
+								(dialog)->
+								vbox));
+		n = 0;
+		child = ewl_container_child_next(EWL_CONTAINER(EWL_DIALOG
+							       (dialog)->
+							       vbox));
+		while (child) {
+			n++;
+			child =
+			    ewl_container_child_next(EWL_CONTAINER
+						     (EWL_DIALOG(dialog)->
+						      vbox));
+		}
+		dialog->separator = ewl_hseparator_new();
+		ewl_container_child_insert(EWL_CONTAINER(dialog->vbox),
+					   dialog->separator, n);
+		ewl_object_fill_policy_set(EWL_OBJECT(dialog->separator),
+					   EWL_FLAG_FILL_SHRINK);
+		ewl_widget_show(dialog->separator);
+
+	} else if (!has_sep && (dialog->separator != NULL)) {
+		ewl_widget_destroy(dialog->separator);
+		dialog->separator = NULL;
 	}
-      dialog->separator = ewl_hseparator_new();
-      ewl_container_child_insert(EWL_CONTAINER(dialog->vbox),
-				 dialog->separator, n);
-      ewl_object_fill_policy_set (EWL_OBJECT (dialog->separator), 
-				  EWL_FLAG_FILL_SHRINK);
-      ewl_widget_show(dialog->separator);
-      
-    }
-  else if (!has_sep && (dialog->separator != NULL))
-    {
-      ewl_widget_destroy (dialog->separator);
-      dialog->separator = NULL;
-    }
 
-  DLEAVE_FUNCTION(DLEVEL_STABLE);
+	DLEAVE_FUNCTION(DLEVEL_STABLE);
 }
-
--- e17/libs/ewl/src/ewl_dialog.h
+++ e17/libs/ewl/src/ewl_dialog.h
@@ -28,15 +28,15 @@ typedef enum _Ewl_Response_Type Ewl_Response_Type;
  */
 enum _Ewl_Response_Type
 {
-  EWL_RESPONSE_OPEN   = -5,
-  EWL_RESPONSE_SAVE   = -6,
-  EWL_RESPONSE_OK     = -10,
-  EWL_RESPONSE_CANCEL = -11,
-  EWL_RESPONSE_APPLY  = -12,
-  EWL_RESPONSE_PLAY   = -20,
-  EWL_RESPONSE_PAUSE  = -21,
-  EWL_RESPONSE_STOP   = -22,
-  EWL_RESPONSE_QUIT   = -23,
+	EWL_RESPONSE_OPEN   = -5,
+	EWL_RESPONSE_SAVE   = -6,
+	EWL_RESPONSE_OK     = -10,
+	EWL_RESPONSE_CANCEL = -11,
+	EWL_RESPONSE_APPLY  = -12,
+	EWL_RESPONSE_PLAY   = -20,
+	EWL_RESPONSE_PAUSE  = -21,
+	EWL_RESPONSE_STOP   = -22,
+	EWL_RESPONSE_QUIT   = -23,
 };
 
 /**
@@ -60,28 +60,27 @@ typedef struct _Ewl_Dialog Ewl_Dialog;
  */
 struct _Ewl_Dialog
 {
-  /* public */
-  Ewl_Window window; /* Inherit from a window */
-  
-  Ewl_Widget *vbox;        /* the box where messages are displayed */
-  Ewl_Widget *action_area; /* The box where the buttons are added */
-  
-  /* private */
-  Ewl_Widget *separator;   /* The separator between vbox and action_area */
+	/* public */
+	Ewl_Window window; /* Inherit from a window */
 
-  Ewl_Position position;  /* position of the action_area */
+	Ewl_Widget *vbox;        /* the box where messages are displayed */
+	Ewl_Widget *action_area; /* The box where the buttons are added */
+
+	/* private */
+	Ewl_Widget *separator;   /* The separator between vbox and action_area */
+
+	Ewl_Position position;  /* position of the action_area */
 };
   
 Ewl_Widget *ewl_dialog_new (Ewl_Position pos);
 int         ewl_dialog_init (Ewl_Dialog *dialog, Ewl_Position pos);
 
-void        ewl_dialog_widget_add      (Ewl_Dialog *dialog, Ewl_Widget *w);
-Ewl_Widget *ewl_dialog_button_add      (Ewl_Dialog *dialog, 
-					char       *button_text,
-					int         response_id);
-Ewl_Widget *ewl_dialog_button_left_add (Ewl_Dialog *dialog, 
-					char       *button_text,
-					int         response_id);
+void        ewl_dialog_widget_add(Ewl_Dialog *dialog, Ewl_Widget *w);
+Ewl_Widget *ewl_dialog_button_add(Ewl_Dialog *dialog, 
+				  char       *button_text,
+				  int         response_id);
+Ewl_Widget *ewl_dialog_button_left_add(Ewl_Dialog *dialog, char *button_text,
+				       int response_id);
 
 unsigned int ewl_dialog_has_separator_get (Ewl_Dialog *dialog);
 void         ewl_dialog_has_separator_set (Ewl_Dialog *dialog,
