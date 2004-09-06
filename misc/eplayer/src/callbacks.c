dfe3f6c11e618ecca2023d69fe169805eeafd39a
fletch3k <fletch3k@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-09-06 11:26:03 +0000

    EWL Api Changed
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@11530 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/eplayer/src/callbacks.c
+++ misc/eplayer/src/callbacks.c
@@ -527,19 +527,19 @@ EDJE_CB(playlist_add) {
 	}
 
 	fd_win = ewl_window_new();
-	ewl_window_set_title(EWL_WINDOW(fd_win), "Eplayer Add File...");
-	ewl_window_set_name(EWL_WINDOW(fd_win), "Eplayer Add File...");
-	ewl_object_request_size(EWL_OBJECT(fd_win), 500, 400);
-	ewl_object_set_fill_policy(EWL_OBJECT(fd_win), EWL_FLAG_FILL_FILL |
+	ewl_window_title_set(EWL_WINDOW(fd_win), "Eplayer Add File...");
+	ewl_window_name_set(EWL_WINDOW(fd_win), "Eplayer Add File...");
+	ewl_object_size_request(EWL_OBJECT(fd_win), 500, 400);
+	ewl_object_fill_policy_set(EWL_OBJECT(fd_win), EWL_FLAG_FILL_FILL |
 	                           EWL_FLAG_FILL_SHRINK);
 
 	ewl_callback_append(fd_win, EWL_CALLBACK_DELETE_WINDOW,
 	                    destroy_ewl_filedialog, NULL);
 
 	vbox = ewl_vbox_new();
-	ewl_object_set_fill_policy(EWL_OBJECT(vbox), EWL_FLAG_FILL_FILL |
+	ewl_object_fill_policy_set(EWL_OBJECT(vbox), EWL_FLAG_FILL_FILL |
 	                           EWL_FLAG_FILL_SHRINK);
-	ewl_container_append_child(EWL_CONTAINER(fd_win), vbox);
+	ewl_container_child_append(EWL_CONTAINER(fd_win), vbox);
 	ewl_widget_show(vbox);
 
 	fd = ewl_filedialog_new(EWL_FILEDIALOG_TYPE_OPEN);
@@ -547,11 +547,11 @@ EDJE_CB(playlist_add) {
 	ewl_callback_append(fd, EWL_CALLBACK_VALUE_CHANGED,
 	                    cb_file_dialog_value_changed, player);
 
-	ewl_container_append_child(EWL_CONTAINER(vbox), fd);
+	ewl_container_child_append(EWL_CONTAINER(vbox), fd);
 	ewl_widget_show(fd);
 
 	_fd_win = fd_win;
-	ewl_widget_set_data(_fd_win, "player", player);
+	ewl_widget_data_set(_fd_win, "player", player);
     ewl_widget_show(_fd_win);
 }
 
