503fdf685be4b1958c9510baa4415f5ce406a3a4
fletch3k <fletch3k@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-09-07 07:37:46 +0000

    Configuration/Usage Implimented
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@11542 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/envision/src/callbacks.h
+++ misc/envision/src/callbacks.h
@@ -16,18 +16,18 @@
          const char *emission, const char *source)
 
 // callbacks.c
-void canvas_resize(Ecore_Evas *ee);
-void update_timer(void *data, Evas_Object *obj, void *event_info);
-void callback_evas_keydown(void *data, Evas *e, Evas_Object *obj,
-      void *event_info);
+void canvas_resize (Ecore_Evas * ee);
+void update_timer (void *data, Evas_Object * obj, void *event_info);
+void callback_evas_keydown (void *data, Evas * e, Evas_Object * obj,
+			    void *event_info);
 
 // Edje Callbacks
-EDJE_CALLBACK(quit);
-EDJE_CALLBACK(play);
-EDJE_CALLBACK(pause);
-EDJE_CALLBACK(volume_raise);
-EDJE_CALLBACK(volume_lower);
-EDJE_CALLBACK(seek_forward);
-EDJE_CALLBACK(seek_backward);
+EDJE_CALLBACK (quit);
+EDJE_CALLBACK (play);
+EDJE_CALLBACK (pause);
+EDJE_CALLBACK (volume_raise);
+EDJE_CALLBACK (volume_lower);
+EDJE_CALLBACK (seek_forward);
+EDJE_CALLBACK (seek_backward);
 
 #endif /* __CALLBACKS_H */
