78c032f2ec3f5499b13371cada51f8fb5cb3b70a
atmosphere <atmosphere@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-10-09 18:56:01 +0000

    use new functions
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@11802 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/epsilon/src/bin/epsilon_main.c
+++ e17/libs/epsilon/src/bin/epsilon_main.c
@@ -4,42 +4,63 @@
 #include"../config.h"
 
 int
-main(int argc, char *argv[])
+main (int argc, char *argv[])
 {
-    Epsilon *e = NULL;	
+  Epsilon *e = NULL;
 
-    epsilon_init();
-    while(--argc)
+  epsilon_init ();
+  while (--argc)
     {
-	if((e = epsilon_new((const char*)argv[argc])))
+      if ((e = epsilon_new ((const char *) argv[argc])))
 	{
-	    if(epsilon_exists(e) == EPSILON_FAIL)
+	  if (epsilon_exists (e) == EPSILON_FAIL)
 	    {
-		fprintf(stderr, 
-		"Thumbnail for %s needs to be generated: ", argv[argc]);
-		if(epsilon_generate(e) == EPSILON_OK)
-		    fprintf(stderr, "OK\n");
-		else
-		    fprintf(stderr, "FAILED\n");
+	      fprintf (stderr,
+		       "Thumbnail for %s needs to be generated: ",
+		       argv[argc]);
+	      if (epsilon_generate (e) == EPSILON_OK)
+		fprintf (stderr, "OK\n");
+	      else
+		fprintf (stderr, "FAILED\n");
 	    }
-	    else
+	  else
 	    {
-		    Epsilon_Info *info;
-		    if((info = epsilon_info_get(e)))
+	      Epsilon_Info *info;
+	      Epsilon_Exif_Info *eei;
+	      fprintf (stderr, "\nThumbnail already exists\n%s\n",
+		       epsilon_thumb_file_get (e));
+	      fprintf (stderr, "Thumbnail already exists\n");
+	      if ((info = epsilon_info_get (e)))
+		{
+		  if (info->uri)
+		    printf ("URI: %s\n", info->uri);
+		  if (info->mimetype)
+		    printf ("MimeType: %s\n", info->mimetype);
+		  printf ("Source Image Width: %d\n", info->w);
+		  printf ("Source Image Height: %d\n", info->h);
+		  printf ("Source Image Mtime: %d\n", (int) info->mtime);
+		  fprintf (stderr, "Trying EXIF Info: ");
+		  if (epsilon_info_has_exif_get (info))
 		    {
-			if(info->uri)
-			    printf("URI: %s\n", info->uri);
-			if(info->mimetype)
-			    printf("MimeType: %s\n", info->mimetype);
-			printf("Source Image Width: %d\n", info->w);
-			printf("Source Image Height: %d\n", info->h);
-			printf("Source Image Mtime: %d\n", (int)info->mtime);
-			epsilon_info_free(info);
+			fprintf (stderr, "Found!\n");
+#if 0
+		  fprintf (stderr, "%d is direction\n",
+			   epsilon_exif_info_props_as_int_get (eei, 0x0112));
+#endif
+			epsilon_info_exif_props_print (info);
 		    }
-		fprintf(stderr, "Thumbnail already exists\n");
+		    else {
+			fprintf (stderr, "Not Found!\n");
+		    }
+		  epsilon_info_free (info);
+		}
+	      else
+		{
+		  fprintf (stderr, "Meta Info Not Found!\n");
+		}
 	    }
-	    epsilon_free(e);
+	  epsilon_free (e);
 	}
     }
-    return(0);
+  return (0);
 }
