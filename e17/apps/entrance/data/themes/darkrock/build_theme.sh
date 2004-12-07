fba837813f8ec6387c439110baa917326b961d0d
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2004-12-07 03:50:28 +0000

    clean theme building significantly
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@12390 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/entrance/data/themes/darkrock/build_theme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-if [ "x$1" != "x" ]; then
-    SRCDIR=$1
-else
-    SRCDIR=.
-fi
-edje_cc -v -id $SRCDIR/img $SRCDIR/darkrock.edc $SRCDIR/darkrock.eet
--- e17/apps/entrance/data/themes/taillights/build_theme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-THEME="taillights"
-if [ -f $THEME.eet ]; then
-    rm -f $THEME.eet
-fi
-edje_cc -v -id ./images $THEME.edc $THEME.eet
-#edje $THEME.eet
-#sudo cp -f $THEME.eet /usr/share/entrance/themes/
