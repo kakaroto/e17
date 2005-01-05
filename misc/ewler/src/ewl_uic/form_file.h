36fe353a7bdbdfd9b152f120bb3ddae6580ce016
werkt <werkt@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2005-01-05 05:21:03 +0000

    Cleaning out src for new version
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@12762 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/ewler/src/ewl_uic/form_file.h
+++
@@ -1,6 +0,0 @@
-#ifndef FORM_FILE_H
-#define FORM_FILE_H
-
-Ecore_List *form_file_read( int fd );
-
-#endif
--- misc/ewler/src/layout.h
+++
@@ -1,8 +0,0 @@
-#ifndef LAYOUT_H
-#define LAYOUT_H
-
-void layout_horizontal_cb( Ewl_Widget *w, void *ev_data, void *user_data );
-void layout_vertical_cb( Ewl_Widget *w, void *ev_data, void *user_data );
-void layout_break_cb( Ewl_Widget *w, void *ev_data, void *user_data );
-
-#endif
