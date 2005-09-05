696558f92ede37a8d590032d1ff166f53e17083f
sebastid <sebastid@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2005-09-05 15:05:53 +0000

    Add macros.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@16571 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/etox/src/style/Etox_Style.h
+++ e17/libs/etox/src/style/Etox_Style.h
@@ -4,6 +4,23 @@
 #include <Edb.h>
 #include <Evas.h>
 
+#define IF_FREE(ptr) if (ptr) free(ptr); ptr = NULL;
+#define FREE(ptr) free(ptr); ptr = NULL;
+
+#define CHECK_PARAM_POINTER_RETURN(sparam, param, ret) \
+     if (!(param)) \
+	 { \
+	    fprintf(stderr, "Fix: func: %s, param: %s\n", __FUNCTION__, sparam); \
+	    return ret; \
+	 }
+
+#define CHECK_PARAM_POINTER(sparam, param) \
+     if (!(param)) \
+	 { \
+	    fprintf(stderr, "Fix: func: %s, param: %s\n", __FUNCTION__, sparam); \
+	    return; \
+	 }
+
 
 #ifdef __cplusplus
 extern "C"
