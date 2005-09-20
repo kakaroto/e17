bb54988a54fb995f7364a18a015583cc7771788b
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2005-09-20 08:49:24 +0000

    include math! and stuff.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@16783 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/imlib2/src/lib/color_helpers.h
+++ e17/libs/imlib2/src/lib/color_helpers.h
@@ -1,6 +1,12 @@
 #ifndef __COLOR_HELPERS 
 #define __COLOR_HELPERS 1
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+
 void        __imlib_rgb_to_hsv(int r, int g, int b, float *hue, float *saturation, float *value);
 void        __imlib_hsv_to_rgb(float hue, float saturation, float value, int *r, int *g, int *b);
 void        __imlib_rgb_to_hls(int r, int g, int b, float *hue, float *lightness, float *saturation);
