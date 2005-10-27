d2343456999e69c8ce09ff6f5ab93361a5269f71
vapier <vapier@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2005-10-27 07:46:14 +0000

    dont need to touch README
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@18024 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/etk/autogen.sh
+++ e17/libs/etk/autogen.sh
@@ -3,8 +3,6 @@
 rm -rf autom4te.cache
 rm -f aclocal.m4 ltmain.sh
 
-touch README
-
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
