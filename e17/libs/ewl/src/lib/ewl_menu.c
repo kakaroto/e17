fc459da8deb0a2e07ac14ba2ade1d94880479632
lordchaos <lordchaos@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2005-10-28 11:39:14 +0000

    Turn off until we can figure out why this stops menus moving
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@18062 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/lib/ewl_menu.c
+++ e17/libs/ewl/src/lib/ewl_menu.c
@@ -62,7 +62,9 @@ ewl_menu_init(Ewl_Menu *menu)
 	menu->base.popup = ewl_window_new();
 	ewl_window_keyboard_grab_set(EWL_WINDOW(menu->base.popup), TRUE);
 	ewl_window_pointer_grab_set(EWL_WINDOW(menu->base.popup), TRUE);
-	ewl_window_override_set(EWL_WINDOW(menu->base.popup), TRUE);
+	
+	/*Turn this off until we can figure out why this stops windows being moved*/
+	/*ewl_window_override_set(EWL_WINDOW(menu->base.popup), TRUE);*/ 
 	ewl_window_borderless_set(EWL_WINDOW(menu->base.popup));
 	ewl_widget_internal_set(menu->base.popup, TRUE);
 	ewl_widget_appearance_set(EWL_WIDGET(menu->base.popup), "menu");
