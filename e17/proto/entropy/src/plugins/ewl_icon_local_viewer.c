5c4c8ec356088f6a2a637307c3510b328a01ef6c
lordchaos <lordchaos@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2005-10-30 01:35:37 +0000

    Add a 'delete' option
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@18121 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/entropy/src/plugins/ewl_icon_local_viewer.c
+++ e17/proto/entropy/src/plugins/ewl_icon_local_viewer.c
@@ -482,6 +482,12 @@ entropy_gui_component_instance* entropy_plugin_init(entropy_core* core,entropy_g
 	ewl_widget_show(context);
 
 
+	/*Icon menu*/
+	context = ewl_menu_item_new();
+	ewl_menu_item_text_set(EWL_MENU_ITEM(context), "Delete file");
+	ewl_widget_show(context);
+	ewl_iconbox_icon_menu_item_add(EWL_ICONBOX(viewer->iconbox), context);
+	//ewl_callback_append(context, EWL_CALLBACK_MOUSE_DOWN, icon_properties_cb, instance);
 
 	/*Icon menu*/
 	context = ewl_menu_item_new();
