72e298c0dcb5eba789c58bc5a8ffcb3350524eb9
rbdpngn <rbdpngn@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2005-11-13 06:48:00 +0000

    Forgot the header with the function prototype change.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@18451 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/lib/ewl_tree.h
+++ e17/libs/ewl/src/lib/ewl_tree.h
@@ -117,6 +117,7 @@ void ewl_tree_node_resize_cb(Ewl_Container *c, Ewl_Widget *w, int size,
 void ewl_tree_row_select_cb(Ewl_Widget *w, void *ev_data, void *user_data);
 void ewl_tree_row_hide_cb(Ewl_Widget *w, void *ev_data, void *user_data);
 void ewl_tree_configure_cb(Ewl_Widget *w, void *ev_data, void *user_data);
+void ewl_tree_header_configure_cb(Ewl_Widget *w, void *ev_data, void *user_data);
 void ewl_tree_destroy_cb(Ewl_Widget *w, void *ev_data, void *user_data);
 void ewl_tree_hscroll_cb(Ewl_Widget *w, void *ev_data, void *user_data);
 void ewl_tree_child_resize_cb(Ewl_Container *c);
