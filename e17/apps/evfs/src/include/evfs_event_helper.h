ed9381076f9da6694449ed779ff3c3a41c835572
lordchaos <lordchaos@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2005-11-20 12:12:33 +0000

    * Stop gcc complaining
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@18551 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/evfs/src/include/evfs_event_helper.h
+++ e17/apps/evfs/src/include/evfs_event_helper.h
@@ -1,8 +1,14 @@
+#ifndef __EVFS_EVENT_HELPER_
+#define __EVFS_EVENT_HELPER_
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include "evfs.h"
 
 void evfs_file_monitor_event_create(evfs_client* client, int type, const char* pathi, const char* plugin);
 void evfs_stat_event_create(evfs_client* client, evfs_command* command, struct stat* stat_obj);
 void evfs_list_dir_event_create(evfs_client* client, evfs_command* command, Ecore_List* files);
 void evfs_file_progress_event_create(evfs_client* client, evfs_command* command, double progress,evfs_progress_type type);
+
+#endif
