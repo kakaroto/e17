de89e2058c93b5184c6c842ad94b5a39273d2bd7
rbdpngn <rbdpngn@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2005-11-20 19:48:04 +0000

    limits.h again.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@18556 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/evfs/src/plugins/evfs_fs_posix.c
+++ e17/apps/evfs/src/plugins/evfs_fs_posix.c
@@ -32,6 +32,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
