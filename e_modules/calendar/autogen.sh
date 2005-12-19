5e04d9034c03692c5911fa936341474968c1a394
nasa01 <nasa01@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2005-12-19 01:56:38 +0000

    Added calendar module to e_modules
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@19130 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e_modules/calendar/autogen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -rf autom4te.cache
+rm -f aclocal.m4
+
+echo "Running aclocal..."; aclocal $ACLOCAL_FLAGS -I m4 \
+&& echo "Running autoheader..."; autoheader \
+&& echo "Running autoconf..."; autoconf \
+&& echo "Running libtoolize..."; (libtoolize --automake || glibtoolize --automake) \
+&& echo "Running automake..."; automake --add-missing --copy --gnu
+
+if [ -z "$NOCONFIGURE" ]; then
+	./configure "$@"
+fi
