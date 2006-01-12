bbadb09625d0ac296af1d75f453640569d023a30
dj2 <dj2@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-01-12 14:58:33 +0000

    - make the test window larger
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@19741 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/bin/ewl_test.c
+++ e17/libs/ewl/src/bin/ewl_test.c
@@ -319,7 +319,7 @@ main(int argc, char **argv)
 	printf("HEAP SIZE:\t%u bytes\n", heap_end - heap_start);
 
 	main_win = ewl_window_new();
-	ewl_object_size_request(EWL_OBJECT(main_win), 400, 400);
+	ewl_object_size_request(EWL_OBJECT(main_win), 600, 400);
 	ewl_window_title_set(EWL_WINDOW(main_win),
 			     "The Enlightenment Widget Library");
 	ewl_window_name_set(EWL_WINDOW(main_win), "EWL Test Application");
