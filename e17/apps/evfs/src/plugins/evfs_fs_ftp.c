3ccd21a219c3da92593f9a85f06be0f63a97ea75
lordchaos <lordchaos@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-01-17 11:16:27 +0000

    * Fix warning
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@19875 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/evfs/src/plugins/evfs_fs_ftp.c
+++ e17/apps/evfs/src/plugins/evfs_fs_ftp.c
@@ -317,7 +317,7 @@ int evfs_client_disconnect(evfs_client* client)
 {
 	/*Temp, move to plugin unloading when avaliable.  Closes the curl library, and
 		resets the connection cache.  This will just destroy the cache when moved.*/
-	printf ("Received disconnect for client at evfs_fs_ftp.c for client %d\n", client->id);
+	printf ("Received disconnect for client at evfs_fs_ftp.c for client %ld\n", client->id);
 	curl_global_cleanup();
 	ecore_hash_destroy(connections);
 	ecore_hash_new(ecore_str_hash, ecore_str_compare);
