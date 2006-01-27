82b66ea7accb7f1c004e02c7265b8f5cf34403b6
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-01-27 15:54:19 +0000

    Add module
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@20071 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/proto/emenu/src/module/e_mod_main.h
@@ -0,0 +1,30 @@
+#ifndef E_MOD_MAIN_H
+#define E_MOD_MAIN_H
+
+typedef struct _Config Config;
+struct _Config 
+{
+   
+};
+
+typedef struct _EMenu EMenu;
+struct _EMenu 
+{
+   E_Int_Menu_Augmentation *augment;
+   
+   E_Config_DD *conf_edd;
+   Config *conf;
+};
+
+EAPI extern E_Module_Api e_modapi;
+
+EAPI void *e_modapi_init     (E_Module *m);
+EAPI int   e_modapi_shutdown (E_Module *m);
+EAPI int   e_modapi_save     (E_Module *m);
+EAPI int   e_modapi_info     (E_Module *m);
+EAPI int   e_modapi_about    (E_Module *m);
+
+/* Disabled for now
+EAPI int   e_modapi_config   (E_Module *m); */
+
+#endif
