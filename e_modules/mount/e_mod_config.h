cc220674af258ed07b0ecb810585823db7a89d27
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-01-30 03:47:08 +0000

    Pulling my modules out of cvs. I'll maintain them myself, using a code format that I like.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@20111 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/mount/e_mod_config.h
+++
@@ -1,8 +0,0 @@
-#ifdef E_TYPEDEFS
-#else
-#ifndef E_MOD_CONFIG_H
-#define E_MOD_CONFIG_h
-#include "e_mod_main.h"
-void                _config_mount_module(E_Container * con, Mount * m);
-#endif
-#endif
