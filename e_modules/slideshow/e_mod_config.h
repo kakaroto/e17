d021dd0d0333826bc79b5f560e234840d2ed2867
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-01-30 03:55:47 +0000

    Pulled out of cvs so I can maintain a code format I like without someone changing it on me.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@20113 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/slideshow/e_mod_config.h
+++
@@ -1,8 +0,0 @@
-#ifdef E_TYPEDEFS
-#else
-#ifndef E_MOD_CONFIG_H
-#define E_MOD_CONFIG_H
-#include "e_mod_main.h"
-void                _config_slideshow_module(E_Container * con, Slide * s);
-#endif
-#endif
