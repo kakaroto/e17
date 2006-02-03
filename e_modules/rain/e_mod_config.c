efdbf1f3867b55c21487c8322d5f8cc9b6267d1c
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-02-03 01:32:41 +0000

    Formatting
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@20208 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/rain/e_mod_config.c
+++ e_modules/rain/e_mod_config.c
@@ -9,137 +9,137 @@
 
 struct _E_Config_Dialog_Data
 {
-  int show_clouds;
-  int density;
+   int show_clouds;
+   int density;
 };
 
 /* Protos */
-static void *_create_data (E_Config_Dialog * cfd);
-static void _free_data (E_Config_Dialog * cfd, E_Config_Dialog_Data * cfdata);
-static Evas_Object *_basic_create_widgets (E_Config_Dialog * cfd, Evas * evas,
-					   E_Config_Dialog_Data * cfdata);
-static int _basic_apply_data (E_Config_Dialog * cfd,
-			      E_Config_Dialog_Data * cfdata);
-static void _fill_data (Rain * rn, E_Config_Dialog_Data * cfdata);
+static void *_create_data(E_Config_Dialog *cfd);
+static void _free_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
+static Evas_Object *_basic_create_widgets(E_Config_Dialog *cfd, Evas *evas,
+                                          E_Config_Dialog_Data *cfdata);
+static int _basic_apply_data(E_Config_Dialog *cfd,
+                             E_Config_Dialog_Data *cfdata);
+static void _fill_data(Rain *rn, E_Config_Dialog_Data *cfdata);
 
 void
-_config_rain_module (E_Container * con, Rain * r)
+_config_rain_module(E_Container *con, Rain *r)
 {
-  E_Config_Dialog *cfd;
-  E_Config_Dialog_View *v;
+   E_Config_Dialog *cfd;
+   E_Config_Dialog_View *v;
 
-  v = E_NEW (E_Config_Dialog_View, 1);
+   v = E_NEW(E_Config_Dialog_View, 1);
 
-  if (v)
-    {
-      v->create_cfdata = _create_data;
-      v->free_cfdata = _free_data;
-      v->basic.apply_cfdata = _basic_apply_data;
-      v->basic.create_widgets = _basic_create_widgets;
+   if (v)
+     {
+        v->create_cfdata = _create_data;
+        v->free_cfdata = _free_data;
+        v->basic.apply_cfdata = _basic_apply_data;
+        v->basic.create_widgets = _basic_create_widgets;
 
-      cfd = e_config_dialog_new (con, _("Rain Module"), NULL, 0, v, r);
-      r->config_dialog = cfd;
-    }
+        cfd = e_config_dialog_new(con, _("Rain Module"), NULL, 0, v, r);
+        r->config_dialog = cfd;
+     }
 }
 
 static void
-_fill_data (Rain * rn, E_Config_Dialog_Data * cfdata)
+_fill_data(Rain *rn, E_Config_Dialog_Data *cfdata)
 {
-  cfdata->show_clouds = rn->conf->show_clouds;
-  switch (rn->conf->cloud_count)
-    {
-    case 5:
-      cfdata->density = DENSITY_SPRINKLE;
-      break;
-    case 10:
-      cfdata->density = DENSITY_DRIZZLE;
-      break;
-    case 20:
-      cfdata->density = DENSITY_DOWNPOUR;
-      break;
-    default:
-      break;
-    }
+   cfdata->show_clouds = rn->conf->show_clouds;
+   switch (rn->conf->cloud_count)
+     {
+     case 5:
+        cfdata->density = DENSITY_SPRINKLE;
+        break;
+     case 10:
+        cfdata->density = DENSITY_DRIZZLE;
+        break;
+     case 20:
+        cfdata->density = DENSITY_DOWNPOUR;
+        break;
+     default:
+        break;
+     }
 }
 
 static void *
-_create_data (E_Config_Dialog * cfd)
+_create_data(E_Config_Dialog *cfd)
 {
-  E_Config_Dialog_Data *cfdata;
-  Rain *r;
+   E_Config_Dialog_Data *cfdata;
+   Rain *r;
 
-  r = cfd->data;
-  cfdata = E_NEW (E_Config_Dialog_Data, 1);
-  _fill_data (r, cfdata);
-  return cfdata;
+   r = cfd->data;
+   cfdata = E_NEW(E_Config_Dialog_Data, 1);
+   _fill_data(r, cfdata);
+   return cfdata;
 }
 
 static void
-_free_data (E_Config_Dialog * cfd, E_Config_Dialog_Data * cfdata)
+_free_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata)
 {
-  Rain *rn;
+   Rain *rn;
 
-  rn = cfd->data;
-  rn->config_dialog = NULL;
-  free (cfdata);
+   rn = cfd->data;
+   rn->config_dialog = NULL;
+   free(cfdata);
 }
 
 static Evas_Object *
-_basic_create_widgets (E_Config_Dialog * cfd, Evas * evas,
-		       E_Config_Dialog_Data * cfdata)
+_basic_create_widgets(E_Config_Dialog *cfd, Evas *evas,
+                      E_Config_Dialog_Data *cfdata)
 {
-  Evas_Object *o, *of, *ob;
-  E_Radio_Group *rg;
+   Evas_Object *o, *of, *ob;
+   E_Radio_Group *rg;
 
-  o = e_widget_list_add (evas, 0, 0);
+   o = e_widget_list_add(evas, 0, 0);
 
-  of = e_widget_framelist_add (evas, _("General Settings"), 0);
-  ob = e_widget_check_add (evas, _("Show Clouds"), &(cfdata->show_clouds));
-  e_widget_framelist_object_append (of, ob);
-  e_widget_list_object_append (o, of, 1, 1, 0.5);
+   of = e_widget_framelist_add(evas, _("General Settings"), 0);
+   ob = e_widget_check_add(evas, _("Show Clouds"), &(cfdata->show_clouds));
+   e_widget_framelist_object_append(of, ob);
+   e_widget_list_object_append(o, of, 1, 1, 0.5);
 
-  of = e_widget_framelist_add (evas, _("Rain Density"), 0);
-  rg = e_widget_radio_group_new (&(cfdata->density));
-  ob = e_widget_radio_add (evas, _("Sprinkle"), DENSITY_SPRINKLE, rg);
-  e_widget_framelist_object_append (of, ob);
-  ob = e_widget_radio_add (evas, _("Drizzle"), DENSITY_DRIZZLE, rg);
-  e_widget_framelist_object_append (of, ob);
-  ob = e_widget_radio_add (evas, _("Downpour"), DENSITY_DOWNPOUR, rg);
-  e_widget_framelist_object_append (of, ob);
-  e_widget_list_object_append (o, of, 1, 1, 0.5);
+   of = e_widget_framelist_add(evas, _("Rain Density"), 0);
+   rg = e_widget_radio_group_new(&(cfdata->density));
+   ob = e_widget_radio_add(evas, _("Sprinkle"), DENSITY_SPRINKLE, rg);
+   e_widget_framelist_object_append(of, ob);
+   ob = e_widget_radio_add(evas, _("Drizzle"), DENSITY_DRIZZLE, rg);
+   e_widget_framelist_object_append(of, ob);
+   ob = e_widget_radio_add(evas, _("Downpour"), DENSITY_DOWNPOUR, rg);
+   e_widget_framelist_object_append(of, ob);
+   e_widget_list_object_append(o, of, 1, 1, 0.5);
 
-  return o;
+   return o;
 }
 
 static int
-_basic_apply_data (E_Config_Dialog * cfd, E_Config_Dialog_Data * cfdata)
+_basic_apply_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata)
 {
-  Rain *rn;
+   Rain *rn;
 
-  rn = cfd->data;
-  e_border_button_bindings_ungrab_all ();
-  switch (cfdata->density)
-    {
-    case 0:
-      rn->conf->cloud_count = 5;
-      rn->conf->drop_count = 20;
-      break;
-    case 1:
-      rn->conf->cloud_count = 10;
-      rn->conf->drop_count = 60;
-      break;
-    case 2:
-      rn->conf->cloud_count = 20;
-      rn->conf->drop_count = 150;
-      break;
-    default:
-      break;
-    }
-  rn->conf->show_clouds = cfdata->show_clouds;
+   rn = cfd->data;
+   e_border_button_bindings_ungrab_all();
+   switch (cfdata->density)
+     {
+     case 0:
+        rn->conf->cloud_count = 5;
+        rn->conf->drop_count = 20;
+        break;
+     case 1:
+        rn->conf->cloud_count = 10;
+        rn->conf->drop_count = 60;
+        break;
+     case 2:
+        rn->conf->cloud_count = 20;
+        rn->conf->drop_count = 150;
+        break;
+     default:
+        break;
+     }
+   rn->conf->show_clouds = cfdata->show_clouds;
 
-  e_config_save_queue ();
-  e_border_button_bindings_grab_all ();
+   e_config_save_queue();
+   e_border_button_bindings_grab_all();
 
-  _rain_cb_config_updated (rn);
-  return 1;
+   _rain_cb_config_updated(rn);
+   return 1;
 }
