49cb599e72b038a56378977461bca655132dad63
onefang <onefang@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-02-07 18:01:46 +0000

    Doh!  Forgot to actually add the files.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@20337 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/apps/e_utils/src/bin/e17genmenu/src/bin/xmlame.c
@@ -0,0 +1,24 @@
+#include "xmlame.h"
+
+/** xmlame.c Extensively Mocked Language Approximately Mangled for Enlightenment. 
+ *
+ * This is NOT a real XML parser.  There were a few ways we could go when it came
+ * to parsing the freedesktop.org (fdo) XML menu files.  Whatever way we went, we 
+ * needed some sort of XML parser if we wanted to fully support fdo menu files.  
+ * Nothing we can do about that, fdo set the standard and they choose XML to do it.
+ *
+ * After a discussion with raster, three things led to the decision to do it this 
+ * way.  It is likely that this will get included as a core part of the E17 window 
+ * manager (E17) coz E17 needs this functionality.  E17 is in a dependency freeze
+ * and there is no XML parser in it's current dependencies.  The fdo XML menu files
+ * look to be simple enough to parse that this sort of fake, brain dead, XML parser
+ * may get away with it.  Much testing on lots of systems is highly recommended.
+ */
+
+Dumb_List *xmlame_new(char *buffer)
+{
+   Dumb_List *list;
+
+   list = dumb_list_new(buffer);
+   return list;
+}
---
+++ e17/apps/e_utils/src/bin/e17genmenu/src/bin/xmlame.h
@@ -0,0 +1,8 @@
+#ifndef _XMLAME_H
+# define _XMLAME_H
+
+#include "dumb_list.h"
+
+Dumb_List *xmlame_new(char *buffer);
+
+#endif
