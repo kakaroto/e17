184e7f2a2ec7d4eefa8056cd33de821c90e7399a
mej <mej@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-03-01 12:20:21 +0000

    Wed Mar  1 07:20:17 2006                        Michael Jennings (mej)
    Bye-bye old monitor.
    ----------------------------------------------------------------------
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@20874 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/monitor/e_mod_config.h
+++
@@ -1,8 +0,0 @@
-#ifdef E_TYPEDEFS
-#else
-#ifndef E_MOD_CONFIG_H
-#define E_MOD_CONFIG_H
-#include "e_mod_main.h"
-EAPI void _config_monitor_module(E_Container *con, Monitor_Face *face);
-#endif
-#endif
