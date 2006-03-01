88299cb3cd0ac5668e84a6d66277b4611c1ee87c
handyande <handyande@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-03-01 12:09:51 +0000

    update to ewl api
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@20873 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/enotes/src/menu.c
+++ misc/enotes/src/menu.c
@@ -15,7 +15,7 @@ menu_create()
 	ewl_window_move((Ewl_Window *) p->win, 1, 1);
 
 	p->menu = ewl_imenu_new();
-	ewl_menu_item_text_set(EWL_MENU_ITEM(p->menu), "Note Menu");
+	ewl_button_label_set(EWL_BUTTON(p->menu), "Note Menu");
 	ewl_container_child_append((Ewl_Container *) p->win, p->menu);
 
 	return (p);
@@ -64,7 +64,7 @@ menu_item_add(Menu * menu, char *name, void (*func) (void *data), void *data)
 	item->data = data;
 
 	item->item = ewl_menu_item_new();
-	ewl_menu_item_text_set(EWL_MENU_ITEM(item->item), name);
+	ewl_button_label_set(EWL_BUTTON(item->item), name);
 	ewl_container_child_append((Ewl_Container *) menu->menu, item->item);
 	ewl_callback_append(item->item, EWL_CALLBACK_FOCUS_IN, cb_menu_process,
 			    item);
