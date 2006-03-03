7796b2961f174dbb61eb56b8d8f30860d5ebbd9a
dj2 <dj2@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-03-03 03:14:49 +0000

    - add the ewl_calendar test
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@20933 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/libs/ewl/src/bin/tests/ewl_calendar.c
@@ -0,0 +1,42 @@
+#include "Ewl_Test2.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static int create_test(Ewl_Container *box);
+static void ewl_calendar_test(Ewl_Widget *w, void *ev, void *data);
+
+void 
+test_info(Ewl_Test *test)
+{
+	test->name = "Calendar";
+	test->tip = "Defines a calendar widget.";
+	test->filename = "ewl_calendar.c";
+	test->func = create_test;
+	test->type = EWL_TEST_TYPE_MISC;
+}
+
+static int
+create_test(Ewl_Container *box)
+{
+	Ewl_Widget *cal;
+
+	cal = ewl_calendar_new();
+	ewl_container_child_append(EWL_CONTAINER(box), cal);
+	ewl_callback_append(EWL_WIDGET(cal),
+			EWL_CALLBACK_VALUE_CHANGED, ewl_calendar_test,
+			cal);
+	ewl_widget_show(cal);
+
+	return 1;
+}
+
+static void
+ewl_calendar_test(Ewl_Widget *w __UNUSED__, void *ev __UNUSED__, void *data)
+{
+	char *date;
+
+	date = ewl_calendar_ascii_time_get(EWL_CALENDAR(data));
+	printf ("Selected: %s\n", date);
+	free(date);
+}
+
