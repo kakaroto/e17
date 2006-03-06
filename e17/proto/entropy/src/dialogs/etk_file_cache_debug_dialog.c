ff80e46158efa667dea3643316af0158a0a8a32c
lordchaos <lordchaos@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-03-06 08:26:13 +0000

    * Start of the file cache debug dialog
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@21044 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/entropy/src/dialogs/etk_file_cache_debug_dialog.c
+++ e17/proto/entropy/src/dialogs/etk_file_cache_debug_dialog.c
@@ -1,7 +1,7 @@
 #include "entropy.h"
 #include <Etk.h>
 
-void etk_mime_dialog_create()
+void etk_file_cache_dialog_create()
 {
 	Etk_Widget* window = NULL;
 	Etk_Widget* tree = NULL;
@@ -10,15 +10,14 @@ void etk_mime_dialog_create()
 	Etk_Tree_Col* col1;
 	Etk_Tree_Col* col2;
 
-
 	char* key;
 	entropy_mime_action* action;
 	Ecore_List* keys;
 
 	window = etk_window_new();
 
-	etk_window_title_set(ETK_WINDOW(window), "Program Associations");
-	etk_window_wmclass_set(ETK_WINDOW(window), "mimedialog", "mimedialog");
+	etk_window_title_set(ETK_WINDOW(window), "File Cache");
+	etk_window_wmclass_set(ETK_WINDOW(window), "entropyfilecache", "entropyfilecache");
 
 	etk_widget_size_request_set(ETK_WIDGET(window), 450, 500);
 
@@ -40,15 +39,6 @@ void etk_mime_dialog_create()
 	col1 = etk_tree_nth_col_get(ETK_TREE(tree), 0);
 	col2 = etk_tree_nth_col_get(ETK_TREE(tree), 1);
 	
-	keys = ecore_hash_keys(entropy_core_get_core()->mime_action_hint);
-	while ((key = ecore_list_remove_first(keys))) {
-	     etk_tree_append(ETK_TREE(tree), 
-		  col1, key, 
-		  col2,  ((entropy_mime_action*)ecore_hash_get(entropy_core_get_core()->mime_action_hint, key))->executable,
-		  NULL);
-	}
-
-
 	etk_widget_show_all(window);
 }
 
