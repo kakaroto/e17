52e4eae5a63da04e90539beec2a57626c317c5e2
dj2 <dj2@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-03-06 02:10:12 +0000

    - add icon to ewl_test2
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@21040 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/libs/ewl/src/bin/tests/ewl_icon.c
@@ -0,0 +1,50 @@
+#include "Ewl_Test2.h"
+#include <stdio.h>
+
+static int create_test(Ewl_Container *box);
+
+void 
+test_info(Ewl_Test *test)
+{
+	test->name = "Icon";
+	test->tip = "";
+	test->filename = "ewl_icon.c";
+	test->func = create_test;
+	test->type = EWL_TEST_TYPE_ADVANCED;
+}
+
+static int
+create_test(Ewl_Container *box)
+{
+	Ewl_Widget *o, *o2;
+
+        o = ewl_icon_new();
+        ewl_icon_image_set(EWL_ICON(o),
+                                PACKAGE_DATA_DIR "/images/Draw.png", NULL);
+        ewl_icon_label_set(EWL_ICON(o), "Draw (Editable)");
+        ewl_icon_editable_set(EWL_ICON(o), TRUE);
+        ewl_container_child_append(EWL_CONTAINER(box), o);
+        ewl_widget_show(o);
+
+        o = ewl_hseparator_new();
+        ewl_container_child_append(EWL_CONTAINER(box), o);
+        ewl_widget_show(o);
+
+        o2 = ewl_text_new();
+        ewl_text_text_set(EWL_TEXT(o2), "This icon has\nextended data\n set "
+                                        "on it.\n\n That data is just \n"
+                                        "text, but could\nbe any widget.");
+        ewl_widget_show(o2);
+
+        o = ewl_icon_new();
+        ewl_icon_image_set(EWL_ICON(o),
+                                PACKAGE_DATA_DIR "/images/World.png", NULL);
+        ewl_icon_extended_data_set(EWL_ICON(o), o2);
+        ewl_icon_label_set(EWL_ICON(o), "World");
+        ewl_icon_type_set(EWL_ICON(o), EWL_ICON_TYPE_LONG);
+        ewl_container_child_append(EWL_CONTAINER(box), o);
+        ewl_widget_show(o);
+
+	return 1;
+}
+
