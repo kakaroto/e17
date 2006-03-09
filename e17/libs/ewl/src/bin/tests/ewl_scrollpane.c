a6d6c1af766727ff46c7eb3b1bf547ce8eb34fb6
dj2 <dj2@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-03-09 20:36:57 +0000

    - port the scrollpane test
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@21153 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/libs/ewl/src/bin/tests/ewl_scrollpane.c
@@ -0,0 +1,34 @@
+#include "Ewl_Test2.h"
+#include <stdio.h>
+
+static int create_test(Ewl_Container *box);
+
+void 
+test_info(Ewl_Test *test)
+{
+	test->name = "Scrollpane";
+	test->tip = "A scrollpane.";
+	test->filename = "ewl_scrollpane.c";
+	test->func = create_test;
+	test->type = EWL_TEST_TYPE_CONTAINER;
+}
+
+static int
+create_test(Ewl_Container *box)
+{
+	Ewl_Widget *scrollpane;
+	Ewl_Widget *button;
+
+	scrollpane = ewl_scrollpane_new();
+	ewl_container_child_append(box, scrollpane);
+	ewl_widget_show(scrollpane);
+
+	button = ewl_button_new();
+	ewl_object_size_request(EWL_OBJECT(button), 500, 500);
+	ewl_object_padding_set(EWL_OBJECT(button), 20, 20, 20, 20);
+	ewl_container_child_append(EWL_CONTAINER(scrollpane), button);
+	ewl_widget_show(button);
+
+	return 1;
+}
+
