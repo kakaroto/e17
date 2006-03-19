e395482ec59f514a246170e6121a1cb6bb44edab
codewarrior <codewarrior@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-03-19 19:06:15 +0000

    add a callback to the perl demo
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@21422 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/etk_server/examples/etk.pl
+++ e17/proto/etk_server/examples/etk.pl
@@ -22,21 +22,27 @@ etk "etk_init";
   
 my $win = etk "etk_window_new";
   
-my $button = etk "etk_button_new_with_label \"Perl Rules\"";
+my $button1 = etk "etk_button_new_with_label \"Perl Rules\"";
 
 my $vbox = etk "etk_vbox_new 0 0";
-etk "etk_box_pack_start $vbox $button 1 1 0";
+etk "etk_box_pack_start $vbox $button1 1 1 0";
 
-etk "etk_container_add $win var3";
+etk "etk_container_add $win $vbox";
   
 etk "etk_widget_show_all $win";
 
+etk "etk_server_signal_connect \"clicked\" $button1 \"button_1_click\"";
 etk "etk_server_signal_connect \"delete_event\" $win \"win_delete\"";
   
 $event = "";
 while ( $event ne "win_delete" )
 {
     $event = etk "etk_server_callback";
+    
+    if($event eq "button_1_click")
+    {
+	print "button 1 clicked!\n";
+    }
 }
 
 etk "etk_main_quit";
