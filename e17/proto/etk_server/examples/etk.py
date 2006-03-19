835fb63ae8c94b7fdaf2040c0d92caada6aac820
codewarrior <codewarrior@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-03-19 19:39:14 +0000

    - Etk programs in Python anyone? here's an example
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@21424 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/proto/etk_server/examples/etk.py
@@ -0,0 +1,46 @@
+#!/usr/local/bin/python
+
+import os
+import time
+
+def etk(str) :
+    out_file = open("/tmp/etk_server_fifo","w")
+    out_file.write("etk_" + str + "\0")
+    out_file.close()    
+    
+    in_file = open("/tmp/etk_server_fifo","r")
+    text = in_file.read()
+    in_file.close()
+    
+    return text
+
+os.system("etk_server /tmp/etk_server_fifo &")
+time.sleep(0.5)
+
+etk("init")
+
+win = etk("window_new")
+
+button = etk("button_new_with_label \"Python rules!\"")
+
+etk("container_add " + win + " " + button)
+etk("window_title_set " + win + " \"Etk-Python Demo\"")
+etk("widget_show_all " + win)
+
+etk("server_signal_connect \"clicked\" " + button + " \"button_click\"")
+etk("server_signal_connect \"delete_event\" " + win + " \"window_delete\"")
+
+while 1:
+    
+    event = etk("server_callback")
+    
+    if event == "window_delete":
+	print "Bye!"
+	break    
+    
+    if event == "button_click":
+	print "Button clicked!"
+    
+etk("main_quit")
+etk("server_shutdown")
+
