0a4d1999097353eb1bffd92c4d32f7416f008fe5
codewarrior <codewarrior@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-03-20 14:05:15 +0000

    - turning this into a simple todo list app so we can see whats missing along the way
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@21444 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/etk_server/examples/etk.php
+++ e17/proto/etk_server/examples/etk.php
@@ -4,13 +4,13 @@
 /* Etk-Php Demo. Required Php 5.x */
 
 define(FIFO, "/tmp/etk_server_fifo");
-define(EtkTrue, 1);
-define(EtkFalse, 0);
 
 class Etk
 {    
     static private $loop = true;
     static private $callbacks = Array();
+    const True = 1;
+    const False = 0;
     
     function Etk()
     { }
@@ -18,7 +18,7 @@ class Etk
     function Connect()
     {
 	exec("etk_server ".FIFO." > /dev/null &");
-	sleep(1);
+	usleep(50000);
     }
     
     function Call($str)
@@ -55,17 +55,7 @@ class Etk
 	
 	return $ret;
     }
-    
-    function True()
-    {
-	return 1;
-    }
-    
-    function False()
-    {
-	return 0;
-    }
-    
+        
     function AddCallback($name, $callback)
     {
 	if(!is_array(self::$callbacks[$name]))
@@ -76,7 +66,7 @@ class Etk
     
     function Main()
     {
-	$this->loop = EtkTrue;
+	$this->loop = Etk::True;
 	
 	while ( $this->loop )
 	{
@@ -95,7 +85,7 @@ class Etk
     
     function MainQuit()
     {
-	$this->loop = EtkFalse;
+	$this->loop = Etk::False;
 	$this->Call("main_quit");
 	$this->Call("server_shutdown");
     }
@@ -109,6 +99,16 @@ class Object extends Etk
     {
 	parent::Etk();
     }
+    
+    function Set($w)
+    {
+	$this->object = $w;
+    }
+    
+    function Get()
+    {
+	return $this->object;
+    }    
     
     function SignalConnect($name, $callback)
     {
@@ -125,21 +125,16 @@ class Widget extends Object
     {
 	parent::Object();
     }
-    
-    function Set($w)
-    {
-	$this->object = $w;
-    }
-    
-    function Get()
-    {
-	return $this->object;
-    }
-    
+        
     function ShowAll()
     {
 	$this->Call("widget_show_all ".$this->Get());
     }    
+    
+    function SizeRequestSet($width, $height)
+    {
+	$this->Call("widget_size_request_set ".$this->Get()." $width $height");
+    }
 }
 
 class Image extends Widget
@@ -259,7 +254,7 @@ class Box Extends Widget
 	parent::Widget();
     }
     
-    function PackStart($child, $fill = EtkTrue, $expand = EtkTrue, $padding = 0)
+    function PackStart($child, $fill = Etk::True, $expand = Etk::True, $padding = 0)
     {
 	$this->Call("box_pack_start ". $this->Get(). " " .$child->Get(). " $fill $expand $padding");
     }
@@ -267,7 +262,7 @@ class Box Extends Widget
 
 class VBox Extends Box
 {
-    function VBox($homogenous = EtkFalse, $padding = 0)
+    function VBox($homogenous = Etk::False, $padding = 0)
     {
 	parent::Box();
 	$this->Set($this->Call("vbox_new $homogenous $padding"));
@@ -276,12 +271,103 @@ class VBox Extends Box
 
 class HBox Extends Box
 {
-    function HBox($homogenous = EtkFalse, $padding = 0)
+    function HBox($homogenous = Etk::False, $padding = 0)
     {
 	parent::Box();
 	$this->Set($this->Call("hbox_new $homogenous $padding"));
     }
 }
+
+class TreeModel
+{
+    var $model;
+    var $etk;
+    
+    function TreeModel()
+    {
+	$this->etk = new Etk();
+    }
+    
+    function Get()
+    {
+	return $this->model;
+    }    
+}
+
+class TreeModelText extends TreeModel
+{    
+    function TreeModelText($tree)
+    {
+	parent::TreeModel();	
+	$this->model = $this->etk->Call("tree_model_text_new ".$tree->Get());
+    }    
+}
+
+class TreeModelCheck extends TreeModel
+{    
+    function TreeModelCheck($tree)
+    {
+	parent::TreeModel();
+	$this->model = $this->etk->Call("tree_model_checkbox_new ".$tree->Get());
+    }    
+}
+
+class TreeCol extends Object
+{
+    function TreeCol($tree, $title, $model, $width)
+    {
+	parent::Object();
+	$this->Set($this->Call("tree_col_new ".$tree->Get()." \"$title\" ".$model->Get()." $width"));
+    }    	
+}
+
+class Tree extends Container
+{
+    const ModeList = 0;
+    const ModeTree = 1;
+    
+    function Tree()
+    {
+	parent::Container();
+	$this->Set($this->Call("tree_new"));
+    }
+    
+    function ModeSet($mode)
+    {
+	$this->Call("tree_mode_set $mode");
+    }
+    
+    function Append()
+    {
+	$args = Array();
+	$numargs = func_num_args();
+	$arg_list = func_get_args();
+	for ($i = 0; $i < $numargs; $i++)
+	{
+	    $arg = $arg_list[$i];
+
+	    if(get_parent_class($arg) == "Object")
+	      array_push($args, $arg->Get());
+	    else
+	    {
+		if(is_string($arg))
+		  array_push($args, "\"$arg\"");
+		elseif($arg == NULL)
+		  array_push($args, NULL);
+		else
+		  array_push($args, $arg);
+	    }
+	}
+
+	return $this->Call("tree_append ".$this->Get()." ".implode(" ", $args));
+    }
+
+    function Build()
+    {
+	$this->Call("tree_build ".$this->Get());
+    }    
+}
+    
 
 /* Main Application */
 
@@ -289,29 +375,40 @@ $etk = new Etk();
 $etk->Connect();
 $etk->Call("init");
 
-$win = new Window("Etk-Php Demo");
+$win = new Window("My Todo");
 $win->SignalConnect("delete_event", _window_deleted_cb);
 
-$vbox = new VBox(EtkFalse, 0);
+$vbox = new VBox(Etk::False, 0);
 
-$button1 = new Button("Php owns!");
-$button1->SignalConnect("clicked", _button_1_clicked_cb);
+$tree = new Tree();
+$tree->SizeRequestSet(320, 200);
+$tree->ModeSet(Tree::ModeList);
+$col1 = new TreeCol($tree, "Done", new TreeModelCheck($tree), 20);
+$col2 = new TreeCol($tree, "Task", new TreeModelText($tree), 170);
+$col3 = new TreeCol($tree, "Priority", new TreeModelText($tree), 50);
+$col4 = new TreeCol($tree, "Deadline", new TreeModelText($tree), 50);
 
-$button2 = new Button("Etk owns!");
-$button2->SignalConnect("clicked", _button_2_clicked_cb);
+$tree->Build();
+$tree->Append($col1, Etk::False, $col2, "write an Etk-Php todo app", $col3, "High", $col4, date("d/m/y"), NULL);
+$tree->Append($col1, Etk::True, $col2, "and make it work!", $col3, "High", $col4, date("d/m/y"), NULL);
+$tree->Append($col1, Etk::False, $col2, "create some more classes", $col3, "Low", $col4, date("d/m/y"), NULL);
+$tree->Append($col1, Etk::False, $col2, "bind this to sqlite", $col3, "Normal", $col4, date("d/m/y"), NULL);
 
-$check_button1 = new CheckButton("Check me!");
+$hbox = new HBox();
 
-$entry = new Entry("Moo!");
+$clean_btn = new Button("Remove Done");
 
-$button3 = new Button("Get Text");
-$button3->SignalConnect("clicked", _button_3_clicked_cb);
+$new_btn = new Button("New Task");
 
-$vbox->PackStart($button1);
-$vbox->PackStart($button2);
-$vbox->PackStart($check_button1);
-$vbox->PackStart($entry);
-$vbox->PackStart($button3);
+$quit_btn = new Button("Quit");
+$quit_btn->SignalConnect("clicked", _quit_btn_clicked_cb);
+
+$hbox->PackStart($clean_btn, Etk::False, Etk::False);
+$hbox->PackStart($new_btn, Etk::False, Etk::False);
+$hbox->PackStart($quit_btn, Etk::False, Etk::False);
+
+$vbox->PackStart($tree);
+$vbox->PackStart($hbox, Etk::False, Etk::False);
 
 $win->Add($vbox);
 
@@ -329,11 +426,11 @@ function _button_2_clicked_cb()
     printf("Button 2 clicked!\n");
 }
 		
-function _button_3_clicked_cb()
+function _quit_btn_clicked_cb()
 {
-    global $entry;
+    global $etk;
     
-    printf("Text = ".$entry->TextGet()."\n");
+    $etk->MainQuit();
 }
 		
 function _window_deleted_cb()
