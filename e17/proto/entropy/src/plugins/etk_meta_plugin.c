cd9fa7fe5cc7d63f4fbc6deeccb58c5a3c144603
chaos <chaos@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-03-28 10:33:25 +0000

    * Add the metadata plugins.  These are still not async - this will me moving to evfs soon
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@21527 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/proto/entropy/src/plugins/etk_meta_plugin.c
@@ -0,0 +1,170 @@
+#include "entropy.h"
+#include "entropy_gui.h"
+#include "entropy_config.h"
+#include <unistd.h>
+#include <limits.h>
+#include <Etk.h>
+
+#define _INCREMENT 8
+#define _MAX_SIZE 250
+
+
+struct _etk_meta_plugin {
+	Etk_Widget* window;
+	Etk_Widget* list;
+	Etk_Widget* parent_window;
+	Ecore_Timer* timer;
+	int flip;
+	int pos;
+};
+typedef struct _etk_meta_plugin _etk_meta_plugin;
+
+
+int
+entropy_plugin_type_get ()
+{
+  return ENTROPY_PLUGIN_GUI_COMPONENT;
+}
+
+int
+entropy_plugin_sub_type_get()
+{
+	return ENTROPY_PLUGIN_GUI_COMPONENT_INFO_PROVIDER;
+}
+
+char *
+entropy_plugin_identify ()
+{
+  return (char *) "ETK slide metadata plugin";
+}
+
+char*
+entropy_plugin_toolkit_get() 
+{
+	return ENTROPY_TOOLKIT_ETK;
+}
+
+int _etk_metadata_timer(void* data) {
+	entropy_gui_component_instance* instance = (entropy_gui_component_instance*)data;
+	_etk_meta_plugin* view = instance->data;
+	
+	int x,y,w,h;
+	int newsize = 0;
+	etk_window_geometry_get(ETK_WINDOW(view->window),&x,&y,&w,&h);
+
+	newsize = w + (view->pos);
+		etk_window_resize(ETK_WINDOW(view->window), newsize, h);
+
+	if (view->flip) {
+		view->flip = 0;
+		view->timer = NULL;
+		ecore_timer_add(0.02, _etk_metadata_timer, instance);
+		
+		return 0;
+	}
+
+	if (view->pos > 0) {
+		if (w<_MAX_SIZE)
+			return 1;
+		else {
+			view->pos = -_INCREMENT;
+			view->flip = 1;
+
+			view->timer = NULL;
+			
+			ecore_timer_add(5, _etk_metadata_timer, instance);
+			return 0;
+		}
+	} else {
+		if (w+view->pos > 0)
+			return 1;
+		else {
+			etk_widget_hide_all(view->window);
+			view->timer = NULL;
+			return 0;
+		}
+	}
+
+}
+
+
+void
+gui_event_callback (entropy_notify_event * eevent, void *requestor, void *obj,
+		    entropy_gui_component_instance * comp)
+{
+	switch (eevent->event_type) {
+		case ENTROPY_NOTIFY_FILE_METADATA_AVAILABLE: {
+			_etk_meta_plugin* view = comp->data;								     
+			Ecore_List* list = (Ecore_List*)obj;
+			Entropy_Metadata_Object* object;
+			Etk_Tree_Col *col1, *col2;
+			int w,h,x,y;
+
+			col1 = etk_tree_nth_col_get(ETK_TREE(view->list), 0);
+			col2 = etk_tree_nth_col_get(ETK_TREE(view->list), 1);
+
+			etk_tree_clear(ETK_TREE(view->list));
+			ecore_list_goto_first(list);
+			while ( (object = ecore_list_next(list))) {
+				etk_tree_append(ETK_TREE(view->list),
+						col1, object->key,
+						col2, object->value,
+						NULL);
+				
+			}
+
+			view->pos = _INCREMENT;
+
+			/*Set this windows size to be that of the parent*/
+			etk_window_geometry_get(ETK_WINDOW(view->parent_window),&x,&y,&w,&h);
+			etk_window_move(ETK_WINDOW(view->window), x+w, y);
+			etk_widget_size_request_set(ETK_WIDGET(view->window), 2, h);
+
+			etk_widget_show_all(view->window);
+
+			if (view->timer) {
+				ecore_timer_del(view->timer);
+				view->timer = NULL;
+			}
+
+			view->timer = ecore_timer_add(0.02, _etk_metadata_timer, comp);
+			printf("Received an ecore_list...\n");
+		}
+		break;
+	}
+}
+
+
+entropy_gui_component_instance *
+entropy_plugin_init (entropy_core * core,
+		     entropy_gui_component_instance * layout, void* parent_visual, void *data)
+{
+  Etk_Tree_Col* col;
+  _etk_meta_plugin* view = entropy_malloc(sizeof(_etk_meta_plugin));
+  entropy_gui_component_instance *instance = entropy_gui_component_instance_new ();
+  instance->layout_parent = layout;
+  instance->core = core;
+
+  entropy_core_component_event_register (instance,
+					 entropy_core_gui_event_get
+					 (ENTROPY_GUI_EVENT_FILE_METADATA_AVAILABLE));
+
+  instance->data = view;
+  view->parent_window = (Etk_Widget*)parent_visual;
+  view->window = etk_window_new();
+  etk_window_decorated_set(ETK_WINDOW(view->window), ETK_FALSE);
+  
+  view->list = etk_tree_new();
+  etk_tree_mode_set(ETK_TREE(view->list), ETK_TREE_MODE_LIST);
+  
+  etk_tree_col_new(ETK_TREE(view->list), _("Keyword"), 
+		  etk_tree_model_text_new(ETK_TREE(view->list)), 80);
+  etk_tree_col_new(ETK_TREE(view->list), _("Value"), 
+		  etk_tree_model_text_new(ETK_TREE(view->list)), 200);
+  etk_tree_build(ETK_TREE(view->list));
+  etk_container_add(ETK_CONTAINER(view->window), view->list);
+  
+
+
+  return instance;
+}
---
+++ e17/proto/entropy/src/plugins/metadata_extract.c
@@ -0,0 +1,94 @@
+#include "entropy.h"
+#include "entropy_gui.h"
+#include "entropy_config.h"
+#include <unistd.h>
+#include <limits.h>
+#include <extractor.h>
+
+static  EXTRACTOR_ExtractorList *_extractors = NULL;
+
+int
+entropy_plugin_type_get ()
+{
+  return ENTROPY_PLUGIN_METADATA_READ;
+}
+
+char *
+entropy_plugin_identify ()
+{
+  return (char *) "libextract based metadata provider";
+}
+
+void
+gui_event_callback (entropy_notify_event * eevent, void *requestor, void *obj,
+		    entropy_gui_component_instance * comp)
+{
+	switch (eevent->event_type) {
+		case ENTROPY_NOTIFY_FILE_METADATA_REQUEST: {
+			entropy_gui_event* gui_event;
+			Ecore_List* list;
+			EXTRACTOR_KeywordList   *keywords;
+			const char              *key;
+			entropy_generic_file* file = (entropy_generic_file*)obj;
+			char buffer[PATH_MAX];
+
+			snprintf(buffer, PATH_MAX, "%s/%s", file->path, file->filename);
+
+			keywords=EXTRACTOR_getKeywords(_extractors, buffer);
+			keywords=EXTRACTOR_removeDuplicateKeywords(keywords,0);
+
+			/*EXTRACTOR_printKeywords(stdout, keywords);
+				*/
+	
+			list = ecore_list_new();
+			while(keywords) {
+				Entropy_Metadata_Object* object = 
+					entropy_malloc(sizeof(Entropy_Metadata_Object));
+				
+				key=EXTRACTOR_getKeywordTypeAsString(keywords->keywordType);
+		
+				object->key = key;
+				object->value = keywords->keyword;
+				ecore_list_append(list,object);
+				
+				keywords = keywords->next;
+			}
+			
+			/*Send the metadata back to the requesting layout*/
+			gui_event = entropy_malloc (sizeof (entropy_gui_event));
+			gui_event->event_type =
+				entropy_core_gui_event_get (ENTROPY_GUI_EVENT_FILE_METADATA_AVAILABLE);
+			gui_event->data = list;
+			gui_event->key = 0;
+			entropy_core_layout_notify_event ((entropy_gui_component_instance *)requestor, 
+					gui_event, ENTROPY_EVENT_LOCAL);
+
+			ecore_list_destroy(list);
+			EXTRACTOR_freeKeywords(keywords);
+		}
+			
+	}
+	
+}
+
+entropy_gui_component_instance *
+entropy_plugin_init (entropy_core * core)
+{
+  int config;
+
+  entropy_gui_component_instance *instance =
+    entropy_gui_component_instance_new ();
+  entropy_gui_component_instance *layout =
+    entropy_core_global_layout_get (core);
+  instance->layout_parent = layout;
+  instance->core = core;
+
+  entropy_core_component_event_register (instance,
+					 entropy_core_gui_event_get
+					 (ENTROPY_GUI_EVENT_FILE_METADATA));
+
+  _extractors = EXTRACTOR_loadDefaultLibraries();
+
+  
+  return instance;
+}
