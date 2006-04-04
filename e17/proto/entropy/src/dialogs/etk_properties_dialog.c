02595da3660bc1e4d836196c622a48533c88f56d
chaos <chaos@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-04-04 06:45:03 +0000

    * More properties dialog work
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@21800 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/entropy/src/dialogs/etk_properties_dialog.c
+++ e17/proto/entropy/src/dialogs/etk_properties_dialog.c
@@ -12,7 +12,7 @@ Etk_Bool _etk_window_deleted_cb (Etk_Object * object, void *data)
 	Entropy_Etk_Properties_Dialog* dialog= data;
 
 	/*Remove the file reference*/
-	entropy_core_file_cache_remove_reference(dialog->file);
+	entropy_core_file_cache_remove_reference(dialog->file->md5);
 
 	etk_object_destroy(ETK_OBJECT(dialog->window));
 	entropy_free(dialog);
@@ -23,6 +23,14 @@ Etk_Bool _etk_window_deleted_cb (Etk_Object * object, void *data)
 void etk_properties_dialog_new(Entropy_Generic_File* file) 
 {
 	Entropy_Etk_Properties_Dialog* dialog;
+	Etk_Widget* notebook;
+	Etk_Widget* vbox;
+	Etk_Widget* button;
+	Etk_Widget* hbox;
+	Etk_Widget* ivbox;
+	Etk_Widget* icon;
+	Etk_Widget* label;
+	Etk_Widget* pvbox;
 
 	/*First up, add a reference to this file*/
 	printf("Adding reference to '%s/%s'- > %s\n", file->path, file->filename, file->md5);
@@ -35,5 +43,51 @@ void etk_properties_dialog_new(Entropy_Generic_File* file)
 	etk_window_title_set(ETK_WINDOW(dialog->window), file->filename);
 
 	etk_widget_size_request_set(dialog->window, 450, 520);
+
+
+	vbox = etk_vbox_new(ETK_TRUE,0);
+	etk_container_add(ETK_CONTAINER(dialog->window), vbox);
+
+	notebook = etk_notebook_new();
+	etk_box_pack_start(ETK_BOX(vbox), notebook, ETK_TRUE, ETK_TRUE, 0);
+
+	/*Main page*/
+	ivbox = etk_vbox_new(ETK_TRUE,0);	
+	etk_notebook_page_append(ETK_NOTEBOOK(notebook), "General", ivbox);
+
+	hbox = etk_hbox_new(ETK_TRUE,0);
+	etk_box_pack_end(ETK_BOX(ivbox), hbox, ETK_FALSE, ETK_FALSE, 0);
+
+	if (file->thumbnail) {
+		icon = etk_image_new_from_file(file->thumbnail->thumbnail_filename);
+		etk_box_pack_start(ETK_BOX(hbox), icon, ETK_FALSE, ETK_FALSE, 0);
+	}
+
+	pvbox = etk_vbox_new(ETK_TRUE,0);
+	etk_box_pack_start(ETK_BOX(hbox), pvbox , ETK_FALSE, ETK_FALSE, 0);
+
+	label = etk_label_new(file->path);
+	etk_box_pack_start(ETK_BOX(pvbox), label, ETK_FALSE, ETK_FALSE, 0);	
+	
+	label = etk_label_new(file->filename);
+	etk_box_pack_start(ETK_BOX(pvbox), label, ETK_FALSE, ETK_FALSE, 0);
+
+	
+	
+	/*------------------*/
+
+	hbox = etk_hbox_new(ETK_TRUE,0);
+	etk_box_pack_end(ETK_BOX(vbox), hbox, ETK_FALSE, ETK_FALSE, 0);
+
+	button = etk_button_new_with_label("OK");
+	etk_box_pack_start(ETK_BOX(hbox), button, ETK_FALSE, ETK_FALSE, 0);
+
+	button = etk_button_new_with_label("Apply");
+	etk_box_pack_start(ETK_BOX(hbox), button, ETK_FALSE, ETK_FALSE, 0);
+
+	button = etk_button_new_with_label("Cancel");
+	etk_box_pack_start(ETK_BOX(hbox), button, ETK_FALSE, ETK_FALSE, 0);
+	
+	
 	etk_widget_show_all(dialog->window);
 }
