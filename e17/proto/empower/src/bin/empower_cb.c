346bf914c5f65652702895530456ca96e3623260
mekius <mekius@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-04-04 02:47:03 +0000

    Split up code because i was bored.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@21792 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/proto/empower/src/bin/empower_cb.c
@@ -0,0 +1,57 @@
+#include "Empower.h"
+
+void key_down_cb(Ewl_Widget *w, void *event, void *data)
+{
+	Ewl_Event_Key_Down *ev;
+	
+	ev = event;
+	
+	if(!ev->modifiers)
+	{
+		if(strcmp(ev->keyname, "Escape") == 0)
+		{
+			ewl_widget_destroy(EWL_WIDGET(win));
+			ewl_main_quit();
+		}
+	}
+}
+
+void destroy_cb(Ewl_Widget *w, void *event, void *data)
+{
+	ewl_widget_destroy(EWL_WIDGET(win));
+	ewl_main_quit();
+}
+
+void reveal_cb(Ewl_Widget *w, void *event, void *data)
+{
+	ewl_window_move(EWL_WINDOW(win), xpos, ypos);
+}
+
+void pipe_to_sudo_cb(Ewl_Widget *w, void *event, void *data)
+{	
+	FILE *sudo_pipe;
+	
+	const char *pass = ewl_password_text_get(EWL_PASSWORD(data));
+	
+	if(pass)
+	{
+		snprintf(password, 1024, "%s", pass);
+		
+		pid_t pid = fork();
+		
+		if(pid == 0)
+		{	
+			ewl_window_keyboard_grab_set(EWL_WINDOW(win), 0);
+			ewl_window_pointer_grab_set(EWL_WINDOW(win), 0);
+			sudo_pipe = popen(buf, "w");
+			fprintf(sudo_pipe, "%s\n", password);
+			pclose(sudo_pipe);
+		}	
+		else
+		{		
+			ewl_widget_destroy(win);
+			wait(pid);
+		}
+	}
+	ewl_main_quit();
+}
