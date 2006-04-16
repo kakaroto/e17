c2ad0b9d312ce996aa123099f9fdd52a4ab6ee1c
vapier <vapier@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-04-16 04:10:35 +0000

    use the simpler autogen script
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@22153 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e16/epplets/autogen.sh
+++ e16/epplets/autogen.sh
@@ -1,59 +1,14 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
-# $Id$
 
-DIE=0
+rm -rf autom4te.cache
+rm -f aclocal.m4 ltmain.sh
 
-echo "Generating configuration files for Epplets, please wait...."
+echo "Running aclocal..." ; aclocal -I . $ACLOCAL_FLAGS || exit 1
+echo "Running autoheader..." ; autoheader || exit 1
+echo "Running autoconf..." ; autoconf || exit 1
+echo "Running libtoolize..." ; (libtoolize --copy --force --automake || glibtoolize --automake) || exit 1
+echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-        echo "You must have autoconf installed to compile Epplets."
-        echo "Download the appropriate package for your distribution,"
-        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-        DIE=1
-}
-
-(libtool --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have libtool installed to compile Epplets."
-        echo "Download the appropriate package for your distribution,"
-        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-        DIE=1
-}
-
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have automake installed to compile Epplets."
-        echo "Download the appropriate package for your distribution,"
-        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-        DIE=1
-}
-
-if test "$DIE" -eq 1; then
-        exit 1
-fi
-
-echo "  libtoolize --copy --force"
-libtoolize --copy --force
-echo "  aclocal -I . $ACLOCAL_FLAGS"
-aclocal -I . $ACLOCAL_FLAGS
-echo "  autoheader"
-autoheader
-echo "  automake --add-missing"
-automake --add-missing
-echo "  autoconf"
-autoconf
-
-if [ -x config.status -a -z "$*" ]; then
-  ./config.status --recheck
-else
-  if test -z "$*"; then
-    echo "I am going to run ./configure with no arguments - if you wish "
-    echo "to pass any to it, please specify them on the $0 command line."
-    echo "If you do not wish to run ./configure, press Ctrl-C now."
-    trap 'echo "configure aborted" ; exit 0' 1 2 15
-    sleep 1
-  fi
-  ./configure "$@"
+if [ -z "$NOCONFIGURE" ]; then
+	./configure "$@"
 fi
