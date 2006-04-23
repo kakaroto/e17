f320d4d49780b463c2033e23fea00e6dafa478ba
onefang <onefang@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-04-23 08:18:22 +0000

    A gadcon version of the emu.  You won't be able to use it until raster writes that part of the shelf config utility, as the modules that are in the shelves are currently hard coded.  No point hard coding third party modules into E.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@22311 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e_modules/emu/src/modules/emu_gadcon/border_props.h
@@ -0,0 +1,15 @@
+/*
+ * vim:ts=8:sw=3:sts=8:noexpandtab:cino=>5n-3f0^-2{2
+ */
+#ifdef E_TYPEDEFS
+
+#else
+#ifndef BORDER_PROPS_H
+#define BORDER_PROPS_H
+#include "e_mod_main.h"
+
+EAPI void border_props_dialog(E_Container *con, E_Border * bd);
+EAPI char *border_props_substitute(E_Border * bd, char type);
+
+#endif
+#endif
---
+++ e_modules/emu/src/modules/emu_gadcon/e_mod_main.h
@@ -0,0 +1,80 @@
+#include "e.h"
+#include "easy_menu.h"
+#include "e_mod_config.h"
+#include "config.h"
+
+#define D_(str) dgettext(PACKAGE, str)
+
+#ifndef E_MOD_MAIN_H
+#define E_MOD_MAIN_H
+
+typedef struct _Config Config;
+typedef struct _Config_Emu Config_Emu;
+
+#define EMU_WIDTH_AUTO -1
+#define EMU_WIDTH_FIXED -2
+
+struct _Config
+{
+   char *appdir;
+   int follower;
+   double follow_speed;
+   double autoscroll_speed;
+   int iconsize;
+   int width;
+   Evas_List *emus;
+};
+
+struct _Config_Emu
+{
+   unsigned char enabled;
+};
+
+typedef struct _Emu Emu;
+struct _Emu
+{
+   Ecore_Event_Handler *del;
+
+//   Evas_List   *bars;
+   E_Menu *config_menu;
+
+   Config *conf;
+   E_Config_Dialog *config_dialog;
+};
+
+typedef struct _Emu_Face Emu_Face;
+struct _Emu_Face
+{
+//   Emu *emu;
+
+   E_Gadcon_Client *gcc;
+   Evas_Object     *o_button;
+
+   const char *name;
+   const char *command;
+   Ecore_Exe *exe;
+   Ecore_Event_Handler *add;
+   Ecore_Event_Handler *read;
+
+   void *data;
+               /**< the raw binary data from the child process that was recieved */
+   int size;
+               /**< the size of this data in bytes */
+   Ecore_Exe_Event_Data_Line *lines; /**< a NULL terminated array of line data if line buffered */
+
+   Evas_Hash *menus;
+};
+
+EAPI extern E_Module_Api e_modapi;
+
+EAPI void *e_modapi_init(E_Module *m);
+EAPI int e_modapi_shutdown(E_Module *m);
+EAPI int e_modapi_save(E_Module *m);
+EAPI int e_modapi_info(E_Module *m);
+EAPI int e_modapi_about(E_Module *m);
+
+EAPI int   e_modapi_config   (E_Module *m);
+
+void _emu_cb_config_updated(void *data);
+
+#endif
