6fe48246e80aaafd5a68b43ca2e4b8600f7cbec8
handyande <handyande@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-05-10 12:18:04 +0000

    oops, debug--
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@22511 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/engage/src/module/e_mod_main.c
+++ misc/engage/src/module/e_mod_main.c
@@ -2216,7 +2216,7 @@ _engage_bar_cb_mouse_in(void *data, Evas *e, Evas_Object *obj, void *event_info)
    Evas_Coord x, y, w, h;
    E_Gadman_Edge edge;
    double multiplier;
-printf("mouse in\n");
+
    ev = event_info;
    eb = data;
 
@@ -2261,7 +2261,7 @@ _engage_bar_cb_mouse_out(void *data, Evas *e, Evas_Object *obj, void *event_info
 {
    Evas_Event_Mouse_Out *ev;
    Engage_Bar *eb;
-printf("mouse out\n");
+
    ev = event_info;
    eb = data;
 
