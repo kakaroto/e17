f02687b3996946f747acf0465cf5e82091d6e827
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-06-01 21:06:11 +0000

    Update for E Module api changes. Remove e_modapi_info from header file and remove commented section from C file.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@23063 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/weather/e_mod_main.c
+++ e_modules/weather/e_mod_main.c
@@ -346,16 +346,6 @@ e_modapi_shutdown(E_Module *m)
    return 1;
 }
 
-//EAPI int
-//e_modapi_info(E_Module *m)
-//{
-//   char buf[4096];
-//
-//   snprintf(buf, sizeof(buf), "%s/module_icon.png", e_module_dir_get(m));
-//   m->icon_file = strdup(buf);
-//   return 1;
-//}
-
 EAPI int
 e_modapi_save(E_Module *m)
 {
