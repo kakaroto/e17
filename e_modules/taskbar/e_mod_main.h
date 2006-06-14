f9cbbe7b0ac71aa9a6bea5831d397137382da718
mej <mej@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-06-14 00:09:11 +0000

    Tue Jun 13 20:09:09 2006                        Michael Jennings (mej)
    Indent.
    ----------------------------------------------------------------------
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@23425 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/taskbar/e_mod_main.h
+++ e_modules/taskbar/e_mod_main.h
@@ -4,23 +4,23 @@
 #ifndef E_MOD_MAIN_H
 #define E_MOD_MAIN_H
 
-typedef struct _Config      Config;
+typedef struct _Config Config;
 
 struct _Config
 {
    /* just config state */
-   E_Module        *module;
-   Evas_List       *instances;
-   E_Menu          *menu;
-   Evas_List       *handlers;
+   E_Module *module;
+   Evas_List *instances;
+   E_Menu *menu;
+   Evas_List *handlers;
 };
 
 EAPI extern E_Module_Api e_modapi;
 
-EAPI void *e_modapi_init     (E_Module *m);
-EAPI int   e_modapi_shutdown (E_Module *m);
-EAPI int   e_modapi_save     (E_Module *m);
-EAPI int   e_modapi_info     (E_Module *m);
-EAPI int   e_modapi_about    (E_Module *m);
+EAPI void *e_modapi_init(E_Module *m);
+EAPI int e_modapi_shutdown(E_Module *m);
+EAPI int e_modapi_save(E_Module *m);
+EAPI int e_modapi_info(E_Module *m);
+EAPI int e_modapi_about(E_Module *m);
 
 #endif
