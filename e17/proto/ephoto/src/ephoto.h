db954d8a13c5b69f0e63c384c0a92d4bd1e15811
titan <titan@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-07-20 05:27:13 +0000

    Compiles now.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@24056 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/proto/ephoto/src/ephoto.h
@@ -0,0 +1,41 @@
+#ifndef _EPHOTO_H_
+#define _EPHOTO_H_
+
+#include <Ecore_File.h>
+#include <Ewl.h>
+#include <limits.h>
+#include <sqlite3.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void destroy_cb(Ewl_Widget *w, void *event, void *data);
+void add_album_cb(Ewl_Widget *w, void *event, void *data);
+void add_slideshow_cb(Ewl_Widget *w, void *event, void *data);
+void album_clicked_cb(Ewl_Widget *w, void *event, void *data);
+void slideshow_clicked_cb(Ewl_Widget *w, void *event, void *data);
+
+typedef struct _Main Main;
+
+struct _Main
+{
+ Ewl_Widget *win;
+ Ewl_Widget *vbox;
+ Ewl_Widget *hbox;
+ Ewl_Widget *groups;
+ Ewl_Widget *menubar;
+ Ewl_Widget *menu;
+ Ewl_Widget *menu_item;
+ Ewl_Widget *paned;
+ Ewl_Widget *albums;
+ Ewl_Widget *albums_border;
+ Ewl_Widget *slideshows;
+ Ewl_Widget *slideshows_border;
+ Ewl_Widget *viewer;
+ Ewl_Widget *viewer_border;
+ Ewl_Widget *viewer_freebox;
+ Ewl_Widget *icon;
+};
+
+extern Main *m;
+
+#endif
