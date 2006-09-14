30d7fe4057e35158bc09f5a46e815cac2fcd1e39
dj2 <dj2@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-09-14 05:49:16 +0000

    - nicer test
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@25827 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/bin/tests/ewl_io_manager.c
+++ e17/libs/ewl/src/bin/tests/ewl_io_manager.c
@@ -1,11 +1,14 @@
 #include "Ewl_Test.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include "ewl_test_private.h"
 
 static int create_test(Ewl_Container *box);
 static int ext_to_icon_name_test(char *buf, int len);
 static int mime_to_icon_name_test(char *buf, int len);
 static int uri_mime_type_get_test(char *buf, int len);
+
+static void cb_clicked(Ewl_Widget *w, void *ev, void *data);
 
 static Ewl_Unit_Test io_manager_unit_tests[] = {
 		{"extension to icon name mapping", ext_to_icon_name_test},
@@ -28,18 +31,56 @@ test_info(Ewl_Test *test)
 static int
 create_test(Ewl_Container *box)
 {
-	Ewl_Widget *t;
+	Ewl_Widget *o, *o2, *hbox;
 
-	t = ewl_io_manager_uri_read(PACKAGE_DATA_DIR "/examples/ewl_io_manager.c");
-	if (!t)
-	{
-		printf("Unable to create text widget from file");
-		return 0;
-	}
-	ewl_container_child_append(box, t);
-	ewl_widget_show(t);
+	hbox = ewl_hbox_new();
+	ewl_container_child_append(box, hbox);
+	ewl_object_fill_policy_set(EWL_OBJECT(hbox), EWL_FLAG_FILL_VSHRINK | EWL_FLAG_FILL_HFILL);
+	ewl_widget_show(hbox);
+
+	o = ewl_entry_new();
+	ewl_container_child_append(EWL_CONTAINER(hbox), o);
+	ewl_widget_show(o);
+
+	o2 = ewl_button_new();
+	ewl_button_label_set(EWL_BUTTON(o2), "fetch");
+	ewl_container_child_append(EWL_CONTAINER(hbox), o2);
+	ewl_callback_append(o2, EWL_CALLBACK_CLICKED, cb_clicked, o);
+	ewl_object_fill_policy_set(EWL_OBJECT(o2), EWL_FLAG_FILL_SHRINK);
+	ewl_widget_show(o2);
+
+	o = ewl_scrollpane_new();
+	ewl_scrollpane_hscrollbar_flag_set(EWL_SCROLLPANE(o),
+						EWL_SCROLLPANE_FLAG_ALWAYS_HIDDEN);
+	ewl_container_child_append(box, o);
+	ewl_widget_name_set(o, "scroll");
+	ewl_widget_show(o);
 
 	return 1;
+}
+
+static void
+cb_clicked(Ewl_Widget *w, void *ev, void *data)
+{
+	Ewl_Widget *scroll, *t, *entry;
+	char *s;
+
+	entry = data;
+
+	scroll = ewl_widget_name_find("scroll");
+	ewl_container_reset(EWL_CONTAINER(scroll));
+
+	s = ewl_text_text_get(EWL_TEXT(entry));
+	t = ewl_io_manager_uri_read(s);
+	if (!t)
+	{
+		printf("Unable to create text widget from file (%s)", s);
+		return;
+	}
+	ewl_container_child_append(EWL_CONTAINER(scroll), t);
+	ewl_widget_show(t);
+
+	if (s) free(s);
 }
 
 static int
