21f77c139ffa9b142b85754bfe4ce92b3b628bc7
onefang <onefang@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-09-17 15:34:25 +0000

    .desktopification.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@25923 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/net/e_mod_config.c
+++ e_modules/net/e_mod_config.c
@@ -38,7 +38,7 @@ _config_net_module (Config_Item * ci)
   v->basic.apply_cfdata = _basic_apply_data;
   v->basic.create_widgets = _basic_create_widgets;
 
-  snprintf (buf, sizeof (buf), "%s/module.eap",
+  snprintf (buf, sizeof (buf), "%s/module.edj",
 	    e_module_dir_get (net_config->module));
   con = e_container_current_get (e_manager_current_get ());
   cfd = e_config_dialog_new (con, D_ ("Net Configuration"), "Net", "_e_modules_net_config_dialog", buf, 0, v, ci);
