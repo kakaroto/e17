ee55b2fe97e2517ccd089e63bc067d32ccac567e
onefang <onefang@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-09-17 16:35:34 +0000

    .desktopification.  The task is almost complete.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@25929 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/taskbar/e_mod_main.c
+++ e_modules/taskbar/e_mod_main.c
@@ -220,7 +220,7 @@ _gc_icon(Evas *evas)
    char buf[4096];
 
    o = edje_object_add(evas);
-   snprintf(buf, sizeof(buf), "%s/module.eap", e_module_dir_get(taskbar_config->module));
+   snprintf(buf, sizeof(buf), "%s/module.edj", e_module_dir_get(taskbar_config->module));
    edje_object_file_set(o, buf, "icon");
    return o;
 }
