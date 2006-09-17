f5824c4a5abac38655fc9116831930b832c2468e
onefang <onefang@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-09-17 14:19:51 +0000

    .desktopification.  Burn you little eaps, burn I say!
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@25918 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/flame/e_mod_config.c
+++ e_modules/flame/e_mod_config.c
@@ -39,7 +39,7 @@ _config_flame_module (E_Container * con, Flame * fl)
   v->advanced.apply_cfdata = _advanced_apply_data;
   v->advanced.create_widgets = _advanced_create_widgets;
 
-  snprintf (buf, sizeof (buf), "%s/module.eap",
+  snprintf (buf, sizeof (buf), "%s/module.edj",
 	    e_module_dir_get (fl->module));
   cfd = e_config_dialog_new (con, D_ ("Flame Configuration"), "Flame", "_e_modules_flame_config_dialog", buf, 0, v, fl);
   fl->config_dialog = cfd;
