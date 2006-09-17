3230cc10d1584b4b600cda58bb1961b3665863cb
onefang <onefang@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-09-17 14:55:36 +0000

    .desktopification, I think, can't quite recall.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@25921 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/mem/e_mod_config.c
+++ e_modules/mem/e_mod_config.c
@@ -37,7 +37,7 @@ _config_mem_module (Config_Item * ci)
   v->basic.apply_cfdata = _basic_apply_data;
   v->basic.create_widgets = _basic_create_widgets;
 
-  snprintf (buf, sizeof (buf), "%s/module.eap",
+  snprintf (buf, sizeof (buf), "%s/module.edj",
 	    e_module_dir_get (mem_config->module));
   con = e_container_current_get (e_manager_current_get ());
   cfd = e_config_dialog_new (con, D_ ("Mem Configuration"), "Mem", "_e_modules_mem_config_dialog", buf, 0, v, ci);
