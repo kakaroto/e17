ef778a11e85db1954b993560fcc1696aaee887a9
onefang <onefang@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-09-17 16:46:53 +0000

    .desktopification.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@25930 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/tclock/e_mod_config.c
+++ e_modules/tclock/e_mod_config.c
@@ -35,7 +35,7 @@ _config_tclock_module (Config_Item * ci)
   v->basic.apply_cfdata = _basic_apply_data;
   v->basic.create_widgets = _basic_create_widgets;
 
-  snprintf (buf, sizeof (buf), "%s/module.eap",
+  snprintf (buf, sizeof (buf), "%s/module.edj",
 	    e_module_dir_get (tclock_config->module));
   con = e_container_current_get (e_manager_current_get ());
   cfd = e_config_dialog_new (con, D_ ("Tclock Configuration"), "TClock", "_e_modules_tclock_config_dialog", buf, 0, v, ci);
