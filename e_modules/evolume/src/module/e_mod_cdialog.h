b22db3186227a51bf48a25ad1cb9607a13caf2f4
mej <mej@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-10-10 03:01:04 +0000

    Mon Oct  9 23:01:00 2006                        Michael Jennings (mej)
    devilhorns says they're dead.
    ----------------------------------------------------------------------
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@26466 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/evolume/src/module/e_mod_cdialog.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef E_MOD_CDIALOG_H_INCLUDED
-#define E_MOD_CDIALOG_H_INCLUDED
-
-void e_volume_config_module(E_Container *con, Volume_Face *face);
-
-#endif // E_MOD_CDIALOG_H_INCLUDED
--- e_modules/evolume/src/module/e_mod_cmdialog.h
+++
@@ -1,6 +0,0 @@
-#ifndef E_MOD_CMDIALOG_H_INCLUDED
-#define E_MOD_CMDIALOG_H_INCLUDED
-
-void e_volume_config_mixer(void *data, void *data2);
-
-#endif // E_MOD_CMDIALOG_H_INCLUDED
