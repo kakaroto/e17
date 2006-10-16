f8708ae7a17eb7bafac4863421a47d713e86818e
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-10-16 03:16:22 +0000

    Patch from metrics for printfing the epsilon warning even if not in debug.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@26646 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/epsilon/src/lib/epsilon_thumb.c
+++ e17/libs/epsilon/src/lib/epsilon_thumb.c
@@ -293,7 +293,7 @@ epsilon_add(char *path, char *dst, int size, void *data)
 
 	if (!epsilon_server) {
 		if (!epsilon_client_connect()) {
-			if (debug) printf("Failed to connect to server\n");
+			fprintf(stderr, "Failed to connect to server\n");
 			exit(0);
 		}
 	}
