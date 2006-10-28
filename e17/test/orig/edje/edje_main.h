ccbb3c4b84914a5778c5a081ae49b1f2fb50ef0a
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-10-28 09:02:00 +0000

    add edje test data
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@26829 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/test/orig/edje/edje_main.h
@@ -0,0 +1,22 @@
+#ifndef EDJE_MAIN_H
+#define EDJE_MAIN_H
+
+#include <Evas.h>
+#include <Ecore.h>
+#include <Ecore_X.h>
+#include <Ecore_Evas.h>
+#include <Eet.h>
+#include "Edje.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <locale.h>
+#include <ctype.h>
+
+#endif
