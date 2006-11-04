7e5af5a5265b25c950e2488c971555b4c80426a2
ningerso <ningerso@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-11-04 16:26:05 +0000

    Add initial ARGB cursor implementation. This needs some additional work for tracking engine specific cursor information.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@26947 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/libs/ewl/src/lib/ewl_cursor.c
@@ -0,0 +1,88 @@
+#include <Ewl.h>
+#include "ewl_cursor.h"
+#include "ewl_debug.h"
+#include "ewl_macros.h"
+#include "ewl_private.h"
+
+static void ewl_cursor_cb_render(Ewl_Widget *w, void *ev, void *data);
+static void ewl_cursor_cb_destroy(Ewl_Widget *w, void *ev, void *data);
+
+int
+ewl_cursor_init(Ewl_Cursor *cursor)
+{
+	Ewl_Widget *w = EWL_WIDGET(cursor);
+
+	DENTER_FUNCTION(DLEVEL_UNSTABLE);
+	DCHECK_PARAM_PTR_RET("cursor", cursor, FALSE);
+	DCHECK_TYPE_RET("cursor", cursor, EWL_CURSOR_TYPE, FALSE);
+
+	if (!ewl_window_init(EWL_WINDOW(cursor)))
+		DRETURN_INT(FALSE, DLEVEL_UNSTABLE);
+
+	ewl_widget_appearance_set(w, EWL_CURSOR_TYPE);
+
+	ewl_object_fill_policy_set(EWL_OBJECT(w), EWL_FLAG_FILL_ALL);
+	ewl_object_size_request(EWL_OBJECT(w), 64, 64);
+	ewl_embed_engine_name_set(EWL_EMBED(cursor), "evas_buffer");
+	ewl_callback_append(w, EWL_CALLBACK_VALUE_CHANGED,
+			ewl_cursor_cb_render, NULL);
+	ewl_callback_append(w, EWL_CALLBACK_DESTROY, ewl_cursor_cb_destroy,
+			NULL);
+
+	DRETURN_INT(TRUE, DLEVEL_UNSTABLE);
+}
+
+static void
+ewl_cursor_cb_render(Ewl_Widget *w, void *ev, void *data)
+{
+       	int handle;
+	int width, height;
+	Ewl_Widget *parent;
+	int old = 0;
+	Ewl_Cursor *cursor = EWL_CURSOR(w);
+
+	DENTER_FUNCTION(DLEVEL_UNSTABLE);
+	DCHECK_PARAM_PTR("w", w);
+	DCHECK_TYPE("w", w, EWL_CURSOR_TYPE);
+
+	width = ewl_object_current_w_get(EWL_OBJECT(cursor));
+	height = ewl_object_current_h_get(EWL_OBJECT(cursor));
+
+	/*
+	 * Find the widget setting the current cursor.
+	 */
+	parent = ewl_widget_focused_get();
+	if (parent)
+		parent = EWL_WIDGET(ewl_embed_widget_find(parent));
+
+	if (parent)
+		old = EWL_EMBED(parent)->cursor;
+
+	if (cursor->handle)
+		ewl_engine_pointer_free(EWL_EMBED(parent), cursor->handle);
+
+	handle = ewl_engine_pointer_data_new(EWL_EMBED(parent),
+			EWL_EMBED(cursor)->evas_window, width, height);
+
+	/* FIXME: Needs to be done for all widgets with this cursor
+	ewl_attach_mouse_cursor_set(parent, handle);
+	if (EWL_EMBED(parent)->cursor == old)
+		ewl_embed_mouse_cursor_set(entry);
+	*/
+
+	cursor->handle = handle;
+
+	DLEAVE_FUNCTION(DLEVEL_UNSTABLE);
+}
+
+static void
+ewl_cursor_cb_destroy(Ewl_Widget *w, void *ev, void *data)
+{
+	Ewl_Cursor *cursor = EWL_CURSOR(w);
+
+	/* FIXME: Also needs to be handled for the correct engine refs
+	if (cursor->handle)
+		ewl_engine_pointer_free(parent, cursor->handle);
+		*/
+	cursor->handle = 0;
+}
---
+++ e17/libs/ewl/src/lib/ewl_cursor.h
@@ -0,0 +1,51 @@
+#ifndef EWL_CURSOR_H
+#define EWL_CURSOR_H
+
+/**
+ * @addtogroup Ewl_Cursor Ewl_Cursor: Container to create custom mouse cursors
+ * @brief Defines the Ewl_Cursor class used for generating ARGB cursor data.
+ *
+ * @{
+ */
+
+/**
+ * @themekey /cursor/file
+ * @themekey /cursor/group
+ */
+
+/**
+ * @def EWL_CURSOR_TYPE
+ * The type name for the Ewl_Cursor widget
+ */
+#define EWL_CURSOR_TYPE "cursor"
+
+/**
+ * Ewl_Cursor provides a container that has a labeled cursor.
+ */
+typedef struct Ewl_Cursor Ewl_Cursor;
+
+/**
+ * @def EWL_CURSOR(cursor)
+ * Typecast a pointer to an Ewl_Separator pointer.
+ */
+#define EWL_CURSOR(cursor) ((Ewl_Cursor *) cursor)
+
+/**
+ * @brief Inherits from Ewl_Window to allow packing widgets as cursor objects.
+ */
+struct Ewl_Cursor
+{
+	Ewl_Window      window;       /**< Inherit from Ewl_Window */
+	int             refcount;     /**< Number of references to cursor */
+	int             handle;       /**< Engine id for generated cursor */
+};
+
+Ewl_Widget     *ewl_cursor_new(void);
+int             ewl_cursor_init(Ewl_Cursor * c);
+
+/**
+ * @}
+ */
+
+#endif
+
