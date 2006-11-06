54a7fcb79a4617bfcafefc7d799e78eaa1e83046
essiene <essiene@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-11-06 09:57:53 +0000

    forgot this baby
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@26984 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/proto/entrance_edit_gui/src/gui/egui.c
@@ -0,0 +1,69 @@
+#include <limits.h>
+#include <Entrance_Edit.h>
+#include <Entrance_Widgets.h>
+#include "Egui.h"
+
+void
+egui_load_button(void *w, const char *key, int ktype)
+{
+	if(ktype == EGUI_TYPE_INT)
+	 ew_toggle_button_active_set(w, entrance_edit_int_get(key));
+}
+
+void
+egui_load_entry(void *w, const char *key, int ktype)
+{
+	if(ktype == EGUI_TYPE_STR)
+		ew_entry_set(w, entrance_edit_string_get(key));
+	else if(ktype == EGUI_TYPE_INT)
+	{
+		char msg[PATH_MAX];
+		snprintf(msg, PATH_MAX, "%d", entrance_edit_int_get(key));
+		ew_entry_set(w, msg);
+	}
+}
+
+void
+egui_load_checkbox(void *w, const char *key, int ktype)
+{
+	if(ktype == EGUI_TYPE_INT)
+	{
+		if(entrance_edit_int_get(key))
+			ew_checkbox_toggle(w);
+	}
+}
+
+void
+egui_save_button(void *w, const char *key, int ktype)
+{
+	if(ktype == EGUI_TYPE_INT)
+		entrance_edit_int_set(key, ew_toggle_button_active_get(w));
+}
+
+void
+egui_save_entry(void *w, const char *key, int ktype)
+{
+	if(ktype == EGUI_TYPE_STR)
+		entrance_edit_string_set(key, ew_entry_get(w));
+	else if(ktype == EGUI_TYPE_INT)
+		entrance_edit_int_set(key, atoi(ew_entry_get(w)));
+}
+
+void
+egui_save_list(void *w, const char *key, int ktype)
+{
+	if(ktype == EGUI_TYPE_STR)
+		entrance_edit_string_set(key, ew_list_selected_data_get(w));
+	else if(ktype = EGUI_TYPE_INT)
+	{
+		char *s = ew_list_selected_data_get(w);
+		entrance_edit_int_set(key, atoi(s));
+	}
+}
+
+void
+egui_save_checkbox(void *w, const char *key, int ktype)
+{
+	if(ktype == EGUI_TYPE_INT)
+		entrance_edit_int_set(key, ew_checkbox_is_active(w));
+}
