08573efa6b61c87f16568dfeb71f0cdfaffe170a
kwo <kwo@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-11-20 17:32:12 +0000

    Bad - revert.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@27187 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e16/e/src/ewins.c
+++ e16/e/src/ewins.c
@@ -2168,13 +2168,6 @@ EwinHandleEventsRoot(Win win __UNUSED__, XEvent * ev, void *prm __UNUSED__)
 
    switch (ev->type)
      {
-     case ButtonRelease:
-	ActionsEnd(NULL);
-	break;
-     case MotionNotify:
-	ActionsHandleMotion();
-	break;
-
      case MapRequest:
 	EwinEventMapRequest(NULL, ev->xmaprequest.window);
 	break;
