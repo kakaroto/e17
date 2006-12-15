a33dd06c1b7971b34958d629704f4a270b342813
titan <titan@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2006-12-15 21:07:24 +0000

    Add the ewl_shadow test!
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@27466 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/libs/ewl/src/bin/tests/shadow/ewl_shadow.c
@@ -0,0 +1,38 @@
+#include "Ewl_Test.h"
+#include "ewl_test_private.h"
+
+static int create_test(Ewl_Container *box);
+
+void test_info(Ewl_Test *test)
+{
+	test->name = "Shadow";
+	test->tip = "Defines the Ewl_Shadow class for displaying\n"
+			"a container that will shadow other widgets.";
+	test->filename = __FILE__;
+	test->func = create_test;
+	test->type = EWL_TEST_TYPE_CONTAINER;
+}
+
+static int
+create_test(Ewl_Container *box)
+{
+	Ewl_Widget *shadow;
+	Ewl_Widget *image;
+
+	shadow = ewl_shadow_new();
+	ewl_object_minimum_size_set(EWL_OBJECT(shadow), 280, 250);
+	ewl_object_alignment_set(EWL_OBJECT(shadow), EWL_FLAG_ALIGN_CENTER);
+	ewl_container_child_append(EWL_CONTAINER(box), shadow);
+	ewl_widget_show(shadow);
+	
+	image = ewl_image_new();
+	ewl_image_file_set(EWL_IMAGE(image), 
+			PACKAGE_DATA_DIR "/ewl/images/elicit.png", NULL);
+	ewl_image_size_set(EWL_IMAGE(image), 220, 210);
+	ewl_object_alignment_set(EWL_OBJECT(image), EWL_FLAG_ALIGN_CENTER);
+	ewl_container_child_append(EWL_CONTAINER(shadow), image);
+	ewl_widget_show(image);
+
+	return 1;
+}
+
