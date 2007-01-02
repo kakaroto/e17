e4e87c0a7046ee1b5b5b96afa2c6369f1253daeb
dj2 <dj2@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-01-02 17:01:44 +0000

    - formatting
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@27728 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/lib/ewl_embed.h
+++ e17/libs/ewl/src/lib/ewl_embed.h
@@ -111,10 +111,16 @@ void 		 ewl_embed_mouse_up_feed(Ewl_Embed *embed, int b, int x,
 void 		 ewl_embed_mouse_move_feed(Ewl_Embed *embed, int x, int y,
 					  unsigned int modifiers);
 
-const char 	*ewl_embed_dnd_position_feed(Ewl_Embed *embed, int x, int y,int*,int*,int*,int*);
-const char 	*ewl_embed_dnd_drop_feed(Ewl_Embed* embed, int x, int y, int internal);
-void		 ewl_embed_dnd_data_received_feed(Ewl_Embed* embed, char *type, void *data, unsigned int len, unsigned int format);
-void		 ewl_embed_dnd_data_request_feed(Ewl_Embed* embed, void *handle, char *type);
+const char 	*ewl_embed_dnd_position_feed(Ewl_Embed *embed, int x, int y, 
+						int *, int *, int *, int *);
+const char 	*ewl_embed_dnd_drop_feed(Ewl_Embed* embed, int x, int y, 
+							int internal);
+void		 ewl_embed_dnd_data_received_feed(Ewl_Embed* embed, char *type, 
+							void *data, 
+							unsigned int len,
+							unsigned int format);
+void		 ewl_embed_dnd_data_request_feed(Ewl_Embed* embed, void *handle, 
+								char *type);
 
 void 		 ewl_embed_mouse_out_feed(Ewl_Embed *embed, int x, int y,
 					 unsigned int modifiers);
