0e9e0cfaaac13c08b938f5ef2ffbce5f5edd54b2
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-01-13 00:35:13 +0000

    Fixes for tclock config dialog.       - Check for existing dialog and return.       - No need to explicitly set the check_widget value.       - Remove CamelCase functions.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@27933 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/tclock/e_mod_config.c
+++ e_modules/tclock/e_mod_config.c
@@ -16,8 +16,8 @@ static Evas_Object *_basic_create_widgets (E_Config_Dialog * cfd, Evas * evas,
 					   E_Config_Dialog_Data * cfdata);
 static int _basic_apply_data (E_Config_Dialog * cfd,
 			      E_Config_Dialog_Data * cfdata);
-static void onTimeCheckChange (void *data, Evas_Object * obj);
-static void onDateCheckChange (void *data, Evas_Object * obj);
+static void _cb_time_check (void *data, Evas_Object * obj);
+static void _cb_date_check (void *data, Evas_Object * obj);
 
 void
 _config_tclock_module (Config_Item * ci)
@@ -27,6 +27,8 @@ _config_tclock_module (Config_Item * ci)
   E_Container *con;
   char buf[4096];
 
+   if (e_config_dialog_find("TClock", "_tclock_config_dialog")) return;
+   
   v = E_NEW (E_Config_Dialog_View, 1);
 
   v->create_cfdata = _create_data;
@@ -37,7 +39,8 @@ _config_tclock_module (Config_Item * ci)
   snprintf (buf, sizeof (buf), "%s/module.edj",
 	    e_module_dir_get (tclock_config->module));
   con = e_container_current_get (e_manager_current_get ());
-  cfd = e_config_dialog_new (con, D_ ("Tclock Configuration"), "TClock", "_e_modules_tclock_config_dialog", buf, 0, v, ci);
+  cfd = e_config_dialog_new (con, D_ ("Tclock Configuration"), "TClock", 
+			     "_tclock_config_dialog", buf, 0, v, ci);
   tclock_config->config_dialog = cfd;
 }
 
@@ -58,7 +61,6 @@ _create_data (E_Config_Dialog * cfd)
 
   ci = cfd->data;
   cfdata = E_NEW (E_Config_Dialog_Data, 1);
-
   _fill_data (ci, cfdata);
   return cfdata;
 }
@@ -66,31 +68,26 @@ _create_data (E_Config_Dialog * cfd)
 static void
 _free_data (E_Config_Dialog * cfd, E_Config_Dialog_Data * cfdata)
 {
-  if (!tclock_config)
-    return;
-  tclock_config->config_dialog = NULL;
-  free (cfdata);
-  cfdata = NULL;
+   if (!tclock_config) return;
+   tclock_config->config_dialog = NULL;
+   E_FREE(cfdata);
 }
 
 static Evas_Object *
-_basic_create_widgets (E_Config_Dialog * cfd, Evas * evas,
-		       E_Config_Dialog_Data * cfdata)
+_basic_create_widgets (E_Config_Dialog * cfd, Evas * evas, E_Config_Dialog_Data * cfdata)
 {
-  Evas_Object *o, *of, *ob, *time_entry, *time_check, *date_entry,
-    *date_check;
-  E_Radio_Group *rg;
+   Evas_Object *o, *of, *ob;
+   Evas_Object *time_entry, *time_check, *date_entry, *date_check;
+   E_Radio_Group *rg;
 
   o = e_widget_list_add (evas, 0, 0);
 
   of = e_widget_frametable_add (evas, D_ ("Date"), 1);
   date_check =
     e_widget_check_add (evas, D_ ("Show Date"), &(cfdata->show_date));
-  if (cfdata->show_date)
-    e_widget_check_checked_set (date_check, 1);
   e_widget_frametable_object_append (of, date_check, 0, 0, 1, 1, 1, 0, 1, 0);
   date_entry = e_widget_entry_add (evas, &cfdata->date_format);
-  e_widget_on_change_hook_set (date_check, onDateCheckChange, date_entry);
+  e_widget_on_change_hook_set (date_check, _cb_date_check, date_entry);
   e_widget_disabled_set (date_entry, !cfdata->show_date);
   e_widget_min_size_set (date_entry, 150, 1);
   e_widget_frametable_object_append (of, date_entry, 0, 1, 1, 1, 1, 0, 1, 0);
@@ -102,11 +99,9 @@ _basic_create_widgets (E_Config_Dialog * cfd, Evas * evas,
   of = e_widget_frametable_add (evas, D_ ("Time"), 1);
   time_check =
     e_widget_check_add (evas, D_ ("Show Time"), &(cfdata->show_time));
-  if (cfdata->show_time)
-    e_widget_check_checked_set (time_check, 1);
   e_widget_frametable_object_append (of, time_check, 0, 0, 1, 1, 1, 0, 1, 0);
   time_entry = e_widget_entry_add (evas, &cfdata->time_format);
-  e_widget_on_change_hook_set (time_check, onTimeCheckChange, time_entry);
+  e_widget_on_change_hook_set (time_check, _cb_time_check, time_entry);
   e_widget_disabled_set (time_entry, !cfdata->show_time);
   e_widget_min_size_set (time_entry, 150, 1);
   e_widget_frametable_object_append (of, time_entry, 0, 1, 1, 1, 1, 0, 1, 0);
@@ -140,17 +135,19 @@ _basic_apply_data (E_Config_Dialog * cfd, E_Config_Dialog_Data * cfdata)
 }
 
 static void
-onTimeCheckChange (void *data, Evas_Object * obj)
+_cb_time_check (void *data, Evas_Object *obj)
 {
-  int checked = e_widget_check_checked_get (obj);
-
-  e_widget_disabled_set (data, !checked);
+  int checked;
+   
+   checked = e_widget_check_checked_get (obj);
+   e_widget_disabled_set (data, !checked);
 }
 
 static void
-onDateCheckChange (void *data, Evas_Object * obj)
+_cb_date_check (void *data, Evas_Object *obj)
 {
-  int checked = e_widget_check_checked_get (obj);
-
-  e_widget_disabled_set (data, !checked);
+   int checked;
+   
+   checked = e_widget_check_checked_get (obj);
+   e_widget_disabled_set (data, !checked);
 }
