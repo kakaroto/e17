600c017796c41d050eeb3749bbc98af5d81e91a1
lok <lok@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-01-16 15:18:09 +0000

    Moving to Etk_Tree2.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@28016 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/enity/src/bin/enity.h
+++ e17/apps/enity/src/bin/enity.h
@@ -15,7 +15,7 @@ typedef struct _Enity_Tree_Col Enity_Tree_Col;
 
 struct _Enity_Tree_Col
 {
-   Etk_Tree_Col *col;
+   Etk_Tree2_Col *col;
    enum {
       ENITY_COL_MODEL_TEXT,
       ENITY_COL_MODEL_CHECK,
@@ -24,7 +24,7 @@ struct _Enity_Tree_Col
 };
 
 static Evas_List *_en_arg_data_get(En_Argument *args, char *key);
-static void _en_tree_checkbox_toggled_cb(Etk_Object *object, Etk_Tree_Row *row, void *data);
+static void _en_tree_checkbox_toggled_cb(Etk_Object *object, Etk_Tree2_Row *row, void *data);
 static void _en_ok_print_stdout_cb(Etk_Object *obj, int response_id, void *data);
 static void _en_entry_cb(En_Argument *args, int index);
 static void _en_error_cb(En_Argument *args, int index);
