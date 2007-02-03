ea70d82add0e174577328d62e4f9c6d4f73cb1f0
pfritz <pfritz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-02-03 03:09:26 +0000

    formatting
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@28204 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/lib/ewl_dialog.h
+++ e17/libs/ewl/src/lib/ewl_dialog.h
@@ -64,8 +64,8 @@ Ewl_Position 	 ewl_dialog_action_position_get(Ewl_Dialog *dialog);
 void 		 ewl_dialog_active_area_set(Ewl_Dialog *d, Ewl_Position pos);
 Ewl_Position 	 ewl_dialog_active_area_get(Ewl_Dialog *d);
 
-unsigned int 	 ewl_dialog_has_separator_get (Ewl_Dialog *dialog);
-void 		 ewl_dialog_has_separator_set (Ewl_Dialog *dialog,
+unsigned int 	 ewl_dialog_has_separator_get(Ewl_Dialog *dialog);
+void 		 ewl_dialog_has_separator_set(Ewl_Dialog *dialog,
 						unsigned int has_sep);
 
 /**
