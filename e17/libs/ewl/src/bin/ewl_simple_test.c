de34cef82bc2b792ead26e4c99667bb9238fcd23
ningerso <ningerso@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-02-09 04:45:06 +0000

    Fix the simple test to flip through the slideshow again.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@28291 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/bin/ewl_simple_test.c
+++ e17/libs/ewl/src/bin/ewl_simple_test.c
@@ -18,135 +18,36 @@
 #endif
 
 static void
-entice_text(void *data, Evas_Object *obj __UNUSED__, 
-		const char *emission __UNUSED__, const char *source __UNUSED__)
-{
-	Ewl_Widget *label = data;
-	char *text = "\nEntice is an image viewer that\n"
-			"allows for a fully themeable\n"
-			"experience. Every part of the\n"
-			"interface can be modified in\n"
-			"look and feel. Entice also\n"
-			"provides basic image editing\n"
-			"features to allow quick editing\n"
-			"while viewing.";
-
-	ewl_text_align_set(EWL_TEXT(label), EWL_FLAG_ALIGN_LEFT);
-	ewl_text_font_set(EWL_TEXT(label), "ewl/default");
-	ewl_text_font_size_set(EWL_TEXT(label), 12);
-	ewl_text_styles_set(EWL_TEXT(label), EWL_TEXT_STYLE_SOFT_SHADOW);
-	ewl_text_text_set(EWL_TEXT(label), "Entice");
-
-	ewl_text_styles_set(EWL_TEXT(label), EWL_TEXT_STYLE_NONE);
-	ewl_text_font_set(EWL_TEXT(label), "ewl/default");
-	ewl_text_font_size_set(EWL_TEXT(label), 7);
-	ewl_text_text_append(EWL_TEXT(label), text);
-}
-
-static void
-entrance_text(void *data, Evas_Object *obj __UNUSED__,
-		const char *emission __UNUSED__, const char *source __UNUSED__)
-{
-	Ewl_Widget *label = data;
-	char *text = "\nEntrance [en-'trans], v.\n"
-			"  1: to carry away with delight,\n"
-			"     wonder, or rapture\n"
-			"  2: to put into a trance\n"
-			"Entrance is the Enlightenment\n"
-			"Display Manager. And like\n"
-			"Enlightenment, it takes beauty\n"
-			"and customization to new levels\n";
-
-	ewl_text_align_set(EWL_TEXT(label), EWL_FLAG_ALIGN_LEFT);
-	ewl_text_font_set(EWL_TEXT(label), "ewl/default");
-	ewl_text_font_size_set(EWL_TEXT(label), 12);
-	ewl_text_styles_set(EWL_TEXT(label), EWL_TEXT_STYLE_SOFT_SHADOW);
-	ewl_text_text_set(EWL_TEXT(label), "Entrance");
-
-	ewl_text_styles_set(EWL_TEXT(label), EWL_TEXT_STYLE_NONE);
-	ewl_text_font_set(EWL_TEXT(label), "ewl/default");
-	ewl_text_font_size_set(EWL_TEXT(label), 7);
-	ewl_text_text_append(EWL_TEXT(label), text);
-}
-
-static void
-elicit_text(void *data, Evas_Object *obj __UNUSED__,
-		const char *emission __UNUSED__, const char *source __UNUSED__)
-{
-	Ewl_Widget *label = data;
-	char *text = "\nElicit is a tool for examining\n"
-		     "images on your desktop,\n"
-		     "providing both a global color\n"
-		     "picker and a zoom tool.\n"
-		     "Graphic artists and designers\n"
-		     "can quickly examine graphics\n"
-		     "without needed to rely on\n"
-		     "larger tools for simple\n"
-		     "examinations and color checks.";
-
-	ewl_text_align_set(EWL_TEXT(label), EWL_FLAG_ALIGN_LEFT);
-	ewl_text_font_set(EWL_TEXT(label), "ewl/default");
-	ewl_text_font_size_set(EWL_TEXT(label), 12);
-	ewl_text_styles_set(EWL_TEXT(label), EWL_TEXT_STYLE_SOFT_SHADOW);
-	ewl_text_text_set(EWL_TEXT(label), "Elicit");
-
-	ewl_text_styles_set(EWL_TEXT(label), EWL_TEXT_STYLE_NONE);
-	ewl_text_font_set(EWL_TEXT(label), "ewl/default");
-	ewl_text_font_size_set(EWL_TEXT(label), 7);
-	ewl_text_text_append(EWL_TEXT(label), text);
-}
-
-static void
-evidence_text(void *data, Evas_Object *obj __UNUSED__,
-		const char *emission __UNUSED__, const char *source __UNUSED__)
-{
-	Ewl_Widget *label = data;
-	char *text = "\nEvidence is a file-manager\n"
-		     "with plugins for everything --\n"
-		     "from ultra-fast JPEG thumbnailers\n"
-		     "to MP3/ID3 and Ogg tag editing.\n"
-		     "No bloat -- what you don't need,\n"
-		     "you don't load.\n"
-		     "Micro-shell, MP3 thumbnailing and\n"
-		     "extensive theming opportunities\n"
-		     "complete this slightly different browser.";
-
-	ewl_text_align_set(EWL_TEXT(label), EWL_FLAG_ALIGN_LEFT);
-	ewl_text_font_set(EWL_TEXT(label), "ewl/default");
-	ewl_text_font_size_set(EWL_TEXT(label), 12);
-	ewl_text_styles_set(EWL_TEXT(label), EWL_TEXT_STYLE_SOFT_SHADOW);
-	ewl_text_text_set(EWL_TEXT(label), "Evidence");
-
-	ewl_text_styles_set(EWL_TEXT(label), EWL_TEXT_STYLE_NONE);
-	ewl_text_font_set(EWL_TEXT(label), "ewl/default");
-	ewl_text_font_size_set(EWL_TEXT(label), 7);
-	ewl_text_text_append(EWL_TEXT(label), text);
-}
-
-static void
 edje_text(void *data, Evas_Object *obj __UNUSED__,
 		const char *emission, const char *source)
 {
+	char *buf;
 	char *text;
+	int len;
 	Ewl_Widget *label = data;
-
-	printf("In %s, %s\n", emission, source);
 
 	if (strstr(emission, "Present") != emission)
 		return;
 
 	text = (char *)emission + strlen("Present");
+	len = strlen(text);
+	buf = NEW(char, len);
+	snprintf(buf, len, text);
 
-	ewl_text_align_set(EWL_TEXT(label), EWL_FLAG_ALIGN_LEFT);
+	ewl_text_clear(EWL_TEXT(label));
+
 	ewl_text_font_set(EWL_TEXT(label), "ewl/default");
 	ewl_text_font_size_set(EWL_TEXT(label), 12);
 	ewl_text_styles_set(EWL_TEXT(label), EWL_TEXT_STYLE_SOFT_SHADOW);
 	ewl_text_text_set(EWL_TEXT(label), (char *)source);
 
+	ewl_text_align_set(EWL_TEXT(label), EWL_FLAG_ALIGN_LEFT);
 	ewl_text_styles_set(EWL_TEXT(label), EWL_TEXT_STYLE_NONE);
 	ewl_text_font_set(EWL_TEXT(label), "ewl/default");
 	ewl_text_font_size_set(EWL_TEXT(label), 7);
 	ewl_text_text_append(EWL_TEXT(label), text);
+
+	FREE(buf);
 }
 
 static void
@@ -172,10 +73,17 @@ start_text(Ewl_Widget *w __UNUSED__, void *ev_data __UNUSED__,
 }
 
 static void
-realize_logo_cb(Ewl_Widget *w, void *ev_data __UNUSED__, void *user_data)
+reveal_logo_cb(Ewl_Widget *w, void *ev_data __UNUSED__, void *user_data)
 {
 	edje_object_signal_callback_add(w->theme_object, "Present*", "*",
 			edje_text, user_data);
+}
+
+static void
+obscure_logo_cb(Ewl_Widget *w, void *ev_data __UNUSED__, void *user_data)
+{
+	edje_object_signal_callback_del(w->theme_object, "Present*", "*",
+			edje_text);
 }
 
 static void
@@ -242,7 +150,8 @@ main(int argc, char **argv)
 	logo = NEW(Ewl_Widget, 1);
 	ewl_widget_init(logo);
 	ewl_widget_appearance_set(logo, "demo_logo");
-	ewl_callback_append(logo, EWL_CALLBACK_REALIZE, realize_logo_cb, label);
+	ewl_callback_append(logo, EWL_CALLBACK_REVEAL, reveal_logo_cb, label);
+	ewl_callback_append(logo, EWL_CALLBACK_OBSCURE, obscure_logo_cb, label);
 	ewl_theme_data_str_set(logo, "/demo_logo/file", tmp);
 	ewl_theme_data_str_set(logo, "/demo_logo/group", "tour");
 	ewl_object_preferred_inner_size_set(EWL_OBJECT(logo), 150, 150);
@@ -280,11 +189,5 @@ main(int argc, char **argv)
 
 	ewl_main();
 	return 0;
-
-	/* ugly, but shuts the compiler warning up */
-	entice_text(NULL, NULL, NULL, NULL);
-	entrance_text(NULL, NULL, NULL, NULL);
-	elicit_text(NULL, NULL, NULL, NULL);
-	evidence_text(NULL, NULL, NULL, NULL);
 }
 
