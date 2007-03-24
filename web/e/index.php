86a4f97bdddeb20fced76c9c049621db0deb065e
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-03-24 11:16:58 +0000

    remove old www stuff... making room for new
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@29001 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- web/e/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-   $hostname = strtolower($_SERVER['HTTP_HOST']);
-   if ($hostname == 'www.enlightenment.org')
-      header("Location: http://enlightenment.org/pages/main.html");
-   else
-      header("Location: /pages/main.html");
-   exit; 
-?>
