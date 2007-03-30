f9f6612475ce4beecf280c0a8a4eefaf9e9edb06
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-03-30 20:30:34 +0000

    Memory module converted to new desktop spec/icon.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@29247 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/mem/e_mod_main.c
+++ e_modules/mem/e_mod_main.c
@@ -113,7 +113,7 @@ _gc_icon (Evas * evas)
   char buf[4096];
 
   o = edje_object_add (evas);
-  snprintf (buf, sizeof (buf), "%s/module.edj",
+  snprintf (buf, sizeof (buf), "%s/e-module-mem.edj",
 	    e_module_dir_get (mem_config->module));
   edje_object_file_set (o, buf, "icon");
   return o;
