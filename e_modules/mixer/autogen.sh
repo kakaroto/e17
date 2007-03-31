82b81dbea0503695c6d42d12ffd3061621eebf8f
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-03-31 22:14:42 +0000

    Add autopoint for mixer module.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@29292 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/mixer/autogen.sh
+++ e_modules/mixer/autogen.sh
@@ -3,6 +3,7 @@
 rm -rf autom4te.cache
 rm -f aclocal.m4 ltmain.sh
 
+echo "Running autopoint..." ; autopoint -f || :
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
