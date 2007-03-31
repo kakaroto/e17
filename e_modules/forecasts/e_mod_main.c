4f9656c919308ea554620d876f8c27b453f91b44
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-03-31 09:06:31 +0000

    As per saturn's request, convert forecasts module to use new .desktop stuff for efreet so that the module shows in the list.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@29275 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/forecasts/e_mod_main.c
+++ e_modules/forecasts/e_mod_main.c
@@ -248,7 +248,7 @@ _gc_icon(Evas * evas)
    char buf[4096];
 
    o = edje_object_add(evas);
-   snprintf(buf, sizeof(buf), "%s/module.edj",
+   snprintf(buf, sizeof(buf), "%s/e-module-forecasts.edj",
 	 e_module_dir_get(forecasts_config->module));
    edje_object_file_set(o, buf, "icon");
    return o;
