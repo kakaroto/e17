b7beeb843b02bd4cd5af9630df22e64c69e73740
barbieri <barbieri@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-05-11 01:37:10 +0000

    Add trove classifier and description, will upload to PyPI soon.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@29939 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/python-efl/python-evas/setup.py
+++ e17/proto/python-efl/python-evas/setup.py
@@ -40,6 +40,42 @@ headers = ['evas/evas.c_evas.pxd',
            'evas/python_evas_utils.h',
            ]
 
+trove_classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Environment :: Console :: Framebuffer",
+    "Environment :: X11 Applications",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: POSIX",
+    "Programming Language :: C",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Software Development :: User Interfaces",
+    ]
+
+long_description = """\
+Python bindings for Evas, part of Enlightenment Foundation Libraries.
+
+Evas is a hardware-accelerated canvas API for X-Windows that can draw
+anti-aliased text, smooth super and sub-sampled images, alpha-blend,
+as well as drop down to using normal X11 primitives such as pixmaps,
+lines and rectangles for speed if your CPU or graphics hardware are
+too slow.
+
+Evas abstracts any need to know much about what the characteristics of
+your XServer's display are, what depth or what magic visuals etc, it
+has. The most you need to tell Evas is how many colors (at a maximum)
+to use if the display is not a truecolor display. By default it is
+suggested to use 216 colors (as this equates to a 6x6x6 color cube -
+exactly the same color cube Netscape, Mozilla, gdkrgb etc. use so
+colors will be shared). If Evas can't allocate enough colors it keeps
+reducing the size of the color cube until it reaches plain black and
+white. This way, it can display on anything from a black and white
+only terminal to 16 color VGA to 256 color and all the way up through
+15, 16, 24 and 32bit color.
+"""
+
 setup(name = 'python-evas',
       version = '0.1.0',
       license = 'BSD',
@@ -47,6 +83,9 @@ setup(name = 'python-evas',
       author_email='barbieri@gmail.com',
       url='http://www.enlightenment.org/',
       description = 'Python bindings for Evas',
+      long_description = long_description,
+      keywords = 'wrapper binding enlightenment graphics raster evas canvas',
+      classifiers = trove_classifiers,
       packages = find_packages(),
       headers = headers,
       ext_modules = [evasmodule],
