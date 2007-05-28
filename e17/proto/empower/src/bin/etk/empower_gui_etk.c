46ac69507a6db7dc80c737fb9ce472788e400b90
lok <lok@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-05-28 19:01:37 +0000

    Works with the latest API changement
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@30131 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/empower/src/bin/etk/empower_gui_etk.c
+++ e17/proto/empower/src/bin/etk/empower_gui_etk.c
@@ -16,14 +16,14 @@ void display_window()
    
    etk_window_title_set(ETK_WINDOW(em->dialog), "Empower");
    etk_window_wmclass_set(ETK_WINDOW(em->dialog), "Empower", "Empower");
-   etk_signal_connect("delete_event", ETK_OBJECT(em->dialog), ETK_CALLBACK(_em_dialog_delete_event_cb), em);
-   etk_signal_connect("size_request", ETK_OBJECT(em->dialog), ETK_CALLBACK(_em_dialog_size_request_cb), em);
+   etk_signal_connect("destroyed", ETK_OBJECT(em->dialog), ETK_CALLBACK(_em_dialog_delete_event_cb), em);
+   etk_signal_connect("size-requested", ETK_OBJECT(em->dialog), ETK_CALLBACK(_em_dialog_size_request_cb), em);
    etk_signal_connect("response", ETK_OBJECT(em->dialog), ETK_CALLBACK(_em_dialog_response_cb), em);
    
-   etk_signal_connect("key_down", ETK_OBJECT(em->entry), ETK_CALLBACK(_em_entry_key_down), em);
+   etk_signal_connect("key-down", ETK_OBJECT(em->entry), ETK_CALLBACK(_em_entry_key_down), em);
    etk_entry_password_mode_set(ETK_ENTRY(em->entry), ETK_TRUE);
    
-   etk_dialog_pack_in_main_area(ETK_DIALOG(em->dialog), em->hbox, ETK_FALSE, ETK_FALSE, 4, ETK_FALSE);
+   etk_dialog_pack_in_main_area(ETK_DIALOG(em->dialog), em->hbox, ETK_BOX_START, ETK_BOX_NONE, 4);
    etk_box_append(ETK_BOX(em->hbox), em->icon, ETK_BOX_START, ETK_BOX_NONE, 3);
    etk_box_append(ETK_BOX(em->hbox), em->label, ETK_BOX_START, ETK_BOX_NONE, 2);   
    etk_box_append(ETK_BOX(em->hbox), em->entry, ETK_BOX_START, ETK_BOX_NONE, 2);
