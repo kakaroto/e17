4f90633f3d0fd191b33ba2b1c8c5b1351ee58043
inc <inc@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-05-30 22:45:52 +0000

    *** empty log message ***
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@30160 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- web/www/p/news/latestnews.php
+++ web/www/p/news/latestnews.php
@@ -1,17 +1,21 @@
-<?php require_once 'class.news.php'; setlocale(LC_ALL, 'en_EN'); $news = 
-new news; $list = $news->get(2); foreach($list as $new) {
+<?php
+require_once 'p/news/class.news.php';
+
+setlocale(LC_ALL, 'en_EN');
+
+$news = new news;
+$list = $news->get(2);
+foreach($list as $new)
+{
    if (($new = $news->read_content($new)) !== NULL)
    {
-     echo "<div class='post'>\n";
-     echo "<p class='news-title'><a 
-href='p.php?p=news/show&l=".$_GET['l']."&news_id=$new[id]'>".$new['title']."</a></p>";
-     echo "<p class='tiny'><a 
-href='mailto:".$new['email']."'>".$new['author']."</a> - 
-".$new['date']."</p>";
-     echo "<p class='news-content'>".$new['content']."</p>\n";
-     echo "</div>\n";
+     echo "<p><a href='p.php?p=news/show&l=".$_GET['l']."&news_id=$new[id]'>".$new['title']."</a> - <em>".$new['date']."</em></p>";
+     echo "<p>".$new['content']."</p>\n";
    }
-echo "<p align='right'><a href='p.php?p=news&l=".$_GET['l']."' 
-title='News'>More news ... </a></p>";
+
+
+
 }
+echo "<p align='right'><a href='p.php?p=news&l=".$_GET['l']."' title='News'>More news ... </a></p>";
 ?>
+
