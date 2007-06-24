2dfffb71f0dd80311fb5ff861e7f1f5e4ee58cae
barbieri <barbieri@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-06-24 16:17:28 +0000

    Move stop_pulsing to edje, app must not know that there is such thing, it's all from theme. Minor fixes.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@30451 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/python-efl/python-edje/examples/evas-demo/01-app_launcher.py
+++ e17/proto/python-efl/python-edje/examples/evas-demo/01-app_launcher.py
@@ -6,6 +6,7 @@ import ecore
 import ecore.evas
 import edje
 import sys
+import os
 
 ecore.init()
 ecore.evas.init()
@@ -14,10 +15,19 @@ edje.init()
 if "-x11" in sys.argv:
     ee = ecore.evas.SoftwareX11(w=800, h=480)
 else:
-    ee = ecore.evas.SoftwareX11_16(w=800, h=480)
-canvas = ee.evas
+    if ecore.evas.engine_type_supported_get("software_x11_16"):
+        ee = ecore.evas.SoftwareX11_16(w=800, h=480)
+    else:
+        ee = ecore.evas.SoftwareX11(w=800, h=480)
 
-edje_obj = edje.Edje(canvas, file="01-app_launcher.edj", group="main")
+canvas = ee.evas
+edje_file = os.path.join(os.path.dirname(sys.argv[0]), "01-app_launcher.edj")
+
+try:
+    edje_obj = edje.Edje(canvas, file=edje_file, group="main")
+except Exception, e: # should be EdjeLoadError, but it's wrong on python2.5
+    raise SystemExit("Failed to load Edje file: %s" % edje_file)
+
 edje_obj.size = canvas.size
 edje_obj.show()
 ee.data["edje"] = edje_obj
@@ -33,32 +43,17 @@ def key_down_cb(bg, event):
         ecore.main_loop_quit()
 edje_obj.on_key_down_add(key_down_cb)
 
-stop_pulsing_id = None
-def stop_pulsing(edje_obj, icon):
-    global stop_pulsing_id
-    stop_pulsing_id = None
-    edje_obj.signal_emit("stop_pulsing_" + icon, "")
-    return False
-
-
 parts = ("web_browser", "audio_player", "image_viewer", "video_player")
-def icon_clicked_cb(edje_obj, emission, source):
-    icon = source.replace("_area", "")
-    if edje_obj.part_state_get(icon) == ("default", 0.0):
-        global stop_pulsing_id
-        if stop_pulsing_id is not None:
-            stop_pulsing_id.stop()
-        stop_pulsing_id = ecore.timer_add(STOP_PULSING_TIMEOUT,
-                                          stop_pulsing, edje_obj, icon)
-        edje_obj.signal_emit("select_" + icon, "")
+def icon_clicked_cb(edje_obj, emission, source, part):
+    if edje_obj.part_state_get(part) == ("default", 0.0):
+        edje_obj.signal_emit("select_" + part, "")
         for p in parts:
-            if p != icon:
+            if p != part:
                 edje_obj.signal_emit("unselect_" + p, "")
 
 for p in parts:
-    edje_obj.signal_callback_add("mouse,clicked,1",
-                                 p + "_area",
-                                 icon_clicked_cb)
+    edje_obj.signal_callback_add("mouse,clicked,1", p + "_area",
+                                 icon_clicked_cb, p)
 
 edje_obj.focus = True
 ee.fullscreen = "-nofs" not in sys.argv
