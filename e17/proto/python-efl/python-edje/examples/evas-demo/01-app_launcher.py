621182a25c1eed0118f9c29560c78c696cf4f4c5
barbieri <barbieri@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-06-25 18:51:50 +0000

    Python code is now clean, no UI there, everything handled in Edje, Python also parse command line with optparse and few comments were added.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@30463 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/python-efl/python-edje/examples/evas-demo/01-app_launcher.py
+++ e17/proto/python-efl/python-edje/examples/evas-demo/01-app_launcher.py
@@ -10,14 +10,54 @@ ecore.init()
 ecore.evas.init()
 edje.init()
 
-if "-x11" in sys.argv:
-    ee = ecore.evas.SoftwareX11(w=800, h=480)
-else:
-    if ecore.evas.engine_type_supported_get("software_x11_16"):
-        ee = ecore.evas.SoftwareX11_16(w=800, h=480)
-    else:
-        ee = ecore.evas.SoftwareX11(w=800, h=480)
+# Parse command line
+from optparse import OptionParser
 
+def parse_geometry(option, opt, value, parser):
+    try:
+        w, h = value.split("x")
+        w = int(w)
+        h = int(h)
+    except Exception, e:
+        raise optparse.OptionValueError("Invalid format for %s" % option)
+    parser.values.geometry = (w, h)
+
+usage = "usage: %prog [options]"
+op = OptionParser(usage=usage)
+op.add_option("-e", "--engine", type="choice",
+              choices=("x11", "x11-16"), default="x11-16",
+              help=("which display engine to use (x11, x11-16), "
+                    "default=%default"))
+op.add_option("-n", "--no-fullscreen", action="store_true",
+              help="do not launch in fullscreen")
+op.add_option("-g", "--geometry", type="string", metavar="WxH",
+              action="callback", callback=parse_geometry,
+              default=(800, 480),
+              help="use given window geometry")
+op.add_option("-f", "--fps", type="int", default=50,
+              help="frames per second to use, default=%default")
+
+
+# Handle options and create output window
+options, args = op.parse_args()
+if options.engine == "x11":
+    f = ecore.evas.SoftwareX11
+elif options.engine == "x11-16":
+    if ecore.evas.engine_type_supported_get("software_x11_16"):
+        f = ecore.evas.SoftwareX11_16
+    else:
+        print "warning: x11-16 is not supported, fallback to x11"
+        f = ecore.evas.SoftwareX11
+
+w, h = options.geometry
+ee = f(w=w, h=h)
+ee.fullscreen = not options.no_fullscreen
+edje.frametime_set(1.0 / options.fps)
+
+
+# Load and setup UI
+ee.title = "Edje App Launcher Demo"
+ee.name_class = ("Edje_App_Launcher_Demo", "Edje_App_Launcher_Demo")
 canvas = ee.evas
 edje_file = os.path.join(os.path.dirname(sys.argv[0]), "01-app_launcher.edj")
 
@@ -26,34 +66,37 @@ try:
 except Exception, e: # should be EdjeLoadError, but it's wrong on python2.5
     raise SystemExit("Failed to load Edje file: %s" % edje_file)
 
+# resize edje to fit our window, show and remeber it for later use
 edje_obj.size = canvas.size
 edje_obj.show()
 ee.data["edje"] = edje_obj
 
+
+# Setup callbacks for resize, keydown and selected item
 def resize_cb(ee):
     r = ee.evas.rect
     ee.data["edje"].size = r.size
+
 ee.callback_resize = resize_cb
 
-def key_down_cb(bg, event):
+
+def key_down_cb(bg, event, ee):
     k = event.key
     if k == "Escape":
         ecore.main_loop_quit()
-edje_obj.on_key_down_add(key_down_cb)
+    if k in ("F6", "f"):
+        ee.fullscreen = not ee.fullscreen
 
-parts = ("web_browser", "audio_player", "image_viewer", "video_player")
-def icon_clicked_cb(edje_obj, emission, source, part):
-    if edje_obj.part_state_get(part) == ("default", 0.0):
-        edje_obj.signal_emit("select_" + part, "")
-        for p in parts:
-            if p != part:
-                edje_obj.signal_emit("unselect_" + p, "")
+edje_obj.on_key_down_add(key_down_cb, ee)
 
-for p in parts:
-    edje_obj.signal_callback_add("mouse,clicked,1", p + "_area",
-                                 icon_clicked_cb, p)
 
+def icon_selected(edje_obj, signal, source):
+    print "icon_selected", source
+
+edje_obj.signal_callback_add("selected", "*", icon_selected)
+
+
+# Give focus to object, show window and enter event loop
 edje_obj.focus = True
-ee.fullscreen = "-nofs" not in sys.argv
 ee.show()
 ecore.main_loop_begin()
