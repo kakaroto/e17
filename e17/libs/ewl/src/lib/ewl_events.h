d8f9a45ca7345752a06720c6694d5558a8581bac
pfritz <pfritz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-06-28 23:05:48 +0000

    add cast macros for the events
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@30485 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/lib/ewl_events.h
+++ e17/libs/ewl/src/lib/ewl_events.h
@@ -10,6 +10,12 @@
  */
 
 /**
+ * @def EWL_EVENT_WINDOW_EXPOSE(e)
+ * Typedcasts a pointer to an Ewl_Event_Window_Expose pointer
+ */
+#define EWL_EVENT_WINDOW_EXPOSE(e) ((Ewl_Event_Window_Expose *)(e))
+
+/**
  * The Ewl_Event_Window_Expose structure
  */
 typedef struct Ewl_Event_Window_Expose Ewl_Event_Window_Expose;
@@ -26,6 +32,13 @@ struct Ewl_Event_Window_Expose
 };
 
 /**
+ * @def EWL_EVENT_KEY(e)
+ * Typedcasts a pointer to an Ewl_Event_Key pointer
+ */
+#define EWL_EVENT_KEY(e) ((Ewl_Event_Key *)(e))
+
+
+/**
  * The Ewl_Event_Key structure
  */
 typedef struct Ewl_Event_Key Ewl_Event_Key;
@@ -38,6 +51,12 @@ struct Ewl_Event_Key
 	unsigned int modifiers;	/**< Key modifiers that were pressed */
 	char *keyname;		/**< Name of the key that was pressed */
 };
+
+/**
+ * @def EWL_EVENT_KEY_DOWN(e)
+ * Typedcasts a pointer to an Ewl_Event_Key_Down pointer
+ */
+#define EWL_EVENT_KEY_DOWN(e) ((Ewl_Event_Key_Down *)(e))
 
 /**
  *  The Ewl_Event_Window_Key_Down structure 
@@ -53,6 +72,12 @@ struct Ewl_Event_Key_Down
 };
 
 /**
+ * @def EWL_EVENT_KEY_UP(e)
+ * Typedcasts a pointer to an Ewl_Event_Key_Up pointer
+ */
+#define EWL_EVENT_KEY_DOWN(e) ((Ewl_Event_Key_Up *)(e))
+
+/**
  * The Ewl_Event_Key_Up structure
  */
 typedef struct Ewl_Event_Key_Up Ewl_Event_Key_Up;
@@ -64,6 +89,12 @@ struct Ewl_Event_Key_Up
 {
 	Ewl_Event_Key base;	/**< Basic key information */
 };
+
+/**
+ * @def EWL_EVENT_MOUSE(e)
+ * Typedcasts a pointer to an Ewl_Event_Mouse pointer
+ */
+#define EWL_EVENT_MOUSE(e) ((Ewl_Event_Mouse *)(e))
 
 /**
  * The Ewl_Event_Mouse structure
@@ -81,6 +112,12 @@ struct Ewl_Event_Mouse
 };
 
 /**
+ * @def EWL_EVENT_MOUSE_DOWN(e)
+ * Typedcasts a pointer to an Ewl_Event_Mouse_Down pointer
+ */
+#define EWL_EVENT_MOUSE_DOWN(e) ((Ewl_Event_Mouse_Down *)(e))
+
+/**
  * The Ewl_Event_Mouse_Down structure
  */
 typedef struct Ewl_Event_Mouse_Down Ewl_Event_Mouse_Down;
@@ -94,6 +131,12 @@ struct Ewl_Event_Mouse_Down
 	int button;		/**< The mouse button that was released */
 	int clicks;		/**< Number of consecutive clicks */
 };
+
+/**
+ * @def EWL_EVENT_MOUSE_UP(e)
+ * Typedcasts a pointer to an Ewl_Event_Mouse_Up pointer
+ */
+#define EWL_EVENT_MOUSE_UP(e) ((Ewl_Event_Mouse_Up *)(e))
 
 /**
  * The Ewl_Event_Mouse_Up structure
@@ -110,6 +153,12 @@ struct Ewl_Event_Mouse_Up
 };
 
 /**
+ * @def EWL_EVENT_MOUSE_MOVE(e)
+ * Typedcasts a pointer to an Ewl_Event_Mouse_Move pointer
+ */
+#define EWL_EVENT_MOUSE_MOVE(e) ((Ewl_Event_Mouse_Move *)(e))
+
+/**
  * The Ewl_Event_Mouse_Move structure
  */
 typedef struct Ewl_Event_Mouse_Move Ewl_Event_Mouse_Move;
@@ -121,6 +170,12 @@ struct Ewl_Event_Mouse_Move
 {
 	Ewl_Event_Mouse base;	/**< Base mouse information */
 };
+
+/**
+ * @def EWL_EVENT_MOUSE_IN(e)
+ * Typedcasts a pointer to an Ewl_Event_Mouse_In pointer
+ */
+#define EWL_EVENT_MOUSE_IN(e) ((Ewl_Event_Mouse_In *)(e))
 
 /**
  * The Ewl_Event_Mouse_In structure
@@ -136,6 +191,12 @@ struct Ewl_Event_Mouse_In
 };
 
 /**
+ * @def EWL_EVENT_MOUSE_OUT(e)
+ * Typedcasts a pointer to an Ewl_Event_Mouse_Out pointer
+ */
+#define EWL_EVENT_MOUSE_OUT(e) ((Ewl_Event_Mouse_Out *)(e))
+
+/**
  * The Ewl_Event_Mouse_Out structure
  */
 typedef struct Ewl_Event_Mouse_Out Ewl_Event_Mouse_Out;
@@ -147,6 +208,12 @@ struct Ewl_Event_Mouse_Out
 {
 	Ewl_Event_Mouse base; /**< Base mouse information */
 };
+
+/**
+ * @def EWL_EVENT_MOUSE_WHEEL(e)
+ * Typedcasts a pointer to an Ewl_Event_Mouse_Wheel pointer
+ */
+#define EWL_EVENT_MOUSE_WHEEL(e) ((Ewl_Event_Mouse_Wheel *)(e))
 
 /**
  * The Ewl_Event_Mouse_Wheel structure
@@ -164,6 +231,12 @@ struct Ewl_Event_Mouse_Wheel
 };
 
 /**
+ * @def EWL_DND_TYPES(t)
+ * Typedcasts a pointer to an Ewl_Dnd_Types pointer
+ */
+#define EWL_DND_TYPES(t) ((Ewl_Dnd_Types *)(t))
+
+/**
  * The Ewl_Dnd_Types type 
  */
 typedef struct Ewl_Dnd_Types Ewl_Dnd_Types;
@@ -176,6 +249,12 @@ struct Ewl_Dnd_Types
 	int num_types;	/**< The number of DND types */
 	char** types;	/**< The list of DND types */
 };
+
+/**
+ * @def EWL_EVENT_DND_POSITION(e)
+ * Typedcasts a pointer to an Ewl_Event_Dnd_Position pointer
+ */
+#define EWL_EVENT_DND_POSITION(e) ((Ewl_Event_Dnd_Position *)(e))
 
 /**
  * The Ewl_Event_Dnd_Position type
@@ -192,6 +271,12 @@ struct Ewl_Event_Dnd_Position
 };
 
 /**
+ * @def EWL_EVENT_DND_DROP(e)
+ * Typedcasts a pointer to an Ewl_Event_Dnd_Drop pointer
+ */
+#define EWL_EVENT_DND_DROP(e) ((Ewl_Event_Dnd_Drop *)(e))
+
+/**
  * The Ewl_Event_Dnd_Drop type
  */
 typedef struct Ewl_Event_Dnd_Drop Ewl_Event_Dnd_Drop;
@@ -205,6 +290,12 @@ struct Ewl_Event_Dnd_Drop
 	int y; /**< Y coordinate the mouse moved to */
 	void* data; /**< Data from drop source 	    */
 };
+
+/**
+ * @def EWL_EVENT_DND_DATA_RECEIVED(e)
+ * Typedcasts a pointer to an Ewl_Event_Dnd_Data_Received pointer
+ */
+#define EWL_EVENT_DND_DATA_RECEIVED(e) ((Ewl_Event_Dnd_Data_Received *)(e))
 
 /**
  * The Ewl_Event_Dnd_Data_Received type
@@ -223,6 +314,12 @@ struct Ewl_Event_Dnd_Data_Received
 };
 
 /**
+ * @def EWL_EVENT_DND_DATA_REQUEST(e)
+ * Typedcasts a pointer to an Ewl_Event_Dnd_Data_Request pointer
+ */
+#define EWL_EVENT_DND_DATA_REQUEST(e) ((Ewl_Event_Dnd_Data_Request *)(e))
+
+/**
  * The Ewl_Event_Dnd_Data_Requested type
  */
 typedef struct Ewl_Event_Dnd_Data_Request Ewl_Event_Dnd_Data_Request;
@@ -235,6 +332,12 @@ struct Ewl_Event_Dnd_Data_Request
 	void *handle; /**< Engine specific handle for responding to request */
 	char *type;   /**< Type of data requested */
 };
+
+/**
+ * @def EWL_EVENT_ACTION_RESPONSE(e)
+ * Typedcasts a pointer to an Ewl_Event_Action_Response pointer
+ */
+#define EWL_EVENT_ACTION_RESPONSE(e) ((Ewl_Event_Action_Response*)(e))
 
 /**
  * The Ewl_Event_Action_Response type
