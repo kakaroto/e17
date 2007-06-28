d887f2f2f392ce2eff76deb5628de2767dc7420c
ningerso <ningerso@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-06-28 04:19:58 +0000

    Add header for 16 bit software x11 engine.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@30481 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/libs/ewl/src/engines/evas_software_16_x11/Ewl_Engine_Evas_Software_16_X11.h
@@ -0,0 +1,20 @@
+/* vim: set sw=8 ts=8 sts=8 noexpandtab: */
+#ifndef EWL_ENGINE_EVAS_SOFTWARE_16_X11_H
+#define EWL_ENGINE_EVAS_SOFTWARE_16_X11_H
+
+#include "ewl_base.h"
+#include <Ecore_X.h>
+#include <Evas.h>
+#include <Evas_Engine_Software_16_X11.h>
+
+#define EWL_ENGINE_EVAS_SOFTWARE_16_X11(engine) \
+		((Ewl_Engine_Evas_Software_16_X11 *)engine)
+
+typedef struct Ewl_Engine_Evas_Software_16_X11 Ewl_Engine_Evas_Software_16_X11;
+struct Ewl_Engine_Evas_Software_16_X11
+{
+	Ewl_Engine engine;
+};
+
+#endif
+
