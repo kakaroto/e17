67c073ef4d481ad0fee4452a0994b79d3655e720
rephorm <rephorm@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-07-11 00:48:49 +0000

    implement more of the daemon side
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@30738 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/e_dbus/src/lib/notification/E_Notification_Daemon.h
+++ e17/proto/e_dbus/src/lib/notification/E_Notification_Daemon.h
@@ -1,40 +1,56 @@
 #ifndef E_NOTIFICATION_DAEMON_H
 #define E_NOTIFICATION_DAEMON_H
 
-/* daemon events */
-extern int E_NOTIFICATION_EVENT_NOTIFY;
-extern int E_NOTIFICATION_EVENT_CLOSE;
+#define E_NOTIFICATION_DAEMON_VERSION "0.9"
+
+#include <E_Notify.h>
 
 
-typedef struct E_Notification_Context E_Notification_Daemon;
+typedef struct E_Notification_Daemon E_Notification_Daemon;
 
-/* daemon events */
-typedef struct E_Notification_Event_Daemon E_Notification_Event_Daemon;
-typedef struct E_Notification_Event_Daemon E_Notification_Event_Notify;
-typedef struct E_Notification_Event_Daemon E_Notification_Event_Close;
+/* daemon callbacks */
+typedef int (*E_Notification_Daemon_Callback_Notify) (E_Notification_Daemon *daemon, E_Notification *notification);
+typedef void (*E_Notification_Daemon_Callback_Close_Notification) (E_Notification_Daemon *daemon, int notification_id);
 
 /* gui */
 typedef struct E_Notification_View E_Notification_View;
 
-struct E_Notification_Context
+struct E_Notification_Daemon
 {
   E_DBus_Connection *conn;
-};
+  E_DBus_Interface *iface;
+  E_DBus_Object *obj;
 
-/* daemon event data */
-struct E_Notification_Event_Daemon
-{
-  E_Notification *notification;
+  char *name;
+  char *vendor;
+
+  struct 
+  {
+    E_Notification_Daemon_Callback_Notify notify;
+    E_Notification_Daemon_Callback_Close_Notification close_notification;
+  } func;
+  void *data;
+
+  int state;
 };
 
 /* daemon */
-E_Notification_Daemon *e_notification_daemon_add(DBusConnection *conn);
+E_Notification_Daemon *e_notification_daemon_add(const char *name, const char *vendor);
 void e_notification_daemon_free(E_Notification_Daemon *d);
 void e_notification_daemon_close(E_Notification_Daemon *d,
                                  E_Notification *n, unsigned int reason);
 void e_notification_daemon_action_invoke(E_Notification_Daemon *d,
                                  E_Notification *n, const char *action_id);
 
+void  e_notification_daemon_data_set(E_Notification_Daemon *daemon, void *data);
+void *e_notification_daemon_data_get(E_Notification_Daemon *daemon);
+
+void e_notification_daemon_callback_notify_set(E_Notification_Daemon *daemon, E_Notification_Daemon_Callback_Notify func);
+void e_notification_daemon_callback_close_notification_set(E_Notification_Daemon *daemon, E_Notification_Daemon_Callback_Close_Notification func);
+
+
+void e_notification_daemon_signal_notification_closed(E_Notification_Daemon *daemon, unsigned int id, E_Notification_Closed_Reason reason);
+void e_notification_daemon_signal_action_invoked(E_Notification_Daemon *daemon, unsigned int notification_id, const char *action_id);
 
 /***** gui *****/
 E_Notification_View *e_notification_view_add(E_Notification_Daemon *d, E_Notification *n);
