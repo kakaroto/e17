37d19f36df4796e96b444b3fb08c5f19eb9d0900
rephorm <rephorm@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-07-11 00:48:59 +0000

    add daemon test and implement more of client test
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@30739 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/e_dbus/src/bin/notify.c
+++ e17/proto/e_dbus/src/bin/notify.c
@@ -16,19 +16,65 @@ cb_sent(void *data, void *ret, DBusError *err)
   {
     printf("Error: %s\n", err->message);
   }
-  ecore_main_loop_quit();
+
+  free(notify);
+}
+
+int
+cb_timer(void *data)
+{
+  E_Notification *n;
+  char buf[1024];
+  static int num = 0;
+  static const char *icons[] = {
+    "xterm",
+    "firefox",
+    "gvim"
+  };
+
+  snprintf(buf, sizeof(buf), "<i>%s</i> says <b>Hello</b> #%d", icons[num%3], num / 3); 
+  n = e_notification_full_new(icons[num%3], 0, icons[num%3], "Summary", buf, -1);
+  e_notification_send(n, cb_sent, NULL);
+  e_notification_unref(n);
+  num++;
+
+  return 1;
+}
+
+void
+cb_action_invoked(void *data, int type, void *event)
+{
+  E_Notification_Event_Action_Invoked *ev;
+
+  ev = event;
+  printf("Action (%d): %s\n", ev->notification_id, ev->action_id);
+  free(ev);
+}
+
+void
+cb_note_closed(void *data, int type, void *event)
+{
+  E_Notification_Event_Notification_Closed *ev;
+  static const char *reasons[] = {
+    "Expired",
+    "Dismissed",
+    "Requested",
+    "Undefined"
+  };
+
+  ev = event;
+  printf("Note %d closed: %s\n", ev->notification_id, reasons[ev->reason]);
+  free(ev);
 }
 
 int
 main(int argc, char **argv)
 {
   int ret = 0;
-  E_Notification *n;
   ecore_init();
   if (e_notification_init())
   {
-    n = e_notification_full_new("Elicit", 0, "elicit", "Summary", "The <b>body</b>", -1);
-    e_notification_send(n, cb_sent, NULL);
+    ecore_timer_add(1, cb_timer, NULL);
     ecore_main_loop_begin();
     e_notification_shutdown();
   }
