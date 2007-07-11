181cce3273a896d4a8c2fbdee9c5e8af4cd6c1d5
rephorm <rephorm@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-07-11 00:46:29 +0000

    update api
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@30727 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/e_dbus/src/bin/notify.c
+++ e17/proto/e_dbus/src/bin/notify.c
@@ -8,37 +8,26 @@ cb_sent(void *data, void *ret)
 {
   E_Notification_Return_Notify *notify;
   notify = ret;
-  printf("id: %d\n", notify->notification_id);
+  if (notify) {
+    printf("id: %d\n", notify->notification_id);
+  }
   ecore_main_loop_quit();
 }
 
 int
 main(int argc, char **argv)
 {
-  E_DBus_Connection *conn;
   int ret = 0;
+  E_Notification *n;
   ecore_init();
-  e_dbus_init();
-
-  conn = e_dbus_bus_get(DBUS_BUS_SESSION);
-  if (conn)
+  if (e_notification_init())
   {
-    E_Notification *n;
-    E_Notification_Context *cl;
-
-    cl = malloc(sizeof(E_Notification_Context));
-    cl->conn = conn;
     n = e_notification_full_new("Elicit", 0, "elicit", "Summary", "The <b>body</b>", -1);
-    e_notification_send(cl, n, cb_sent, NULL);
+    e_notification_send(n, cb_sent, NULL);
     ecore_main_loop_begin();
-  }
-  else
-  {
-    printf("Error: could not connect to session bus.\n");
-    ret = 1;
+    e_notification_shutdown();
   }
 
-  e_dbus_shutdown();
   ecore_shutdown();
   return ret;
 }
