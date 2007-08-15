71f01fcd8a383ce3ee105e3808fddf87993057fd
barbieri <barbieri@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-08-15 17:19:59 +0000

    Utilities to easy callback connections
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@31330 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/proto/python-efl/python-ecore/ecore/evas/utils.py
@@ -0,0 +1,57 @@
+__callbacks = (
+    "resize",
+    "move",
+    "show",
+    "hide",
+    "delete_request",
+    "destroy",
+    "focus_in",
+    "focus_out",
+    "sticky",
+    "unsticky",
+    "mouse_in",
+    "mouse_out",
+    "pre_render",
+    "post_render",
+    )
+
+def __get_callback(observer, name):
+    try:
+        attr = getattr(observer, "cb_on_%s" % name)
+        if callable(attr):
+            return attr
+    except AttributeError, e:
+        return None
+
+def connect_observer(ecore_evas, observer):
+    """Connect methods from observer to Ecore_Evas callbacks.
+
+    Observer must have methods with name scheme: cb_on_<callback>,
+    examples:
+     * cb_on_resize
+     * cb_on_move
+     * cb_on_show
+    """
+    for cb_name in __callbacks:
+        cb = __get_callback(observer, cb_name)
+        if cb:
+            setter = getattr(ecore_evas, "callback_%s_set" % cb_name)
+            setter(cb)
+
+
+def connect_callbacks_by_name(ecore_evas, mapping):
+    """Connect callbacks specified in mapping to Ecore_Evas callbacks.
+
+    Mapping must be a dict or a list of tuples with callback name and
+    desired function, example:
+     * mapping = (("resize", my_on_resize), ("show", my_on_show))
+     * mapping = {"resize": my_on_resize, "show": my_on_show}
+    """
+    if isinstance(mapping, dict):
+        mapping = mapping.iteritems()
+    for name, func in mapping:
+        try:
+            setter = getattr(ecore_evas, "callback_%s_set" % name)
+        except AttributeError, e:
+            raise ValueError("invalid callback name: %s" % name)
+        setter(func)
