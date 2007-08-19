4e3c98178b904039fa490e55b0df159aeb1a3df9
kwo <kwo@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-08-19 19:36:24 +0000

    Remove unmaintained debian stuff.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@31409 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e16/e/debian/menumangler.pl
+++
@@ -1,4 +0,0 @@
-$R1 = qq{BEGIN_NEW_FILE_MENU("DEBIAN_MENU","ROOT","/var/lib/enlightenment/debian.menu")};
-$R2 = qq{ADD_MENU_SUBMENU_TEXT_ITEM("Debian","DEBIAN_MENU")};
-s/(^BEGIN_NEW_FILE_MENU..APPS_SUBMENU.*$)/$1\nEND_MENU\n$R1/;
-s/(^ADD_MENU_SUBMENU_TEXT_ITEM..User Menus.*$)/$1\n$R2/;
