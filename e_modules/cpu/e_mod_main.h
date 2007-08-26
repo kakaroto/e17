549ab0e10dfb6bcc6be51f24ca694e501e886319
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-08-26 15:10:16 +0000

    Revert recent graph-ness. There is a much better way todo this type of stuff.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@31527 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/cpu/e_mod_main.h
+++ e_modules/cpu/e_mod_main.h
@@ -16,9 +16,7 @@ struct _Config
 struct _Config_Item 
 {
    const char *id;
-   double poll_time;
-   int show_popup;
-   int max_points;
+   double interval;
 };
 
 EAPI extern E_Module_Api e_modapi;
@@ -28,8 +26,7 @@ EAPI int   e_modapi_shutdown (E_Module *m);
 EAPI int   e_modapi_save     (E_Module *m);
 EAPI int   e_modapi_about    (E_Module *m);
 
-void _cpu_config_updated     (const char *id);
-void _config_cpu_module      (Config_Item *ci, int max);
+//void _config_cpu_module      (Config_Item *ci);
 
 extern Config *cpu_conf;
 
