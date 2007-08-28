79948a72f5d26710ee4d2adca802edba742f0e8a
doursse <doursse@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-08-28 17:54:57 +0000

    replace the build shell script by a Makefile
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@31565 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- misc/eflame/build.sh
+++
@@ -1,4 +0,0 @@
-USE_ECORE_EVAS=`ecore-config --libs | grep evas`
-if [ -n "USE_ECORE_EVAS" ] ; then gcc -O2 -Wall `ecore-config --cflags --libs` eflame.c -o eflame;
-else echo Error: you must compile Ecore with Evas support;
-fi
