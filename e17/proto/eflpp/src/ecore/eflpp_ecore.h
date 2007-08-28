51b0b41aa01f001179c5457bba0b883950369605
andreas <andreas@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-08-28 22:02:00 +0000

    Moved EcoreEvasWindow stuff to extra files
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@31571 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/proto/eflpp/src/ecore/eflpp_ecore.h
+++ e17/proto/eflpp/src/ecore/eflpp_ecore.h
@@ -7,6 +7,7 @@
 
 /* EFL++ */
 #include <eflpp_evas.h>
+#include <eflpp_ecore_window.h>
 
 /* EFL */
 #include <Evas.h>
@@ -103,169 +104,6 @@ class EcoreApplication : public Trackable
 };
 
 //===============================================================================================
-// EcoreEvasWindow
-//===============================================================================================
-
-// TODO: Test for supported backends?
-class EcoreEvasWindow : public Trackable
-{
-  public:
-    enum Event { Resize, Move, Show, Hide, DeleteRequest, Destroy,
-                    FocusIn, FocusOut, MouseIn, MouseOut, PreRender, PostRender };
-
-  public:
-    virtual ~EcoreEvasWindow();
-
-    EvasCanvas* canvas() const { return _canvas; };
-
-    /* Events */
-    void setEventEnabled( Event, bool );
-    virtual void resizeEvent();
-    virtual void moveEvent();
-    virtual void showEvent();
-    virtual void hideEvent();
-    virtual void deleteRequestEvent();
-    virtual void destroyEvent();
-    virtual void focusInEvent();
-    virtual void focusOutEvent();
-    virtual void mouseInEvent();
-    virtual void mouseOutEvent();
-    virtual void preRenderEvent();
-    virtual void postRenderEvent();
-
-   /**
-    * @returns a boolean specifying whether the window should be closed upon a delete request or not.
-    * The default implementation returns true.
-    **/
-    virtual bool canClose() const;
-
-    /* Window Management */
-    void move( const Point& );
-    void resize( const Size& );
-    void setGeometry( const Rect& );
-    Rect geometry() const;
-
-    void setRotation( int );
-    int rotation() const;
-    void setShaped( bool );
-    bool isShaped() const;
-
-    void show();
-    void hide();
-    void raise();
-    void lower();
-
-    void setTitle( const char* );
-    const char* title() const;
-
-    // void        ecore_evas_name_class_set(Ecore_Evas *ee, const char *n, const char *c);
-    // void        ecore_evas_name_class_get(Ecore_Evas *ee, const char **n, const char **c);
-
-    void setMinimumSize( const Size& );
-    Size minimumSize() const;
-    void setMaximumSize( const Size& );
-    Size maximumSize() const;
-    void setBaseSize( const Size& );
-    Size baseSize() const;
-    void setStepSize( const Size& );
-    Size stepSize() const;
-
-    void setCursor( const char* file, int layer, int hot_x, int hot_y );
-    // void        ecore_evas_cursor_get(Ecore_Evas *ee, char **file, int *layer, int *hot_x, int *hot_y);
-
-    void setLayer( int layer );
-    int layer() const;
-
-    void setFocus( int on );
-    int hasFocus() const;
-
-    void setIconified( int on );
-    int isIconified() const;
-
-    void setBorderless( int on );
-    int isBorderless() const;
-
-    void setOverride( int on );
-    int isOverride() const;
-
-    void setMaximized( int on );
-    int isMaximized() const;
-
-    void setFullscreen( int on );
-    int isFullscreen() const;
-
-    void setAvoidDamage( int on );
-    int isAvoidDamage() const;
-
-    void setWithdrawn( int on );
-    int isWithdrawn() const;
-
-    void setSticky( int on );
-    int isSticky() const;
-
-  protected:
-    EvasCanvas* _canvas;
-    Ecore_Evas* _ee;
-
-    EcoreEvasWindow();
-    static EcoreEvasWindow* objectLink( Ecore_Evas* );
-
-  private:
-    bool operator=( const EcoreEvasWindow& );
-    bool operator==( const EcoreEvasWindow& );
-};
-
-class EcoreEvasWindowSoftwareX11 : public EcoreEvasWindow
-{
-  public:
-    EcoreEvasWindowSoftwareX11( int width, int height, const char* display = 0 );
-    virtual ~EcoreEvasWindowSoftwareX11();
-  
-  private:
-    EcoreEvasWindowSoftwareX11();
-    bool operator=( const EcoreEvasWindowSoftwareX11& );
-    bool operator==( const EcoreEvasWindowSoftwareX11& );
-};
-
-class EcoreEvasWindowGLX11 : public EcoreEvasWindow
-{
-  public:
-    EcoreEvasWindowGLX11( int width, int height, const char* display = 0 );
-    virtual ~EcoreEvasWindowGLX11();
-  
-  private:
-    EcoreEvasWindowGLX11();
-    bool operator=( const EcoreEvasWindowGLX11& );
-    bool operator==( const EcoreEvasWindowGLX11& );
-};
-
-class EcoreEvasWindowXRenderX11 : public EcoreEvasWindow
-{
-  public:
-    EcoreEvasWindowXRenderX11( int width, int height, const char* display = 0 );
-    virtual ~EcoreEvasWindowXRenderX11();
-  
-  private:
-    EcoreEvasWindowXRenderX11();
-    bool operator=( const EcoreEvasWindowXRenderX11& );
-    bool operator==( const EcoreEvasWindowXRenderX11& );
-};
-
-class EcoreEvasWindowFB : public EcoreEvasWindow
-{
-  public:
-    EcoreEvasWindowFB( int width, int height, const char* display = 0, int rotation = 0 );
-    virtual ~EcoreEvasWindowFB();
-
-    // TODO: Test for supported backends?
-  
-  private:
-    EcoreEvasWindowFB();
-    bool operator=( const EcoreEvasWindowFB& );
-    bool operator==( const EcoreEvasWindowFB& );
-};
-
-//===============================================================================================
 // EcoreAnimator
 //===============================================================================================
 
@@ -313,5 +151,6 @@ class EcoreTimer
     static int __dispatcher( void* data );
 };
 
-}
-#endif
+} // end namespace efl
+
+#endif // ECORE_BASE_H
