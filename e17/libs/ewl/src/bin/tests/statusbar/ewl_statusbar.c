c3f8329e3f85ff251f84497b09d70f089258a834
dj2 <dj2@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-09-11 01:57:48 +0000

    - put the buttons into an hbox - set a default value into the statusbar
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@31683 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/bin/tests/statusbar/ewl_statusbar.c
+++ e17/libs/ewl/src/bin/tests/statusbar/ewl_statusbar.c
@@ -25,19 +25,25 @@ test_info(Ewl_Test *test)
 static int
 create_test(Ewl_Container *box)
 {
-	Ewl_Widget *statusbar = NULL, *button = NULL;
+	Ewl_Widget *statusbar = NULL, *button = NULL, *hbox = NULL;
 
 	statusbar = ewl_statusbar_new();
 	ewl_container_child_append(EWL_CONTAINER(box), statusbar);
 	ewl_statusbar_left_hide(EWL_STATUSBAR(statusbar));
 	ewl_widget_show(statusbar);
 
+	button_push_cb(NULL, NULL, statusbar);
+
+	hbox = ewl_hbox_new();
+	ewl_container_child_append(EWL_CONTAINER(box), hbox);
+	ewl_widget_show(hbox);
+
 	button = ewl_button_new();
 	ewl_button_label_set(EWL_BUTTON(button), "push");
 	ewl_callback_append(button, EWL_CALLBACK_CLICKED, button_push_cb,
 								statusbar);
 	ewl_object_fill_policy_set(EWL_OBJECT(button), EWL_FLAG_FILL_SHRINK);
-	ewl_container_child_append(EWL_CONTAINER(box), button);
+	ewl_container_child_append(EWL_CONTAINER(hbox), button);
 	ewl_widget_show(button);
 
 	button = ewl_button_new();
@@ -45,7 +51,7 @@ create_test(Ewl_Container *box)
 	ewl_callback_append(button, EWL_CALLBACK_CLICKED, button_pop_cb,
 								statusbar);
 	ewl_object_fill_policy_set(EWL_OBJECT(button), EWL_FLAG_FILL_SHRINK);
-	ewl_container_child_append(EWL_CONTAINER(box), button);
+	ewl_container_child_append(EWL_CONTAINER(hbox), button);
 	ewl_widget_show(button);
 
 	return 1;
