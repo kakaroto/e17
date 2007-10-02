15927510cd76fb1fdf5f6da44e7cd7aee29e7c52
barbieri <barbieri@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-10-02 01:18:31 +0000

    Scripts to help build system.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@31925 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/proto/python-efl/build-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PREFIX=$1
+[ -z "$PREFIX" ] && PREFIX="$HOME/usr"
+
+die() {
+    echo "ERROR: $@"
+    exit 1
+}
+
+echo "Install to $PREFIX"
+CWD=$PWD
+for m in evas ecore edje emotion e_dbus; do
+    cd $CWD/python-$m || die "cd python-$m"
+    ./build.sh --force $PREFIX || die "failed to build python-$m"
+done
