6b22e08389a516f7a6d5e02757b00b835be2193a
doursse <doursse@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-11-04 09:59:42 +0000

    autostuff improvements:
     * use non deprecated version of AC_INIT and AM_INIT_AUTOMAKE
       and check the required minimal versions.
     * add bzipped distribution archive
     * forbid libtool to check fortran and c++ compilers
     * compute libtool versioning from the version of the package
     * pass the directories based on ${prefix} to the preoprocessor
       with the -D option
     * replace INCLUDES, wich is deprecated since 2001 by AM_CPPFLAGS
     * remove useless -L flags in *_la_LDFLAGS
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@32344 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/e_dbus/autogen.sh
+++ e17/libs/e_dbus/autogen.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 
-echo "Running autoheader..."; autoheader || exit 1
+rm -rf autom4te.cache
+rm -f aclocal.m4 ltmain.sh
+
 echo "Running aclocal..."; aclocal $ACLOCAL_FLAGS || exit 1
+echo "Running autoheader..."; autoheader || exit 1
 echo "Running libtoolize..."; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
 echo "Running automake..."; automake --gnu --add-missing --copy || exit 1
 echo "Running autoconf..."; autoconf || exit 1
