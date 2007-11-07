1e610f24e2f3f27fdfa94ddf7d7992cad73d707f
doursse <doursse@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-11-07 18:03:50 +0000

    add xcb engine
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@32419 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e17/libs/ewl/src/engines/evas_software_xcb/Ewl_Engine_Evas_Software_Xcb.h
@@ -0,0 +1,20 @@
+/* vim: set sw=8 ts=8 sts=8 noexpandtab: */
+#ifndef EWL_ENGINE_EVAS_SOFTWARE_XCB_H
+#define EWL_ENGINE_EVAS_SOFTWARE_XCB_H
+
+#include "ewl_base.h"
+#include <Ecore_X.h>
+#include <Evas.h>
+#include <Evas_Engine_Software_Xcb.h>
+
+#define EWL_ENGINE_EVAS_SOFTWARE_XCB(engine) \
+		((Ewl_Engine_Evas_Software_Xcb *)engine)
+
+typedef struct Ewl_Engine_Evas_Software_Xcb Ewl_Engine_Evas_Software_Xcb;
+struct Ewl_Engine_Evas_Software_Xcb
+{
+	Ewl_Engine engine;
+};
+
+#endif
+
---
+++ e17/libs/ewl/src/engines/xcb/Ewl_Engine_Xcb.h
@@ -0,0 +1,17 @@
+/* vim: set sw=8 ts=8 sts=8 noexpandtab: */
+#ifndef EWL_ENGINE_XCB_H
+#define EWL_ENGINE_XCB_H
+
+#include "ewl_base.h"
+#include <Ecore_X.h>
+#include <Ecore_X_Atoms.h>
+
+#define EWL_ENGINE_XCB(engine) ((Ewl_Engine_Xcb *)engine)
+typedef struct Ewl_Engine_Xcb Ewl_Engine_Xcb;
+struct Ewl_Engine_Xcb
+{
+	Ewl_Engine engine;
+};
+
+#endif
+
