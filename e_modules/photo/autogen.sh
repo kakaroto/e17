b7756c4a42641cddeb795bb43183072c2bb4942c
englebass <englebass@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-11-08 04:56:54 +0000

    needed for gettext
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@32482 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_modules/photo/autogen.sh
+++ e_modules/photo/autogen.sh
@@ -10,7 +10,7 @@ rm -Rf  aclocal.m4 autom4te.cache/ ltmain.sh config.log config.status config.cac
 	po/Makefile
 
 echo "Running autopoint..." ; autopoint -f || :
-echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
+echo "Running aclocal..." ; aclocal -I m4 $ACLOCAL_FLAGS || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
 echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
