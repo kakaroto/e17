f761226084ff2da2f25fc91673a0d3a4e26467fc
dj2 <dj2@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-11-09 05:27:42 +0000

    - allow a marker to truncate front page news and add a more link. should   keep long posts from making the front page look bad
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@32514 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- web/www/p/news/latestnews.php
+++ web/www/p/news/latestnews.php
@@ -9,11 +9,22 @@ foreach($list as $new)
 {
    if (($new = $news->read_content($new)) !== NULL)
    {
-     echo "<p class='news-latest-title'><a href='p.php?p=news/show&l=".$news->lang."&news_id=$new[id]'>".$new['title']."</a><br />";
+     $n = $new['content'];
+     $link = "p.php?p=news/show&l=" . $news->lang . "&news_id=$new[id]";
+
+     $pos = strpos($n, '<!-- cut -->');
+     if ($pos != FALSE) 
+     {
+	     $before = substr($n, 0, $pos);
+	     $before .= "<div align='right' class='news-latest-date'><a href='$link'>more &raquo;</div>";
+     }
+     else $before = $n;
+
+     echo "<p class='news-latest-title'><a href='$link'>" . $new['title'] . "</a><br />";
      echo "<em class='news-latest-date'>" . $new['date'] . "</em></p>";
-     echo "<p class='news-latest-body'>".$new['content']."</p>\n";
+     echo "<p class='news-latest-body'>" . $before . "</p>\n";
    }
 }
-echo "<p align='right'><a href='p.php?p=news&l=".$news->lang."' title='News'>more ... </a></p>";
+echo "<p align='right'><a href='p.php?p=news&l=" . $news->lang . "' title='News'>Archives</a></p>";
 ?>
 
