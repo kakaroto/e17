2b3b5f8795ea1f74a4c83b5b67292dc9e53cd1f5
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-11-11 19:25:19 +0000

    Add example efm module. Doesn't do much and may still have buggers, use with caution. Packagers & users should really ignore this module for now, still needs more testing.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@32585 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ e_modules/efm_path/src/e_mod_main.c
@@ -0,0 +1,340 @@
+#include <e.h>
+#include "e_mod_main.h"
+
+typedef struct _Instance Instance;
+struct _Instance 
+{
+   E_Gadcon_Client *gcc;
+   Evas_Object *o_entry, *o_base, *o_loc;
+   E_Menu *menu;
+   E_Toolbar *tbar;
+   char *path;
+};
+
+/* local function protos */
+static E_Gadcon_Client *_gc_init          (E_Gadcon *gc, const char *name, 
+					   const char *id, const char *style);
+static void             _gc_shutdown      (E_Gadcon_Client *gcc);
+static void             _gc_orient        (E_Gadcon_Client *gcc);
+static char            *_gc_label         (void);
+static Evas_Object     *_gc_icon          (Evas *evas);
+static const char      *_gc_id_new        (void);
+static void             _cb_changed       (void *data, Evas_Object *obj, 
+					   void *event_info);
+static void             _cb_dir_changed   (void *data, Evas_Object *obj, 
+					   void *event_info);
+static void             _cb_dir_deleted   (void *data, Evas_Object *obj, 
+					   void *event_info);
+static void             _cb_files_deleted (void *data, Evas_Object *obj, 
+					   void *event_info);
+static void             _cb_selected      (void *data, Evas_Object *obj, 
+					   void *event_info);
+static void             _cb_sel_changed   (void *data, Evas_Object *obj, 
+					   void *event_info);
+static void             _cb_key_down      (void *data, Evas_Object *obj, 
+					   void *event_info);
+static void             _cb_mouse_down    (void *data, Evas *e, 
+					   Evas_Object *obj, void *event_info);
+static void             _cb_menu_post     (void *data, E_Menu *m);
+
+static Evas_List *instances = NULL;
+static E_Module *path_mod = NULL;
+
+static const E_Gadcon_Client_Class _gc_class = 
+{
+   GADCON_CLIENT_CLASS_VERSION, "efm_path", 
+     {_gc_init, _gc_shutdown, _gc_orient, _gc_label, _gc_icon, _gc_id_new, NULL},
+   E_GADCON_CLIENT_STYLE_PLAIN
+};
+
+static E_Gadcon_Client *
+_gc_init(E_Gadcon *gc, const char *name, const char *id, const char *style) 
+{
+   Instance *inst = NULL;
+   char buf[4096];
+
+   snprintf(buf, sizeof(buf), "%s/e-module-efm_path.edj", 
+	    e_module_dir_get(path_mod));
+
+   inst = E_NEW(Instance, 1);
+   if (!inst) return NULL;
+
+   inst->o_base = edje_object_add(gc->evas);
+   if (!e_theme_edje_object_set(inst->o_base, "base/theme/modules/efm_path", 
+				"modules/efm_path/main"))
+     edje_object_file_set(inst->o_base, buf, "modules/efm_path/main");
+   evas_object_event_callback_add(inst->o_base, EVAS_CALLBACK_MOUSE_DOWN, 
+				  _cb_mouse_down, inst);
+   evas_object_show(inst->o_base);
+
+   inst->o_loc = e_widget_label_add(gc->evas, _("Location:"));
+   edje_object_part_swallow(inst->o_base, "e.swallow.location", inst->o_loc);
+   evas_object_show(inst->o_loc);
+
+   inst->o_entry = e_widget_entry_add(gc->evas, &(inst->path), NULL, NULL, NULL);
+   e_widget_can_focus_set(inst->o_entry, 0);
+   edje_object_part_swallow(inst->o_base, "e.swallow.entry", inst->o_entry);
+   evas_object_show(inst->o_entry);
+
+   /* add hook to know when user changes entry */
+   evas_object_smart_callback_add(inst->o_entry, "key_down", 
+				  _cb_key_down, inst);
+
+   /* add the hooks to get signals from efm */
+   evas_object_smart_callback_add(inst->o_base, "changed", 
+				  _cb_changed, inst);
+   evas_object_smart_callback_add(inst->o_base, "dir_changed", 
+				  _cb_dir_changed, inst);
+   evas_object_smart_callback_add(inst->o_base, "dir_deleted", 
+				  _cb_dir_deleted, inst);
+   evas_object_smart_callback_add(inst->o_base, "files_deleted", 
+				  _cb_files_deleted, inst);
+   evas_object_smart_callback_add(inst->o_base, "selected", 
+				  _cb_selected, inst);
+   evas_object_smart_callback_add(inst->o_base, "selection_change", 
+				  _cb_sel_changed, inst);
+
+   inst->gcc = e_gadcon_client_new(gc, name, id, style, inst->o_base);
+   inst->gcc->data = inst;
+
+   e_gadcon_client_resizable_set(inst->gcc, 1);
+   instances = evas_list_append(instances, inst);
+   return inst->gcc;
+}
+
+static void 
+_gc_shutdown(E_Gadcon_Client *gcc) 
+{
+   Instance *inst = NULL;
+
+   inst = gcc->data;
+   if (!inst) return;
+   instances = evas_list_remove(instances, inst);
+   if (inst->o_loc) evas_object_del(inst->o_loc);
+   if (inst->o_entry) evas_object_del(inst->o_entry);
+   if (inst->o_base) evas_object_del(inst->o_base);
+   if (inst->path) free(inst->path);
+   E_FREE(inst);
+}
+
+static void 
+_gc_orient(E_Gadcon_Client *gcc) 
+{
+   e_gadcon_client_aspect_set(gcc, 16, 16);
+   e_gadcon_client_min_size_set(gcc, 16, 16);
+}
+
+static char *
+_gc_label(void) 
+{
+   return "EFM Path";
+}
+
+static Evas_Object *
+_gc_icon(Evas *evas) 
+{
+   Evas_Object *o = NULL;
+   char buf[4096];
+
+   snprintf(buf, sizeof(buf), "%s/e-module-efm_path.edj", 
+	    e_module_dir_get(path_mod));
+   o = edje_object_add(evas);
+   edje_object_file_set(o, buf, "icon");
+   return o;
+}
+
+static const char *
+_gc_id_new(void) 
+{
+   char buf[4096];
+
+   snprintf(buf, sizeof(buf), "%s.%d", _gc_class.name, 
+	    (evas_list_count(instances) + 1));
+   return strdup(buf);
+}
+
+/* E Module API functions */
+EAPI E_Module_Api e_modapi = 
+{
+   E_MODULE_API_VERSION, "EFM Path"
+};
+
+EAPI void *
+e_modapi_init(E_Module *m) 
+{
+   path_mod = m;
+   e_gadcon_provider_register(&_gc_class);
+   return m;
+}
+
+EAPI int 
+e_modapi_shutdown(E_Module *m) 
+{
+   e_gadcon_provider_unregister(&_gc_class);
+   path_mod = NULL;
+   return 1;
+}
+
+EAPI int 
+e_modapi_save(E_Module *m) 
+{
+   return 1;
+}
+
+/* local functions */
+static void 
+_cb_changed(void *data, Evas_Object *obj, void *event_info) 
+{
+   Instance *inst;
+   E_Toolbar *tbar;
+   Evas_Object *o_fm;
+   E_Win *fwin;
+
+   inst = data;
+   tbar = event_info;
+   o_fm = e_toolbar_fm2_get(tbar);
+   if (!o_fm) return;
+   fwin = e_toolbar_fwin_get(tbar);
+   if (!fwin) return;
+//   printf("\n\nPath Module Changed\n\n");
+}
+
+static void 
+_cb_dir_changed(void *data, Evas_Object *obj, void *event_info) 
+{
+   Instance *inst;
+   E_Toolbar *tbar;
+   Evas_Object *o_fm;
+   const char *path;
+
+   inst = data;
+   tbar = event_info;
+   inst->tbar = tbar;
+   o_fm = e_toolbar_fm2_get(tbar);
+   if (!o_fm) return;
+   path = e_fm2_real_path_get(o_fm);
+   if (!inst->o_entry) return;
+   e_widget_entry_text_set(inst->o_entry, path);
+}
+
+static void 
+_cb_dir_deleted(void *data, Evas_Object *obj, void *event_info) 
+{
+   Instance *inst;
+   E_Toolbar *tbar;
+   Evas_Object *o_fm;
+   E_Win *fwin;
+
+   inst = data;
+   tbar = event_info;
+   o_fm = e_toolbar_fm2_get(tbar);
+   if (!o_fm) return;
+   fwin = e_toolbar_fwin_get(tbar);
+   if (!fwin) return;
+//   printf("\n\nPath Module Dir Deleted\n\n");
+}
+
+static void 
+_cb_files_deleted(void *data, Evas_Object *obj, void *event_info) 
+{
+   Instance *inst;
+   E_Toolbar *tbar;
+   Evas_Object *o_fm;
+   E_Win *fwin;
+
+   inst = data;
+   tbar = event_info;
+   o_fm = e_toolbar_fm2_get(tbar);
+   if (!o_fm) return;
+   fwin = e_toolbar_fwin_get(tbar);
+   if (!fwin) return;
+//   printf("\n\nPath Module Files Deleted\n\n");
+}
+
+static void 
+_cb_selected(void *data, Evas_Object *obj, void *event_info) 
+{
+   Instance *inst;
+   E_Toolbar *tbar;
+   Evas_Object *o_fm;
+   E_Win *fwin;
+
+   inst = data;
+   tbar = event_info;
+   o_fm = e_toolbar_fm2_get(tbar);
+   if (!o_fm) return;
+   fwin = e_toolbar_fwin_get(tbar);
+   if (!fwin) return;
+//   printf("\n\nPath Module Selected\n\n");
+}
+
+static void 
+_cb_sel_changed(void *data, Evas_Object *obj, void *event_info) 
+{
+   Instance *inst;
+   E_Toolbar *tbar;
+   Evas_Object *o_fm;
+   E_Win *fwin;
+
+   inst = data;
+   tbar = event_info;
+   o_fm = e_toolbar_fm2_get(tbar);
+   if (!o_fm) return;
+   fwin = e_toolbar_fwin_get(tbar);
+   if (!fwin) return;
+//   printf("\n\nPath Module Selection Change\n\n");
+}
+
+static void 
+_cb_key_down(void *data, Evas_Object *obj, void *event_info) 
+{
+   Evas_Event_Key_Down *ev;
+   Instance *inst;
+   Evas_Object *o_fm;
+   const char *p;
+
+   ev = event_info;
+   if (strcmp(ev->keyname, "Return")) return;
+
+   inst = data;
+   if ((!inst) || (!inst->tbar)) return;
+   p = e_widget_entry_text_get(inst->o_entry);
+   o_fm = e_toolbar_fm2_get(inst->tbar);
+   if (!o_fm) return;
+   e_fm2_path_set(o_fm, p, "/");
+}
+
+static void 
+_cb_mouse_down(void *data, Evas *e, Evas_Object *obj, void *event_info) 
+{
+   Instance *inst;
+   Evas_Event_Mouse_Down *ev;
+   E_Menu *mn;
+   E_Zone *zone;
+   int x, y;
+
+   inst = data;
+   ev = event_info;
+   if ((ev->button != 3) || (inst->menu)) return;
+   zone = e_util_zone_current_get(e_manager_current_get());
+
+   mn = e_menu_new();
+   e_menu_post_deactivate_callback_set(mn, _cb_menu_post, inst);
+   inst->menu = mn;
+   e_gadcon_client_util_menu_items_append(inst->gcc, mn, 0);
+   ecore_x_pointer_xy_get(zone->container->win, &x, &y);
+   e_menu_activate_mouse(inst->menu, zone, x, y, 1, 1, 
+			 E_MENU_POP_DIRECTION_DOWN, ev->timestamp);
+   e_util_evas_fake_mouse_up_later(e, ev->button);
+}
+
+static void 
+_cb_menu_post(void *data, E_Menu *m) 
+{
+   Instance *inst;
+   
+   inst = data;
+   if (!inst->menu) return;
+   e_object_del(E_OBJECT(inst->menu));
+   inst->menu = NULL;
+}
