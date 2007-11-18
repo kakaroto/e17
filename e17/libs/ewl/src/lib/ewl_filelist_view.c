fe5de39b5037282d5b5a88222d5319f49110fd35
jethomas <jethomas@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-11-18 21:43:35 +0000

    Make the filelist icons a bit less cramped.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@32763 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/lib/ewl_filelist_view.c
+++ e17/libs/ewl/src/lib/ewl_filelist_view.c
@@ -20,6 +20,7 @@ Ewl_Widget *ewl_filelist_view_widget_fetch(void *data,
 
 	/* Create icon */
 	ret = ewl_icon_simple_new();
+	ewl_icon_constrain_set(EWL_ICON(ret), 32);
 	ewl_box_orientation_set(EWL_BOX(ret),
 			EWL_ORIENTATION_HORIZONTAL);
 	ewl_object_alignment_set(EWL_OBJECT(ret), EWL_FLAG_ALIGN_LEFT);
