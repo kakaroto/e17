e549c4a9c244fd1f0d3eceeb1ce114af48f264bf
pfritz <pfritz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-11-30 23:30:03 +0000

    add a second button
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@32921 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/bin/tests/tooltip/ewl_tooltip.c
+++ e17/libs/ewl/src/bin/tests/tooltip/ewl_tooltip.c
@@ -29,6 +29,13 @@ create_test(Ewl_Container *box)
 	ewl_object_fill_policy_set(EWL_OBJECT(button), EWL_FLAG_FILL_SHRINK);
 	ewl_attach_tooltip_text_set(button, "and see.");
 	ewl_widget_show(button);
+	
+	button = ewl_button_new();
+	ewl_button_label_set(EWL_BUTTON(button), "Hover on this button");
+	ewl_container_child_append(EWL_CONTAINER(box), button);
+	ewl_object_fill_policy_set(EWL_OBJECT(button), EWL_FLAG_FILL_SHRINK);
+	ewl_attach_tooltip_text_set(button, "and see more.");
+	ewl_widget_show(button);
 
 	return 1;
 }
