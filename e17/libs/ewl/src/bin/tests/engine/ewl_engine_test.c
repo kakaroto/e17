95bf7b3f44c21731fad7ad263b0eca0185b14e9b
ningerso <ningerso@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-12-04 05:28:05 +0000

    Rename test files to include a _test prefix to avoid conflicting file names with widget source file. This makes it easier to read backtraces.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@32972 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/bin/tests/engine/ewl_engine_test.c
+++ e17/libs/ewl/src/bin/tests/engine/ewl_engine_test.c
@@ -8,6 +8,7 @@
 #include <Evas.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * @addtogroup Ewl_Embed
@@ -39,7 +40,14 @@ static void cb_mouse_down(Ewl_Widget *w, void *ev, void *data);
 static void cb_mouse_up(Ewl_Widget *w, void *ev, void *data);
 static void cb_mouse_move(Ewl_Widget *w, void *ev, void *data);
 
+static int engine_test_set_get(char *buf, int len);
+
 #define WIN_NAME "buffer_engine_window"
+
+static Ewl_Unit_Test engine_unit_tests[] = {
+		{"engine set/get", engine_test_set_get, NULL, -1, 1},
+		{NULL, NULL, NULL, -1, 0}
+	};
 
 void
 test_info(Ewl_Test *test)
@@ -49,6 +57,7 @@ test_info(Ewl_Test *test)
 	test->filename = __FILE__;
 	test->func = create_test;
 	test->type = EWL_TEST_TYPE_ADVANCED;
+	test->unit_tests = engine_unit_tests;
 }
 
 static int
@@ -162,3 +171,98 @@ cb_mouse_move(Ewl_Widget *w, void *ev, void *data __UNUSED__)
 
 	ewl_embed_mouse_move_feed(EWL_EMBED(pointer_win), x, y, event->modifiers);
 }
+
+/*
+static void *window_funcs[EWL_ENGINE_WINDOW_MAX] =
+	{
+		engine_window_new,
+		engine_window_destroy,
+		engine_window_move,
+		engine_window_resize,
+		engine_window_min_max_size_set,
+		engine_window_show,
+		engine_window_hide,
+		engine_window_title_set,
+		engine_window_name_class_set,
+		engine_window_borderless_set,
+		engine_window_dialog_set,
+		engine_window_states_set,
+		engine_window_hints_set,
+		engine_window_transient_for,
+		engine_window_leader_set,
+		engine_window_raise,
+		engine_window_lower,
+		engine_keyboard_grab,
+		engine_keyboard_ungrab,
+		engine_pointer_grab,
+		engine_pointer_ungrab,
+		engine_window_selection_text_set,
+		engine_window_geometry_set,
+		engine_dnd_aware_set,
+		engine_desktop_size_get,
+		engine_dnd_drag_types_set,
+		engine_dnd_drag_begin,
+		engine_dnd_drag_drop,
+		engine_dnd_drag_data_send,
+	};
+
+static void *pointer_funcs[EWL_ENGINE_POINTER_MAX] =
+	{
+		engine_pointer_data_new,
+		engine_pointer_free,
+		engine_pointer_get,
+		engine_pointer_set,
+	};
+	*/
+
+static Ewl_Widget *
+create_unit_test_window(void)
+{
+	Ewl_Widget *window;
+
+	window = ewl_window_new();
+	ewl_embed_engine_name_set(EWL_EMBED(window), "ewl_engine");
+	ewl_widget_show(window);
+
+	return window;
+}
+
+static int
+engine_test_set_get(char *buf, int len)
+{
+	int ret = 0;
+	const char *engine_name;
+	Ewl_Widget *window;
+
+	ecore_path_group_add(ewl_engines_path, PACKAGE_LIB_DIR "/ewl/tests");
+
+	window = create_unit_test_window();
+	engine_name = ewl_embed_engine_name_get(EWL_EMBED(window));
+
+	if (strcmp(engine_name, "ewl_engine"))
+		snprintf(buf, len, "engine name doesn't match");
+	else
+		ret = 1;
+
+	return ret;
+}
+
+/*
+static int
+engine_init(Ewl_Engine *engine, int *argc, char **argv)
+{
+	info = NEW(Ewl_Engine_Info, 1);
+	info->shutdown = engine_shutdown;
+	info->hooks.window = window_funcs;
+	info->hooks.pointer = pointer_funcs;
+
+	engine->functions = info;
+
+	return TRUE;
+}
+
+static void
+ee_shutdown(Ewl_Engine *engine)
+{
+}
+*/
