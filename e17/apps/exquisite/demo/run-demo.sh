0215938a1a555a0dcdb0340b9a363e8b942db2dc
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-12-14 05:57:16 +0000

    patches, caching fixes, zone dynamic creation/deletion handling, stuff.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@33127 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/apps/exquisite/demo/run-demo.sh
+++ e17/apps/exquisite/demo/run-demo.sh
@@ -7,6 +7,7 @@ WRITE="../src/bin/exquisite-write"
 # start up gui - some examples here for different resolutions and the fb
 #$MAIN -t ../data/themes/default.edj -x 480 -y 640 &
 $MAIN -t ../data/themes/default.edj -x 640 -y 480 &
+#$MAIN -t ../data/themes/default.edj -x 240 -y 320 &
 #$MAIN -t ../data/themes/default.edj -x 1280 -y 1024 &
 #$MAIN -fb -fs
 # run 'exquisite -h' for more options
