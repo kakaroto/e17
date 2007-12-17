822c3d7d928b1a0ba6cab40672b7e102c51903a3
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-12-17 03:21:19 +0000

    Formatting fixes.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@33167 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/esmart/src/lib/esmart_text_entry/Esmart_Text_Entry.h
+++ e17/libs/esmart/src/lib/esmart_text_entry/Esmart_Text_Entry.h
@@ -33,13 +33,13 @@ extern "C" {
 /* create a new text entry */
 EAPI Evas_Object *esmart_text_entry_new (Evas * e);
 
-EAPI void esmart_text_entry_text_set (Evas_Object * o, const char *str);
-EAPI void esmart_text_entry_is_password_set (Evas_Object * o, int val);
-EAPI void esmart_text_entry_max_chars_set (Evas_Object * o, int max);
-EAPI void esmart_text_entry_edje_part_set (Evas_Object * o, Evas_Object * edje,
-					   const char *part);
-EAPI Evas_Object *esmart_text_entry_edje_object_get(Evas_Object * o);
-EAPI const char *esmart_text_entry_edje_part_get(Evas_Object * o);
+EAPI void esmart_text_entry_text_set(Evas_Object *o, const char *str);
+EAPI void esmart_text_entry_is_password_set(Evas_Object *o, int val);
+EAPI void esmart_text_entry_max_chars_set(Evas_Object *o, int max);
+EAPI void esmart_text_entry_edje_part_set(Evas_Object *o, Evas_Object *edje,
+					  const char *part);
+EAPI Evas_Object *esmart_text_entry_edje_object_get(Evas_Object *o);
+EAPI const char *esmart_text_entry_edje_part_get(Evas_Object *o);
 
 /* you've gotta free this resut */
 EAPI const char *esmart_text_entry_text_get(Evas_Object *o);
@@ -49,7 +49,7 @@ EAPI const char *esmart_text_entry_text_get(Evas_Object *o);
   * @param func The callback function.
   * @param data The data that is passed to the callback function.
   */
-EAPI void esmart_text_entry_return_key_callback_set (Evas_Object * o,
+EAPI void esmart_text_entry_return_key_callback_set(Evas_Object *o,
 						     void (*func) (void *data,
 								   const char *str),
 						     void *data);
