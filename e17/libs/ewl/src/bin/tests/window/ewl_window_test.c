5604a91c0985ffa5a1fd477964daf68719b9ba17
ningerso <ningerso@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2007-12-28 04:41:47 +0000

    Add unit tests for various window flags.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@33269 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/bin/tests/window/ewl_window_test.c
+++ e17/libs/ewl/src/bin/tests/window/ewl_window_test.c
@@ -10,6 +10,16 @@
 static int title_set_get(char *buf, int len);
 static int name_set_get(char *buf, int len);
 static int class_set_get(char *buf, int len);
+static int borderless_set_get(char *buf, int len);
+static int dialog_set_get(char *buf, int len);
+static int fullscreen_set_get(char *buf, int len);
+static int skip_taskbar_set_get(char *buf, int len);
+static int skip_pager_set_get(char *buf, int len);
+static int urgent_set_get(char *buf, int len);
+static int modal_set_get(char *buf, int len);
+static int keyboard_grab_set_get(char *buf, int len);
+static int pointer_grab_set_get(char *buf, int len);
+static int override_set_get(char *buf, int len);
 
 
 /*
@@ -20,6 +30,16 @@ static Ewl_Unit_Test window_unit_tests[] = {
 		{"title set/get", title_set_get, NULL, -1, 0},
 		{"name set/get", name_set_get, NULL, -1, 0},
 		{"class set/get", class_set_get, NULL, -1, 0},
+		{"borderless set/get", borderless_set_get, NULL, -1, 0},
+		{"dialog set/get", dialog_set_get, NULL, -1, 0},
+		{"fullscreen set/get", fullscreen_set_get, NULL, -1, 0},
+		{"skip_taskbar set/get", skip_taskbar_set_get, NULL, -1, 0},
+		{"skip_pager set/get", skip_pager_set_get, NULL, -1, 0},
+		{"urgent set/get", urgent_set_get, NULL, -1, 0},
+		{"modal set/get", modal_set_get, NULL, -1, 0},
+		{"keyboard_grab set/get", keyboard_grab_set_get, NULL, -1, 0},
+		{"pointer_grab set/get", pointer_grab_set_get, NULL, -1, 0},
+		{"override set/get", override_set_get, NULL, -1, 0},
 		{NULL, NULL, NULL, -1, 0}
 	};
 
@@ -135,6 +155,325 @@ class_set_get(char *buf, int len)
 				ret = 1;
 		}
 	}
+
+	ewl_widget_destroy(win);
+
+	return ret;
+}
+
+/*
+ * Set a window as borderless and check that the value is set.
+ */
+static int
+borderless_set_get(char *buf, int len)
+{
+	Ewl_Widget *win;
+	int ret = 0;
+
+	win = ewl_window_new();
+	ewl_window_borderless_set(EWL_WINDOW(win));
+
+	if (EWL_WINDOW(win)->flags & EWL_WINDOW_BORDERLESS)
+		ret = 1;
+	else
+		snprintf(buf, len, "borderless set failed");
+
+	ewl_widget_destroy(win);
+
+	return ret;
+}
+
+/*
+ * Set a window as a dialog and check that the value is set.
+ */
+static int
+dialog_set_get(char *buf, int len)
+{
+	Ewl_Widget *win;
+	int ret = 0;
+
+	win = ewl_window_new();
+
+	if (!ewl_window_dialog_get(EWL_WINDOW(win))) {
+
+		ewl_window_dialog_set(EWL_WINDOW(win), TRUE);
+		if (ewl_window_dialog_get(EWL_WINDOW(win))) {
+
+			ewl_window_dialog_set(EWL_WINDOW(win), FALSE);
+			if (!ewl_window_dialog_get(EWL_WINDOW(win)))
+				ret = 1;
+			else
+				snprintf(buf, len, "dialog unset failed");
+		}
+		else
+			snprintf(buf, len, "dialog set failed");
+	}
+	else
+		snprintf(buf, len, "default dialog set");
+
+	ewl_widget_destroy(win);
+
+	return ret;
+}
+
+/*
+ * Set a window as a fullscreen and check that the value is set.
+ */
+static int
+fullscreen_set_get(char *buf, int len)
+{
+	Ewl_Widget *win;
+	int ret = 0;
+
+	win = ewl_window_new();
+
+	if (!ewl_window_fullscreen_get(EWL_WINDOW(win))) {
+
+		ewl_window_fullscreen_set(EWL_WINDOW(win), TRUE);
+		if (ewl_window_fullscreen_get(EWL_WINDOW(win))) {
+
+			ewl_window_fullscreen_set(EWL_WINDOW(win), FALSE);
+			if (!ewl_window_fullscreen_get(EWL_WINDOW(win)))
+				ret = 1;
+			else
+				snprintf(buf, len, "fullscreen unset failed");
+		}
+		else
+			snprintf(buf, len, "fullscreen set failed");
+	}
+	else
+		snprintf(buf, len, "default fullscreen set");
+
+	ewl_widget_destroy(win);
+
+	return ret;
+}
+
+/*
+ * Set a window as a skip_taskbar and check that the value is set.
+ */
+static int
+skip_taskbar_set_get(char *buf, int len)
+{
+	Ewl_Widget *win;
+	int ret = 0;
+
+	win = ewl_window_new();
+
+	if (!ewl_window_skip_taskbar_get(EWL_WINDOW(win))) {
+
+		ewl_window_skip_taskbar_set(EWL_WINDOW(win), TRUE);
+		if (ewl_window_skip_taskbar_get(EWL_WINDOW(win))) {
+
+			ewl_window_skip_taskbar_set(EWL_WINDOW(win), FALSE);
+			if (!ewl_window_skip_taskbar_get(EWL_WINDOW(win)))
+				ret = 1;
+			else
+				snprintf(buf, len, "skip_taskbar unset failed");
+		}
+		else
+			snprintf(buf, len, "skip_taskbar set failed");
+	}
+	else
+		snprintf(buf, len, "default skip_taskbar set");
+
+	ewl_widget_destroy(win);
+
+	return ret;
+}
+
+/*
+ * Set a window as a skip_pager and check that the value is set.
+ */
+static int
+skip_pager_set_get(char *buf, int len)
+{
+	Ewl_Widget *win;
+	int ret = 0;
+
+	win = ewl_window_new();
+
+	if (!ewl_window_skip_pager_get(EWL_WINDOW(win))) {
+
+		ewl_window_skip_pager_set(EWL_WINDOW(win), TRUE);
+		if (ewl_window_skip_pager_get(EWL_WINDOW(win))) {
+
+			ewl_window_skip_pager_set(EWL_WINDOW(win), FALSE);
+			if (!ewl_window_skip_pager_get(EWL_WINDOW(win)))
+				ret = 1;
+			else
+				snprintf(buf, len, "skip_pager unset failed");
+		}
+		else
+			snprintf(buf, len, "skip_pager set failed");
+	}
+	else
+		snprintf(buf, len, "default skip_pager set");
+
+	ewl_widget_destroy(win);
+
+	return ret;
+}
+
+/*
+ * Set a window as a urgent and check that the value is set.
+ */
+static int
+urgent_set_get(char *buf, int len)
+{
+	Ewl_Widget *win;
+	int ret = 0;
+
+	win = ewl_window_new();
+
+	if (!ewl_window_urgent_get(EWL_WINDOW(win))) {
+
+		ewl_window_urgent_set(EWL_WINDOW(win), TRUE);
+		if (ewl_window_urgent_get(EWL_WINDOW(win))) {
+
+			ewl_window_urgent_set(EWL_WINDOW(win), FALSE);
+			if (!ewl_window_urgent_get(EWL_WINDOW(win)))
+				ret = 1;
+			else
+				snprintf(buf, len, "urgent unset failed");
+		}
+		else
+			snprintf(buf, len, "urgent set failed");
+	}
+	else
+		snprintf(buf, len, "default urgent set");
+
+	ewl_widget_destroy(win);
+
+	return ret;
+}
+
+/*
+ * Set a window as a modal and check that the value is set.
+ */
+static int
+modal_set_get(char *buf, int len)
+{
+	Ewl_Widget *win;
+	int ret = 0;
+
+	win = ewl_window_new();
+
+	if (!ewl_window_modal_get(EWL_WINDOW(win))) {
+
+		ewl_window_modal_set(EWL_WINDOW(win), TRUE);
+		if (ewl_window_modal_get(EWL_WINDOW(win))) {
+
+			ewl_window_modal_set(EWL_WINDOW(win), FALSE);
+			if (!ewl_window_modal_get(EWL_WINDOW(win)))
+				ret = 1;
+			else
+				snprintf(buf, len, "modal unset failed");
+		}
+		else
+			snprintf(buf, len, "modal set failed");
+	}
+	else
+		snprintf(buf, len, "default modal set");
+
+	ewl_widget_destroy(win);
+
+	return ret;
+}
+
+/*
+ * Set a window as a keyboard_grab and check that the value is set.
+ */
+static int
+keyboard_grab_set_get(char *buf, int len)
+{
+	Ewl_Widget *win;
+	int ret = 0;
+
+	win = ewl_window_new();
+
+	if (!ewl_window_keyboard_grab_get(EWL_WINDOW(win))) {
+
+		ewl_window_keyboard_grab_set(EWL_WINDOW(win), TRUE);
+		if (ewl_window_keyboard_grab_get(EWL_WINDOW(win))) {
+
+			ewl_window_keyboard_grab_set(EWL_WINDOW(win), FALSE);
+			if (!ewl_window_keyboard_grab_get(EWL_WINDOW(win)))
+				ret = 1;
+			else
+				snprintf(buf, len, "keyboard_grab unset failed");
+		}
+		else
+			snprintf(buf, len, "keyboard_grab set failed");
+	}
+	else
+		snprintf(buf, len, "default keyboard_grab set");
+
+	ewl_widget_destroy(win);
+
+	return ret;
+}
+
+/*
+ * Set a window as a pointer_grab and check that the value is set.
+ */
+static int
+pointer_grab_set_get(char *buf, int len)
+{
+	Ewl_Widget *win;
+	int ret = 0;
+
+	win = ewl_window_new();
+
+	if (!ewl_window_pointer_grab_get(EWL_WINDOW(win))) {
+
+		ewl_window_pointer_grab_set(EWL_WINDOW(win), TRUE);
+		if (ewl_window_pointer_grab_get(EWL_WINDOW(win))) {
+
+			ewl_window_pointer_grab_set(EWL_WINDOW(win), FALSE);
+			if (!ewl_window_pointer_grab_get(EWL_WINDOW(win)))
+				ret = 1;
+			else
+				snprintf(buf, len, "pointer_grab unset failed");
+		}
+		else
+			snprintf(buf, len, "pointer_grab set failed");
+	}
+	else
+		snprintf(buf, len, "default pointer_grab set");
+
+	ewl_widget_destroy(win);
+
+	return ret;
+}
+
+/*
+ * Set a window as a override and check that the value is set.
+ */
+static int
+override_set_get(char *buf, int len)
+{
+	Ewl_Widget *win;
+	int ret = 0;
+
+	win = ewl_window_new();
+
+	if (!ewl_window_override_get(EWL_WINDOW(win))) {
+
+		ewl_window_override_set(EWL_WINDOW(win), TRUE);
+		if (ewl_window_override_get(EWL_WINDOW(win))) {
+
+			ewl_window_override_set(EWL_WINDOW(win), FALSE);
+			if (!ewl_window_override_get(EWL_WINDOW(win)))
+				ret = 1;
+			else
+				snprintf(buf, len, "override unset failed");
+		}
+		else
+			snprintf(buf, len, "override set failed");
+	}
+	else
+		snprintf(buf, len, "default override set");
 
 	ewl_widget_destroy(win);
 
