9c414df84b9b08ac503927921c4b9e45dddcad94
kwo <kwo@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-03-22 14:51:24 +0000

    Forgot 2008.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@34083 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e16/e/eesh/comms.c
+++ e16/e/eesh/comms.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2007 Kim Woelders
+ * Copyright (C) 2004-2008 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -21,7 +21,6 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 #include "E.h"
 
 static Window       root_win;
