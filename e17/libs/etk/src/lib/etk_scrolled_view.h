03c18d3de5aecf03dfa25ba923f0187e263f87c4
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-04-10 09:03:08 +0000

    ticks finger scrolling stuff for etk.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@34235 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/etk/src/lib/etk_scrolled_view.h
+++ e17/libs/etk/src/lib/etk_scrolled_view.h
@@ -33,9 +33,24 @@ typedef enum
    ETK_POLICY_AUTO,     /**< The scrollbar is shown and hidden automatically whether or not the child can fit
                          * entirely in the scrolled view */
    ETK_POLICY_SHOW,     /**< The scrollbar is always visible */
-   ETK_POLICY_HIDE      /**< The scrollbar is always hidden */
+   ETK_POLICY_HIDE,     /**< The scrollbar is always hidden */
 } Etk_Scrolled_View_Policy;
 
+struct Etk_Scrolled_View_Mouse_Drag 
+{
+   Etk_Bool mouse_down;
+   Etk_Bool bar_pressed;
+   Etk_Bool dragable;
+   Etk_Bool bouncy;
+   int scroll_flag;
+   Etk_Position position;
+   Etk_Position bar_position;
+   double sample_magic;
+   unsigned int damping_magic;
+   double timestamp;
+   double old_timestamp;
+   double Vx,Vy;
+};
 
 /**
  * @brief @widget The structure of a scrolled view
@@ -52,7 +67,10 @@ struct Etk_Scrolled_View
 
    Etk_Scrolled_View_Policy hpolicy;
    Etk_Scrolled_View_Policy vpolicy;
+
+   struct Etk_Scrolled_View_Mouse_Drag drag;
 };
+
 
 
 Etk_Type   *etk_scrolled_view_type_get(void);
@@ -63,6 +81,14 @@ Etk_Range  *etk_scrolled_view_vscrollbar_get(Etk_Scrolled_View *scrolled_view);
 void        etk_scrolled_view_add_with_viewport(Etk_Scrolled_View *scrolled_view, Etk_Widget *child);
 void        etk_scrolled_view_policy_set(Etk_Scrolled_View *scrolled_view, Etk_Scrolled_View_Policy hpolicy, Etk_Scrolled_View_Policy vpolicy);
 void        etk_scrolled_view_policy_get(Etk_Scrolled_View *scrolled_view, Etk_Scrolled_View_Policy *hpolicy, Etk_Scrolled_View_Policy *vpolicy);
+void        etk_scrolled_view_dragable_set(Etk_Scrolled_View *scrolled_view, Etk_Bool dragable);
+Etk_Bool    etk_scrolled_view_dragable_get(Etk_Scrolled_View *scrolled_view);
+void        etk_scrolled_view_drag_bouncy_set(Etk_Scrolled_View *scrolled_view, Etk_Bool bouncy);
+Etk_Bool    etk_scrolled_view_drag_bouncy_get(Etk_Scrolled_View *scrolled_view);
+double      etk_scrolled_view_drag_sample_interval_magic_set(Etk_Scrolled_View *scrolled_view,double interval);
+double      etk_scrolled_view_drag_sample_interval_magic_get(Etk_Scrolled_View *scrolled_view);
+unsigned int etk_scrolled_view_drag_damping_magic_set(Etk_Scrolled_View *scrolled_view,unsigned int damping);
+unsigned int etk_scrolled_view_drag_damping_magic_get(Etk_Scrolled_View *scrolled_view);
 
 /** @} */
 
