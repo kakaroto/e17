868dc7ef2912e50385e9ce842a6948a3fa6e2e34
vapier <vapier@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-04-20 12:16:31 +0000

    fix running with libtool-2.2+
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@34324 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/imlib2/autogen.sh
+++ e17/libs/imlib2/autogen.sh
@@ -4,11 +4,12 @@ rm -rf autom4te.cache
 rm -f aclocal.m4 ltmain.sh
 
 touch README
+libtoolize -n --install 2>/dev/null && LIBTOOL_FLAGS="--install"
 
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
-echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
+echo "Running libtoolize..." ; (libtoolize --copy --automake $LIBTOOL_FLAGS || glibtoolize --automake) || exit 1
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
