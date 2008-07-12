a0710a28f32f212f7972fc3662059a61b85a327a
pfritz <pfritz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-07-12 19:30:58 +0000

    remove spaces between functions and parenthesises
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@35082 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/lib/ewl_dialog.h
+++ e17/libs/ewl/src/lib/ewl_dialog.h
@@ -62,8 +62,8 @@ struct Ewl_Dialog
         Ewl_Position active_area; /**< Which section of the dialog is active */
 };
 
-Ewl_Widget      *ewl_dialog_new (void);
-int              ewl_dialog_init (Ewl_Dialog *dialog);
+Ewl_Widget      *ewl_dialog_new(void);
+int              ewl_dialog_init(Ewl_Dialog *dialog);
 
 void             ewl_dialog_action_position_set(Ewl_Dialog *d, Ewl_Position pos);
 Ewl_Position     ewl_dialog_action_position_get(Ewl_Dialog *dialog);
