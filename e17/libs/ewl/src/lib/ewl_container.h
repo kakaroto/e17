bbfcc14a6533e7efbd602309386712c943edcda4
pfritz <pfritz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-08-05 19:30:39 +0000

    and make it public
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@35329 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/ewl/src/lib/ewl_container.h
+++ e17/libs/ewl/src/lib/ewl_container.h
@@ -107,6 +107,7 @@ struct Ewl_Container
         Ewl_Container_Iterator iterator; /**< Function to find next child */
 };
 
+Ewl_Widget      *ewl_container_new(void);
 int              ewl_container_init(Ewl_Container *container);
 
 void             ewl_container_add_notify_set(Ewl_Container *container,
