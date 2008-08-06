cb33591e1680a6845533b052da5ff511842d0c0c
codewarrior <codewarrior@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-08-06 18:14:00 +0000

    License changes (BSD -> LGPL)
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@35359 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e17/libs/etk/src/lib/etk_box.h
+++ e17/libs/etk/src/lib/etk_box.h
@@ -1,3 +1,21 @@
+/* ETK - The Enlightened ToolKit
+ * Copyright (C) 2006-2008 Simon Treny, Hisham Mardam-Bey, Vincent Torri, Viktor Kojouharov
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /** @file etk_box.h */
 #ifndef _ETK_BOX_H_
 #define _ETK_BOX_H_
