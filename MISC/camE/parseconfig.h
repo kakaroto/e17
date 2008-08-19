6b594ecd4fe26b5470ee5bdc475cf93f90979201
mej <mej@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-08-19 00:18:05 +0000

    More stuff that's not here any more.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@35569 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- MISC/camE/parseconfig.h
+++
@@ -1,6 +0,0 @@
-int    cfg_parse_file(char *filename);
-char** cfg_list_sections(void);
-char** cfg_list_entries(char *name);
-char*  cfg_get_str(char *sec, char *ent);
-int    cfg_get_int(char *sec, char *ent);
-float  cfg_get_float(char *sec, char *ent);
