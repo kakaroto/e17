754cc4a97da6804d4d87f1cb4fade9db5573b2f6
kwo <kwo@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-08-23 17:47:10 +0000

    Autofoo cleanups.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@35631 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E16/epplets/autogen.sh
+++ E16/epplets/autogen.sh
@@ -4,8 +4,8 @@ rm -rf autom4te.cache
 rm -f aclocal.m4 ltmain.sh
 
 echo "Running aclocal..." ; aclocal -I . $ACLOCAL_FLAGS || exit 1
-echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
+echo "Running autoheader..." ; autoheader || exit 1
 echo "Running libtoolize..." ; (libtoolize --copy --force --automake || glibtoolize --automake) || exit 1
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
