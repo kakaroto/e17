10b5c64ca0a5c2b0e6ba816dc08e1d3c74e90e31
kwo <kwo@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-08-23 08:07:09 +0000

    Old e16menuedit is obsolete.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@35617 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E16/e16menuedit/file.h
+++
@@ -1,3 +0,0 @@
-char *field (char *s, int field);
-void fword (char *s, int num, char *wd);
-char *homedir (int uid);
--- E16/e16menuedit/menus.h
+++
@@ -1,5 +0,0 @@
-GtkWidget *CreateBarSubMenu (GtkWidget * menu, char *szName);
-GtkWidget *CreateRightAlignBarSubMenu (GtkWidget * menu, char *szName);
-GtkWidget *CreateMenuItem (GtkWidget * menu, char *szName,
-			   char *szAccel, char *szTip,
-			   GtkSignalFunc func, gpointer data);
