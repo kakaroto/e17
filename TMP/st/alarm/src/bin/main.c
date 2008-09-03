a5217b964aea7e8d7f96d7addb587adfcc551adb
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-09-03 11:53:13 +0000

    use callbacks!
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@35805 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- TMP/st/alarm/src/bin/main.c
+++ TMP/st/alarm/src/bin/main.c
@@ -25,7 +25,7 @@ main(int argc, char **argv)
    Elm_Win *win;
 
    /* init Elementary (all Elementary calls begin with elm_ and all data
-    * types, enums and macros will be Elm_ and ELM_ etc. */
+    * types, enums and macros will be Elm_ and ELM_ etc.) */
    elm_init(argc, argv);
 
    win = elm_win_new(); /* create a window */
@@ -33,11 +33,15 @@ main(int argc, char **argv)
 				* manager. make it uniqie for windows within 
 				* this application */
    win->title_set(win, "Alarm"); /* set the title */
-   
-   win->callback_add(win, ELM_CALLBACK_RESIZE, on_win_resize, NULL);
+   win->autodel = 0; /* dont auto delete the window if someone closes it.
+		      * this means the del+req handler has to delete it. by
+		      * default it is on */
+   /* add a callback that is called when the user tries to close the window */
    win->callback_add(win, ELM_CALLBACK_DEL_REQ, on_win_del_req, NULL);
+   /* add a callback that gets called when the window is resized */
+   win->callback_add(win, ELM_CALLBACK_RESIZE, on_win_resize, NULL);
    
-   win->show(win); /* show it */
+   win->show(win); /* show the window */
    
    elm_run(); /* and run the program now  and handle all events etc. */
    
