6d230b095392d54de035b6bf2a9ed62d8fbe04bd
toma <toma@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-09-22 12:17:04 +0000

    Fix a copy/paste error in exchange for application GETIT
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@36162 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- web/exchange/exchange/apps/exchange/modules/home/templates/cvsSuccess.php
+++ web/exchange/exchange/apps/exchange/modules/home/templates/cvsSuccess.php
@@ -2,5 +2,5 @@
 <h1>Download from official Enlightenment SVN</h1>
 This code is available from the Enlightenment SVN at: <strong><?php echo $path ?></strong><br/>
 To download, run the following command:<br/>
-<strong>svn co http://svn.enlightenment.org/svn/e/trunk<?php echo $path ?></strong><br/>
+<strong>svn co http://svn.enlightenment.org/svn/e/trunk/<?php echo $path ?></strong><br/>
 For information on using SVN, please visit the <a href="http://wiki.enlightenment.org/index.php/E17_User_Guide/Installing_from_Source_Repository">wiki</a>.
