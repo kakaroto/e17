a8411333061abcc7d07be3e7b6466bf9f2c300c5
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-10-01 04:26:05 +0000

    and finally fully fix.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@36371 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- TMP/oe/oe-flash-gta02.sh
+++ TMP/oe/oe-flash-gta02.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DISTRO="openmoko"
 MACHINE="om-gta02"
-KERNEL="./tmp.$DISTRO.$MACHINE/deploy/glibc/images/uImage-om-gta02-latest.bin"
+KERNEL="./tmp.$DISTRO.$MACHINE/deploy/glibc/images/neo1973/uImage-om-gta02-latest.bin"
 ROOTFS=`find ./tmp.$DISTRO.$MACHINE/deploy/glibc/images -name "*.jffs2*" | sort | tail -1`
 
 echo "KERNEL: $KERNEL"
@@ -9,7 +9,7 @@ echo "ROOTFS: $ROOTFS"
 
 sleep 5
 
-DFU="tmp.openmoko.om-gta02/deploy/glibc/tools"
+DFU="./tmp.openmoko.om-gta02/deploy/glibc/tools/dfu-util"
 
 sudo $DFU -a kernel -D $KERNEL
 sudo $DFU -a rootfs -D $ROOTFS
