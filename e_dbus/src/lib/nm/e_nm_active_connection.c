daf186f1a31da24d50278b91cf9a69f877c41943
englebass <englebass@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-11-05 20:33:40 +0000

    TODO++
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@37506 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_dbus/src/lib/nm/e_nm_active_connection.c
+++ e_dbus/src/lib/nm/e_nm_active_connection.c
@@ -41,31 +41,7 @@ EAPI void
 e_nm_active_connection_free(E_NM_Active_Connection *conn)
 {
   if (!conn) return;
-#if 0
-  if (dev->dev.udi) free(dev->dev.udi);
-  if (dev->dev.interface) free(dev->dev.interface);
-  if (dev->dev.driver) free(dev->dev.driver);
-  if (dev->dev.ip4_config) free(dev->dev.ip4_config);
-  if (dev->dev.dhcp4_config) free(dev->dev.dhcp4_config);
-  switch (dev->dev.device_type)
-  {
-    case E_NM_DEVICE_TYPE_WIRED:
-      if (dev->dev.wired.hw_address) free(dev->dev.wired.hw_address);
-      break;
-    case E_NM_DEVICE_TYPE_WIRELESS:
-      if (dev->dev.wireless.hw_address) free(dev->dev.wireless.hw_address);
-      if (dev->dev.wireless.active_access_point) free(dev->dev.wireless.active_access_point);
-      break;
-  }
-  if (dev->handlers)
-  {
-    E_DBus_Signal_Handler *sh;
-
-    while ((sh = ecore_list_first_remove(dev->handlers)))
-      e_dbus_signal_handler_del(dev->nmi->conn, sh);
-    ecore_list_destroy(dev->handlers);
-  }
-#endif
+  /* TODO */
   free(conn);
 }
 
@@ -74,105 +50,7 @@ e_nm_active_connection_dump(E_NM_Active_Connection *conn)
 {
   if (!conn) return;
   printf("E_NM_Active_Connection:\n");
-#if 0
-  printf("udi                  : %s\n", dev->udi);
-  printf("interface            : %s\n", dev->interface);
-  printf("driver               : %s\n", dev->driver);
-  printf("capabilities         :");
-  if (dev->capabilities & E_NM_DEVICE_CAP_NM_SUPPORTED)
-    printf(" E_NM_DEVICE_CAP_NM_SUPPORTED");
-  if (dev->capabilities & E_NM_DEVICE_CAP_CARRIER_DETECT)
-    printf(" E_NM_DEVICE_CAP_CARRIER_DETECT");
-  if (dev->capabilities == E_NM_DEVICE_CAP_NONE)
-    printf(" E_NM_DEVICE_CAP_NONE");
-  printf("\n");
-  printf("ip4_address          : %i.%i.%i.%i\n",
-         ((dev->ip4_address      ) & 0xff),
-         ((dev->ip4_address >> 8 ) & 0xff),
-         ((dev->ip4_address >> 16) & 0xff),
-         ((dev->ip4_address >> 24) & 0xff)
-         );
-  printf("state                : ");
-  switch (dev->state)
-  {
-    case E_NM_DEVICE_STATE_UNKNOWN:
-      printf("E_NM_DEVICE_STATE_UNKNOWN\n");
-      break;
-    case E_NM_DEVICE_STATE_UNMANAGED:
-      printf("E_NM_DEVICE_STATE_UNMANAGED\n");
-      break;
-    case E_NM_DEVICE_STATE_UNAVAILABLE:
-      printf("E_NM_DEVICE_STATE_UNAVAILABLE\n");
-      break;
-    case E_NM_DEVICE_STATE_DISCONNECTED:
-      printf("E_NM_DEVICE_STATE_DISCONNECTED\n");
-      break;
-    case E_NM_DEVICE_STATE_PREPARE:
-      printf("E_NM_DEVICE_STATE_PREPARE\n");
-      break;
-    case E_NM_DEVICE_STATE_CONFIG:
-      printf("E_NM_DEVICE_STATE_CONFIG\n");
-      break;
-    case E_NM_DEVICE_STATE_NEED_AUTH:
-      printf("E_NM_DEVICE_STATE_NEED_AUTH\n");
-      break;
-    case E_NM_DEVICE_STATE_IP_CONFIG:
-      printf("E_NM_DEVICE_STATE_IP_CONFIG\n");
-      break;
-    case E_NM_DEVICE_STATE_ACTIVATED:
-      printf("E_NM_DEVICE_STATE_ACTIVATED\n");
-      break;
-    case E_NM_DEVICE_STATE_FAILED:
-      printf("E_NM_DEVICE_STATE_FAILED\n");
-      break;
-  }
-  printf("ip4_config           : %s\n", dev->ip4_config);
-  printf("dhcp4_config         : %s\n", dev->dhcp4_config);
-  printf("managed              : %d\n", dev->managed);
-  printf("device_type          : %u\n", dev->device_type);
-  switch (dev->device_type)
-  {
-    case E_NM_DEVICE_TYPE_WIRED:
-      printf("hw_address           : %s\n", dev->wired.hw_address);
-      printf("speed                : %u\n", dev->wired.speed);
-      printf("carrier              : %d\n", dev->wired.carrier);
-      break;
-    case E_NM_DEVICE_TYPE_WIRELESS:
-      printf("hw_address           : %s\n", dev->wireless.hw_address);
-      printf("mode                 : ");
-      switch (dev->wireless.mode)
-      {
-        case E_NM_802_11_MODE_UNKNOWN:
-          printf("E_NM_802_11_MODE_UNKNOWN\n");
-          break;
-        case E_NM_802_11_MODE_ADHOC:
-          printf("E_NM_802_11_MODE_ADHOC\n");
-          break;
-        case E_NM_802_11_MODE_INFRA:
-          printf("E_NM_802_11_MODE_INFRA\n");
-          break;
-      }
-      printf("bitrate              : %u\n", dev->wireless.bitrate);
-      printf("active_access_point  : %s\n", dev->wireless.active_access_point);
-      printf("wireless_capabilities:");
-      if (dev->wireless.wireless_capabilities & E_NM_802_11_DEVICE_CAP_CIPHER_WEP40)
-        printf(" E_NM_802_11_DEVICE_CAP_CIPHER_WEP40");
-      if (dev->wireless.wireless_capabilities & E_NM_802_11_DEVICE_CAP_CIPHER_WEP104)
-        printf(" E_NM_802_11_DEVICE_CAP_CIPHER_WEP104");
-      if (dev->wireless.wireless_capabilities & E_NM_802_11_DEVICE_CAP_CIPHER_TKIP)
-        printf(" E_NM_802_11_DEVICE_CAP_CIPHER_TKIP");
-      if (dev->wireless.wireless_capabilities & E_NM_802_11_DEVICE_CAP_CIPHER_CCMP)
-        printf(" E_NM_802_11_DEVICE_CAP_CIPHER_CCMP");
-      if (dev->wireless.wireless_capabilities & E_NM_802_11_DEVICE_CAP_WPA)
-        printf(" E_NM_802_11_DEVICE_CAP_WPA");
-      if (dev->wireless.wireless_capabilities & E_NM_802_11_DEVICE_CAP_RSN)
-        printf(" E_NM_802_11_DEVICE_CAP_RSN");
-      if (dev->wireless.wireless_capabilities == E_NM_802_11_DEVICE_CAP_NONE)
-        printf(" E_NM_802_11_DEVICE_CAP_NONE");
-      printf("\n");
-      break;
-  }
-#endif
+  /* TODO */
   printf("\n");
 }
 
