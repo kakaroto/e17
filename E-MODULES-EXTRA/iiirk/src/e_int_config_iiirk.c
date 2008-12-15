79d3548c1b5d270b16e1e00866436495d3522f04
englebass <englebass@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2008-12-15 20:23:24 +0000

    Adding missing Name entry in efreet_menu.
    Add fix from Massimiliano Calamelli.
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@38153 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/iiirk/src/e_int_config_iiirk.c
+++ E-MODULES-EXTRA/iiirk/src/e_int_config_iiirk.c
@@ -568,7 +568,7 @@ _save_menu(E_Config_Dialog_Data *cfdata)
    Efreet_Menu *menu = NULL;
    int i, ret;
 
-   menu = efreet_menu_new();
+   menu = efreet_menu_new("Iiirk");
    for (i = 0, l = e_widget_ilist_items_get(cfdata->o_sel); l; l = l->next, i++) 
      {
 	E_Ilist_Item *item = NULL;
