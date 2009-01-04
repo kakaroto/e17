fecb6f6cf829b870d3cd25781c4e43ddc9a2a338
mickeyl <mickeyl@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-01-04 19:14:43 +0000

    fix build script so that it actually builds
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@38452 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/python/python-elementary/build.sh
+++ BINDINGS/python/python-elementary/build.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
+
 PREFIX="$HOME/usr"
+PYX_FILES="elementary/elementary.c_elementary.pyx"
 
 for arg in $@; do
     case $arg in
+        --force)
+            touch $PYX_FILES
+            ;;
         *)
             PREFIX="$arg"
             ;;
     esac
 done
 
-python setup.py install --prefix="$PREFIX"
+PYINCDIR=`python -c "import distutils.sysconfig; print distutils.sysconfig.get_python_inc()" | cut -d/ -f3-`
+INCDIR="$PREFIX/$PYINCDIR"
+
+python setup.py build_ext --include-dirs="$INCDIR" develop --prefix="$PREFIX" install_headers --install-dir="$INCDIR"
