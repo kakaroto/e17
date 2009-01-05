36d75481f4f8832590a17df24702624e86a367e9
andreas <andreas@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-01-05 22:28:42 +0000

    EtkBox implemented
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@38470 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/cxx/src/etk/eflpp_etkbox.cpp
+++ BINDINGS/cxx/src/etk/eflpp_etkbox.cpp
@@ -4,9 +4,6 @@
 
 #include "eflpp_etkbox.h"
 
-/* EFL */
-#include <etk/etk_widget.h>
-
 namespace efl {
 
 EtkBox::EtkBox( EtkObject* parent, const char* type, const char* name )
@@ -17,6 +14,71 @@ EtkBox::EtkBox( EtkObject* parent, const char* type, const char* name )
 
 EtkBox::~EtkBox()
 {
+}
+
+void EtkBox::prepend (EtkWidget &child, Etk_Box_Group group, Etk_Box_Fill_Policy fillPolicy, int padding)
+{
+    etk_box_prepend (ETK_BOX (_o), ETK_WIDGET (child.obj ()), group, fillPolicy, padding);
+}
+
+void EtkBox::append (EtkWidget &child, Etk_Box_Group group, Etk_Box_Fill_Policy fillPolicy, int padding)
+{
+    etk_box_append (ETK_BOX (_o), ETK_WIDGET (child.obj ()), group, fillPolicy, padding);
+}
+
+void EtkBox::insert (EtkWidget &child, Etk_Box_Group group, EtkWidget &after, Etk_Box_Fill_Policy fillPolicy, int padding)
+{
+    etk_box_insert (ETK_BOX (_o), ETK_WIDGET (child.obj ()), group, ETK_WIDGET (after.obj ()), fillPolicy, padding);
+}
+
+void EtkBox::insertAt (EtkWidget &child, Etk_Box_Group group, int pos, Etk_Box_Fill_Policy fillPolicy, int padding)
+{
+    etk_box_insert_at (ETK_BOX (_o), ETK_WIDGET (child.obj ()), group, pos, fillPolicy, padding);
+}
+
+/*EtkWidget *EtkBox::getChildAt (Etk_Box_Group group, int pos)
+{
+  //~ Etk_Widget *etk_box_child_get_at(ETK_BOX (_o), Etk_Box_Group group, int pos);
+}*/
+
+void EtkBox::setChildPosition (EtkWidget &child, Etk_Box_Group group, int pos)
+{
+    etk_box_child_position_set(ETK_BOX (_o), ETK_WIDGET (child.obj ()), group, pos);
+}
+
+bool EtkBox::getChildPosition (EtkWidget &child, Etk_Box_Group &outGroup, int &outPos)
+{
+    return etk_box_child_position_get(ETK_BOX (_o), ETK_WIDGET (child.obj ()), &outGroup, &outPos);
+}
+
+void EtkBox::setChildPacking (EtkWidget &child, Etk_Box_Fill_Policy fillPolicy, int padding)
+{
+    etk_box_child_packing_set(ETK_BOX (_o), ETK_WIDGET (child.obj ()), fillPolicy, padding);
+}
+
+bool EtkBox::getChildPacking (EtkWidget &child, Etk_Box_Fill_Policy &outFillPolicy, int &outPadding)
+{
+    return etk_box_child_packing_get(ETK_BOX (_o), ETK_WIDGET (child.obj ()), &outFillPolicy, &outPadding);
+}
+
+void EtkBox::setSpacing (int spacing)
+{
+    etk_box_spacing_set(ETK_BOX (_o), spacing);
+}
+
+int EtkBox::getSpacing ()
+{
+    return etk_box_spacing_get(ETK_BOX (_o));
+}
+
+void EtkBox::setHomogeneous (bool homogeneous)
+{
+    etk_box_homogeneous_set(ETK_BOX (_o), homogeneous);
+}
+
+bool EtkBox::getHomogeneous ()
+{
+    return etk_box_homogeneous_get(ETK_BOX (_o));
 }
 
 } // end namespace efl
--- BINDINGS/cxx/src/etk/eflpp_etkbox.h
+++ BINDINGS/cxx/src/etk/eflpp_etkbox.h
@@ -4,6 +4,9 @@
 /* STL */
 #include <string>
 
+/* EFL */
+#include <etk/etk_box.h>
+
 /* EFL++ */
 #include <eflpp_common.h>
 #include <eflpp_evas.h>
@@ -21,30 +24,32 @@ class EtkBox : public EtkContainer
     EtkBox( EtkObject* parent = 0, const char* type = "Box", const char* name = 0 );
     virtual ~EtkBox();
     
-    //void prepend (EtkWidget &child, Etk_Box_Group group, Etk_Box_Fill_Policy fill_policy, int padding);
-    //~ void        etk_box_prepend(Etk_Box *box, Etk_Widget *child, Etk_Box_Group group, Etk_Box_Fill_Policy fill_policy, int padding);
-    
-    //void append (EtkWidget &child, Etk_Box_Group group, Etk_Box_Fill_Policy fill_policy, int padding);
-    //~ void        etk_box_append(Etk_Box *box, Etk_Widget *child, Etk_Box_Group group, Etk_Box_Fill_Policy fill_policy, int padding);
-    
-    
-    //~ void        etk_box_insert(Etk_Box *box, Etk_Widget *child, Etk_Box_Group group, Etk_Widget *after, Etk_Box_Fill_Policy fill_policy, int padding);
-    //~ void        etk_box_insert_at(Etk_Box *box, Etk_Widget *child, Etk_Box_Group group, int pos, Etk_Box_Fill_Policy fill_policy, int padding);
-
-    //~ Etk_Widget *etk_box_child_get_at(Etk_Box *box, Etk_Box_Group group, int pos);
-    //~ void        etk_box_child_position_set(Etk_Box *box, Etk_Widget *child, Etk_Box_Group group, int pos);
-    //~ Etk_Bool    etk_box_child_position_get(Etk_Box *box, Etk_Widget *child, Etk_Box_Group *group, int *pos);
-    //~ void        etk_box_child_packing_set(Etk_Box *box, Etk_Widget *child, Etk_Box_Fill_Policy fill_policy, int padding);
-    //~ Etk_Bool    etk_box_child_packing_get(Etk_Box *box, Etk_Widget *child, Etk_Box_Fill_Policy *fill_policy, int *padding);
-
-    //~ void        etk_box_spacing_set(Etk_Box *box, int spacing);
-    //~ int         etk_box_spacing_get(Etk_Box *box);
-    //~ void        etk_box_homogeneous_set(Etk_Box *box, Etk_Bool homogeneous);
-    //~ Etk_Bool    etk_box_homogeneous_get(Etk_Box *box);
+    void prepend (EtkWidget &child, Etk_Box_Group group, Etk_Box_Fill_Policy fillPolicy, int padding);
    
-    
-};
+    void append (EtkWidget &child, Etk_Box_Group group, Etk_Box_Fill_Policy fillPolicy, int padding);
+   
+    void insert (EtkWidget &child, Etk_Box_Group group, EtkWidget &after, Etk_Box_Fill_Policy fillPolicy, int padding);
 
+    void insertAt (EtkWidget &child, Etk_Box_Group group, int pos, Etk_Box_Fill_Policy fillPolicy, int padding);
+
+    //EtkWidget *getChildAt (Etk_Box_Group group, int pos); // TODO: CountedPtr
+    
+    void setChildPosition (EtkWidget &child, Etk_Box_Group group, int pos);
+    
+    bool getChildPosition (EtkWidget &child, Etk_Box_Group &outGroup, int &outPos);
+    
+    void setChildPacking (EtkWidget &child, Etk_Box_Fill_Policy fillPolicy, int padding);
+    
+    bool getChildPacking (EtkWidget &child, Etk_Box_Fill_Policy &outFillPolicy, int &outPadding);
+
+    void setSpacing (int spacing);
+    
+    int getSpacing ();
+    
+    void setHomogeneous (bool homogeneous);
+    
+    bool getHomogeneous (); 
+};
 
 } // end namespace efl
 
