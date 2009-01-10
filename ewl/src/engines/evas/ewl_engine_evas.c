5ffd0f1bb6c548673600a2208669193efabd653e
pfritz <pfritz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-01-10 17:18:41 +0000

    remove that stuff for now, it isn't used yet, and will be dated soon
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@38538 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- ewl/src/engines/evas/ewl_engine_evas.c
+++ ewl/src/engines/evas/ewl_engine_evas.c
@@ -8,8 +8,6 @@
 
 #include <Edje.h>
 
-static Evas_Smart *widget_smart = NULL;
-
 static int ee_init(Ewl_Engine *engine);
 static void ee_shutdown(Ewl_Engine *engine);
 
@@ -19,13 +17,6 @@ static void ee_canvas_render(Ewl_Embed *embed);
 static void ee_canvas_freeze(Ewl_Embed *embed);
 static void ee_canvas_thaw(Ewl_Embed *embed);
 static void ee_canvas_damage_add(Ewl_Embed *embed, int x, int y, int w, int h);
-static void *ee_canvas_smart_new(Ewl_Embed *embed);
-static void *ee_canvas_clip_add(Ewl_Embed *embed);
-static void ee_canvas_stack_add(Ewl_Widget *w);
-static void ee_canvas_layer_update(Ewl_Widget *w);
-
-static Evas_Object *ewl_widget_layer_neighbor_find_above(Ewl_Widget *w);
-static Evas_Object *ewl_widget_layer_neighbor_find_below(Ewl_Widget *w);
 
 static void *canvas_funcs[EWL_ENGINE_CANVAS_MAX] =
         {
@@ -46,7 +37,7 @@ static void *theme_funcs[EWL_ENGINE_THEME_MAX] =
                 NULL, //edje_freeze,
                 NULL, //edje_thaw,
                 edje_file_data_get,
-                ee_canvas_smart_new,
+                NULL,
                 NULL,
                 evas_object_del,
                 evas_object_move,
@@ -58,12 +49,12 @@ static void *theme_funcs[EWL_ENGINE_THEME_MAX] =
                 NULL, NULL,
                 NULL, NULL,
                 NULL, NULL, NULL,
-                ee_canvas_clip_add,
+                NULL,
                 evas_object_del,
                 evas_object_clipees_get,
                 evas_object_color_set,
-                ee_canvas_stack_add,
-                ee_canvas_layer_update,
+                NULL,
+                NULL,
         };
 
 Ecore_DList *
@@ -179,188 +170,3 @@ ee_canvas_damage_add(Ewl_Embed *embed, int x, int y, int w, int h)
         DLEAVE_FUNCTION(DLEVEL_STABLE);
 }
 
-static void *
-ee_canvas_smart_new(Ewl_Embed *embed)
-{
-        Evas_Object *smart_object;
-
-        DENTER_FUNCTION(DLEVEL_STABLE);
-        DCHECK_PARAM_PTR_RET(embed, NULL);
-
-        if (!widget_smart) {
-                widget_smart = evas_smart_new("Ewl Widget Smart Object",
-                                NULL, NULL, NULL, NULL,
-                                NULL, NULL, NULL, NULL,
-                                NULL, NULL, NULL, NULL,
-                                NULL, NULL, NULL);
-        }
-
-        smart_object = evas_object_smart_add(embed->canvas, widget_smart);
-        DRETURN_PTR(smart_object, DLEVEL_STABLE);
-}
-
-static void *
-ee_canvas_clip_add(Ewl_Embed *embed)
-{
-        Evas_Object *obj;
-
-        DENTER_FUNCTION(DLEVEL_STABLE);
-        DCHECK_PARAM_PTR_RET(embed, NULL);
-
-        obj = evas_object_rectangle_add(embed->canvas);
-        if (obj)
-                evas_object_pass_events_set(obj, TRUE);
-
-        DRETURN_PTR(obj, DLEVEL_STABLE);
-}
-
-static void
-ee_canvas_stack_add(Ewl_Widget *w)
-{
-        Evas_Object *smart_parent;
-
-        DENTER_FUNCTION(DLEVEL_STABLE);
-        DCHECK_PARAM_PTR(w);
-        DCHECK_TYPE(w, EWL_WIDGET_TYPE);
-
-        if (w->parent && !REVEALED(w->parent))
-                DRETURN(DLEVEL_STABLE);
-
-
-        if (w->parent && !TOPLAYERED(w))
-                smart_parent = w->parent->smart_object;
-        else {
-                Ewl_Embed *emb;
-
-                emb = ewl_embed_widget_find(w);
-                smart_parent = emb->smart;
-        }
-
-        evas_object_smart_member_add(w->smart_object, smart_parent);
-
-        if (w->theme_object)
-                evas_object_smart_member_add(w->theme_object, w->smart_object);
-
-        if (w->fx_clip_box)
-                evas_object_smart_member_add(w->fx_clip_box, w->smart_object);
-
-        if (w->theme_object && w->fx_clip_box)
-                evas_object_stack_below(w->theme_object, w->fx_clip_box);
-
-        DLEAVE_FUNCTION(DLEVEL_STABLE);
-}
-
-static void
-ee_canvas_layer_update(Ewl_Widget *w)
-{
-        Ewl_Widget *p;
-        int layer;
-
-        DENTER_FUNCTION(DLEVEL_STABLE);
-        DCHECK_PARAM_PTR(w);
-        DCHECK_TYPE(w, EWL_WIDGET_TYPE);
-
-        if (!(p = w->parent))
-                DRETURN(DLEVEL_STABLE);
-
-        /* check first if the widget should be on the top */
-        if (TOPLAYERED(w))
-        {
-                evas_object_raise(w->smart_object);
-                DRETURN(DLEVEL_STABLE);
-        }
-
-        layer = ewl_widget_layer_priority_get(w);
-        if (layer == 0)
-                evas_object_stack_above(w->smart_object, p->fx_clip_box);
-        else if (layer > 0) {
-                Evas_Object *above;
-
-                if (!(above = ewl_widget_layer_neighbor_find_above(w)))
-                        DWARNING("No object above.");
-
-                evas_object_stack_above(w->smart_object, above);
-        }
-        else {
-                Evas_Object *below;
-
-                if (!(below = ewl_widget_layer_neighbor_find_below(w)))
-                        DWARNING("No object below.");
-
-                evas_object_stack_below(w->smart_object, below);
-        }
-
-        DLEAVE_FUNCTION(DLEVEL_STABLE);
-}
-
-static Evas_Object *
-ewl_widget_layer_neighbor_find_above(Ewl_Widget *w)
-{
-        Evas_Object *o, *ol;
-
-        DENTER_FUNCTION(DLEVEL_STABLE);
-        DCHECK_PARAM_PTR_RET(w, NULL);
-        DCHECK_TYPE_RET(w, EWL_WIDGET_TYPE, NULL);
-
-        if (!w->parent)
-                DRETURN_PTR(NULL, DLEVEL_STABLE);
-
-        o = ol = w->parent->fx_clip_box;
-
-        while ((o = evas_object_above_get(o)))
-        {
-                Ewl_Widget *found;
-
-                found = evas_object_data_get(o, "EWL");
-                /*
-                 * Perhaps it is a cached object so no reason to stop iterating
-                 */
-                if (found) {
-                        /* ignore the widget itself */
-                        if (w == found)
-                                continue;
-                        if (ewl_widget_layer_priority_get(w) <=
-                                        ewl_widget_layer_priority_get(found))
-                                break;
-                        ol = o;
-                }
-        }
-
-        DRETURN_PTR(ol, DLEVEL_STABLE);
-}
-
-static Evas_Object *
-ewl_widget_layer_neighbor_find_below(Ewl_Widget *w)
-{
-        Evas_Object *o, *ol;
-
-        DENTER_FUNCTION(DLEVEL_STABLE);
-        DCHECK_PARAM_PTR_RET(w, NULL);
-        DCHECK_TYPE_RET(w, EWL_WIDGET_TYPE, NULL);
-
-        if (!w->parent)
-                DRETURN_PTR(NULL, DLEVEL_STABLE);
-
-        o = ol = w->parent->fx_clip_box;
-
-        while ((o = evas_object_below_get(o)))
-        {
-                Ewl_Widget *found;
-
-                found = evas_object_data_get(o, "EWL");
-                /*
-                 * Perhaps it is a cached object so no reason to stop iterating
-                 */
-                if (found) {
-                        /* ignore the widget itself */
-                        if (w == found)
-                                continue;
-                        if (ewl_widget_layer_priority_get(w) >=
-                                        ewl_widget_layer_priority_get(found))
-                                break;
-                        ol = o;
-                }
-        }
-
-        DRETURN_PTR(ol, DLEVEL_STABLE);
-}
