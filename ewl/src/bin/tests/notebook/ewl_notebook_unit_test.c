befa9b21e672de2b3ab4050a21b3b8dc63c7a75c
pfritz <pfritz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-03-05 21:51:16 +0000

    add unit test for the visible pages of a notebook
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@39383 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- ewl/src/bin/tests/notebook/ewl_notebook_unit_test.c
+++ ewl/src/bin/tests/notebook/ewl_notebook_unit_test.c
@@ -7,9 +7,11 @@
 #include <stdlib.h>
 
 static int test_constructor(char *buf, int len);
+static int test_visible_page(char *buf, int len);
 
 Ewl_Unit_Test notebook_unit_tests[] = {
                 {"constructor", test_constructor, NULL, -1, 0},
+                {"visible page", test_visible_page, NULL, -1, 0},
                 {NULL, NULL, NULL, -1, 0}
         };
 
@@ -42,3 +44,108 @@ test_constructor(char *buf, int len)
         return ret;
 }
 
+static int
+test_visible_page(char *buf, int len)
+{
+        Ewl_Widget *notebook;
+        Ewl_Widget *w1, *w2, *w3;
+        int ret = 0;
+
+        notebook = ewl_notebook_new();
+
+        /* before every thing the visible page has to be NULL */
+        if (ewl_notebook_visible_page_get(EWL_NOTEBOOK(notebook)))
+        {
+                LOG_FAILURE(buf, len, "there is a visible page, although"
+                               " there is no page");
+                goto CLEANUP;
+        }
+
+        /* append the first widget */
+        w1 = ewl_cell_new();
+        ewl_container_child_append(EWL_CONTAINER(notebook), w1);
+        
+        if (ewl_notebook_visible_page_get(EWL_NOTEBOOK(notebook)) != w1)
+        {
+                LOG_FAILURE(buf, len, "the visible page is not the newly "
+                                "added widget");
+                goto CLEANUP;
+        }
+        
+        /* append the second widget */
+        w2 = ewl_cell_new();
+        ewl_container_child_append(EWL_CONTAINER(notebook), w2);
+        
+        if (ewl_notebook_visible_page_get(EWL_NOTEBOOK(notebook)) != w1)
+        {
+                LOG_FAILURE(buf, len, "the visible page is not the first "
+                                "added widget");
+                goto CLEANUP;
+        }
+        
+        /* prepend the third widget */
+        w3 = ewl_cell_new();
+        ewl_container_child_append(EWL_CONTAINER(notebook), w3);
+        
+        if (ewl_notebook_visible_page_get(EWL_NOTEBOOK(notebook)) != w1)
+        {
+                LOG_FAILURE(buf, len, "the visible page is not the first "
+                                "added widget");
+                goto CLEANUP;
+        }
+
+        ewl_widget_destroy(w2);
+        if (ewl_notebook_visible_page_get(EWL_NOTEBOOK(notebook)) != w1)
+        {
+                LOG_FAILURE(buf, len, "the visible page is not the first "
+                                "added widget");
+                goto CLEANUP;
+        }
+
+        ewl_widget_destroy(w3);
+        if (ewl_notebook_visible_page_get(EWL_NOTEBOOK(notebook)) != w1)
+        {
+                LOG_FAILURE(buf, len, "the visible page is not the first "
+                                "added widget");
+                goto CLEANUP;
+        }
+        
+        ewl_widget_destroy(w1);
+        if (ewl_notebook_visible_page_get(EWL_NOTEBOOK(notebook)))
+        {
+                LOG_FAILURE(buf, len, "there is a visible page, although "
+                                "the notebook is empty");
+                goto CLEANUP;
+        }
+        
+        /* and now test the set function */
+        w1 = ewl_cell_new();
+        ewl_container_child_append(EWL_CONTAINER(notebook), w1);
+        w2 = ewl_cell_new();
+        ewl_container_child_prepend(EWL_CONTAINER(notebook), w2);
+        w3 = ewl_cell_new();
+        ewl_container_child_append(EWL_CONTAINER(notebook), w3);
+
+        ewl_notebook_visible_page_set(EWL_NOTEBOOK(notebook), w2);
+        if (ewl_notebook_visible_page_get(EWL_NOTEBOOK(notebook)) != w2)
+        {
+                LOG_FAILURE(buf, len, "the visible page is not the second "
+                                "added widget");
+                goto CLEANUP;
+        }
+        
+        ewl_notebook_visible_page_set(EWL_NOTEBOOK(notebook), w3);
+        if (ewl_notebook_visible_page_get(EWL_NOTEBOOK(notebook)) != w3)
+        {
+                LOG_FAILURE(buf, len, "the visible page is not the third "
+                                "added widget");
+                goto CLEANUP;
+        }
+
+        ret = 1;
+
+CLEANUP:
+        ewl_widget_destroy(notebook);
+
+        return ret;
+}
