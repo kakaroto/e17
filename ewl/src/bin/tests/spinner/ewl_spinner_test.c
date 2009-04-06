daa2a32aecef38e3c63757da972f7a6b605b29f8
pfritz <pfritz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-04-06 10:31:09 +0000

    simplify and beautify the spinner test
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@39877 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- ewl/src/bin/tests/spinner/ewl_spinner_test.c
+++ ewl/src/bin/tests/spinner/ewl_spinner_test.c
@@ -2,9 +2,9 @@
 #include "Ewl_Test.h"
 #include "ewl_test_private.h"
 #include "ewl_button.h"
-#include "ewl_separator.h"
 #include "ewl_spinner.h"
-#include "ewl_text.h"
+#include "ewl_grid.h"
+#include "ewl_label.h"
 
 #include <stdio.h>
 
@@ -30,106 +30,53 @@ test_info(Ewl_Test *test)
 static int
 create_test(Ewl_Container *box)
 {
-        Ewl_Widget *spinner_row, *text, *separator;
+        Ewl_Widget *grid, *button;
+        int i;
+        const struct
+        {
+                const char *label;
+                double min, max, step;
+        } spinner_vals[] = {
+                {"X", 0.0, 1280.0, 1.0},
+                {"Y", 0.0, 1280.0, 1.0},
+                {"W", 0.0, 1280.0, 1.0},
+                {"H", 0.0, 1280.0, 1.0},
+                {NULL, 0.0, 0.0, 0.0}
+        };
 
-        spinner_row = ewl_hbox_new();
-        ewl_container_child_append(EWL_CONTAINER(box), spinner_row);
-        ewl_widget_show(spinner_row);
+        grid = ewl_grid_new();
+        ewl_grid_dimensions_set(EWL_GRID(grid), 2, 3);
+        ewl_grid_column_preferred_w_use(EWL_GRID(grid), 0);
+        ewl_container_child_append(EWL_CONTAINER(box), grid);
+        ewl_widget_show(grid);
 
-        text = ewl_text_new();
-        ewl_text_text_set(EWL_TEXT(text), "X");
-        ewl_object_fill_policy_set(EWL_OBJECT(text), EWL_FLAG_FILL_NONE);
-        ewl_object_alignment_set(EWL_OBJECT(text), EWL_FLAG_ALIGN_CENTER);
-        ewl_container_child_append(EWL_CONTAINER(spinner_row), text);
-        ewl_widget_show(text);
+        for (i = 0; spinner_vals[i].label; i++)
+        {
+                Ewl_Widget *label;
 
-        spinner[0] = ewl_spinner_new();
-        ewl_spinner_digits_set(EWL_SPINNER(spinner[0]), 0);
-        ewl_range_minimum_value_set(EWL_RANGE(spinner[0]), 0);
-        ewl_range_maximum_value_set(EWL_RANGE(spinner[0]), 1280);
-        ewl_range_value_set(EWL_RANGE(spinner[0]), 0.0);
-        ewl_range_step_set(EWL_RANGE(spinner[0]), 1.0);
-        ewl_container_child_append(EWL_CONTAINER(spinner_row), spinner[0]);
-        ewl_widget_show(spinner[0]);
+                label = ewl_label_new();
+                ewl_label_text_set(EWL_LABEL(label), spinner_vals[i].label);
+                ewl_container_child_append(EWL_CONTAINER(grid), label);
+                ewl_widget_show(label);
 
-        separator = ewl_hseparator_new();
-        ewl_container_child_append(EWL_CONTAINER(box), separator);
-        ewl_widget_show(separator);
+                spinner[i] = ewl_spinner_new();
+                ewl_spinner_digits_set(EWL_SPINNER(spinner[i]), 0);
+                ewl_range_minimum_value_set(EWL_RANGE(spinner[i]), 
+                                spinner_vals[i].min);
+                ewl_range_maximum_value_set(EWL_RANGE(spinner[i]),
+                                spinner_vals[i].max);
+                ewl_range_step_set(EWL_RANGE(spinner[i]),
+                                spinner_vals[i].step);
+                ewl_container_child_append(EWL_CONTAINER(grid), spinner[i]);
+                ewl_widget_show(spinner[i]);
+        }
 
-        spinner_row = ewl_hbox_new();
-        ewl_container_child_append(EWL_CONTAINER(box), spinner_row);
-        ewl_widget_show(spinner_row);
-
-        text = ewl_text_new();
-        ewl_text_text_set(EWL_TEXT(text), "Y");
-        ewl_object_fill_policy_set(EWL_OBJECT(text), EWL_FLAG_FILL_NONE);
-        ewl_object_alignment_set(EWL_OBJECT(text), EWL_FLAG_ALIGN_CENTER);
-        ewl_container_child_append(EWL_CONTAINER(spinner_row), text);
-        ewl_widget_show(text);
-
-        spinner[1] = ewl_spinner_new();
-        ewl_spinner_digits_set(EWL_SPINNER(spinner[1]), 0);
-        ewl_range_minimum_value_set(EWL_RANGE(spinner[1]), 0);
-        ewl_range_maximum_value_set(EWL_RANGE(spinner[1]), 1024);
-        ewl_range_value_set(EWL_RANGE(spinner[1]), 0.0);
-        ewl_range_step_set(EWL_RANGE(spinner[1]), 1.0);
-        ewl_container_child_append(EWL_CONTAINER(spinner_row), spinner[1]);
-        ewl_widget_show(spinner[1]);
-
-        separator = ewl_hseparator_new();
-        ewl_container_child_append(EWL_CONTAINER(box), separator);
-        ewl_widget_show(separator);
-
-        spinner_row = ewl_hbox_new();
-        ewl_container_child_append(EWL_CONTAINER(box), spinner_row);
-        ewl_widget_show(spinner_row);
-
-        text = ewl_text_new();
-        ewl_text_text_set(EWL_TEXT(text), "W");
-        ewl_object_fill_policy_set(EWL_OBJECT(text), EWL_FLAG_FILL_NONE);
-        ewl_object_alignment_set(EWL_OBJECT(text), EWL_FLAG_ALIGN_CENTER);
-        ewl_container_child_append(EWL_CONTAINER(spinner_row), text);
-        ewl_widget_show(text);
-
-        spinner[2] = ewl_spinner_new();
-        ewl_spinner_digits_set(EWL_SPINNER(spinner[2]), 0);
-        ewl_range_minimum_value_set(EWL_RANGE(spinner[2]), 0.0);
-        ewl_range_maximum_value_set(EWL_RANGE(spinner[2]), 1280);
-        ewl_range_value_set(EWL_RANGE(spinner[2]), 0.0);
-        ewl_range_step_set(EWL_RANGE(spinner[2]), 1.0);
-        ewl_container_child_append(EWL_CONTAINER(spinner_row), spinner[2]);
-        ewl_widget_show(spinner[2]);
-
-        separator = ewl_hseparator_new();
-        ewl_container_child_append(EWL_CONTAINER(box), separator);
-        ewl_widget_show(separator);
-
-        spinner_row = ewl_hbox_new();
-        ewl_container_child_append(EWL_CONTAINER(box), spinner_row);
-        ewl_widget_show(spinner_row);
-
-        text = ewl_text_new();
-        ewl_text_text_set(EWL_TEXT(text), "H");
-        ewl_object_fill_policy_set(EWL_OBJECT(text), EWL_FLAG_FILL_NONE);
-        ewl_object_alignment_set(EWL_OBJECT(text), EWL_FLAG_ALIGN_CENTER);
-        ewl_container_child_append(EWL_CONTAINER(spinner_row), text);
-        ewl_widget_show(text);
-
-        spinner[3] = ewl_spinner_new();
-        ewl_spinner_digits_set(EWL_SPINNER(spinner[3]), 0);
-        ewl_range_minimum_value_set(EWL_RANGE(spinner[3]), 0.0);
-        ewl_range_maximum_value_set(EWL_RANGE(spinner[3]), 1024);
-        ewl_range_value_set(EWL_RANGE(spinner[3]), 0.0);
-        ewl_range_step_set(EWL_RANGE(spinner[3]), 1.0);
-        ewl_container_child_append(EWL_CONTAINER(spinner_row), spinner[3]);
-        ewl_widget_show(spinner[3]);
-
-        separator = ewl_button_new();
-        ewl_button_label_set(EWL_BUTTON(separator), "Get Values");
-        ewl_container_child_append(EWL_CONTAINER(box), separator);
-        ewl_callback_append(separator, EWL_CALLBACK_CLICKED,
+        button = ewl_button_new();
+        ewl_button_label_set(EWL_BUTTON(button), "Get Values");
+        ewl_container_child_append(EWL_CONTAINER(box), button);
+        ewl_callback_append(button, EWL_CALLBACK_CLICKED,
                                         cb_value_show, NULL);
-        ewl_widget_show(separator);
+        ewl_widget_show(button);
 
         return 1;
 }
