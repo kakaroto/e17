d5620f4b512d80db5ab80373ea3247c0401b6536
quaker <quaker@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-04-28 13:54:15 +0000

    Updated gen-doc-api for python*
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@40439 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/python/python-ecore/gen-doc-api.sh
+++ BINDINGS/python/python-ecore/gen-doc-api.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+pyver=`python -c 'import sys; print "%d.%d" % sys.version_info[0:2]'`
+regex=lib\.[a-z0-9]*-[a-z0-9]*-$pyver
+
+for d in `ls build`; do
+    if [[ $d == $regex ]]; then
+        break
+    fi
+done
+
+export PYTHONPATH="`pwd`/build/$d"
 
 epydoc --html -o doc/api/html \
     -n "Python-Ecore API" \
