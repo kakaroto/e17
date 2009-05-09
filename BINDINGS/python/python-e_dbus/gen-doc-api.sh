0710b0d6b641a80e3e61f13c4ae0cb57f99f60c3
rbelem <rbelem@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-05-09 02:23:49 +0000

    Checking for the documetation dir before python-* doc generation
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@40561 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/python/python-e_dbus/gen-doc-api.sh
+++ BINDINGS/python/python-e_dbus/gen-doc-api.sh
@@ -2,6 +2,7 @@
 
 pyver=`python -c 'import sys; print "%d.%d" % sys.version_info[0:2]'`
 regex=lib\.[a-z0-9]*-[a-z0-9]*-$pyver
+destdir="doc/api/html"
 
 for d in `ls build`; do
     if [[ $d == $regex ]]; then
@@ -11,6 +12,10 @@ done
 
 export PYTHONPATH="`pwd`/build/$d"
 
-epydoc --html -o doc/api/html \
+if [ -d $destdir ]; then
+    mkdir -p $destdir
+fi
+
+epydoc --html -o $destdir \
     -n "Python-E_Dbus API" \
     e_dbus
--- BINDINGS/python/python-epsilon/gen-doc-api.sh
+++ BINDINGS/python/python-epsilon/gen-doc-api.sh
@@ -2,6 +2,7 @@
 
 pyver=`python -c 'import sys; print "%d.%d" % sys.version_info[0:2]'`
 regex=lib\.[a-z0-9]*-[a-z0-9]*-$pyver
+destdir="doc/api/html"
 
 for d in `ls build`; do
     if [[ $d == $regex ]]; then
@@ -11,7 +12,11 @@ done
 
 export PYTHONPATH="`pwd`/build/$d"
 
-epydoc --html -o doc/api/html \
+if [ -d $destdir ]; then
+    mkdir -p $destdir
+fi
+
+epydoc --html -o $destdir \
     -n "Python-Epsilon API" \
     epsilon.c_epsilon \
     epsilon.request
