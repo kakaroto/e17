6b3397dd994d089d1ebc720d343a518045db3653
urandom <urandom@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-05-31 23:13:44 +0000

    the dirwatcher plugin will now start the selected file with the default application known by e
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@40870 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/drawer/src/plugins/sources/directory_watcher.c
+++ E-MODULES-EXTRA/drawer/src/plugins/sources/directory_watcher.c
@@ -215,6 +215,7 @@ EAPI void
 drawer_source_activate(Drawer_Source *s, Drawer_Source_Item *si, E_Zone *zone)
 {
    Dirwatcher_Priv *p = NULL;
+   Instance *inst = DRAWER_PLUGIN(s)->data;
 
    if (si->data_type != SOURCE_DATA_TYPE_FILE_PATH)
      return;
@@ -224,11 +225,11 @@ drawer_source_activate(Drawer_Source *s, Drawer_Source_Item *si, E_Zone *zone)
      return _dirwatcher_directory_activate(p->inst, zone, si->data);
    if (si->data)
      {
+        Efreet_Desktop *desktop;
+
 	if ((e_util_glob_case_match(si->data, "*.desktop")) ||
 	    (e_util_glob_case_match(si->data, "*.directory")))
 	  {
-	     Efreet_Desktop *desktop;
-
 	     desktop = efreet_desktop_new(si->data);
 	     if (!desktop) return;
 
@@ -238,7 +239,25 @@ drawer_source_activate(Drawer_Source *s, Drawer_Source_Item *si, E_Zone *zone)
 	       e_exehist_mime_desktop_add(p->mime, desktop);
 
 	     efreet_desktop_free(desktop);
+             return;
 	  }
+        else if (p->mime)
+          {
+             char pcwd[4096];
+             Eina_List *files = NULL;
+
+             desktop = e_exehist_mime_desktop_get(p->mime);
+             if (!desktop) return;
+
+	     getcwd(pcwd, sizeof(pcwd));
+	     chdir(inst->conf->dir);
+
+             files = eina_list_append(files, si->data);
+             e_exec(zone, desktop, NULL, files, "drawer");
+             eina_list_free(files);
+
+	     chdir(pcwd);
+          }
 
 	/* XXX: open the file with the default application */
 	return;
