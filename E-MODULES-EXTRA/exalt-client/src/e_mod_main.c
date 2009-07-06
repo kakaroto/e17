b04ea92ccef56ea4a09ff9cd0286c3f6955aad84
watchwolf <watchwolf@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-07-06 12:32:51 +0000

    fix compilation problem
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@41255 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/exalt-client/src/e_mod_main.c
+++ E-MODULES-EXTRA/exalt-client/src/e_mod_main.c
@@ -28,7 +28,7 @@ static const E_Gadcon_Client_Class _gc_class =
     GADCON_CLIENT_CLASS_VERSION, "exalt",
     {
        _gc_init, _gc_shutdown, _gc_orient, _gc_label, _gc_icon,
-       _gc_id_new, NULL, e_gadcon_site_is_not_toolbar
+       _gc_id_new, NULL
     },
     E_GADCON_CLIENT_STYLE_PLAIN
 };
