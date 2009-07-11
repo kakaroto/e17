8ddc07bc0d5bc084302dab0528aeb37ca772f475
quaker <quaker@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-07-11 09:01:57 +0000

    Empower: Add gettext support.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@41301 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- empower/autogen.sh
+++ empower/autogen.sh
@@ -4,10 +4,12 @@ rm -rf autom4te.cache
 rm -f aclocal.m4 ltmain.sh
 
 touch README
+touch ABOUT-NLS
 
-echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
-echo "Running autoheader..." ; autoheader || exit 1
+echo "Running autopoint..." ; autopoint -f || :
+echo "Running aclocal..." ; aclocal -I m4 $ACLOCAL_FLAGS || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
+echo "Running autoheader..." ; autoheader || exit 1
 echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
