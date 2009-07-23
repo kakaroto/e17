d3a7fe1cf22e71bac9d693127340a728ac1ef700
quaker <quaker@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-07-23 06:01:04 +0000

    Updated script
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@41477 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- packaging/debian/e17builder.sh
+++ packaging/debian/e17builder.sh
@@ -353,8 +353,8 @@ for preparelist in ${distros[@]}; do
 	fi
 	echo "Preparing pool dir..."
 	cp -rf $preparepath/* $distro/pool/$(echo $preparelist | sed 's/#.*//')/binaries-$currentversion/extras
-	cp -rf $distro/pool/$(echo $preparelist | sed 's/#.*//')/binaries-$currentversion/extras/{eina,eet,evas,ecore,embryo,edje,e_dbus,efreet,e} $distro/pool/$(echo $preparelist | sed 's/#.*//')/binaries-$currentversion/main
-	rm -rf $distro/pool/$(echo $preparelist | sed 's/#.*//')/binaries-$currentversion/extras/{eina,eet,evas,ecore,embryo,edje,e_dbus,efreet,e}
+	cp -rf $distro/pool/$(echo $preparelist | sed 's/#.*//')/binaries-$currentversion/extras/{eina,eet,evas,ecore,embryo,edje,e_dbus,efreet,e,elementary} $distro/pool/$(echo $preparelist | sed 's/#.*//')/binaries-$currentversion/main
+	rm -rf $distro/pool/$(echo $preparelist | sed 's/#.*//')/binaries-$currentversion/extras/{eina,eet,evas,ecore,embryo,edje,e_dbus,efreet,e,elementary}
 	echo "Done. If you have binaries for all architectures in the right place, run script with --database. Don't make databases if you have only one arch in the folder, because there are also some files with architecture all, they will be rewritten when you will compile another arch and they will have different checksums."
 done
 }
