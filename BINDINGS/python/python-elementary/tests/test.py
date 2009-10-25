2f2b1c3b9eb5645f8a1d2aceba179a75830309cf
billiob <billiob@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-10-25 16:17:05 +0000

    pyelm: add separator test case
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@43272 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/python/python-elementary/tests/test.py
+++ BINDINGS/python/python-elementary/tests/test.py
@@ -1693,6 +1693,72 @@ def fileselector_clicked(obj, event, data):
     win.show()
 # }}}
 
+#----- Separator -{{{-
+def separator_clicked(obj, event, data):
+    win = elementary.Window("separators", elementary.ELM_WIN_BASIC)
+    win.title_set("Separators")
+    win.autodel_set(True)
+
+    bg = elementary.Background(win)
+    win.resize_object_add(bg)
+    bg.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    bg.show()
+
+    bx0 = elementary.Box(win)
+    bx0.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    bx0.horizontal_set(True)
+    win.resize_object_add(bx0)
+    bx0.show()
+
+    bx = elementary.Box(win)
+    bx.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    bx0.pack_end(bx)
+    bx.show()
+
+    bt = elementary.Button(win)
+    bt.label_set("Left upper corner")
+    bx.pack_end(bt)
+    bt.show()
+
+    sp = elementary.Separator(win)
+    sp.horizontal_set(True)
+    bx.pack_end(sp)
+    sp.show()
+
+    bt = elementary.Button(win)
+    bt.label_set("Left lower corner")
+    bt.disabled_set(True)
+    bx.pack_end(bt)
+    bt.show()
+
+    sp = elementary.Separator(win)
+    bx0.pack_end(sp)
+    sp.show()
+
+    bx = elementary.Box(win)
+    bx.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    bx0.pack_end(bx)
+    bx.show()
+
+    bt = elementary.Button(win)
+    bt.label_set("Right upper corner")
+    bt.disabled_set(True)
+    bx.pack_end(bt)
+    bt.show()
+
+    sp = elementary.Separator(win)
+    sp.horizontal_set(True)
+    bx.pack_end(sp)
+    sp.show()
+
+    bt = elementary.Button(win)
+    bt.label_set("Right lower corner")
+    bx.pack_end(bt)
+    bt.show()
+
+    win.show()
+# }}}
+
 # ----- Progressbar {{{
 my_progressbar_run = False
 my_progressbar_timer = None
@@ -1921,6 +1987,7 @@ if __name__ == "__main__":
                ("Pager", pager_clicked),
                ("Progressbar", progressbar_clicked),
                ("File selector", fileselector_clicked),
+               ("Separator", separator_clicked),
                ("Spinner", spinner_clicked)]
 
 
