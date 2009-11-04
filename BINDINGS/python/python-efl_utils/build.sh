85eeb601fd2ca273b2d123887f51d189f15958f9
barbieri <barbieri@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-11-04 10:56:42 +0000

    better python helper scripts, specially for new users.
    This new scripts supports:
    
       * --help
       * --home to force installing to $HOME/usr
       * --system to force installing to /usr
       * automatically checks for cython
       * automatically checks for pyrex
       * checks for write access to $PREFIX
       * checks for existence of $PYLIBDIR
       * checks for $HOME/usr in $PYTHONPATH before trying to install to it
       * log to files for less output during compiles
    
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@43447 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/python/python-efl_utils/build.sh
+++ BINDINGS/python/python-efl_utils/build.sh
@@ -1,17 +1,89 @@
 #!/bin/sh
 
-case $1 in
-	--force) 
-	PREFIX="$2"
-	[ -z "$PREFIX" ] && PREFIX="$HOME/usr"
-	;;
-	*)
-	PREFIX="$1"
-	[ -z "$PREFIX" ] && PREFIX="$HOME/usr"
-	;;
-esac
+usage() {
+    cat <<EOF
+Usage:
 
-python setup.py build || exit 1
-python setup.py install --prefix=$PREFIX || exit 1
+    $0 <options>
 
-exit 0
+where options:
+
+    --force       force rebuild of whole tree, will not trust cython
+                  dependency checking.
+    --system      install to /usr
+    --home        install to $HOME/usr
+    --prefix=XXX  install to XXX
+
+EOF
+}
+
+die() {
+    echo "ERROR: $*"
+    exit 1
+}
+
+PREFIX=""
+for arg in $@; do
+    case $arg in
+        --force)
+            touch $PYX_FILES
+            ;;
+        --system)
+            PREFIX="/usr"
+            ;;
+        --home)
+            PREFIX="$HOME/usr"
+            ;;
+        --help)
+            usage
+            exit 0
+            ;;
+        --prefix=*)
+            PREFIX=`echo "$arg" | cut -d= -f2`
+            ;;
+        *)
+            die "unknown option $arg"
+            ;;
+    esac
+done
+
+if [ -z "$PREFIX" ]; then
+    if [ ! -z "$PYTHONPATH" ] && \
+        echo "$PYTHONPATH" | tr ':' '\n' | grep -e "^$HOME/usr" >/dev/null 2>/dev/null; then
+        PREFIX="$HOME/usr"
+    else
+        PREFIX="/usr"
+    fi
+
+    echo "INFO: no --system, --home or explicit prefix given, using $PREFIX"
+fi
+
+PYLIBDIR=`python -c "import distutils.sysconfig; print distutils.sysconfig.get_python_lib(prefix='$PREFIX')"`
+
+if ! touch "$PREFIX" 2>/dev/null; then
+    die "no permission to write to $PREFIX. needs sudo?"
+fi
+if [ ! -d "PYLIBDIR" ] && ! mkdir -p $PYLIBDIR 2>/dev/null; then
+    die "could not create $PYLIBDIR"
+fi
+
+if [ -z "$TMPDIR" ]; then
+    TMPDIR="/tmp"
+fi
+DNAME=`dirname $0`
+if [ "$DNAME" = "." ]; then
+    DNAME="$PWD"
+fi
+PRJ=`basename $DNAME`
+LOG="$TMPDIR/$PRJ"
+LOG_OUT="$LOG.out"
+LOG_ERR="$LOG.err"
+
+echo ""
+echo "install: $PRJ prefix=$PREFIX"
+python setup.py build install --prefix=$PREFIX >"$LOG_OUT" 2>"$LOG_ERR"
+if [ $? -eq 0 ]; then
+    echo "successfully installed, see $LOG_OUT and $LOG_ERR"
+else
+    echo "failed to install, see $LOG_OUT and $LOG_ERR"
+fi
