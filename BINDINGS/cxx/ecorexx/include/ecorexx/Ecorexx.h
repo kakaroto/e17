ff0a72560231483ac337b5acd3dad015a428b8be
andreas <andreas@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-11-15 21:53:43 +0000

    new header file
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@43718 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ BINDINGS/cxx/ecorexx/include/ecorexx/Ecorexx.h
@@ -0,0 +1,19 @@
+#ifndef ECOREXX_H
+#define ECOREXX_H
+
+/* This is the main include header for Ecorexx. You should include it in your
+ * application. you could also use a combination of some headers and forward
+ * declarations to speed up compiling. But the default way is to simply 
+ * #include "Ecorexx.h".
+ */
+
+#include "EcoreAnimator.h"
+#include "EcoreApplication.h"
+#include "EcoreConfig.h"
+#include "EcoreEvasWindow.h"
+#include "EcoreList.h"
+#include "EcoreTimer.h"
+#include "EcoreXWindow.h"
+
+#endif // ECOREXX_H
+
