fee9a80c09915f5b33f934c6768b0e938c5ddd70
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-12-12 01:29:10 +0000

    Add files for 'container' widget (not complete yet)
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@44402 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ PROTO/elm_illume/src/bin/elm_ill_container.c
@@ -0,0 +1,9 @@
+#include "elm_illume.h"
+#include "elm_ill_container.h"
+
+EAPI Evas_Object *
+elm_ill_container_add(Evas_Object *parent) 
+{
+   return NULL;
+}
+
---
+++ PROTO/elm_illume/src/bin/elm_ill_container.h
@@ -0,0 +1,6 @@
+#ifndef ELM_ILL_CONTAINER_H
+# define ELM_ILL_CONTAINER_H
+
+EAPI Evas_Object *elm_ill_container_add(Evas_Object *parent);
+
+#endif
