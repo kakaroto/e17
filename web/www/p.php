3197c43efcd008c62134a2b8686a7a0c3c44b152
caro <caro@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-12-12 05:43:21 +0000

    link and meta tags must not be closed
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@44404 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- web/www/p.php
+++ web/www/p.php
@@ -2,12 +2,12 @@
 <html>
 <head>
 <title><?php echo $title; ?></title>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-<link rel="stylesheet" type="text/css" href="style.css"/>
-<link rel="icon" href="favicon.png" type="image/x-icon"/>
-<link rel="shortcut icon" href="favicon.png" type="image/x-icon"/>
-<link rel="icon" href="favicon.png" type="image/ico"/>
-<link rel="shortcut icon" href="favicon.png" type="image/ico"/>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="icon" href="favicon.png" type="image/x-icon">
+<link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+<link rel="icon" href="favicon.png" type="image/ico">
+<link rel="shortcut icon" href="favicon.png" type="image/ico">
 </head>
 
 <body>
