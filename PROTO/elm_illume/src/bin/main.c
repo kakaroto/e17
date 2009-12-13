0b6110de28375c2d9024c1e144ca9016b2f7b924
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-12-13 18:18:33 +0000

    Add a close button (temporary).
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@44428 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elm_illume/src/bin/main.c
+++ PROTO/elm_illume/src/bin/main.c
@@ -4,13 +4,14 @@
 #ifndef ELM_LIB_QUICKLAUNCH
 
 static void _cb_win_del(void *data, Evas_Object *obj, void *event);
+static void _cb_btn_close_clicked(void *data, Evas_Object *obj, void *event);
 
 static Evas_Object *win = NULL;
 
 EAPI int 
 elm_main(int argc, char **argv) 
 {
-   Evas_Object *bg;
+   Evas_Object *bg, *box, *btn;
    Ecore_X_Window xwin;
 
    win = elm_win_add(NULL, "elm_illume", ELM_WIN_BASIC);
@@ -24,6 +25,17 @@ elm_main(int argc, char **argv)
    elm_win_resize_object_add(win, bg);
    evas_object_show(bg);
 
+   box = elm_box_add(win);
+   evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_win_resize_object_add(win, box);
+   evas_object_show(box);
+
+   btn = elm_button_add(win);
+   evas_object_smart_callback_add(btn, "clicked", _cb_btn_close_clicked, NULL);
+   elm_button_label_set(btn, "Click to Close");
+   elm_box_pack_end(box, btn);
+   evas_object_show(btn);
+
    evas_object_resize(win, 200, 200);
    evas_object_show(win);
 
@@ -34,6 +46,12 @@ elm_main(int argc, char **argv)
 
 static void 
 _cb_win_del(void *data, Evas_Object *obj, void *event) 
+{
+   elm_exit();
+}
+
+static void 
+_cb_btn_close_clicked(void *data, Evas_Object *obj, void *event) 
 {
    elm_exit();
 }
