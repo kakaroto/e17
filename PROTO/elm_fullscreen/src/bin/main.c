f66c69545dd192c11c90f8c888aa50fcb973c7f0
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-12-13 20:07:17 +0000

    Add a label to the window so we can identify it easier.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@44432 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elm_fullscreen/src/bin/main.c
+++ PROTO/elm_fullscreen/src/bin/main.c
@@ -11,7 +11,7 @@ static Evas_Object *win = NULL;
 EAPI int 
 elm_main(int argc, char **argv) 
 {
-   Evas_Object *bg, *box, *btn;
+   Evas_Object *bg, *box, *btn, *lbl;
 
    win = elm_win_add(NULL, "elm_fullscreen", ELM_WIN_BASIC);
    elm_win_title_set(win, "Illume Fullscreen Window");
@@ -27,6 +27,11 @@ elm_main(int argc, char **argv)
    evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    elm_win_resize_object_add(win, box);
    evas_object_show(box);
+
+   lbl = elm_label_add(win);
+   elm_label_label_set(lbl, "Fullscreen Window");
+   elm_box_pack_end(box, lbl);
+   evas_object_show(lbl);
 
    btn = elm_button_add(win);
    evas_object_smart_callback_add(btn, "clicked", _cb_btn_close_clicked, NULL);
