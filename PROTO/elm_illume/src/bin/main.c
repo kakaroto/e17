d19ce86b04c73a26d5766905ede548bf94f43b8b
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-12-13 20:08:36 +0000

    Add a label to easily identify this window as conformant.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@44433 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elm_illume/src/bin/main.c
+++ PROTO/elm_illume/src/bin/main.c
@@ -11,7 +11,7 @@ static Evas_Object *win = NULL;
 EAPI int 
 elm_main(int argc, char **argv) 
 {
-   Evas_Object *bg, *box, *btn;
+   Evas_Object *bg, *box, *btn, *lbl;
    Ecore_X_Window xwin;
 
    win = elm_win_add(NULL, "elm_illume", ELM_WIN_BASIC);
@@ -29,6 +29,11 @@ elm_main(int argc, char **argv)
    evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    elm_win_resize_object_add(win, box);
    evas_object_show(box);
+
+   lbl = elm_label_add(win);
+   elm_label_label_set(lbl, "Conformant Window");
+   elm_box_pack_end(box, lbl);
+   evas_object_show(lbl);
 
    btn = elm_button_add(win);
    evas_object_smart_callback_add(btn, "clicked", _cb_btn_close_clicked, NULL);
