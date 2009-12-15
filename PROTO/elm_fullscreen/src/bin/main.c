57cf67e69d274dba88642406c767734a473199b3
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-12-15 04:35:39 +0000

    Change keyboard mode to be ALPHA.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@44465 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elm_fullscreen/src/bin/main.c
+++ PROTO/elm_fullscreen/src/bin/main.c
@@ -16,7 +16,7 @@ elm_main(int argc, char **argv)
    win = elm_win_add(NULL, "elm_fullscreen", ELM_WIN_BASIC);
    elm_win_title_set(win, "Illume Fullscreen Window");
    elm_win_fullscreen_set(win, EINA_TRUE);
-   elm_win_keyboard_mode_set(win, ELM_WIN_KEYBOARD_ON);
+   elm_win_keyboard_mode_set(win, ELM_WIN_KEYBOARD_ALPHA);
    evas_object_smart_callback_add(win, "delete-request", _cb_win_del, NULL);
 
    bg = elm_bg_add(win);
