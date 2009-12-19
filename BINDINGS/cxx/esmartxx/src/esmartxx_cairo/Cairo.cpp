8df094830487eead3bdfc7b0f892109a62e98c19
andreas <andreas@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-12-19 22:54:41 +0000

    added esmart cairo wrapper
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@44583 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ BINDINGS/cxx/esmartxx/src/esmartxx_cairo/Cairo.cpp
@@ -0,0 +1,54 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "Cairo.h"
+
+#include <iostream>
+#include <assert.h>
+
+using namespace std;
+
+namespace Esmartxx {
+  
+Cairo::Cairo( Evasxx::Canvas &canvas, const Eflxx::Size &size, bool alpha)
+{
+  o = esmart_image_cairo_new( canvas.obj(), size.width (), size.height (), alpha );
+  init();
+}
+
+Cairo::Cairo( Evasxx::Canvas &canvas, const Eflxx::Point &pos, const Eflxx::Size &size, bool alpha)
+{
+  o = esmart_image_cairo_new( canvas.obj(), size.width (), size.height (), alpha );
+  init();
+
+  move( pos );
+}
+
+Cairo::Cairo( Evasxx::Canvas &canvas, cairo_surface_t *cairo_surface)
+{
+  o = esmart_image_cairo_new_from_surface( canvas.obj(), cairo_surface );
+  init();
+}
+
+Cairo::~Cairo()
+{
+  evas_object_del( o );
+}
+
+cairo_surface_t* Cairo::getSurface ()
+{
+  return esmart_image_cairo_surface_get (o);
+}
+
+bool Cairo::setSurface (cairo_surface_t *cairo_surface)
+{
+  return esmart_image_cairo_surface_set (o, mCairoSurface);
+}
+
+void Cairo::setAutoFill (bool enable)
+{
+  esmart_image_cairo_fill_auto_set (o, enable);
+}
+
+} // end namespace Esmartxx
---
+++ BINDINGS/cxx/esmartxx/src/esmartxx_cairo/Cairo.h
@@ -0,0 +1,41 @@
+#ifndef ESMARTXX_CAIRO_H
+#define ESMARTXX_CAIRO_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <evasxx/Smart.h>
+
+/* EFL */
+#include <Esmart/Esmart_Cairo.h>
+
+namespace Esmartxx {
+
+class Cairo : public Evasxx::Smart
+{
+public:
+
+public:
+  Cairo( Evasxx::Canvas &canvas, const Eflxx::Size &size, bool alpha);
+  Cairo( Evasxx::Canvas &canvas, const Eflxx::Point &pos, const Eflxx::Size &size, bool alpha);
+  Cairo( Evasxx::Canvas &canvas, cairo_surface_t *cairo_surface);  // TODO:think about supporting cairomm
+  
+  virtual ~Cairo();
+
+public:
+
+  cairo_surface_t* getSurface ();
+  
+  bool setSurface (cairo_surface_t *cairo_surface);
+  
+  void setAutoFill (bool enable);
+  
+private:
+  cairo_surface_t *mCairoSurface;
+
+};
+
+} // end namespace Esmartxx
+
+#endif // ESMARTXX_CAIRO_H
