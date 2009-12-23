ffdcc4f7841f84410b1fe3ab5ce5c0751504544b
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2009-12-23 15:28:03 +0000

    Add an example quickpanel app. Not complete yet.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@44695 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ PROTO/elm_quickpanel/src/bin/elm_quickpanel.h
@@ -0,0 +1,40 @@
+#ifndef ELM_QUICKPANEL_H
+# define ELM_QUICKPANEL_H
+
+# ifdef HAVE_CONFIG_H
+#  include "config.h"
+# endif
+
+# include <Elementary.h>
+
+# ifdef ELM_QUICKPANEL_API
+#  undef ELM_QUICKPANEL_API
+# endif
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define ELM_QUICKPANEL_API __attribute__ ((visibility("default")))
+#  else
+#   define ELM_QUICKPANEL_API
+#  endif
+# else
+#  define ELM_QUICKPANEL_API
+# endif
+
+//# define ELM_QUICKPANEL_TYPEDEFS 1
+//# include "includes.h"
+//# undef ELM_QUICKPANEL_TYPEDEFS
+//# include "includes.h"
+
+# define ELM_QUICKPANEL_NEW(s, n) (s *)calloc(n, sizeof(s))
+# define ELM_QUICKPANEL_FREE(p) do { if (p) {free(p); p = NULL;} } while (0)
+
+# ifdef WANT_DEBUG
+#  define ELM_QUICKPANEL_DEBUG(x) printf("ELM_QUICKPANEL: %s\n", x)
+#  define ELM_QUICKPANEL_DEBUG_VAL(fmt, arg) printf(fmt, arg)
+#  define OBJECT_CHECK
+# else
+#  define ELM_QUICKPANEL_DEBUG(x)
+#  define ELM_QUICKPANEL_DEBUG_VAL(fmt, arg)
+# endif
+
+#endif
---
+++ PROTO/elm_quickpanel/src/bin/main.c
@@ -0,0 +1,53 @@
+#include <Ecore_X.h>
+#include "elm_quickpanel.h"
+
+#ifndef ELM_LIB_QUICKLAUNCH
+
+/* local function prototypes */
+static void _cb_win_del(void *data, Evas_Object *obj, void *event);
+
+/* local variables */
+
+EAPI int 
+elm_main(int argc, char **argv) 
+{
+   Evas_Object *win, *bg, *lbl;
+   Ecore_X_Window xwin;
+   Ecore_X_Window_State states[2];
+
+   win = elm_win_add(NULL, "elm_quickpanel", ELM_WIN_BASIC);
+   elm_win_title_set(win, "Illume Quickpanel Window");
+   elm_win_autodel_set(win, EINA_TRUE);
+   elm_win_borderless_set(win, EINA_TRUE);
+
+   xwin = elm_win_xwindow_get(win);
+   /* set hints to skip taskbar & pager */
+   states[0] = ECORE_X_WINDOW_STATE_SKIP_TASKBAR;
+   states[1] = ECORE_X_WINDOW_STATE_SKIP_PAGER;
+   ecore_x_netwm_window_state_set(xwin, states, 2);
+   /* set hints to not take focus */
+   ecore_x_icccm_hints_set(xwin, 0, 0, 0, 0, 0, 0, 0);
+   /* set this window as a quickpanel */
+   ecore_x_e_illume_quickpanel_set(xwin, 1);
+
+   bg = elm_bg_add(win);
+   evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_win_resize_object_add(win, bg);
+   evas_object_show(bg);
+
+   lbl = elm_label_add(win);
+   elm_label_label_set(lbl, "This is a quickpanel");
+   evas_object_size_hint_weight_set(lbl, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_win_resize_object_add(win, lbl);
+   evas_object_show(lbl);
+
+   evas_object_resize(win, 200, 32);
+   evas_object_show(win);
+
+   elm_run();
+   elm_shutdown();
+   return 0;
+}
+
+#endif
+ELM_MAIN();
