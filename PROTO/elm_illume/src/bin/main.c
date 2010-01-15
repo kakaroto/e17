9c7ae26bd80b283cc952d94791704759faa58930
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-01-15 05:29:53 +0000

    Update elm conformant app to use new conformant widget.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@45163 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elm_illume/src/bin/main.c
+++ PROTO/elm_illume/src/bin/main.c
@@ -11,37 +11,31 @@ static Evas_Object *win = NULL;
 EAPI int 
 elm_main(int argc, char **argv) 
 {
-   Evas_Object *bg, *box, *btn, *lbl;
-   Ecore_X_Window xwin;
+   Evas_Object *bg, *conform, *btn;
 
    win = elm_win_add(NULL, "elm_illume", ELM_WIN_BASIC);
    elm_win_title_set(win, "Illume Conformant Window");
 //   elm_win_fullscreen_set(win, EINA_TRUE);
    evas_object_smart_callback_add(win, "delete-request", _cb_win_del, NULL);
    elm_win_keyboard_mode_set(win, ELM_WIN_KEYBOARD_ALPHA);
-   xwin = elm_win_xwindow_get(win);
-   ecore_x_e_illume_conformant_set(xwin, 1);
+   elm_win_conformant_set(win, 1);
 
    bg = elm_bg_add(win);
    evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    elm_win_resize_object_add(win, bg);
    evas_object_show(bg);
 
-   box = elm_box_add(win);
-   evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-   elm_win_resize_object_add(win, box);
-   evas_object_show(box);
-
-   lbl = elm_label_add(win);
-   elm_label_label_set(lbl, "Conformant Window");
-   elm_box_pack_end(box, lbl);
-   evas_object_show(lbl);
+   conform = elm_conformant_add(win);
+   elm_win_resize_object_add(win, conform);
+   evas_object_size_hint_weight_set(conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_show(conform);
 
    btn = elm_button_add(win);
    evas_object_smart_callback_add(btn, "clicked", _cb_btn_close_clicked, NULL);
    elm_button_label_set(btn, "Click to Close");
-   elm_box_pack_end(box, btn);
    evas_object_show(btn);
+
+   elm_conformant_content_set(conform, btn);
 
    evas_object_resize(win, 200, 200);
    evas_object_show(win);
