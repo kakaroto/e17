1cc75a8701c87f8d6947c67197579377210cf4a6
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-01-15 05:33:09 +0000

    a little cleanup of code (reorganize). Remove vkbd setup.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@45164 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elm_illume/src/bin/main.c
+++ PROTO/elm_illume/src/bin/main.c
@@ -15,10 +15,8 @@ elm_main(int argc, char **argv)
 
    win = elm_win_add(NULL, "elm_illume", ELM_WIN_BASIC);
    elm_win_title_set(win, "Illume Conformant Window");
-//   elm_win_fullscreen_set(win, EINA_TRUE);
-   evas_object_smart_callback_add(win, "delete-request", _cb_win_del, NULL);
-   elm_win_keyboard_mode_set(win, ELM_WIN_KEYBOARD_ALPHA);
    elm_win_conformant_set(win, 1);
+   evas_object_smart_callback_add(win, "delete-request", _cb_win_del, NULL);
 
    bg = elm_bg_add(win);
    evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
@@ -31,8 +29,8 @@ elm_main(int argc, char **argv)
    evas_object_show(conform);
 
    btn = elm_button_add(win);
-   evas_object_smart_callback_add(btn, "clicked", _cb_btn_close_clicked, NULL);
    elm_button_label_set(btn, "Click to Close");
+   evas_object_smart_callback_add(btn, "clicked", _cb_btn_close_clicked, NULL);
    evas_object_show(btn);
 
    elm_conformant_content_set(conform, btn);
