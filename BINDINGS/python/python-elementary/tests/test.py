bf1340c0f991ebefbc06756292f6e5840df27c64
billiob <billiob@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-01-17 10:35:13 +0000

    pyelm: add menu test
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@45255 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/python/python-elementary/tests/test.py
+++ BINDINGS/python/python-elementary/tests/test.py
@@ -2472,6 +2472,131 @@ def notify_clicked(obj, it):
 
 # }}}
 
+#----- Menu -{{{-
+def menu_show(rect, evtinfo, menu):
+    (x,y) = evtinfo.position.canvas
+    menu.move(x, y)
+    menu.show()
+
+def menu_populate_4(menu, item):
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+    menu.item_add(item, "menu 2", ic)
+
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+
+    item2 = menu.item_add(item, "menu 3", ic)
+
+    menu.item_separator_add(item)
+
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+    item3 = menu.item_add(item, "Disabled item", ic);
+    item3.disabled = True
+
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+    item3 = menu.item_add(item, "Disabled item", ic);
+    item3.disabled = True
+
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+    item3 = menu.item_add(item, "Disabled item", ic);
+    item3.disabled = True
+
+def menu_populate_3(menu, item):
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+    menu.item_add(item, "menu 2", ic)
+
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+
+    item2 = menu.item_add(item, "menu 3", ic)
+
+    menu.item_separator_add(item)
+
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+
+    item3 = menu.item_add(item, "Disabled item", ic);
+    item3.disabled = True
+
+def menu_populate_2(menu, item):
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+    menu.item_add(item, "menu 2", ic)
+
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+    item2 = menu.item_add(item, "menu 3", ic)
+
+    menu_populate_3(menu, item2)
+
+    menu.item_separator_add(item)
+    menu.item_separator_add(item)
+    menu.item_separator_add(item)
+    menu.item_separator_add(item)
+    menu.item_separator_add(item)
+    menu.item_separator_add(item)
+    menu.item_separator_add(item)
+
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+    item2 = menu.item_add(item, "menu 2", ic);
+
+    menu.item_separator_add(item)
+
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+    item3 = menu.item_add(item, "Disabled item", ic);
+    item3.disabled = True
+
+    menu_populate_4(menu, item2)
+
+def menu_populate_1(menu, item):
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+    item2 = menu.item_add(item, "menu 1", ic)
+
+    menu_populate_2(menu, item2)
+
+def menu_clicked(obj, it):
+    win = elementary.Window("menu", elementary.ELM_WIN_BASIC)
+    win.title_set("Menu test")
+    win.autodel_set(True)
+
+    bg = elementary.Background(win)
+    win.resize_object_add(bg)
+    bg.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    bg.show()
+
+    rect = evas.Rectangle(win.evas_get())
+    win.resize_object_add(rect)
+    rect.color_set(0, 0, 0, 0)
+    rect.show()
+
+    menu = elementary.Menu(win)
+    item = menu.item_add(None, "first item")
+
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+
+    item = menu.item_add(None, "second item", ic)
+    menu_populate_1(menu, item)
+
+    ic = elementary.Icon(menu)
+    ic.file_set('images/logo_small.png')
+    menu.item_add(item, "sub menu", ic)
+
+    rect.event_callback_add(evas.EVAS_CALLBACK_MOUSE_DOWN, menu_show, menu)
+
+    win.resize(350, 200)
+    win.show()
+
+# }}}
+
 #----- Main -{{{-
 def destroy(obj, str1, str2, str3, str4):
     print "DEBUG: window destroy callback called!"
@@ -2538,7 +2663,8 @@ if __name__ == "__main__":
                ("Separator", separator_clicked),
                ("Scroller", scroller_clicked),
                ("Spinner", spinner_clicked),
-               ("Notify", notify_clicked)]
+               ("Notify", notify_clicked),
+               ("Menu", menu_clicked)]
 
 
 
