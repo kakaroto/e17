4ea253f00a7d0caec33c63026bcf98f9301ff394
billiob <billiob@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-01-23 17:09:25 +0000

    pyelm: add menu to the toolbar test
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@45492 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/python/python-elementary/tests/test.py
+++ BINDINGS/python/python-elementary/tests/test.py
@@ -1118,20 +1118,20 @@ def anchorblock_clicked(obj, it):
 # }}}
 
 #----- Toolbar -{{{-
-def tb_1(obj, it):
-    print "toolbar-item: test 1"
+def tb_1(obj, it, ph):
+    ph.file_set("images/panel_01.jpg")
 
-def tb_2(obj, it):
-    print "toolbar-item: test 2"
+def tb_2(obj, it, ph):
+    ph.file_set("images/rock_01.jpg")
 
-def tb_3(obj, it):
-    print "toolbar-item: test 3"
+def tb_3(obj, it, ph):
+    ph.file_set("images/wood_01.jpg")
 
-def tb_4(obj, it):
-    print "toolbar-item: test 4"
+def tb_4(obj, it, ph):
+    ph.file_set("images/sky_03.jpg")
 
-def tb_5(obj, it):
-    print "toolbar-item: test 5"
+def tb_5(obj, it, ph):
+    ph.file_set(None)
 
 def toolbar_clicked(obj, it):
     win = elementary.Window("entry-scrolled", elementary.ELM_WIN_BASIC)
@@ -1148,43 +1148,99 @@ def toolbar_clicked(obj, it):
     bx.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
     bx.show()
 
-    ic = elementary.Icon(win)
-    ic.file_set("images/logo_small.png")
-    ic.scale_set(0, 0)
-    ic.size_hint_align_set(0.5, 0.5)
-    bx.pack_end(ic)
-    ic.show()
-
     tb = elementary.Toolbar(win)
+    tb.homogenous = False
     tb.size_hint_weight_set(0.0, 0.0)
     tb.size_hint_align_set(evas.EVAS_HINT_FILL, 0.0)
+
+    ph1 = elementary.Photo(win)
+    ph2 = elementary.Photo(win)
+    ph3 = elementary.Photo(win)
+    ph4 = elementary.Photo(win)
 
     ic = elementary.Icon(win)
     ic.file_set("images/logo_small.png")
     item = tb.item_add(ic, "Hello", tb_1)
-    ic.show()
+    item.disabled = True
 
     ic = elementary.Icon(win)
     ic.file_set("images/logo_small.png")
-    item = tb.item_add(ic, "World,", tb_2)
+    item = tb.item_add(ic, "World,", tb_2, ph2)
     item.select()
 
     ic = elementary.Icon(win)
     ic.file_set("images/logo_small.png")
-    tb.item_add(ic, "here", tb_3)
+    tb.item_add(ic, "here", tb_3, ph4)
 
     ic = elementary.Icon(win)
     ic.file_set("images/logo_small.png")
-    tb.item_add(ic, "comes", tb_4)
+    tb.item_add(ic, "comes", tb_4, ph4)
 
     ic = elementary.Icon(win)
     ic.file_set("images/logo_small.png")
-    tb.item_add(ic, "python-elementary!", tb_5)
+    tb.item_add(ic, "python-elementary!", tb_5, ph4)
+
+    ic = elementary.Icon(win)
+    ic.file_set("images/logo_small.png")
+    item = tb.item_add(ic, "Menu", tb_5, ph4)
+    item.menu_set(True)
+    tb.menu_parent_set(win)
+    menu = item.menu_get()
+
+    ic = elementary.Icon(win)
+    ic.file_set("images/logo_small.png")
+    menu.item_add(None, "Here", ic, tb_3, ph4)
+
+    ic = elementary.Icon(win)
+    ic.file_set("images/logo_small.png")
+    menu_item = menu.item_add(None, "Comes", ic, tb_4, ph4)
+
+    ic = elementary.Icon(win)
+    ic.file_set("images/logo_small.png")
+    menu.item_add(menu_item, "hey ho", ic, tb_4, ph4)
+
+    ic = elementary.Icon(win)
+    ic.file_set("images/logo_small.png")
+    menu.item_add(None, "python-elementary", ic, tb_5, ph4)
 
     bx.pack_end(tb)
     tb.show()
 
-    win.resize(320, 320)
+    tb = elementary.Table(win)
+    tb.size_hint_weight_set(0.0, evas.EVAS_HINT_EXPAND)
+    tb.size_hint_align_set(evas.EVAS_HINT_FILL, evas.EVAS_HINT_FILL)
+
+    ph1.size_set(40)
+    ph1.file_set("images/plant_01.jpg")
+    ph1.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    ph1.size_hint_align_set(0.5, 0.5)
+    tb.pack(ph1, 0, 0, 1, 1)
+    ph1.show()
+
+    ph2.size_set(80)
+    ph2.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    ph2.size_hint_align_set(0.5, 0.5)
+    tb.pack(ph2, 1, 0, 1, 1)
+    ph2.show()
+
+    ph3.size_set(40)
+    ph3.file_set("images/sky_01.jpg")
+    ph3.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    ph3.size_hint_align_set(0.5, 0.5)
+    tb.pack(ph3, 0, 1, 1, 1)
+    ph3.show()
+
+    ph4.size_set(60)
+    ph4.file_set("images/sky_02.jpg")
+    ph4.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    ph4.size_hint_align_set(0.5, 0.5)
+    tb.pack(ph4, 1, 1, 1, 1)
+    ph4.show()
+
+    bx.pack_end(tb)
+    tb.show()
+
+    win.resize(320, 300)
     win.show()
 # }}}
 
