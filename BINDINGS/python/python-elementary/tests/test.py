40024371fb6537816ba7e59ddb0695d64b983f2f
billiob <billiob@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-01-24 21:36:26 +0000

    pyelm: add Panel test
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@45535 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/python/python-elementary/tests/test.py
+++ BINDINGS/python/python-elementary/tests/test.py
@@ -2723,6 +2723,42 @@ def menu_clicked(obj, it):
 
 # }}}
 
+#----- Panel -{{{-
+def panel_clicked(obj, it):
+    win = elementary.Window("panel", elementary.ELM_WIN_BASIC)
+    win.title_set("Panel test")
+    win.autodel_set(True)
+
+    bg = elementary.Background(win)
+    win.resize_object_add(bg)
+    bg.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    bg.show()
+
+    bx = elementary.Box(win)
+    bx.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    win.resize_object_add(bx)
+    bx.show()
+
+    panel = elementary.Panel(win)
+    panel.orient = elementary.ELM_PANEL_ORIENT_LEFT
+    panel.size_hint_weight_set(0.0, evas.EVAS_HINT_EXPAND);
+    panel.size_hint_align_set(0.0, evas.EVAS_HINT_FILL);
+
+    bt = elementary.Button(win)
+    bt.label_set("HIDE ME :)")
+    bt.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND);
+    bt.size_hint_align_set(evas.EVAS_HINT_FILL, evas.EVAS_HINT_FILL);
+    bt.show()
+
+    panel.content_set(bt)
+
+    bx.pack_end(panel)
+    panel.show()
+
+    win.resize(300, 300)
+    win.show()
+# }}}
+
 #----- Main -{{{-
 def destroy(obj, str1, str2, str3, str4):
     print "DEBUG: window destroy callback called!"
@@ -2791,7 +2827,8 @@ if __name__ == "__main__":
                ("Scroller", scroller_clicked),
                ("Spinner", spinner_clicked),
                ("Notify", notify_clicked),
-               ("Menu", menu_clicked)]
+               ("Menu", menu_clicked),
+               ("Panel", panel_clicked)]
 
 
 
