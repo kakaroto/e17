e5fceccbc91c84d0b33943825ebfe4948365b423
barbieri <barbieri@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-02-08 13:09:26 +0000

    exorcist is dead (or never born)
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@45978 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/exorcist/src/bin/etk_main_window.h
+++
@@ -1,8 +0,0 @@
-#ifndef __EXO_ETK_MAIN_WINDOW_H__
-#define __EXO_ETK_MAIN_WINDOW_H__
-
-
-void exo_etk_main_window (const char *filename);
-
-
-#endif /* __EXO_ETK_MAIN_WINDOW_H__ */
--- PROTO/exorcist/src/bin/ewl_main_window.h
+++
@@ -1,8 +0,0 @@
-#ifndef __EXO_EWL_MAIN_WINDOW_H__
-#define __EXO_EWL_MAIN_WINDOW_H__
-
-
-void exo_ewl_main_window (const char *filename);
-
-
-#endif /* __EXO_EWL_MAIN_WINDOW_H__ */
