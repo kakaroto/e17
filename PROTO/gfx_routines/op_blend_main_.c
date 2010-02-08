bafac7bcd3a4e3f250a4aa90effa1ea3cb8fc084
barbieri <barbieri@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-02-08 13:12:50 +0000

    playground that was never used (as raster said)
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@45980 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/gfx_routines/op_blend_main_.c
+++
@@ -1,7 +0,0 @@
-
-# include "./op_blend/op_blend_pixel_.c"
-# include "./op_blend/op_blend_color_.c"
-# include "./op_blend/op_blend_pixel_color_.c"
-# include "./op_blend/op_blend_pixel_mask_.c"
-# include "./op_blend/op_blend_mask_color_.c"
-# include "./op_blend/op_blend_pixel_mask_color_.c"
--- PROTO/gfx_routines/op_mul_main_.c
+++
@@ -1,7 +0,0 @@
-
-# include "./op_mul/op_mul_pixel_.c"
-# include "./op_mul/op_mul_color_.c"
-# include "./op_mul/op_mul_pixel_color_.c"
-# include "./op_mul/op_mul_pixel_mask_.c"
-# include "./op_mul/op_mul_mask_color_.c"
-# include "./op_mul/op_mul_pixel_mask_color_.c"
