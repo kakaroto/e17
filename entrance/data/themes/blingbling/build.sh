8243c0a9d6f7b961fbdf99e53010813c957d17d2
quaker <quaker@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-02-27 17:24:21 +0000

    remove entrance and estickies, as i am rewriting them from base..
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@46590 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- entrance/data/themes/blingbling/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# for building a theme without recompiling entrance
-edje_cc $@ -id img -fd . blingbling.edc -o blingbling.edj
--- entrance/src/daemon/util.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ENTRANCED_UTIL_H
-#define __ENTRANCED_UTIL_H
-
-void entranced_debug (char *fmt, ...);
-extern unsigned char entranced_debug_flag;
-
-#endif
-
