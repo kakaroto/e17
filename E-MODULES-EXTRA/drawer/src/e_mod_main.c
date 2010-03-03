58f336a33c63d6ec43f709f8544ed4d5c6b41160
englebass <englebass@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-03-03 22:10:55 +0000

    Use efreet_desktop_new where appropriate
    efreet_desktop_get (will in time) setup a lot of stuff to monitor
    changes. If we just use the results from efreet_desktop_get within one
    function and then free, this is unneeded overhead. efreet_desktop_free
    will more lightweight (but require a bit more mem alloc).
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@46847 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/drawer/src/e_mod_main.c
+++ E-MODULES-EXTRA/drawer/src/e_mod_main.c
@@ -401,7 +401,7 @@ drawer_plugins_list(Drawer_Plugin_Category cat)
 	pi = E_NEW(Drawer_Plugin_Type, 1);
 
 	snprintf(buf2, sizeof(buf2), "%s%s", moddir, mod);
-	if (!(desk = efreet_desktop_get(buf2))) goto end;
+	if (!(desk = efreet_desktop_new(buf2))) goto end;
 	if (desk->x) 
 	  pi->title = eina_stringshare_add(eina_hash_find(desk->x, "X-Drawer-Title"));
 	if (!pi->title) pi->title = eina_stringshare_add(desk->name);
