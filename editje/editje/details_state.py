17d760de5666392ea0701782b81b7276dec74a8a
glima <glima@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-03-31 18:45:25 +0000

    EDJE_EXTERNAL_PARAM_TYPE_CHOICE support in Editje, with hoversels.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@47631 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- editje/editje/details_state.py
+++ editje/editje/details_state.py
@@ -19,12 +19,13 @@ import ecore
 import edje
 
 from details import EditjeDetails
-from details_widget_entry import WidgetEntry
 from details_widget_boolean import WidgetBoolean
-from details_widget_color import WidgetColor
 from details_widget_button import WidgetButton
-from details_widget_partlist import WidgetPartList
+from details_widget_color import WidgetColor
+from details_widget_combo import WidgetCombo
+from details_widget_entry import WidgetEntry
 from details_widget_font import WidgetFont
+from details_widget_partlist import WidgetPartList
 from details_widget_states import WidgetStates
 from prop import Property, PropertyTable
 from filewizard import ImageSelectionWizard
@@ -577,6 +578,10 @@ class PartStateDetails(EditjeDetails):
             if p.type == edje.EDJE_EXTERNAL_PARAM_TYPE_BOOL:
                 wid = WidgetBoolean(self)
                 wid.states_set(p.true_string, p.false_string)
+            elif p.type == edje.EDJE_EXTERNAL_PARAM_TYPE_CHOICE:
+                wid = WidgetCombo(self)
+                for choice in p.choices:
+                    wid.item_add(choice)
             else:
                 wid = WidgetEntry(self)
                 if p.type == edje.EDJE_EXTERNAL_PARAM_TYPE_INT:
