316b52ca02df9b811b41d951e7fcb2f3884e9996
caro <caro@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-04-10 13:00:02 +0000

    update Visual Studio project files
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@47893 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- expedite/win32/set_env.bat
+++ expedite/win32/set_env.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem Set external libraries directory.
-set EXT_DIR=%cd%\..\..\..\..\extern
+set EXT_DIR=%cd%\..\..\..\extern
 
 if not exist %EXT_DIR% (
 	set EXT_DIR=
@@ -15,16 +15,17 @@ set INCLUDE=%EXT_DIR%\include;%INCLUDE%
 set LIB=%EXT_DIR%\lib;%LIB%
 
 rem Add Evil lib path
-set EvilInclude=%cd%\..\..\proto\evil\src\lib
-set EvilCommon=%cd%\..\..\proto\evil\win32\common
-set EvilOut=%cd%\..\..\proto\evil\win32\%PROJECT_TYPE%\out
+set EvilInclude=%cd%\..\..\evil\src\lib
+set EvilCommon=%cd%\..\..\evil\win32\common
+set EvilOut=%cd%\..\..\evil\win32\%PROJECT_TYPE%\out
 
 rem Add Evas lib path
 set EvasOut=%cd%\..\..\evas\win32\%PROJECT_TYPE%\out
 set EvasDirect3DInclude=%cd%\..\..\evas\src\modules\engines\direct3d
 set EvasGLGlewInclude=%cd%\..\..\evas\src\modules\engines\gl_glew
 set EvasDDrawInclude=%cd%\..\..\evas\src\modules\engines\software_ddraw
-set EvasInclude=%EvasDDrawInclude%;%EvasGLGlewInclude%;%EvasDirect3DInclude%;%cd%\..\..\evas\src\lib
+set EvasGDIInclude=%cd%\..\..\evas\src\modules\engines\software_gdi
+set EvasInclude=%EvasGDIInclude%;%EvasDDrawInclude%;%EvasGLGlewInclude%;%EvasDirect3DInclude%;%cd%\..\..\evas\src\lib
 
 rem Add Eina lib path
 set EinaInclude=%cd%\..\..\eina\src\include
