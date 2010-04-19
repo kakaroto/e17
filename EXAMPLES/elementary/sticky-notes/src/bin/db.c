36efc336873af28ba1f2caaab4bcdfc16cc1e431
glima <glima@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-04-19 14:25:11 +0000

    Fixing copyright assignment.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@48132 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- EXAMPLES/elementary/sticky-notes/src/bin/db.c
+++ EXAMPLES/elementary/sticky-notes/src/bin/db.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009  Raphael Kubo da Costa <kubo@profusion.mobi>
+ * Copyright (c) 2009  ProFUSION Embedded Systems
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -21,6 +21,8 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * ( INCLUDING NEGLIGENCE OR OTHERWISE ) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Raphael Kubo da Costa <kubo@profusion.mobi>
  */
 
 #include "db.h"
