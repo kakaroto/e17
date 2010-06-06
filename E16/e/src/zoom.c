c31bfe2fa59357333cdb155b4616156d08814f6d
kwo <kwo@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-06-06 10:08:14 +0000

    Fix logic in ReZoom().
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@49487 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E16/e/src/zoom.c
+++ E16/e/src/zoom.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2004-2009 Kim Woelders
+ * Copyright (C) 2004-2010 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -149,7 +149,7 @@ GetZoomEWin(void)
 void
 ReZoom(EWin * ewin)
 {
-   if ((InZoom()) && (ewin != zoom_last_ewin))
+   if (zoom_last_ewin && ewin == zoom_last_ewin)
      {
 	Zoom(NULL);
 	Zoom(ewin);
