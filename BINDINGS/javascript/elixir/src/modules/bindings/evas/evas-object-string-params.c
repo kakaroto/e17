3e077205aeb2ac6d863a2709ded257d8c55c640c
cedric <cedric@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-06-16 16:07:10 +0000

    * elixir: add key_grab helper bindings.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@49699 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/javascript/elixir/src/modules/bindings/evas/evas-object-string-params.c
+++ BINDINGS/javascript/elixir/src/modules/bindings/evas/evas-object-string-params.c
@@ -4,9 +4,29 @@
 
 #include "evas-bindings.h"
 
+static const elixir_parameter_t*        _evas_string_params[3] = {
+   &evas_parameter,
+   &string_parameter,
+   NULL
+};
 static const elixir_parameter_t*        _evas_object_string_params[3] = {
    &evas_object_parameter,
    &string_parameter,
+   NULL
+};
+static const elixir_parameter_t*        _evas_object_string_2int_bool_params[6] = {
+   &evas_object_parameter,
+   &string_parameter,
+   &int_parameter,
+   &int_parameter,
+   &boolean_parameter,
+   NULL
+};
+static const elixir_parameter_t*        _evas_object_string_2int_params[6] = {
+   &evas_object_parameter,
+   &string_parameter,
+   &int_parameter,
+   &int_parameter,
    NULL
 };
 static const elixir_parameter_t*        _evas_object_text_string_params[3] = {
@@ -19,6 +39,61 @@ static const elixir_parameter_t*        _evas_object_textblock_string_params[3] 
    &string_parameter,
    NULL
 };
+
+static JSBool
+elixir_evas_key_modifier_mask_get(JSContext *cx, uintN argc, jsval *vp)
+{
+   Evas *e = NULL;
+   const char *str;
+   elixir_value_t val[2];
+
+   if (!elixir_params_check(cx, _evas_string_params, val, argc, JS_ARGV(cx, vp)))
+     return JS_FALSE;
+
+   GET_PRIVATE(cx, val[0].v.obj, e);
+   str = elixir_get_string_bytes(val[1].v.str, NULL);
+
+   JS_SET_RVAL(cx, vp, INT_TO_JSVAL(evas_key_modifier_mask_get(e, str)));
+   return JS_TRUE;
+}
+
+static JSBool
+elixir_evas_object_key_grab(JSContext *cx, uintN argc, jsval *vp)
+{
+   Evas_Object *know = NULL;
+   const char *str;
+   Eina_Bool result;
+   elixir_value_t val[5];
+
+   if (!elixir_params_check(cx, _evas_object_string_2int_bool_params, val, argc, JS_ARGV(cx, vp)))
+     return JS_FALSE;
+
+   GET_PRIVATE(cx, val[0].v.obj, know);
+   str = elixir_get_string_bytes(val[1].v.str, NULL);
+
+   result = evas_object_key_grab(know, str, val[2].v.num, val[3].v.num, val[4].v.bol);
+
+   JS_SET_RVAL(cx, vp, BOOLEAN_TO_JSVAL(result));
+   return JS_TRUE;
+}
+
+static JSBool
+elixir_evas_object_key_ungrab(JSContext *cx, uintN argc, jsval *vp)
+{
+   Evas_Object *know = NULL;
+   const char *str;
+   elixir_value_t val[4];
+
+   if (!elixir_params_check(cx, _evas_object_string_2int_params, val, argc, JS_ARGV(cx, vp)))
+     return JS_FALSE;
+
+   GET_PRIVATE(cx, val[0].v.obj, know);
+   str = elixir_get_string_bytes(val[1].v.str, NULL);
+
+   evas_object_key_ungrab(know, str, val[2].v.num, val[3].v.num);
+
+   return JS_TRUE;
+}
 
 static JSBool
 elixir_evas_object_string_params(void (*func)(Evas_Object* obj, const char* str),
@@ -92,6 +167,9 @@ FAST_CALL_PARAMS_SPEC(evas_object_text_font_source_set, elixir_evas_object_strin
 FAST_CALL_PARAMS_SPEC(evas_object_text_text_set, elixir_evas_object_string_params, _evas_object_text_string_params);
 
 static JSFunctionSpec     evas_object_params_function[] = {
+  ELIXIR_FN(evas_key_modifier_mask_get, 2, JSPROP_ENUMERATE, 0 ),
+  ELIXIR_FN(evas_object_key_grab, 5, JSPROP_ENUMERATE, 0 ),
+  ELIXIR_FN(evas_object_key_ungrab, 5, JSPROP_ENUMERATE, 0 ),
   ELIXIR_FN(evas_object_name_set, 2, JSPROP_ENUMERATE, 0 ),
   ELIXIR_FN(evas_object_data_get, 2, JSPROP_ENUMERATE, 0 ),
   ELIXIR_FN(evas_object_data_del, 2, JSPROP_ENUMERATE, 0 ),
