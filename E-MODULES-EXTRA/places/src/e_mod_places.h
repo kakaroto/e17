0decfcf53be68527044297cfcac39dcb8cad507a
davemds <davemds@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-06-20 15:32:02 +0000

    Fix License for the Places module.  LGPL3 -> BSD
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@49771 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/places/src/e_mod_places.h
+++ E-MODULES-EXTRA/places/src/e_mod_places.h
@@ -1,20 +1,3 @@
-/*  Copyright (C) 2008 Davide Andreoli (see AUTHORS)
- *
- *  This file is part of places.
- *  places is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  places is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with places.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef E_MOD_PLACES_H
 #define E_MOD_PLACES_H
 
