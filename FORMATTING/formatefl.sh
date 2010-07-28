a21dda8688cedf9153b152c8b79f45db060a55da
lucas <lucas@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-07-28 11:30:28 +0000

    Fix uncrustify installation
     * PREFIX is $HOME, not $HOME/.uncrustify as we are supposed to install in
       ~/bin
     * Fix script indentation
     * Fix installation when running the script outside of trunk/FORMATTING/
    
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@50592 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- FORMATTING/formatefl.sh
+++ FORMATTING/formatefl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PREFIX="$HOME/.uncrustify"
+PREFIX="$HOME"
 UNC="$PREFIX/bin/uncrustify"
 if [ -z "$1" ]; then
   echo "==================================================================="
@@ -7,28 +7,31 @@ if [ -z "$1" ]; then
   echo "==================================================================="
   VER=`$UNC --version | awk '{printf("%s\n", $2);}'`
   if [ "$VER" != "0.56" ]; then
-  echo "==================================================================="
-  echo " Not there or wrong version. Need to install it in ~/bin"
-  echo " Doing that now."
-  echo "==================================================================="
+    echo "==================================================================="
+    echo " Not there or wrong version. Need to install it in ~/bin"
+    echo " Doing that now."
+    echo "==================================================================="
+    pushd `dirname $0`
     tar zxf uncrustify-0.56.tar.gz
-    cd uncrustify-0.56
+    pushd uncrustify-0.56
       ./configure --prefix=$PREFIX
       make
       make install
-    cd ..
+    popd
     rm -rf uncrustify-0.56
     ln -sf $PWD/efl_uncrustify.cfg $HOME/.uncrustify.cfg
+    popd
   fi
   echo "==================================================================="
   echo " DONE"
   echo "==================================================================="
   exit 0
 fi
-  echo "==================================================================="
-  echo " Uncrustifying your source to match EFL programming guidelines for"
-  echo " formatting etc."
-  echo "==================================================================="
+
+echo "==================================================================="
+echo " Uncrustifying your source to match EFL programming guidelines for"
+echo " formatting etc."
+echo "==================================================================="
 F=`find $1 -name '*.[ch]' -print`
 if [ -n "$F" ]; then $UNC --no-backup --replace -l C $F; fi
 F=`find $1 -name '*.h.in' -print`
