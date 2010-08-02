70711bb4c20ca1703d58d653dc58abd97a629fa5
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-08-02 21:27:21 +0000

    yes good, because we validate the version 5 lines later anyway also fix command substitution from deprecated/broken backticks and avoid calling external test by using double braces
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@50755 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- FORMATTING/formatefl.sh
+++ FORMATTING/formatefl.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+DIR="$1"
+SCRIPTDIR="$0"
 PREFIX="$HOME/.uncrustify"
-UNC="$PREFIX/bin/uncrustify"
-if [ -z "$1" ]; then
+
+if which uncrustify &> /dev/null ;then
+	UNC="$(which uncrustify)"
+else
+	UNC="$PREFIX/bin/uncrustify"
+fi
+if [[ -z "$DIR" ]]; then
   echo "==================================================================="
   echo " Checking if uncrustify installed already and is the right version"
   echo "==================================================================="
-  VER=`$UNC --version | awk '{printf("%s\n", $2);}'`
-  if [ "$VER" != "0.56" ]; then
+  VER=$($UNC --version | awk '{printf("%s\n", $2);}')
+  if [[ "$VER" != "0.56" ]]; then
     echo "==================================================================="
     echo " Not there or wrong version. Need to install it in ~/.uncrustify"
     echo " Doing that now."
     echo "==================================================================="
-    pushd `dirname $0`
+    pushd $(dirname $SCRIPTDIR)
     tar zxf uncrustify-0.56.tar.gz
     pushd uncrustify-0.56
       ./configure --prefix=$PREFIX
@@ -37,12 +44,12 @@ FMT2="$UNC --no-backup --replace -l C"
 FMT3="$UNC --no-backup --replace -l CPP"
 #FMT2="indent -kr"
 #FMT3="indent -kr"
-F=`find $1 -name '*.[chx]' -print`
-if [ -n "$F" ]; then $FMT1 $F; $FMT2 $F; fi
-F=`find $1 -name '*.h.in' -print`
-if [ -n "$F" ]; then $FMT1 $F; $FMT2 $F; fi
-F=`find $1 -name '*.cpp' -print`
-if [ -n "$F" ]; then $FMT1 $F; $FMT3 $F; fi
+F=$(find $1 -name '*.[chx]' -print)
+if [[ -n "$F" ]]; then $FMT1 $F; $FMT2 $F; fi
+F=$(find $1 -name '*.h.in' -print)
+if [[ -n "$F" ]]; then $FMT1 $F; $FMT2 $F; fi
+F=$(find $1 -name '*.cpp' -print)
+if [[ -n "$F" ]]; then $FMT1 $F; $FMT3 $F; fi
   echo "==================================================================="
   echo " DONE"
   echo "==================================================================="
