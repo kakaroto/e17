a2f8fab67a5b1dd6d791bb3f2bf7787d09b0110d
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-08-09 16:34:22 +0000

    When deleting the gadcon, don't ADD a callback but rather delete the existing one.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@50932 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/eweather/src/module/e_mod_gadcon.c
+++ E-MODULES-EXTRA/eweather/src/module/e_mod_gadcon.c
@@ -85,7 +85,7 @@ _gc_shutdown(E_Gadcon_Client *gcc)
      }
    if (inst->obj)
      {
-        evas_object_event_callback_add(inst->obj, EVAS_CALLBACK_MOUSE_DOWN,
+        evas_object_event_callback_del(inst->obj, EVAS_CALLBACK_MOUSE_DOWN,
                                        _gc_cb_mouse_down, inst);
         evas_object_del(inst->obj);
      }
