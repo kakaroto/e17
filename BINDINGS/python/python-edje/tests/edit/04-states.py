360496431d4cbed103ecf4a52c9c36a13d133d4c
glima <glima@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-08-09 22:32:30 +0000

    One more python-edje test.
    By Fidêncio.
    
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@50948 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ BINDINGS/python/python-edje/tests/edit/04-states.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import edje
+import edje.edit
+import ecore.evas
+import unittest
+
+class Basics(unittest.TestCase):
+    def setUp(self):
+        import shutil
+        shutil.copyfile("theme.edj","test.edj")
+        self.canvas = ecore.evas.Buffer(300, 300)
+        self.edj = edje.edit.EdjeEdit(self.canvas.evas, file="test.edj", group="main")
+        self.group = self.edj.current_group
+        self.parts = self.edj.parts
+
+    def tearDown(self):
+        self.edj.delete()
+        del self.canvas
+        self.canvas = None
+        self.edj = None
+        self.group = None
+        import os
+        os.remove("test.edj")
+
+    def test_state_add(self):
+        for p in self.parts:
+            part = self.edj.part_get(self.parts[0])
+            part.state_add("test_state_add", 0.0)
+            self.assertTrue(part.state_exist("test_state_add", 0.0))
+            part.state_add("test_state_add", 0.0)
+            self.assertTrue(part.state_exist("test_state_add", 0.0))
+            part.state_add("default", 0.0)
+            self.assertTrue(part.state_exist("default", 0.0))
+
+    def test_state_copy(self):
+        for p in self.parts:
+            part = self.edj.part_get(self.parts[0])
+            self.assertTrue(part.state_copy("default", 0.0, "new_state", 0))
+            self.assertTrue(part.state_copy("new_state", 0, "another_new_state", 0))
+            self.assertTrue(part.state_copy("another_new_state", 0.0, "default", 0))
+            self.assertFalse(part.state_copy("non-existing", 0.0, "new_state", 0))
+
+    def test_state_del(self):
+        for p in self.parts:
+            part = self.edj.part_get(self.parts[0])
+            part.state_add("test_state_del", 0.0)
+            part.state_del("test_state_del", 0)
+            self.assertFalse(part.state_exist("test_state_del", 0.0))
+            part.state_del("test_state_del", 0)
+            self.assertFalse(part.state_exist("test_state_del", 0.0))
+            #we can't del "default" 0.0 state
+            part.state_del("default", 0.0)
+            self.assertTrue(part.state_exist("default", 0.0))
+
+edje.file_cache_set(0)
+suite = unittest.TestLoader().loadTestsFromTestCase(Basics)
+unittest.TextTestRunner(verbosity=2).run(suite)
+edje.shutdown()
+ecore.evas.shutdown()
