08ffbc060a43bd4b0306dd53d3f121cd26e18c0e
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-08-19 22:49:31 +0000

    ewl is broken these days. to broken we go.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@51454 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- ewl/src/bin/ewl_test_private.h
+++
@@ -1,9 +0,0 @@
-#ifndef EWL_TEST_PRIVATE_H
-#define EWL_TEST_PRIVATE_H
-
-#include <ewl-config.h>
-
-#define LOG_FAILURE(buffer, len, format, args...) \
-        snprintf(buf, len, "%s:%d " format, __FILE__, __LINE__, ## args)
-
-#endif /* EWL_TEST_PRIVATE_H */
