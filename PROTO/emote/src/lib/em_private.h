e2d4c33f43fbc2d197dc83f676d977b550a7e131
devilhorns <devilhorns@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-08-22 15:19:50 +0000

    Fix unterminated ifdef.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@51524 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/emote/src/lib/em_private.h
+++ PROTO/emote/src/lib/em_private.h
@@ -1,29 +1,30 @@
 #ifndef EMOTE_MAIN_H
-#define EMOTE_MAIN_H
+# define EMOTE_MAIN_H
 
 # ifdef HAVE_CONFIG_H
 #  include "config.h"
 # endif
 
-#include "Emote.h"
+# include "Emote.h"
 
-#include <signal.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <ctype.h>
+# include <signal.h>
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <errno.h>
+# include <sys/types.h>
+# include <ctype.h>
 
-#if TIME_WITH_SYS_TIME
-# include <sys/time.h>
-# include <time.h>
-#else
-# if HAVE_SYS_TIME_H
+# if TIME_WITH_SYS_TIME
 #  include <sys/time.h>
-# else
 #  include <time.h>
+# else
+#  if HAVE_SYS_TIME_H
+#   include <sys/time.h>
+#  else
+#   include <time.h>
+#  endif
 # endif
 
 # ifdef HAVE_ALLOCA_H
@@ -43,11 +44,11 @@ extern "C"
 void *alloca (size_t);
 # endif
 
-#include <limits.h>
+# include <limits.h>
 
-#if HAVE_DLFCN_H
-#include <dlfcn.h>
-#endif
+# if HAVE_DLFCN_H
+#  include <dlfcn.h>
+# endif
 
 struct
 {
