0fc1aa14d9ac14ec8d08a38eb493d248c0247123
titan <titan@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-08-23 21:01:45 +0000

    Start to generalize the interface to handle multiple plugins.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@51589 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/emote/src/bin/em_gui.h
+++ PROTO/emote/src/bin/em_gui.h
@@ -11,13 +11,10 @@ struct _Em_Gui
    Em_Object em_obj_inherit;
 
    Evas_Object *win, *o_tb;
-   Evas_Object *o_chansel;
-   Evas_Object *o_chantxt;
-   Evas_Object *o_entry;
+   Evas_Object *o_proto;
 };
 
 EM_INTERN int em_gui_init(void);
-EM_INTERN void em_gui_message_add(const char *text);
 EM_INTERN int em_gui_shutdown(void);
 
 # endif
