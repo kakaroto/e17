e4ba41871ac2dbe727c8d3227dd517814abca46e
sachiel <sachiel@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-09-10 15:24:37 +0000

    Check required Cython version for "core" bindings.
    It's 0.13 btw.
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@52142 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/python/python-edje/setup.py
+++ BINDINGS/python/python-edje/setup.py
@@ -12,6 +12,13 @@ except ImportError:
 if not have_cython and not os.path.exists("edje/edje.c_edje.c"):
     raise SystemExit("You need Cython -- http://cython.org/")
 
+from Cython.Compiler.Version import version as cython_version
+
+req_version = (0, 13)
+cur_version = map(int, cython_version.split('.'))
+if (cur_version[0] < req_version[0]) or (cur_version[1] < req_version[1]):
+    raise SystemExit("You need Cython >= " + '.'.join(map(str, req_version)))
+
 from ez_setup import use_setuptools
 use_setuptools('0.6c9')
 
