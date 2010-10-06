8fd7b795b88c91d15f0e94cd881b21947ec49325
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-10-06 20:32:26 +0000

    [Enjoy] Add nowplaying.c, forgotten on last commit. Bad git-svn :(
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@53116 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ enjoy/src/bin/nowplaying.c
@@ -0,0 +1,20 @@
+#include "private.h"
+
+Evas_Object *
+nowplaying_add(Evas_Object *parent)
+{
+   Evas_Object *layout = elm_layout_add(parent);
+   if (!layout) return NULL;
+
+   if (!elm_layout_file_set(layout, PACKAGE_DATA_DIR "/default.edj", "nowplaying"))
+     {
+        CRITICAL("no theme for 'nowplaying' at %s", PACKAGE_DATA_DIR "/default.edj");
+        goto error;
+     }
+
+   return layout;
+error:
+   evas_object_del(layout);
+   return NULL;
+}
+
