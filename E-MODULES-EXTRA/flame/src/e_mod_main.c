89d31eca00231e29f37e41157e5721718b510c66
morlenxus <morlenxus@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-10-17 17:35:20 +0000

    New module category.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@53523 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/flame/src/e_mod_main.c
+++ E-MODULES-EXTRA/flame/src/e_mod_main.c
@@ -58,8 +58,8 @@ e_modapi_init (E_Module * m)
    bind_textdomain_codeset (PACKAGE, "UTF-8");
 
    snprintf (buf, sizeof (buf), "%s/e-module-flame.edj", e_module_dir_get (m));
-   e_configure_registry_category_add ("appearance", 10, D_ ("Appearance"), NULL, 
-				     "preferences-appearance");
+   e_configure_registry_category_add ("appearance", 10, D_ ("Look"), NULL, 
+				     "preferences-look");
    e_configure_registry_item_add ("appearance/flame", 150, D_ ("Flame"), NULL, 
 				 buf, e_int_config_flame_module);
 
