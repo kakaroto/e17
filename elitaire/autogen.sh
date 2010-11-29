29707b2a77b7e7cf02cdf70be4e11aabe1f02522
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2010-11-29 23:31:52 +0000

    svn mv elitaire ../BROKEN
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@55076 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- elitaire/autogen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-autoreconf -v --install || exit 1
-
-if [ -z "$NOCONFIGURE" ]; then
-	./configure "$@"
-fi
--- elitaire/src/ewl_dialogs.h
+++
@@ -1,6 +0,0 @@
-void ewl_frontend_dialog_config_open(Eli_App * eap);
-void ewl_frontend_dialog_highscore_open(Eli_App * eap, const char * game);
-void ewl_frontend_dialog_highscore_add(Eli_App * eap, float points, pointsType type);
-void ewl_frontend_dialog_about_open(Eli_App * eap);
-void ewl_frontend_dialog_transient_cb(Ewl_Widget * w, void * e, void * data);
-
