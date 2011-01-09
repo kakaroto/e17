fd1838b58180aa2afe3db67f8d617f8192c07bb8
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-01-09 19:58:29 +0000

    this doesn't run make, so don't tell people to make install
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@56004 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/azy/autogen.sh
+++ PROTO/azy/autogen.sh
@@ -7,5 +7,3 @@ echo 'Running autoreconf -if...'
 autoreconf -if || exit 1
 echo 'Configuring...'
 [[ -z "$NOCONFIGURE" ]] && cd "${bs_dir}" && ./configure $@ && cd - &> /dev/null || exit 1
-echo -e "\n\nDone!"
-echo -e "\nYou must now run make and then 'make install' as a privileged user."
