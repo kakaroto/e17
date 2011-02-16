a9ab232a12ecf6c06cd60ceea327ff2c0c876803
jeffdameth <jeffdameth@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-02-16 21:58:31 +0000

    remove deprecated e_util_edje_icon_add
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@57104 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/photo/src/photo_utils.c
+++ E-MODULES-EXTRA/photo/src/photo_utils.c
@@ -24,7 +24,7 @@ void photo_util_icon_set(Evas_Object *ic, char *key)
 void photo_util_menu_icon_set(E_Menu_Item *mi, char *key)
 {
    if (!photo->theme)
-     e_util_menu_item_edje_icon_set(mi, key);
+     e_util_menu_item_theme_icon_set(mi, key);
    else
      e_menu_item_icon_edje_set(mi, photo->theme, key);
 }
