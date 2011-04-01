51bb4c7d3bedf9d60393797f94ea17800b990100
cedric <cedric@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-04-01 16:09:48 +0000

    elixir: add proxy API.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@58262 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/javascript/elixir/src/modules/bindings/evas/evas-object-image-binding.c
+++ BINDINGS/javascript/elixir/src/modules/bindings/evas/evas-object-image-binding.c
@@ -55,6 +55,15 @@ static const elixir_parameter_t*        _evas_object_image_4int_params[6] = {
    &int_parameter,
    NULL
 };
+static const elixir_parameter_t*        _evas_object_image_evas_object_params[3] = {
+   &evas_object_image_parameter,
+   &evas_object_parameter,
+   NULL
+};
+static const elixir_parameter_t*	_evas_object_image_params[2] = {
+   &evas_object_image_parameter,
+   NULL
+};
 
 static JSBool
 elixir_evas_object_image_file_set(JSContext *cx, uintN argc, jsval *vp)
@@ -66,7 +75,7 @@ elixir_evas_object_image_file_set(JSContext *cx, uintN argc, jsval *vp)
 
    if (elixir_params_check(cx, _evas_object_image_file_params, val, argc, JS_ARGV(cx, vp)))
      {
-	know = JS_GetPrivate(cx, val[0].v.obj);
+        GET_PRIVATE(cx, val[0].v.obj, know);
 
 	if (!elixir_evas_file_extract(cx, val[1].v.obj, (const char**) &file, &key))
 	  return JS_FALSE;
@@ -77,7 +86,7 @@ elixir_evas_object_image_file_set(JSContext *cx, uintN argc, jsval *vp)
      {
 	size_t length;
 
-	know = JS_GetPrivate(cx, val[0].v.obj);
+	GET_PRIVATE(cx, val[0].v.obj, know);
 
 	file = elixir_file_canonicalize(elixir_get_string_bytes(val[1].v.str, NULL));
 	key = elixir_get_string_bytes(val[2].v.str, &length);
@@ -107,7 +116,7 @@ elixir_evas_object_image_border_set(JSContext *cx, uintN argc, jsval *vp)
 
    if (elixir_params_check(cx, _evas_object_image_image_border_params, val, argc, JS_ARGV(cx, vp)))
      {
-	know = JS_GetPrivate(cx, val[0].v.obj);
+        GET_PRIVATE(cx, val[0].v.obj, know);
 
 	if (!elixir_evas_border_extract(cx, val[1].v.obj, &l, &r, &t, &b))
 	  return JS_FALSE;
@@ -115,7 +124,7 @@ elixir_evas_object_image_border_set(JSContext *cx, uintN argc, jsval *vp)
 
    if (elixir_params_check(cx, _evas_object_image_4int_params, val, argc, JS_ARGV(cx, vp)))
      {
-	know = JS_GetPrivate(cx, val[0].v.obj);
+        GET_PRIVATE(cx, val[0].v.obj, know);
 
 	l = val[1].v.num;
 	r = val[2].v.num;
@@ -142,7 +151,7 @@ elixir_evas_object_image_fill_set(JSContext *cx, uintN argc, jsval *vp)
 
    if (elixir_params_check(cx, _evas_object_image_geometry_params, val, argc, JS_ARGV(cx, vp)))
      {
-	know = JS_GetPrivate(cx, val[0].v.obj);
+        GET_PRIVATE(cx, val[0].v.obj, know);
 
 	if (!elixir_extract_geometry(cx, val[1].v.obj, &x, &y, &w, &h))
 	  return JS_FALSE;
@@ -150,7 +159,7 @@ elixir_evas_object_image_fill_set(JSContext *cx, uintN argc, jsval *vp)
 
    if (elixir_params_check(cx, _evas_object_image_4int_params, val, argc, JS_ARGV(cx, vp)))
      {
-	know = JS_GetPrivate(cx, val[0].v.obj);
+        GET_PRIVATE(cx, val[0].v.obj, know);
 
 	x = val[1].v.num;
 	y = val[2].v.num;
@@ -241,7 +250,7 @@ elixir_evas_object_image_data_update_add(JSContext *cx, uintN argc, jsval *vp)
 
    if (elixir_params_check(cx, _evas_object_image_geometry_params, val, argc, JS_ARGV(cx, vp)))
      {
-	know = JS_GetPrivate(cx, val[0].v.obj);
+        GET_PRIVATE(cx, val[0].v.obj, know);
 
 	if (!elixir_extract_geometry(cx, val[1].v.obj, &x, &y, &w, &h))
 	  return JS_FALSE;
@@ -249,7 +258,7 @@ elixir_evas_object_image_data_update_add(JSContext *cx, uintN argc, jsval *vp)
 
    if (elixir_params_check(cx, _evas_object_image_4int_params, val, argc, JS_ARGV(cx, vp)))
      {
-	know = JS_GetPrivate(cx, val[0].v.obj);
+        GET_PRIVATE(cx, val[0].v.obj, know);
 
 	x = val[1].v.num;
 	y = val[2].v.num;
@@ -274,8 +283,7 @@ elixir_evas_object_image_preload(JSContext *cx, uintN argc, jsval *vp)
    if (!elixir_params_check(cx, _evas_object_image_boolean_params, val, argc, JS_ARGV(cx, vp)))
      return JS_FALSE;
 
-   know = JS_GetPrivate(cx, val[0].v.obj);
-   if (!know) return JS_FALSE;
+   GET_PRIVATE(cx, val[0].v.obj, know);
 
    evas_object_image_preload(know, val[1].v.bol);
 
@@ -297,13 +305,60 @@ elixir_evas_object_image_load_dpi_set(JSContext *cx, uintN argc, jsval *vp)
    if (!elixir_params_check(cx, _evas_object_image_double_params, val, argc, JS_ARGV(cx, vp)))
      return JS_FALSE;
 
-   know = JS_GetPrivate(cx, val[0].v.obj);
-   if (!know) return JS_FALSE;
-
+   GET_PRIVATE(cx, val[0].v.obj, know);
    dpi = val[1].v.dbl;
+
    evas_object_image_load_dpi_set(know, dpi);
 
    return JS_TRUE;
+}
+
+static JSBool
+elixir_evas_object_image_source_set(JSContext *cx, uintN argc, jsval *vp)
+{
+  Evas_Object *know;
+  Evas_Object *src;
+  elixir_value_t val[2];
+
+  if (!elixir_params_check(cx, _evas_object_image_evas_object_params, val, argc, JS_ARGV(cx, vp)))
+    return JS_FALSE;
+
+  GET_PRIVATE(cx, val[0].v.obj, know);
+  GET_PRIVATE(cx, val[1].v.obj, src);
+
+  JS_SET_RVAL(cx, vp, BOOLEAN_TO_JSVAL(evas_object_image_source_set(know, src)));
+  return JS_TRUE;
+}
+
+static JSBool
+elixir_evas_object_image_source_get(JSContext *cx, uintN argc, jsval *vp)
+{
+  Evas_Object *know;
+  Evas_Object *src;
+  elixir_value_t val[1];
+
+  if (!elixir_params_check(cx, _evas_object_image_params, val, argc, JS_ARGV(cx, vp)))
+    return JS_FALSE;
+
+  GET_PRIVATE(cx, val[0].v.obj, know);
+
+  src = evas_object_image_source_get(know);
+  return evas_object_to_jsval(cx, src, &(JS_RVAL(cx, vp)));
+}
+
+static JSBool
+elixir_evas_object_image_source_unset(JSContext *cx, uintN argc, jsval *vp)
+{
+  Evas_Object *know;
+  elixir_value_t val[1];
+
+  if (!elixir_params_check(cx, _evas_object_image_params, val, argc, JS_ARGV(cx, vp)))
+    return JS_FALSE;
+
+  GET_PRIVATE(cx, val[0].v.obj, know);
+
+  JS_SET_RVAL(cx, vp, BOOLEAN_TO_JSVAL(evas_object_image_source_unset(know)));
+  return JS_TRUE;
 }
 
 static JSFunctionSpec	evas_object_image_functions[] = {
@@ -314,6 +369,9 @@ static JSFunctionSpec	evas_object_image_functions[] = {
   ELIXIR_FN(evas_object_image_data_set, 2, JSPROP_ENUMERATE, 0 ),
   ELIXIR_FN(evas_object_image_data_update_add, 5, JSPROP_ENUMERATE, 0 ),
   ELIXIR_FN(evas_object_image_load_dpi_set, 1, JSPROP_ENUMERATE, 0 ),
+  ELIXIR_FN(evas_object_image_source_set, 2, JSPROP_ENUMERATE, 0 ),
+  ELIXIR_FN(evas_object_image_source_get, 1, JSPROP_ENUMERATE, 0 ),
+  ELIXIR_FN(evas_object_image_source_unset, 1, JSPROP_ENUMERATE, 0 ),
   JS_FS_END
 };
 
