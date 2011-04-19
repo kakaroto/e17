f3d8d49eb830b8058300534d7bfc70c8913f0e92
nash <nash@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-04-19 06:09:34 +0000

    Add filter tests.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@58728 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ expedite/src/bin/filter_object_invert.c
@@ -0,0 +1,106 @@
+#undef FNAME
+#undef NAME
+#undef ICON
+
+/* metadata */
+#define FNAME filter_object_invert
+#define NAME "Filter Object Invert"
+#define ICON "blend.png"
+
+#ifndef PROTO
+# ifndef UI
+#  include "main.h"
+
+/* standard var */
+static int done = 0;
+/* private data */
+static Evas_Object *o_images[OBNUM];
+
+/* setup */
+static void _setup(void)
+{
+   int i;
+   Evas_Object *o,*src;
+
+   for (i = 0; i < OBNUM; i++)
+     {
+	o = evas_object_image_add(evas);
+	evas_object_image_file_set(o, build_path("logo.png"), NULL);
+	evas_object_image_fill_set(o, 0, 0, 120, 160);
+	evas_object_resize(o, 120, 160);
+	evas_object_filter_set(o, EVAS_FILTER_INVERT);
+	evas_object_show(o);
+	src = o;
+	o_images[i] = src;
+     }
+
+   done = 0;
+}
+
+/* cleanup */
+static void _cleanup(void)
+{
+   int i;
+   for (i = 0; i < OBNUM; i++) evas_object_del(o_images[i]);
+}
+
+/* loop - do things */
+static void _loop(double t, int f)
+{
+   int i;
+   Evas_Coord x, y, w, h;
+   for (i = 0; i < OBNUM; i++)
+     {
+        w = 120;
+	h = 160;
+	x = (win_w / 2) - (w / 2);
+	x += sin((double)(f + (i * 13)) / (36.7 * SLOW)) * (w / 2);
+	y = (win_h / 2) - (h / 2);
+	y += cos((double)(f + (i * 28)) / (43.8 * SLOW)) * (h / 2);
+	evas_object_move(o_images[i], x, y);
+     }
+   FPS_STD(NAME);
+}
+
+/* prepend special key handlers if interactive (before STD) */
+static void _key(char *key)
+{
+   KEY_STD;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/* template stuff - ignore */
+# endif
+#endif
+
+#ifdef UI
+_ui_menu_item_add(ICON, NAME, FNAME);
+#endif
+
+#ifdef PROTO
+void FNAME(void);
+#endif
+
+#ifndef PROTO
+# ifndef UI
+void FNAME(void)
+{
+   ui_func_set(_key, _loop);
+   _setup();
+}
+# endif
+#endif
+#undef FNAME
+#undef NAME
+#undef ICON
+
