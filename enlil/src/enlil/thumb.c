8c60647182b3d9a000ccafd34c8fd16818d2abc0
cedric <cedric@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-04-27 18:07:49 +0000

    enlil: use new ethumb exists async API.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@58979 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- enlil/src/enlil/thumb.c
+++ enlil/src/enlil/thumb.c
@@ -6,32 +6,28 @@ typedef struct Enlil_Thumb Enlil_Thumb;
 static void _thumb_connect_cb(void *data, Ethumb_Client *client, Eina_Bool success);
 static void _thumb_die_cb(void *data, Ethumb_Client *client);
 static void _thumb_done_cb(void *data, Ethumb_Client *client, int id, const char *file, const char *key, const char *thumb_path, const char *thumb_key, Eina_Bool success);
-static void _job_next();
-static void _job_free(Enlil_Thumb_Job *job);
-static void _check_exists_thread_cb(void *data, Ecore_Thread *thread);
-static void _check_exists_thread_done_cb(void *data, Ecore_Thread *thread);
-
 
 struct Enlil_Thumb_Job
 {
-   Enlil_Thumb_Job_Type type;
-   Enlil_Photo *photo;
-
    Enlil_Thumb_Done_Cb done_cb;
    Enlil_Thumb_Error_Cb error_cb;
    void *data;
 
-   int try;
+   Ethumb_Exists *thread;
+   Enlil_Photo *photo;
+
+   Enlil_Thumb_Job_Type type;
+   long id;
 };
 
 struct Enlil_Thumb
 {
    Ethumb_Client *ethumb;
+
    int connected;
    int running;
-   Eina_List *jobs;
 
-   Enlil_Thumb_Job *current_job;
+   Eina_List *jobs;
 };
 
 static Enlil_Thumb thumb;
@@ -39,11 +35,12 @@ static Enlil_Thumb thumb;
 static int count = 0;
 int enlil_thumb_init()
 {
-   if(count>0) return ++count;
+   if(count > 0) return ++count;
 
    thumb.running = EINA_FALSE;
    thumb.ethumb = ethumb_client_connect(_thumb_connect_cb, NULL, NULL);
    ethumb_client_on_server_die_callback_set(thumb.ethumb, _thumb_die_cb, NULL, NULL);
+
    return ++count;
 }
 
@@ -57,55 +54,106 @@ int enlil_thumb_shutdown()
    return --count;
 }
 
-
-const char* enlil_thumb_photo_get(Enlil_Photo *photo, Enlil_Thumb_Job_Type type,
-      Enlil_Thumb_Done_Cb done_cb, Enlil_Thumb_Error_Cb error_cb, void *data)
+void
+enlil_exists_cb(Ethumb_Client *client,
+                Ethumb_Exists *request,
+                Eina_Bool exists,
+                void *data)
 {
-   const char *path;
-   Enlil_Thumb_Job *job;
+   Enlil_Thumb_Job *job = data;
 
-   ASSERT_RETURN(photo!=NULL);
+   if (ethumb_client_thumb_exists_check(request))
+     return ;
 
-   switch(type)
+   if (exists)
      {
-      case Enlil_THUMB_FDO_NORMAL:
-	 if( (path = enlil_photo_thumb_fdo_normal_get(photo)) && ecore_file_exists(path))
-	   return path;
-	 break;
-      case Enlil_THUMB_FDO_LARGE:
-	 if( (path = enlil_photo_thumb_fdo_large_get(photo)) && ecore_file_exists(path))
-	   return path;
-	 break;
-     }
- 
-   //maybe the job is already in the list
-   Eina_List *l;
-   Eina_Bool find = EINA_FALSE;
-   EINA_LIST_FOREACH(thumb.jobs, l, job)
-     {
-	if(job->photo == photo && job->type == type)
-	  {
-	     find = EINA_TRUE;
-	     break;
-	  }
-     }
+        const char *path;
 
-   if(find)
-     {
-	thumb.jobs = eina_list_remove(thumb.jobs, job);
+        ethumb_client_thumb_path_get(client, &path, NULL);
+        switch (job->type)
+          {
+           case Enlil_THUMB_FDO_LARGE:
+              enlil_photo_thumb_fdo_large_set(job->photo, path);
+              break;
+           case Enlil_THUMB_FDO_NORMAL:
+              enlil_photo_thumb_fdo_normal_set(job->photo, path);
+              break;
+          }
+
+        if(job->done_cb)
+          job->done_cb(job->data, job->photo, path);
+
+        thumb.jobs = eina_list_remove(thumb.jobs, job);
+        free(job);
      }
    else
      {
-	job = calloc(1, sizeof(Enlil_Thumb_Job));
-	job->photo = photo;
-	job->type = type;
-	job->done_cb = done_cb;
-	job->error_cb = error_cb;
-	job->data = data;
+        char buf[PATH_MAX];
+
+        snprintf(buf, PATH_MAX, "%s/%s",
+                 enlil_photo_path_get(job->photo),
+                 enlil_photo_file_name_get(job->photo));
+
+        job->thread = NULL;
+        job->id = ethumb_client_generate(thumb.ethumb, _thumb_done_cb, job, NULL);
      }
+}
+
+const char* enlil_thumb_photo_get(Enlil_Photo *photo,
+				  Enlil_Thumb_Job_Type type,
+				  Enlil_Thumb_Done_Cb done_cb,
+				  Enlil_Thumb_Error_Cb error_cb,
+				  void *data)
+{
+   const char *path;
+   Enlil_Thumb_Job *job;
+   char buf[PATH_MAX];
+
+   ASSERT_RETURN(photo != NULL);
+
+   switch (type)
+     {
+      case Enlil_THUMB_FDO_NORMAL:
+         if ((path = enlil_photo_thumb_fdo_normal_get(photo))
+             && ecore_file_exists(path))
+           return path;
+	 break;
+      case Enlil_THUMB_FDO_LARGE:
+         if ((path = enlil_photo_thumb_fdo_large_get(photo))
+             && ecore_file_exists(path))
+	   return path;
+	 break;
+     }
+
+   snprintf(buf, PATH_MAX, "%s/%s",
+            enlil_photo_path_get(photo),
+            enlil_photo_file_name_get(photo));
+   ethumb_client_file_set(thumb.ethumb, buf, NULL);
+
+   switch (type)
+     {
+      case Enlil_THUMB_FDO_LARGE:
+         ethumb_client_fdo_set(thumb.ethumb, ETHUMB_THUMB_LARGE);
+         break;
+      case Enlil_THUMB_FDO_NORMAL:
+         ethumb_client_fdo_set(thumb.ethumb, ETHUMB_THUMB_NORMAL);
+         break;
+     }
+
+   job = calloc(1, sizeof (Enlil_Thumb_Job));
+   if (!job) return NULL;
+
+   job->photo = photo;
+   job->type = type;
+   job->done_cb = done_cb;
+   job->error_cb = error_cb;
+   job->data = data;
+   job->thread = ethumb_client_thumb_exists(thumb.ethumb,
+					    enlil_exists_cb,
+					    job);
+
    thumb.jobs = eina_list_prepend(thumb.jobs, job);
 
-   _job_next();
    return NULL;
 }
 
@@ -115,9 +163,11 @@ void enlil_thumb_clear()
 
    EINA_LIST_FREE(thumb.jobs, job)
      {
-	_job_free(job); 
+        if (job->thread)
+          ethumb_client_thumb_exists_cancel(job->thread);
+        else
+          ethumb_client_generate_cancel(thumb.ethumb, job->id, NULL, NULL, NULL);
      }
-   thumb.current_job = NULL;
 }
 
 void enlil_thumb_photo_clear(const Enlil_Photo *photo)
@@ -130,18 +180,13 @@ void enlil_thumb_photo_clear(const Enlil_Photo *photo)
 	if(job->photo == photo)
 	  {
 	     thumb.jobs = eina_list_remove(thumb.jobs, job);
-	     _job_free(job);
+
+             if (job->thread)
+               ethumb_client_thumb_exists_cancel(job->thread);
+             else
+               ethumb_client_generate_cancel(thumb.ethumb, job->id, NULL, NULL, NULL);
 	  }
      }
-   if(thumb.current_job && thumb.current_job->photo == photo)
-	 thumb.current_job = NULL;
-}
-
-static void _job_free(Enlil_Thumb_Job *job)
-{
-   ASSERT_RETURN_VOID(job != NULL);
-
-   free(job);
 }
 
 static void _thumb_connect_cb(__UNUSED__ void *data, __UNUSED__ Ethumb_Client *client, Eina_Bool success)
@@ -149,7 +194,6 @@ static void _thumb_connect_cb(__UNUSED__ void *data, __UNUSED__ Ethumb_Client *c
    if(success)
      {
 	thumb.connected = 1;
-	_job_next(thumb);
      }
    else
      {
@@ -161,125 +205,35 @@ static void _thumb_connect_cb(__UNUSED__ void *data, __UNUSED__ Ethumb_Client *c
 
 static void _thumb_die_cb(__UNUSED__ void *data, __UNUSED__ Ethumb_Client *client)
 {
-   if(thumb.current_job)
-     {
-	if(thumb.current_job->error_cb)
-	  thumb.current_job->error_cb(thumb.current_job->data, thumb.current_job->photo);
-
-	_job_free(thumb.current_job);
-	thumb.current_job = NULL;
-     }
-
-   thumb.running = 0;
-   
-   --count;
-
    if(thumb.connected)
      ethumb_client_disconnect(thumb.ethumb);
 
-   enlil_thumb_init();
+   thumb.running = EINA_FALSE;
+   thumb.ethumb = ethumb_client_connect(_thumb_connect_cb, NULL, NULL);
+   ethumb_client_on_server_die_callback_set(thumb.ethumb, _thumb_die_cb, NULL, NULL);
 }
 
-static void _job_next()
-{
-   char buf[PATH_MAX];
-
-   if(thumb.running)
-     return;
-
-
-   if(!thumb.jobs || !thumb.connected)
-     {
-	thumb.running = 0;
-	return ;
-     }
-
-   Enlil_Thumb_Job *job = eina_list_nth(thumb.jobs,0);
-   thumb.jobs = eina_list_remove(thumb.jobs, job);
-   thumb.current_job = job;
-
-   snprintf(buf, PATH_MAX, "%s/%s", enlil_photo_path_get(job->photo),enlil_photo_file_name_get(job->photo));
-   ethumb_client_file_set(thumb.ethumb, buf, NULL);
-
-   switch(job->type)
-     {
-      case Enlil_THUMB_FDO_LARGE:
-	 ethumb_client_fdo_set(thumb.ethumb, ETHUMB_THUMB_LARGE);
-	 break;
-      case Enlil_THUMB_FDO_NORMAL:
-	 ethumb_client_fdo_set(thumb.ethumb, ETHUMB_THUMB_NORMAL);
-	 break;
-     }
-
-   thumb.running = 1;
-   ecore_thread_run(_check_exists_thread_cb, _check_exists_thread_done_cb, NULL, job);
-}
-
-static void _check_exists_thread_cb(__UNUSED__ void *data, __UNUSED__ Ecore_Thread *thread)
-{
-   ethumb_client_thumb_exists(thumb.ethumb);
-}
-
-
-static void _check_exists_thread_done_cb(void *data, __UNUSED__ Ecore_Thread *thread)
+static void _thumb_done_cb(void *data, __UNUSED__ Ethumb_Client *client,
+                           __UNUSED__ int id,
+                           __UNUSED__ const char *file, __UNUSED__ const char *key,
+                           const char *thumb_path, __UNUSED__ const char *thumb_key,
+                           Eina_Bool success)
 {
    Enlil_Thumb_Job *job = data;
-   const char *path = NULL;
-   char buf[PATH_MAX];
 
-   if(!thumb.current_job)
+   thumb.jobs = eina_list_remove(thumb.jobs, job);
+
+   if (!success)
      {
-	_job_free(job);
-	thumb.running = 0;
-	_job_next();
-	return ;
-     }
-
-   if(ethumb_client_thumb_exists(thumb.ethumb))
-     {
-	ethumb_client_thumb_path_get(thumb.ethumb, &path, NULL);
-	switch(job->type)
-	  {
-	   case Enlil_THUMB_FDO_LARGE:
-	      enlil_photo_thumb_fdo_large_set(job->photo,path);
-	      break;
-	   case Enlil_THUMB_FDO_NORMAL:
-	      enlil_photo_thumb_fdo_normal_set(job->photo, path);
-	      break;
-	  }
-     }
-
-   snprintf(buf, PATH_MAX, "%s/%s", enlil_photo_path_get(job->photo),enlil_photo_file_name_get(job->photo));
-
-   if(path && ecore_file_exists(path))
-	_thumb_done_cb(job, thumb.ethumb, -1, buf, NULL, path, NULL, EINA_TRUE);
-   else
-	ethumb_client_generate(thumb.ethumb, _thumb_done_cb, job, NULL);
-}
-
-static void _thumb_done_cb(void *data, __UNUSED__ Ethumb_Client *client, __UNUSED__ int id,
-		__UNUSED__ const char *file, __UNUSED__ const char *key, const char *thumb_path,
-		__UNUSED__ const char *thumb_key, Eina_Bool success)
-{
-   Enlil_Thumb_Job *job = (Enlil_Thumb_Job*) data;
-  
-   if(!thumb.current_job) goto end; 
-
-   if(!success && job->try<3)
-     {
-	thumb.jobs = eina_list_prepend(thumb.jobs, job);
-	job->try++;
-	goto end_nofree;
-     }
-   else if(!success)
-     {
-	LOG_CRIT("Can't create the thumbnail of the photo %s/%s", enlil_photo_path_get(job->photo), enlil_photo_file_name_get(job->photo));
-	if(job->error_cb)
+	LOG_CRIT("Can't create the thumbnail of the photo %s/%s",
+                 enlil_photo_path_get(job->photo),
+                 enlil_photo_file_name_get(job->photo));
+	if (job->error_cb)
 	  job->error_cb(job->data, job->photo);
 	goto end;
      }
 
-   switch(job->type)
+   switch (job->type)
      {
       case Enlil_THUMB_FDO_LARGE:
 	 enlil_photo_thumb_fdo_large_set(job->photo, thumb_path);
@@ -289,14 +243,10 @@ static void _thumb_done_cb(void *data, __UNUSED__ Ethumb_Client *client, __UNUSE
 	 break;
      }
 
-   if(job->done_cb)
+   if (job->done_cb)
      job->done_cb(job->data, job->photo, thumb_path);
 
-   thumb.current_job = NULL;
-end:
-   _job_free(job);
-end_nofree:
-   thumb.running = 0;
-   _job_next();
+ end:
+   free(job);
 }
 
