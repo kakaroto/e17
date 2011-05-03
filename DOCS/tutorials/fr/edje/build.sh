03a7f74164aa806644c811ec251424d937afa633
captainigloo <captainigloo@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-05-03 11:36:12 +0000

    DOCS : French edje tutorial, more documentation and examples
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@59148 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- DOCS/tutorials/fr/edje/build.sh
+++ DOCS/tutorials/fr/edje/build.sh
@@ -1,29 +1,47 @@
 #!/bin/sh
 
-topdir=./
-builddir=./build
+topdir="./"
+builddir="./build"
+
 
 if [ -e "$1" ]; then
 {
     echo "building $1"
-    if [ -f $topdir/$1/$1*.edc ]; then
-        edje_cc -v -id $topdir/images $topdir/$1/$1.edc -o $builddir/$1.edj
-    fi
+        for f in `ls $1/*.edc`;  do
+            file=`basename $f`
+            file=$(echo $file | cut -f1 -d '.')
+            echo "building $f"
+            edje_cc -id $topdir/images $f -o $builddir/$file.edj
+        done
 
-    if [ -f $topdir/$1/$1*.c ]; then
-        gcc -o $builddir/$1 $topdir/$1/$1*.c `pkg-config elementary --cflags --libs`
-    fi
+        for f in `ls $1/*.c`; do
+            file=`basename $f`
+            file=$(echo $file | cut -f1 -d '.')
+            echo "building $f"
+            gcc -o $builddir/$1 $f `pkg-config elementary --cflags --libs`
+
+        done
 }
 else
     for i in `ls -d tut*`; do
         echo "building $i"
-        if [ -f $topdir/$i/$i*.edc ]; then
-            edje_cc -id $topdir/images $topdir/$i/$i.edc -o $builddir/$i.edj
-        fi
 
-        if [ -f $topdir/$i/$i*.c ]; then
-            gcc -o $builddir/$i $topdir/$i/$i*.c `pkg-config elementary --cflags --libs`
-        fi
+        for f in `ls $i/*.edc`; do
+            file=`basename $f`
+            file=$(echo $file | cut -f1 -d '.')
+            echo "building $f"
+            edje_cc -id $topdir/images $f -o $builddir/$file.edj
+        done
+
+
+        for f in `ls $1/*.c`; do
+            file=`basename $f`
+            file=$(echo $file | cut -f1 -d '.')
+            echo "building $f"
+            gcc -o $builddir/$1 $f `pkg-config elementary --cflags --libs`
+        done
+
+
     done
 fi
 
