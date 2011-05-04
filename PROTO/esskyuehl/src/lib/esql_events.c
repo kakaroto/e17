42c76cccaaeba581cc99c22d6d48a6fb41ff1e11
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-05-04 08:34:57 +0000

    better fix: use the right object
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@59182 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/esskyuehl/src/lib/esql_events.c
+++ PROTO/esskyuehl/src/lib/esql_events.c
@@ -257,7 +257,7 @@ esql_connect_handler(Esql             *e,
         else
           {
              if (ev->connect_cb)
-               ev->connect_cb(ev, e ? e->connect_cb_data : NULL);
+               ev->connect_cb(ev, ev->connect_cb_data);
              else
                ecore_event_add(ESQL_EVENT_ERROR, ev, (Ecore_End_Cb)esql_fake_free, NULL);
           }
