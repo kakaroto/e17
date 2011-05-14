cf407f2060e7c3ea658efe8b222b61ada642f9d8
seoz <seoz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-05-14 04:05:31 +0000

    devs/seoz: Added my build shell.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@59375 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ devs/seoz/build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -e
+
+export PREFIX=/usr/local
+export CFLAGS="-O2 -march=native -ffast-math -g3 -W -Wall -Wextra"
+export CC="ccache gcc"
+export MAKEFLAGS="make -j6"
+alias make='make -j6'
+
+export BUILD_BASIC="eina eet evas ecore embryo edje e_dbus efreet expedite e elementary"
+export BUILD_PYTHON_BINDINGS="BINDINGS/python/python-evas BINDINGS/python/python-elementary BINDINGS/python/python-ecore BINDINGS/python/python-edje BINDINGS/python/python-emotion"
+export BUILD_C_BINDINGS="BINDINGS/cxx/eflxx BINDINGS/cxx/einaxx BINDINGS/cxx/evasxx BINDINGS/cxx/ecorexx BINDINGS/cxx/elementaryxx"
+export BUILD_BINDINGS=$BUILD_PYTHON_BINDINGS" "$BUILD_C_BINDINGS" "
+export BUILD_E_MODULES="E-MODULES-EXTRA/comp-scale E-MODULES-EXTRA/elfe E-MODULES-EXTRA/engage"
+export BUILD_ETC="editje emotion PROTO/eyelight FORMATTING/ecrustify ephoto edje_viewer PROTO/emap PROTO/azy elmdentica eio enlil" #enki
+export BUILD=$BUILD_BASIC" "$BUILD_BINDINGS" "$BUILD_E_MODULES" "$BUILD_ETC
+
+for I in $BUILD; do
+  pushd $I
+	echo ""
+	echo "============ "$I" ============"
+    make clean distclean || true
+    ./autogen.sh --prefix="$PREFIX"
+    make
+    sudo make install
+    sudo ldconfig
+    ctags -R
+  popd
+done
+
+#efenniht theme
+echo ""
+echo "============ efenniht ============"
+pushd THEMES/efenniht
+    make install-home
+popd
+
+#detour theme (elm)
+echo ""
+echo "============ detour elm ============"
+pushd THEMES/detour-elm
+	make clean
+#	make
+#	make install
+popd
+
+#detourious
+echo ""
+echo "============ detourious ============"
+pushd THEMES/detourious
+	make
+	make install
+popd
+
+#darkness theme
+echo ""
+echo "============ darkness ============"
+pushd THEMES/darkness
+    #./build.sh -r
+	./build.sh
+    pushd elm
+        ./build.sh
+        cp *.edj ~/.elementary/themes
+    popd
+popd
+
+ctags -R
+enlightenment_remote -restart
