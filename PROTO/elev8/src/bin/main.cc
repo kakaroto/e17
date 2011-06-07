f76055855f792adc90c3f1ba523829a179733195
mike_m <mike_m@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-06-07 02:58:58 +0000

    Add an empty main window
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@60017 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/bin/main.cc
+++ PROTO/elev8/src/bin/main.cc
@@ -1,8 +1,28 @@
 #include "main.h"
 
+Evas_Object *main_win;
+
+static void
+es_window_delete(void *data, Evas_Object *obj, void *event_info)
+{
+   elm_exit();
+}
+
 int
 elm_main(int argc, char **argv)
 {
+   main_win = elm_win_add(NULL, "main", ELM_WIN_BASIC);
+   elm_win_title_set(main_win, basename(argv[0]));
+   evas_object_focus_set(main_win, 1);
+   evas_object_smart_callback_add(main_win, "delete,request", es_window_delete, NULL);
+   evas_object_resize(main_win, 320, 480);
+   Evas_Object *bg = elm_bg_add(main_win);
+   elm_win_resize_object_add(main_win, bg);
+   evas_object_show(bg);
+
+   evas_object_show(main_win);
+   elm_run();
+
    return 0;
 }
 
