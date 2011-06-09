d1edcaddcc0651ef18c71edbd19d9edc5eaf2dd9
caro <caro@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-06-09 18:42:05 +0000

    itask: fix gettext stuff
    It is strange that there is no po file
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@60147 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/itask/autogen.sh
+++ E-MODULES-EXTRA/itask/autogen.sh
@@ -5,24 +5,12 @@ rm -f aclocal.m4 ltmain.sh
 
 touch README
 
-echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
+echo "Running autopoint..." ; autopoint -f || :
+echo "Running aclocal..." ; aclocal -I m4 $ACLOCAL_FLAGS || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
 echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
-echo "Generating gettext itask.pot.template";\
-xgettext \
--n \
--C \
--d itask \
--p po \
---copyright-holder="Enlightenment development team" \
---foreign-user \
---msgid-bugs-address="enlightenment-devel@lists.sourceforge.net" \
--kD_ \
---from-code=UTF-8 \
--o itask.pot \
-`find . -name "*.[ch]" -print` || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
 	./configure "$@"
