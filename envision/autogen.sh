b2b10c82856eeb731bdc95f674c328ecea48f1bd
caro <caro@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-06-09 07:42:10 +0000

    don't use autoreconf as it can not support optional gettext support (yet)
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@60118 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- envision/autogen.sh
+++ envision/autogen.sh
@@ -17,7 +17,12 @@ fi
 find . -name Makefile -delete
 find . -name Makefile.in -delete
 
-autoreconf -i
+echo "Running autopoint..." ; autopoint -f || :
+echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
+echo "Running autoheader..." ; autoheader || exit 1
+echo "Running autoconf..." ; autoconf || exit 1
+echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
+echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
 cd $ORIGDIR || exit 1
 
