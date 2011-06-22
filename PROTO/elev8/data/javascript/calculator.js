e38669e2a4ade34b7e7c118430ba666e8989ab9a
mike_m <mike_m@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-06-22 07:48:00 +0000

    elev8: Add simple calculator demo
    Signed-off-by: Mike McCormack <mj.mccormack@samsung.com>
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@60579 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ PROTO/elev8/data/javascript/calculator.js
@@ -0,0 +1,231 @@
+#!/usr/local/bin/elev8
+
+var EXPAND_BOTH = { x : 1.0, y : 1.0 };
+var FILL_BOTH = { x : -1.0, y : -1.0 };
+
+var stack = new Array;
+
+function multiply(left, right) {
+	return left * right;
+}
+
+function divide(left, right) {
+	return left / right;
+}
+
+function add(left, right) {
+	return left + right;
+}
+
+function subtract(left, right) {
+	return left - right;
+}
+
+function push(val) {
+	stack.push(val);
+}
+
+function push_entry() {
+	push(calc.elements.vbox.elements.entry.label);
+}
+
+function set_entry(val) {
+	calc.elements.vbox.elements.entry.label = val;
+}
+
+function append_entry(val) {
+	var cur = calc.elements.vbox.elements.entry.label;
+	if (cur == "0")
+		cur = val;
+        else
+		cur += val;
+	calc.elements.vbox.elements.entry.label = cur;
+}
+
+function append_number(b) {
+	append_entry(b.label);
+}
+
+function evaluate() {
+	right = stack.pop();
+	operand = stack.pop();
+	left = stack.pop();
+
+	if (typeof operand != 'function')
+		return;
+	if (typeof left != 'string' && typeof left != 'number')
+		return;
+	if (typeof right != 'string' && typeof right != 'number')
+		return;
+
+	return operand(parseFloat(left), parseFloat(right));
+}
+
+var calc = new elm.main({
+	type : "main",
+	label : "Calculator demo",
+	elements : {
+		the_background : {
+			type : "background",
+			weight : EXPAND_BOTH,
+			resize : true,
+		},
+		vbox : {
+			type : "pack",
+			resize : true,
+			elements : {
+				entry : {
+					type : "label",
+					label : "0",
+					align : { x : 1, y : 0 },
+				},
+				hbox1 : {
+					type : "pack",
+					horizontal : true,
+					homogeneous : true,
+					elements : {
+						b7 : {
+							type : "button",
+							label : "7",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : append_number,
+						},
+						b8 : {
+							type : "button",
+							label : "8",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : append_number,
+						},
+						b9 : {
+							type : "button",
+							label : "9",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : append_number,
+						},
+						divide : {
+							type : "button",
+							label : "/",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : function () {
+								push_entry();
+								set_entry("");
+								push(divide);
+							},
+						},
+					},
+				},
+				hbox2 : {
+					type : "pack",
+					horizontal : true,
+					homogeneous : true,
+					elements : {
+						b4 : {
+							type : "button",
+							label : "4",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : append_number,
+						},
+						b5 : {
+							type : "button",
+							label : "5",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : append_number,
+						},
+						b6 : {
+							type : "button",
+							label : "6",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : append_number,
+						},
+						multiply : {
+							type : "button",
+							label : "*",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : function () {
+								push_entry();
+								set_entry("");
+								push(multiply);
+							},
+						},
+					},
+				},
+				hbox3 : {
+					type : "pack",
+					horizontal : true,
+					homogeneous : true,
+					elements : {
+						b1 : {
+							type : "button",
+							label : "1",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : append_number,
+						},
+						b2 : {
+							type : "button",
+							label : "2",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : append_number,
+						},
+						b3 : {
+							type : "button",
+							label : "3",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : append_number,
+						},
+						minus : {
+							type : "button",
+							label : "-",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : function () {
+								push_entry();
+								set_entry("");
+								push(subtract);
+							},
+						},
+					},
+				},
+				hbox4 : {
+					type : "pack",
+					horizontal : true,
+					homogeneous : true,
+					elements : {
+						b0 : {
+							type : "button",
+							label : "0",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : append_number,
+						},
+						bdot : {
+							type : "button",
+							label : ".",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : append_number,
+						},
+						equals : {
+							type : "button",
+							label : "=",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : function () {
+								push_entry();
+								set_entry("");
+								var answer = evaluate();
+								set_entry(answer);
+							}
+						},
+						plus : {
+							type : "button",
+							label : "+",
+							weight : { x : -1.0, y : -1.0 },
+							on_clicked : function () {
+								push_entry();
+								set_entry("");
+								push(add);
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+});
+
