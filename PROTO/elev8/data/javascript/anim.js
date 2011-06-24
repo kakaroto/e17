e3d5994016812f334ec27008a866d66c265ac724
mike_m <mike_m@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-06-24 04:59:08 +0000

    elev8: Fix layering of shadows
    Signed-off-by: Mike McCormack <mj.mccormack@samsung.com>
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@60649 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/data/javascript/anim.js
+++ PROTO/elev8/data/javascript/anim.js
@@ -8,11 +8,10 @@ var num_bubbles = 3;
 function animator(arg, n, is_shadow) {
 	t = elm.loop_time();
 	fac = 2.0/num_bubbles;
-	PI = 3.14159;
 	r = 48;
-	zz = (((2 + Math.sin(t*6 + (PI * (n * fac))))/3) * 64 ) * 2;
-	xx = (Math.cos(t * 4 + (PI * (n * fac))) * r) * 2;
-	yy = (Math.sin(t * 6 + (PI * (n * fac))) * r) * 2;
+	zz = (((2 + Math.sin(t*6 + (Math.PI * (n * fac))))/3) * 64 ) * 2;
+	xx = (Math.cos(t * 4 + (Math.PI * (n * fac))) * r) * 2;
+	yy = (Math.sin(t * 6 + (Math.PI * (n * fac))) * r) * 2;
 
 	x = my_window.width / 2 + xx - (arg.width / 2);
 	y = my_window.height / 2 + yy - (arg.height / 2);
@@ -34,29 +33,33 @@ function animator(arg, n, is_shadow) {
 	arg.height = h;
 }
 
-function bubble(n) {
+function glyph() {
 	this.type = "image";
-	this.image = "data/images/bubble.png";
 	this.width = 64;
 	this.height = 64;
+}
+
+function bubble(n) {
+	this.image = "data/images/bubble.png";
 	this.on_animate = function (arg) {
 		animator(arg, n, false);
 	}
 }
 
+bubble.prototype = new glyph();
+
 function shadow(n) {
-	this.type = "image";
 	this.image = "data/images/bubble_sh.png";
-	this.width = 64;
-	this.height = 64;
 	this.on_animate = function (arg) {
 		animator(arg, n, true);
 	}
 }
 
+shadow.prototype = new glyph();
+
 var my_window = new elm.main({
 	type : "main",
-	label : "Button demo",
+	label : "Animation demo",
 	width : 480,
 	height : 800,
 	elements : {
@@ -66,11 +69,11 @@ var my_window = new elm.main({
 			image : "data/images/rock_01.jpg",
 			resize : true,
 		},
-		bubble1 : new bubble(0),
-		bubble2 : new bubble(1),
-		bubble3 : new bubble(2),
 		shadow1 : new shadow(0),
 		shadow2 : new shadow(1),
 		shadow3 : new shadow(2),
+		bubble1 : new bubble(0),
+		bubble2 : new bubble(1),
+		bubble3 : new bubble(2),
 	},
 });
