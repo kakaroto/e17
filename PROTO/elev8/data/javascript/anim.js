ee03350611870d3407ebd9dde52730adb0e6ffa0
mike_m <mike_m@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-06-24 04:58:47 +0000

    elev8: Add shadows to animation demo
    Signed-off-by: Mike McCormack <mj.mccormack@samsung.com>
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@60647 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/data/javascript/anim.js
+++ PROTO/elev8/data/javascript/anim.js
@@ -5,25 +5,52 @@ var FILL_BOTH = { x : -1.0, y : -1.0 };
 
 var num_bubbles = 3;
 
+function animator(arg, n, is_shadow) {
+	t = elm.loop_time();
+	fac = 2.0/num_bubbles;
+	PI = 3.14159;
+	r = 48;
+	zz = (((2 + Math.sin(t*6 + (PI * (n * fac))))/3) * 64 ) * 2;
+	xx = (Math.cos(t * 4 + (PI * (n * fac))) * r) * 2;
+	yy = (Math.sin(t * 6 + (PI * (n * fac))) * r) * 2;
+
+	x = my_window.width / 2 + xx - (arg.width / 2);
+	y = my_window.height / 2 + yy - (arg.height / 2);
+	w = zz;
+	h = zz;
+
+	/* FIXME: get pointer position */
+	lx = 0;
+	ly = 0;
+	if (is_shadow) {
+		x -= ((lx - (x + w / 2)) / 4);
+		y -= ((ly - (y + h / 2)) / 4);
+	}
+
+	/* resize and move the object */
+	arg.x = x;
+	arg.y = y;
+	arg.width = w;
+	arg.height = h;
+}
+
 function bubble(n) {
 	this.type = "image";
 	this.image = "data/images/bubble.png";
 	this.width = 64;
 	this.height = 64;
 	this.on_animate = function (arg) {
-		t = elm.loop_time();
-		fac = 2.0/num_bubbles;
-		PI = 3.14159;
-		r = 48;
-		zz = (((2 + Math.sin(t*6 + (PI * (n * fac))))/3) * 64 ) * 2;
-		xx = (Math.cos(t * 4 + (PI * (n * fac))) * r) * 2;
-		yy = (Math.sin(t * 6 + (PI * (n * fac))) * r) * 2;
+		animator(arg, n, false);
+	}
+}
 
-		/* resize and move the object */
-		arg.x = my_window.width / 2 + xx - (arg.width / 2);
-		arg.y = my_window.height / 2 + yy - (arg.height / 2);
-		arg.width = zz;
-		arg.height = zz;
+function shadow(n) {
+	this.type = "image";
+	this.image = "data/images/bubble_sh.png";
+	this.width = 64;
+	this.height = 64;
+	this.on_animate = function (arg) {
+		animator(arg, n, true);
 	}
 }
 
@@ -42,5 +69,8 @@ var my_window = new elm.main({
 		bubble1 : new bubble(0),
 		bubble2 : new bubble(1),
 		bubble3 : new bubble(2),
+		shadow1 : new shadow(0),
+		shadow2 : new shadow(1),
+		shadow3 : new shadow(2),
 	},
 });
