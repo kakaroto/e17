de0bb956906c78bf8e6398a57c85a441d4c18530
mike_m <mike_m@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-06-24 04:58:36 +0000

    elev8: Create multiple bubbles in animation demo
    Signed-off-by: Mike McCormack <mj.mccormack@samsung.com>
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@60646 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/data/javascript/anim.js
+++ PROTO/elev8/data/javascript/anim.js
@@ -3,6 +3,30 @@
 var EXPAND_BOTH = { x : 1.0, y : 1.0 };
 var FILL_BOTH = { x : -1.0, y : -1.0 };
 
+var num_bubbles = 3;
+
+function bubble(n) {
+	this.type = "image";
+	this.image = "data/images/bubble.png";
+	this.width = 64;
+	this.height = 64;
+	this.on_animate = function (arg) {
+		t = elm.loop_time();
+		fac = 2.0/num_bubbles;
+		PI = 3.14159;
+		r = 48;
+		zz = (((2 + Math.sin(t*6 + (PI * (n * fac))))/3) * 64 ) * 2;
+		xx = (Math.cos(t * 4 + (PI * (n * fac))) * r) * 2;
+		yy = (Math.sin(t * 6 + (PI * (n * fac))) * r) * 2;
+
+		/* resize and move the object */
+		arg.x = my_window.width / 2 + xx - (arg.width / 2);
+		arg.y = my_window.height / 2 + yy - (arg.height / 2);
+		arg.width = zz;
+		arg.height = zz;
+	}
+}
+
 var my_window = new elm.main({
 	type : "main",
 	label : "Button demo",
@@ -15,20 +39,8 @@ var my_window = new elm.main({
 			image : "data/images/rock_01.jpg",
 			resize : true,
 		},
-		bubble1 : {
-			type : "image",
-			image : "data/images/bubble.png",
-			width : 50,
-			height : 50,
-			on_animate : function (arg) {
-				t = elm.loop_time();
-				radius_x = my_window.width/2 - 50;
-				radius_y = my_window.height/2 - 50;
-				PI = 3.14159;
-				angle = t*PI;
-				arg.x = Math.sin(angle) * radius_x + my_window.width/2 - arg.width/2;
-				arg.y = Math.cos(angle) * radius_y + my_window.height/2 - arg.height/2;
-			},
-		},
+		bubble1 : new bubble(0),
+		bubble2 : new bubble(1),
+		bubble3 : new bubble(2),
 	},
 });
