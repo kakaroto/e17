3f2649b6194ff74ceeaa2998129a045c8d60f9e1
seoz <seoz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-06-30 01:46:12 +0000

    PROTO/emap EMap.h: Fixed formatting.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@60851 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/emap/src/lib/EMap.h
+++ PROTO/emap/src/lib/EMap.h
@@ -20,37 +20,32 @@ enum emap_route_type
    EMAP_TRACK_TYPE_GPX
 };
 
+EAPI int               emap_init();
+EAPI int               emap_shutdown();
 
-EAPI 	int 			emap_init();
-EAPI 	int 			emap_shutdown();
+EAPI EMap_Route       *emap_route_new();
+EAPI void              emap_route_node_add(EMap_Route *route, EMap_Route_Node *node);
+EAPI void              emap_route_free(EMap_Route *route);
+EAPI void              emap_route_name_set(EMap_Route *route, const char *name);
+EAPI const char       *emap_route_name_get(EMap_Route *route);
+EAPI Eina_List        *emap_route_nodes_get(EMap_Route *route);
+EAPI double            emap_route_distance_get(EMap_Route *route);
+//EAPI double            emap_route_time_get(EMap_Route *route);
+//EAPI double            emap_route_average_speed_get(EMap_Route *route);
 
-EAPI    EMap_Route *            emap_route_new();
-EAPI    void                    emap_route_node_add(EMap_Route *route, EMap_Route_Node *node);
-EAPI    void                    emap_route_free(EMap_Route *route);
-EAPI    void                    emap_route_name_set(EMap_Route *route, const char *name);
-EAPI    const char *            emap_route_name_get(EMap_Route *route);
-EAPI 	Eina_List *		emap_route_nodes_get(EMap_Route *route);
-EAPI 	double 			emap_route_distance_get(EMap_Route *route);
-//EAPI 	double 			emap_route_time_get(EMap_Route *route);
-//EAPI 	double 			emap_route_average_speed_get(EMap_Route *route);
+EAPI EMap_Route_Node  *emap_route_node_new();
+EAPI void              emap_route_node_lon_set(EMap_Route_Node *node, double lon);
+EAPI void              emap_route_node_lat_set(EMap_Route_Node *node, double lat);
+EAPI double            emap_route_node_lon_get(EMap_Route_Node *node);
+EAPI double            emap_route_node_lat_get(EMap_Route_Node *node);
+EAPI void              emap_route_node_elevation_set(EMap_Route_Node *node, double elevation);
+EAPI double            emap_route_node_elevation_get(EMap_Route_Node *node);
+EAPI void              emap_route_node_time_set(EMap_Route_Node *node, time_t time);
+EAPI time_t            emap_route_node_time_get(EMap_Route_Node *node);
+EAPI double            emap_route_nodes_distance_get(EMap_Route_Node *A, EMap_Route_Node *B);
 
+EAPI void              emap_route_node_free(EMap_Route_Node *node);
 
-
-
-EAPI    EMap_Route_Node *       emap_route_node_new();
-EAPI    void                    emap_route_node_lon_set(EMap_Route_Node *node, double lon);
-EAPI    void                    emap_route_node_lat_set(EMap_Route_Node *node, double lat);
-EAPI 	double 			emap_route_node_lon_get(EMap_Route_Node *node);
-EAPI 	double 			emap_route_node_lat_get(EMap_Route_Node *node);
-EAPI 	void 			emap_route_node_elevation_set(EMap_Route_Node *node, double elevation);
-EAPI 	double 			emap_route_node_elevation_get(EMap_Route_Node *node);
-EAPI	void 			emap_route_node_time_set(EMap_Route_Node *node, time_t time);
-EAPI	time_t 			emap_route_node_time_get(EMap_Route_Node *node);
-EAPI 	double 			emap_route_nodes_distance_get(EMap_Route_Node *A, EMap_Route_Node *B);
-
-EAPI    void                    emap_route_node_free(EMap_Route_Node *node);
-
-EAPI    EMap_Route *            emap_route_gpx_new(const char *file);
-
+EAPI EMap_Route *      emap_route_gpx_new(const char *file);
 
 #endif /* EMAP_H_ */
