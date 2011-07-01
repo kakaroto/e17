ffeef8b6ee36ff55f15d312bbac6ffcb4b31e527
seoz <seoz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-07-01 01:21:56 +0000

    devs/seoz: Updated build script.
    Added eeze. stu! happy?
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@60903 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/seoz/build.sh
+++ devs/seoz/build.sh
@@ -8,14 +8,15 @@ export CC="ccache gcc"
 export MAKEFLAGS="make -j6"
 alias make='make -j6'
 
-export BUILD_ELM_DEPENDS="PROTO/emap eio"
-export BUILD_BASIC="eina eet evas ecore embryo edje e_dbus efreet expedite e ethumb "$BUILD_ELM_DEPENDS" elementary"
+export BUILD_E_DEPENDS="eeze"
+export BUILD_ELM_DEPENDS="PROTO/emap eio emotion"
+export BUILD_BASIC="eina eet evas ecore embryo edje e_dbus efreet expedite "$BUILD_E_DEPENDS" e ethumb "$BUILD_ELM_DEPENDS" elementary"
 export BUILD_PYTHON_BINDINGS="BINDINGS/python/python-evas BINDINGS/python/python-elementary BINDINGS/python/python-ecore BINDINGS/python/python-edje BINDINGS/python/python-emotion"
 export BUILD_C_BINDINGS="BINDINGS/cxx/eflxx BINDINGS/cxx/einaxx BINDINGS/cxx/evasxx BINDINGS/cxx/ecorexx BINDINGS/cxx/elementaryxx"
 export BUILD_BINDINGS=$BUILD_PYTHON_BINDINGS" "$BUILD_C_BINDINGS" "
 export BUILD_E_MODULES="E-MODULES-EXTRA/comp-scale E-MODULES-EXTRA/elfe E-MODULES-EXTRA/engage"
 export BUILD_EXAMPLE="EXAMPLES/elementary/calculator EXAMPLES/elementary/converter EXAMPLES/elementary/phonebook EXAMPLES/elementary/sticky-notes"
-export BUILD_ETC="editje emotion PROTO/eyelight FORMATTING/ecrustify ephoto edje_viewer PROTO/emap PROTO/azy elmdentica eio enlil" #enki
+export BUILD_ETC="editje PROTO/eyelight FORMATTING/ecrustify ephoto edje_viewer PROTO/emap PROTO/azy elmdentica eio enlil" #enki
 export BUILD=$BUILD_BASIC" "$BUILD_BINDINGS" "$BUILD_E_MODULES" "$BUILD_EXAMPLE" "$BUILD_ETC
 
 PWD=`pwd`
@@ -25,8 +26,9 @@ touch $LOG_WARN_FILE
 
 for I in $BUILD; do
   pushd $I
-	echo ""
+	echo " "
 	echo "============ "$I" ============"
+	echo "" >> $LOG_WARN_FILE
 	echo "["$I"]" >> $LOG_WARN_FILE
 	make clean distclean || true
 	./autogen.sh
