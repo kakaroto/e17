c7cf32d8f78ff8746528646a27f9a43d164a7438
mike_m <mike_m@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-07-11 01:22:09 +0000

    Update personal build script with Ubuntu/x86-64 notes
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@61219 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/mike_m/build.sh
+++ devs/mike_m/build.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
 
+# Ubuntu (11.04) setup
+#
+# apt-get install autotools-dev automake autopoint libtool zlib1g-dev \
+#		libjpeg62-dev libfreetype6-dev libx11-dev subversion git \
+#		libglib2.0-dev libxext-dev libxcursor-dev libudev-dev \
+#		libcurl4-gnutls-dev libc-ares-dev liblua5.1-0-dev libpng12-dev \
+#		libtiff4-dev
+#
+# Evas fails to build on x86-64 due to this bug.
+# https://bugs.launchpad.net/ubuntu/+source/libgcrypt11/+bug/751142
+# Hack around it as follows as root:
+# cd /lib/x86_64-linux-gnu/ ; ln -s /usr/lib/x86_64-linux-gnu/libgcrypt.la .
+#
+
+# some EFL libraries don't build with warnings by default...
 CFLAGS="-O2 -Wall -g"
-MAKEFLAGS="-j4"
+
+# how many CPUs?
+if [ -f /proc/cpuinfo ]
+then
+	cpus="`grep ^processor /proc/cpuinfo | wc -l`"
+fi
+[ "$cpus" ] || cpus=4
+MAKEFLAGS="-j$cpus"
 
 no_autogen=0
 
