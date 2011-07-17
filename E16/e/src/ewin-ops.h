d002677fa849521011ba4a29e9855186c2779355
kwo <kwo@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-07-17 18:43:21 +0000

    Add some forgotten 2011.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@61446 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E16/e/src/ewin-ops.h
+++ E16/e/src/ewin-ops.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2009 Kim Woelders
+ * Copyright (C) 2005-2011 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
--- E16/e/src/screen.c
+++ E16/e/src/screen.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2000-2007 Carsten Haitzler, Geoff Harrison and various contributors
- * Copyright (C) 2003-2010 Kim Woelders
+ * Copyright (C) 2003-2011 Kim Woelders
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
