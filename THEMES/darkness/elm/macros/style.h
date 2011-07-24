23cc27fe139fad47d948ffacd013707dfb0fef24
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-07-24 10:00:12 +0000

    add more wrap styles for entry
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@61645 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- THEMES/darkness/elm/macros/style.h
+++ THEMES/darkness/elm/macros/style.h
@@ -20,4 +20,70 @@ style { \
    tag: "subtitle" "+ "subtitle; \
    tag: "hilight" "+ "hilight; \
    tag: "description" "+ "description; \
+} \
+style { \
+   name: style_class"-mixed"; \
+   base: "font="fn" font_size="size" text_class="style_class" align=left wrap=mixed "normal_style; \
+   tag: "br" "\n"; \
+   tag: "tab" "\t"; \
+   tag: "left" "+ align=left"; \
+   tag: "/left" "-\n"; \
+   tag: "center" "+ align=center"; \
+   tag: "/center" "- \n"; \
+   tag: "right" "+ align=right"; \
+   tag: "/right" "- \n"; \
+   tag: "link" "+ color=#8888AA underline=on underline_color=#8888AA"; \
+   tag: "em" "+ font=DejaVu-Oblique"; \
+   tag: "b" "+ font="fb; \
+   tag: "i" "+ font="fi; \
+   tag: "bi" "+ font="fbi; \
+   tag: "mono" "+ font="fm; \
+   tag: "title" "+ "title; \
+   tag: "subtitle" "+ "subtitle; \
+   tag: "hilight" "+ "hilight; \
+   tag: "description" "+ "description; \
+} \
+style { \
+   name: style_class"-char"; \
+   base: "font="fn" font_size="size" text_class="style_class" align=left wrap=char "normal_style; \
+   tag: "br" "\n"; \
+   tag: "tab" "\t"; \
+   tag: "left" "+ align=left"; \
+   tag: "/left" "-\n"; \
+   tag: "center" "+ align=center"; \
+   tag: "/center" "- \n"; \
+   tag: "right" "+ align=right"; \
+   tag: "/right" "- \n"; \
+   tag: "link" "+ color=#8888AA underline=on underline_color=#8888AA"; \
+   tag: "em" "+ font=DejaVu-Oblique"; \
+   tag: "b" "+ font="fb; \
+   tag: "i" "+ font="fi; \
+   tag: "bi" "+ font="fbi; \
+   tag: "mono" "+ font="fm; \
+   tag: "title" "+ "title; \
+   tag: "subtitle" "+ "subtitle; \
+   tag: "hilight" "+ "hilight; \
+   tag: "description" "+ "description; \
+} \
+style { \
+   name: style_class"-nowrap"; \
+   base: "font="fn" font_size="size" text_class="style_class" align=left wrap=none "normal_style; \
+   tag: "br" "\n"; \
+   tag: "tab" "\t"; \
+   tag: "left" "+ align=left"; \
+   tag: "/left" "-\n"; \
+   tag: "center" "+ align=center"; \
+   tag: "/center" "- \n"; \
+   tag: "right" "+ align=right"; \
+   tag: "/right" "- \n"; \
+   tag: "link" "+ color=#8888AA underline=on underline_color=#8888AA"; \
+   tag: "em" "+ font=DejaVu-Oblique"; \
+   tag: "b" "+ font="fb; \
+   tag: "i" "+ font="fi; \
+   tag: "bi" "+ font="fbi; \
+   tag: "mono" "+ font="fm; \
+   tag: "title" "+ "title; \
+   tag: "subtitle" "+ "subtitle; \
+   tag: "hilight" "+ "hilight; \
+   tag: "description" "+ "description; \
 }
