cfcf14147ad66ee43d805cef5386e91b14074e34
cedric <cedric@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-08-05 09:30:08 +0000

    espionnage: add libface svn support.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@62129 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- espionnage/src/bin/espionnage_face.cpp
+++ espionnage/src/bin/espionnage_face.cpp
@@ -16,21 +16,37 @@ face_search(char *data, int width, int height, int stride)
 {
 #ifdef HAVE_FACE
    libface::LibFace* libFace = new libface::LibFace(libface::DETECT, ".");
+#ifdef HAVE_FACE2
+   std::vector<libface::Face*> *result;
+#else
    std::vector<libface::Face> result;
+#endif
    unsigned int i;
    Eina_List *back = NULL;
 
    result = libFace->detectFaces(data, width, height, stride, IPL_DEPTH_8U, 1, 0);
+#ifdef HAVE_FACE2
+   for (i = 0; i < result->size(); i++)
+#else
    for (i = 0; i < result.size(); i++)
+#endif
      {
+#ifdef HAVE_FACE2
+        libface::Face *face = result.at(i);
+#else
         libface::Face *face = &result.at(i);
+#endif
         Eina_Rectangle *r;
 
         r = eina_rectangle_new(face->getX1(), face->getY1(), face->getX2() - face->getX1(), face->getY2() - face->getY1());
         back = eina_list_append(back, r);
-
-        result.clear();
      }
+
+#ifdef HAVE_FACE2
+   result->clear();
+#else
+   result.clear();
+#endif
 
    return back;
 #else
