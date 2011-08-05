45c1a064d77c123fe4e80295093a38b25bb8c0b6
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-08-05 01:34:40 +0000

    From: Leif Middelschulte <leif.middelschulte@gmail.com> To: enlightenment-devel@lists.sourceforge.net Subject: [E-devel] espionnage fix for libface(>=0.2)/current svn Date: Fri, 5 Aug 2011 03:06:41 +0200
    Hello all, especially Cedric ;-)
    
    Find attached a patch for espionnage, that makes it work with the
    development version >0.2 of libface.
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@62119 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- espionnage/src/bin/espionnage_face.cpp
+++ espionnage/src/bin/espionnage_face.cpp
@@ -16,20 +16,20 @@ face_search(char *data, int width, int height, int stride)
 {
 #ifdef HAVE_FACE
    libface::LibFace* libFace = new libface::LibFace(libface::DETECT, ".");
-   std::vector<libface::Face> result;
+   std::vector<libface::Face*> *result;
    unsigned int i;
    Eina_List *back = NULL;
 
    result = libFace->detectFaces(data, width, height, stride, IPL_DEPTH_8U, 1, 0);
-   for (i = 0; i < result.size(); i++)
+   for (i = 0; i < result->size(); i++)
      {
-        libface::Face *face = &result.at(i);
+        libface::Face *face = result->at(i);
         Eina_Rectangle *r;
 
         r = eina_rectangle_new(face->getX1(), face->getY1(), face->getX2() - face->getX1(), face->getY2() - face->getY1());
         back = eina_list_append(back, r);
 
-        result.clear();
+        result->clear();
      }
 
    return back;
