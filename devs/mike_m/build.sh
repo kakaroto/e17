7becbb27ed65dadfb1f07a715d7fb6e674252763
mike_m <mike_m@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-08-17 01:12:52 +0000

    build script: build eweather first
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@62524 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/mike_m/build.sh
+++ devs/mike_m/build.sh
@@ -12,7 +12,7 @@ deplist="autotools-dev automake autopoint libtool zlib1g-dev
 	libxcb-shm0-dev libxcb-image0-dev libxss-dev libxp-dev
 	libxtst-dev graphviz libasound2-dev libpam0g-dev"
 
-defpkgs="eina eet evas ecore eeze embryo edje e_dbus efreet elementary e PROTO/libeweather"
+defpkgs="eina eet evas ecore eeze embryo edje e_dbus efreet PROTO/libeweather elementary e"
 #
 # Evas fails to build on x86-64 due to this bug.
 # https://bugs.launchpad.net/ubuntu/+source/libgcrypt11/+bug/751142
