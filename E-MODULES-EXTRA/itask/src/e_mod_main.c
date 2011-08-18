d972abb66a0aeb06e8817289b7adcbc08e611a8c
jeffdameth <jeffdameth@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-08-18 11:37:50 +0000

    e-modules/itask: allow it to be on any site
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@62565 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/itask/src/e_mod_main.c
+++ E-MODULES-EXTRA/itask/src/e_mod_main.c
@@ -55,7 +55,7 @@ static const E_Gadcon_Client_Class _gadcon_class =
   {
     GADCON_CLIENT_CLASS_VERSION,
     "itask",
-    {_gc_init, _gc_shutdown, _gc_orient, _gc_label, _gc_icon, _gc_id_new, NULL, e_gadcon_site_is_shelf},
+    {_gc_init, _gc_shutdown, _gc_orient, _gc_label, _gc_icon, _gc_id_new, NULL, NULL},
     E_GADCON_CLIENT_STYLE_PLAIN
   };
 
