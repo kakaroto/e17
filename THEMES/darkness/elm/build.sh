bf48887c0e10643e49afb8291ad7c6b90b7f1c7a
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-08-19 03:52:31 +0000

    add -p and -i options to elm build.sh
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@62574 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- THEMES/darkness/elm/build.sh
+++ THEMES/darkness/elm/build.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 edje_cc -id . -id ../elm_default/themes -id ../etk -fd ../fonts  darkness-desktop.edc darkness-elm-desktop.edj
 edje_cc -id . -id ../elm_default/themes -id ../etk -fd ../fonts  darkness.edc darkness-elm.edj
+case "$1" in
+    '-p')
+    edje_viewer darkness-elm.edj
+    ;;
+    '-i')
+    cp darkness-elm{,-desktop}.edj ~/.elementary/themes/
+    ;;
+esac
