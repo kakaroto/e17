71798b7d401ded012b3e6b544bb33218ccf50b41
rui <rui@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-09-08 23:20:14 +0000

    Now it compiles (but does nothing other than a tiny window, really).
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@63302 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/e_pattern_lock/src/e_pattern_lock.c
+++ PROTO/e_pattern_lock/src/e_pattern_lock.c
@@ -26,17 +26,56 @@
 #include "gettext.h"
 #define _(string) gettext (string)
 
-#include "Evas.h"
+#include "Elementary.h"
 
 #include "e_pattern_lock.h"
 
+void pat_mouse_down_1(void *data, Evas_Object *obj, const char *emission, const char *source) {
+	printf(_("Mouse down\n"));
+}
 
-int main(int argc, char **argv) {
-	Evas_Object *win=NULL;
+#define mouse_down(foo) edje_object_signal_callback_add(edje, "mouse,down,1", foo, pat_mouse_down_1, NULL)
+
+EAPI int elm_main(int argc, char **argv) {
+	Evas_Object *win=NULL, *bg=NULL, *pattern=NULL, *edje=NULL;
+	char theme[PATH_MAX];
 
 	setlocale(LC_ALL, "");
 	bindtextdomain(PACKAGE, LOCALEDIR);
 	textdomain(PACKAGE);
 
-	printf("Boo\n");
+	win = elm_win_add(NULL, "e_pattern_lock", ELM_WIN_BASIC);
+		elm_win_autodel_set(win, EINA_TRUE);
+		evas_object_size_hint_min_set(win, 480, 480);
+		evas_object_size_hint_max_set(win, 640, 640);
+
+	bg = elm_bg_add(win);
+		evas_object_size_hint_weight_set(bg, 1.0, 1.0);
+		elm_win_resize_object_add(win, bg);
+	evas_object_show(bg);
+
+	snprintf(theme, sizeof(theme), "%s/themes/default.edj", PKGDATADIR);
+    elm_theme_extension_add(NULL, theme);
+
+	pattern = elm_layout_add(win);
+		elm_layout_file_set(pattern, theme, "e_lock/table/default");
+		evas_object_size_hint_weight_set(pattern, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+		elm_win_resize_object_add(win, pattern);
+	evas_object_show(pattern);
+
+	edje = elm_layout_edje_get(pattern);
+	mouse_down("top_left"); mouse_down("top"); mouse_down("top_right");
+	mouse_down("left"); mouse_down("center"); mouse_down("right");
+	mouse_down("bottom_left"); mouse_down("bottom"); mouse_down("bottom_right");
+
+
+	evas_object_show(win);
+
+    elm_run();
+
+    elm_shutdown();
+
+	return(0);
 }
+
+ELM_MAIN()
