c9916179c61670233320fb60b4680afa172602eb
rui <rui@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-09-11 23:07:13 +0000

    It seems to be sort of moving...
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@63330 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/e_pattern_lock/src/e_pattern_lock.c
+++ PROTO/e_pattern_lock/src/e_pattern_lock.c
@@ -22,6 +22,8 @@
 # include "config.h"
 #endif
 
+#include <stdlib.h>
+
 #include <locale.h>
 #include "gettext.h"
 #define _(string) gettext (string)
@@ -30,14 +32,46 @@
 
 #include "e_pattern_lock.h"
 
-void pat_mouse_down_1(void *data, Evas_Object *obj, const char *emission, const char *source) {
-	printf(_("Mouse down\n"));
+
+void on_mouse_down(void *data, Evas *e, Evas_Object *obj, void *event_info) {
+	Evas_Object *object = (Evas_Object*)data;
+	int col, row;
+
+	col = atoi(edje_object_data_get(object, "col"));
+	row = atoi(edje_object_data_get(object, "row"));
+
+	printf(_("Mouse down on %dx%d\n"), col, row);
+	edje_object_signal_emit(obj, "go_selected", NULL);
 }
 
-#define mouse_down(foo) edje_object_signal_callback_add(edje, "mouse,down,1", foo, pat_mouse_down_1, NULL)
+void on_mouse_in(void *data, Evas *e, Evas_Object *obj, void *event_info) {
+	Evas_Object *object = (Evas_Object*)data;
+	int col, row;
+
+	col = atoi(edje_object_data_get(object, "col"));
+	row = atoi(edje_object_data_get(object, "row"));
+
+	printf(_("Mouse in on %dx%d\n"), col, row);
+}
+
+void on_mouse_up(void *data, Evas *e, Evas_Object *obj, void *event_info) {
+	Evas_Object *object = (Evas_Object*)data;
+	int col, row;
+
+	col = atoi(edje_object_data_get(object, "col"));
+	row = atoi(edje_object_data_get(object, "row"));
+	printf(_("Mouse up on %dx%d\n"), col, row);
+}
+
+#define set_event_cbs(col, row, part) \
+		item = edje_object_part_table_child_get(edje, "table", col, row); \
+		object = (Evas_Object*)edje_object_part_object_get(item, part); \
+		evas_object_event_callback_add(object, EVAS_CALLBACK_MOUSE_IN, on_mouse_in, item); \
+		evas_object_event_callback_add(object, EVAS_CALLBACK_MOUSE_DOWN, on_mouse_down, item); \
+		evas_object_event_callback_add(object, EVAS_CALLBACK_MOUSE_UP, on_mouse_up, item);
 
 EAPI int elm_main(int argc, char **argv) {
-	Evas_Object *win=NULL, *bg=NULL, *pattern=NULL, *edje=NULL;
+	Evas_Object *win=NULL, *bg=NULL, *pattern=NULL, *edje=NULL, *object=NULL, *item=NULL;
 	char theme[PATH_MAX];
 
 	setlocale(LC_ALL, "");
@@ -45,29 +79,34 @@ EAPI int elm_main(int argc, char **argv) {
 	textdomain(PACKAGE);
 
 	win = elm_win_add(NULL, "e_pattern_lock", ELM_WIN_BASIC);
-		elm_win_autodel_set(win, EINA_TRUE);
-		evas_object_size_hint_min_set(win, 480, 480);
-		evas_object_size_hint_max_set(win, 640, 640);
+	elm_win_autodel_set(win, EINA_TRUE);
+	elm_win_maximized_set(win, EINA_TRUE); // change to full screen and above everything for lockscreen effect
+	evas_object_size_hint_min_set(win, 480, 480);
+	evas_object_size_hint_max_set(win, 640, 640);
 
 	bg = elm_bg_add(win);
-		evas_object_size_hint_weight_set(bg, 1.0, 1.0);
-		elm_win_resize_object_add(win, bg);
+	evas_object_size_hint_weight_set(bg, 1.0, 1.0);
+	elm_win_resize_object_add(win, bg);
 	evas_object_show(bg);
 
 	snprintf(theme, sizeof(theme), "%s/themes/default.edj", PKGDATADIR);
     elm_theme_extension_add(NULL, theme);
+    elm_theme_overlay_add(NULL, theme);
 
 	pattern = elm_layout_add(win);
+
 		elm_layout_file_set(pattern, theme, "e_lock/table/default");
 		evas_object_size_hint_weight_set(pattern, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+		evas_object_size_hint_align_set(pattern, EVAS_HINT_FILL, EVAS_HINT_FILL);
 		elm_win_resize_object_add(win, pattern);
+
+		edje = elm_layout_edje_get(pattern);
+
+		set_event_cbs(0, 0, "top_left");    set_event_cbs(1, 0, "top");    set_event_cbs(2, 0, "top_right");
+		set_event_cbs(0, 1, "left");        set_event_cbs(1, 1, "center"); set_event_cbs(2, 1, "right");
+		set_event_cbs(0, 2, "bottom_left"); set_event_cbs(1, 2, "bottom"); set_event_cbs(2, 2, "bottom_right");
+
 	evas_object_show(pattern);
-
-	edje = elm_layout_edje_get(pattern);
-	mouse_down("top_left"); mouse_down("top"); mouse_down("top_right");
-	mouse_down("left"); mouse_down("center"); mouse_down("right");
-	mouse_down("bottom_left"); mouse_down("bottom"); mouse_down("bottom_right");
-
 
 	evas_object_show(win);
 
