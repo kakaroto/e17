9c0d767718a6d3ad27017c008d1c0ffbb53e8257
seoz <seoz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-09-27 15:08:26 +0000

    devs/seoz: Updated build script. Moved eio.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@63626 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/seoz/build.sh
+++ devs/seoz/build.sh
@@ -13,15 +13,15 @@ export MAKEFLAGS="make -j6"
 alias make='make -j6'
 
 export BUILD_E_DEPENDS="eeze"
-export BUILD_ETHUMB_DEPENDS="PROTO/epdf emotion"
-export BUILD_ELM_DEPENDS="PROTO/emap eio"
+export BUILD_ETHUMB_DEPENDS="PROTO/epdf eio emotion"
+export BUILD_ELM_DEPENDS="PROTO/emap"
 export BUILD_BASIC="eina eet evas ecore embryo edje e_dbus efreet expedite "$BUILD_E_DEPENDS" e "$BUILD_ETHUMB_DEPENDS" ethumb "$BUILD_ELM_DEPENDS" elementary"
 export BUILD_PYTHON_BINDINGS="BINDINGS/python/python-evas BINDINGS/python/python-elementary BINDINGS/python/python-ecore BINDINGS/python/python-edje BINDINGS/python/python-emotion"
 export BUILD_C_BINDINGS="BINDINGS/cxx/eflxx BINDINGS/cxx/einaxx BINDINGS/cxx/evasxx BINDINGS/cxx/ecorexx BINDINGS/cxx/elementaryxx"
 export BUILD_BINDINGS=$BUILD_PYTHON_BINDINGS" "$BUILD_C_BINDINGS" "
 export BUILD_E_MODULES="E-MODULES-EXTRA/comp-scale E-MODULES-EXTRA/elfe E-MODULES-EXTRA/engage E-MODULES-EXTRA/everything-shotgun"
 export BUILD_EXAMPLE="EXAMPLES/elementary/calculator EXAMPLES/elementary/converter EXAMPLES/elementary/phonebook EXAMPLES/elementary/sticky-notes"
-export BUILD_ETC="editje PROTO/eyelight FORMATTING/ecrustify ephoto edje_viewer PROTO/emap PROTO/azy elmdentica eio enlil PROTO/emote emprint clouseau PROTO/enna-explorer" #enki excessive
+export BUILD_ETC="editje PROTO/eyelight FORMATTING/ecrustify ephoto edje_viewer PROTO/emap PROTO/azy elmdentica enlil PROTO/emote emprint clouseau PROTO/enna-explorer" #enki excessive
 export BUILD=$BUILD_BASIC" "$BUILD_BINDINGS" "$BUILD_E_MODULES" "$BUILD_EXAMPLE" "$BUILD_ETC
 
 PWD=`pwd`
