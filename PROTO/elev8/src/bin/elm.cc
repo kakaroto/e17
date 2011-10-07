26c75b5f5c0bd8af084ae16445eecccb661f5265
sanjeev <sanjeev@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-10-07 01:14:31 +0000

    Delete OLD_APIS in upstream.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@63897 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/bin/elm.cc
+++ PROTO/elev8/src/bin/elm.cc
@@ -1580,15 +1580,9 @@ public:
                     str[i] = v[i]->ToString();
                }
              String::Utf8Value left(str[0]), middle(str[1]), right(str[2]);
-#ifndef OLD_APIS
-         elm_object_text_part_set(eo, name[0], *left);
-         elm_object_text_part_set(eo,name[1], *middle);
-         elm_object_text_part_set(eo,name[2], *right);
-#else
-             elm_actionslider_label_set(eo, ELM_ACTIONSLIDER_LABEL_LEFT, *left);
-             elm_actionslider_label_set(eo, ELM_ACTIONSLIDER_LABEL_CENTER, *middle);
-             elm_actionslider_label_set(eo, ELM_ACTIONSLIDER_LABEL_RIGHT, *right);
-#endif
+             elm_object_text_part_set(eo, name[0], *left);
+             elm_object_text_part_set(eo,name[1], *middle);
+             elm_object_text_part_set(eo,name[2], *right);
           }
      }
 
@@ -1598,7 +1592,6 @@ public:
         return Undefined();
      }
 
-#ifndef OLD_APIS
    bool position_from_string(Handle<Value> val, Elm_Actionslider_Pos &pos)
      {
         if (!val->IsString())
@@ -1618,55 +1611,10 @@ public:
           }
         return true;
      }
-#else
-   bool position_from_string(Handle<Value> val, Elm_Actionslider_Magnet_Pos &pos)
-     {
-        if (!val->IsString())
-          return false;
-
-        String::Utf8Value str(val);
-        if (!strcmp(*str, "left"))
-          pos = ELM_ACTIONSLIDER_MAGNET_LEFT;
-        else if (!strcmp(*str, "center"))
-          pos = ELM_ACTIONSLIDER_MAGNET_CENTER;
-        else if (!strcmp(*str, "right"))
-          pos = ELM_ACTIONSLIDER_MAGNET_RIGHT;
-        else
-          {
-             fprintf(stderr, "Invalid actionslider magnet position: %s\n", *str);
-             return false;
-          }
-        return true;
-     }
-
-   bool position_from_string(Handle<Value> val, Elm_Actionslider_Indicator_Pos &pos)
-     {
-        if (!val->IsString())
-          return false;
-
-        String::Utf8Value str(val);
-        if (!strcmp(*str, "left"))
-          pos = ELM_ACTIONSLIDER_INDICATOR_LEFT;
-        else if (!strcmp(*str, "center"))
-          pos = ELM_ACTIONSLIDER_INDICATOR_CENTER;
-        else if (!strcmp(*str, "right"))
-          pos = ELM_ACTIONSLIDER_INDICATOR_RIGHT;
-        else
-          {
-             fprintf(stderr, "Invalid actionslider indicator position: %s\n", *str);
-             return false;
-          }
-        return true;
-     }
-#endif
 
    virtual void slider_set(Handle<Value> val)
      {
-#ifndef OLD_APIS
         Elm_Actionslider_Pos pos = ELM_ACTIONSLIDER_NONE;
-#else
-        Elm_Actionslider_Indicator_Pos pos = ELM_ACTIONSLIDER_INDICATOR_NONE;
-#endif
         if (position_from_string(val, pos))
           elm_actionslider_indicator_pos_set(eo, pos);
      }
@@ -1678,11 +1626,7 @@ public:
 
    virtual void magnet_set(Handle<Value> val)
      {
-#ifndef OLD_APIS
         Elm_Actionslider_Pos pos = ELM_ACTIONSLIDER_NONE;
-#else
-        Elm_Actionslider_Magnet_Pos pos = ELM_ACTIONSLIDER_MAGNET_NONE;
-#endif
 
         if (position_from_string(val, pos))
           elm_actionslider_magnet_pos_set(eo, pos);
