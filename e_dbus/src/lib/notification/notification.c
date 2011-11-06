4375ba3df6d4c2e6afb34264e6e51de4dde8e283
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-11-06 16:57:30 +0000

    make this an error condition
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@64835 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_dbus/src/lib/notification/notification.c
+++ e_dbus/src/lib/notification/notification.c
@@ -382,6 +382,12 @@ e_notification_image_init(E_Notification_Image *img, Evas_Object *obj)
    img->data = evas_object_image_data_get(obj, EINA_FALSE);
    if (!img->data) return EINA_FALSE;
    evas_object_image_size_get(obj, &img->width, &img->height);
+   if ((!img->width) || (!img->height))
+     {
+        img->data = NULL;
+        img->width = img->height = 0;
+        return EINA_FALSE;
+     }
    img->has_alpha = !!evas_object_image_alpha_get(obj);
    img->channels = img->has_alpha ? 4 : 3;
    img->rowstride = evas_object_image_stride_get(obj);
