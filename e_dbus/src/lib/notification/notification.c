107556310ed300cc794dff1fe8b6aac9fa162b29
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-11-06 16:56:41 +0000

    load_size doesn't work so great here
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@64834 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_dbus/src/lib/notification/notification.c
+++ e_dbus/src/lib/notification/notification.c
@@ -381,7 +381,7 @@ e_notification_image_init(E_Notification_Image *img, Evas_Object *obj)
    EINA_SAFETY_ON_NULL_RETURN_VAL(obj, EINA_FALSE);
    img->data = evas_object_image_data_get(obj, EINA_FALSE);
    if (!img->data) return EINA_FALSE;
-   evas_object_image_load_size_get(obj, &img->width, &img->height);
+   evas_object_image_size_get(obj, &img->width, &img->height);
    img->has_alpha = !!evas_object_image_alpha_get(obj);
    img->channels = img->has_alpha ? 4 : 3;
    img->rowstride = evas_object_image_stride_get(obj);
