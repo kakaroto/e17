2cf39a8c7bf92407b7547a243bd5ae7610e159c7
kakaroto <kakaroto@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-11-11 21:46:26 +0000

    create_tarballs: update script, use sh syntax
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@65080 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/kakaroto/create_tarballs.sh
+++ devs/kakaroto/create_tarballs.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 function autogen_lib {
     what=$1
-    echo -ne "Autogen $what : "
+    echo -ne "Autogen-ing $what : "
     pwd=$(pwd)
     cd $what && \
-        NOCONFIGURE=1 ./autogen.sh >& ../logs/autogen_${what}.log  || \
+        NOCONFIGURE=1 ./autogen.sh > ../logs/autogen_${what}.log 2>&1  || \
         (echo "Error!" && \
           cd $pwd && \
           (tail logs/autogen_${what}.log || true) && \
           echo -ne "\n\nSee logs/autogen_${what}.log for details.\n" && \
           exit 1)
-    echo "Done"
+    echo "Done!"
     cd $pwd
 }
 
@@ -22,7 +22,7 @@ function download {
         rm -rf $what
     fi
     echo -ne "Downloading $what : "
-    svn export http://svn.enlightenment.org/svn/e/trunk/$what >& logs/svn_${what}.log || \
+    svn export http://svn.enlightenment.org/svn/e/trunk/$what > logs/svn_${what}.log  2>&1 || \
         (echo "Error!" && \
           (tail logs/svn_${what}.log  || true ) && \
           echo -ne "\n\n See logs/svn_${what}.log for details.\n" && \
@@ -32,8 +32,8 @@ function download {
 
 function tarball {
     what=$1
-    echo -ne "Tarballing $what : "
-    tar -czvf ${what}_svn${EFL_VERSION}.tar.gz ${what} >& logs/tarball_${what}.log || \
+    echo -ne "Tarball-ing $what : "
+    tar -czvf ${what}_svn${EFL_VERSION}.tar.gz ${what} > logs/tarball_${what}.log 2>&1 || \
         (echo "Error!" && \
           (tail logs/tarball_${what}.log  || true ) && \
           echo -ne "\n\n See logs/tarball_${what}.log for details.\n" && \
@@ -41,7 +41,7 @@ function tarball {
     echo "Done!"
 }
 
-mkdir logs >& /dev/null || true
+mkdir logs > /dev/null 2>&1 || true
 
 EFL_LIBS=""
 if test -z $1 ; then
@@ -54,14 +54,14 @@ else
 fi
 
 echo -ne "Getting SVN revision : "
-EFL_VERSION=$(svn info http://svn.enlightenment.org/svn/e/trunk/ | grep Revision | awk '{print $2}')
+EFL_VERSION=$(LC_ALL=C svn info http://svn.enlightenment.org/svn/e/trunk/ | grep Revision | awk '{print $2}')
 echo "${EFL_VERSION}"
 
 # Download, autogen and tarball
 for lib in $EFL_LIBS ; do
     download $lib
-    autogen_lib $what
-    tarball $what
-    rm -rf $what
+    autogen_lib $lib
+    tarball $lib
+    rm -rf $lib
 done
 
