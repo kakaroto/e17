10aaf34e9a21d66d53232e0bbfbae4bceaa67c3b
raster <raster@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-11-14 08:30:59 +0000

    discomfitor - how is this now for a formatting script?
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@65155 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- FORMATTING/formatefl.sh
+++ FORMATTING/formatefl.sh
@@ -47,26 +47,55 @@ function funcnewlines ()
   while [ -n "$X" ]; do
     shift
     echo "Fixing function newlines in $X..."
-    sed -r 's/^([a-zA-Z].*\*?) ([a-z][a-zA-Z0-9_]*\()/\1\n\2/' < $X > $T
-    cp $T $X
-    rm $T
+#    sed -r 's/^([a-zA-Z].*\*?) ([a-z][a-zA-Z0-9_]*\()/\1\n\2/' < $X > $T
+#    cp $T $X
+#    sed -r 's/{$/\n{/' < $X > $T
+#    cp $T $X
+#    rm $T
     X="$1"
   done
 }
-FMT1="funcnewlines"
-FMT2="$UNC -c $HOME/.ecrustify.cfg --no-backup --replace -l C"
-FMT3="$UNC -c $HOME/.ecrustify.cfg --no-backup --replace -l CPP"
-FMTH="$UNC -c $HOME/.ecrustify-headers.cfg --no-backup --replace -l C"
 
-F=$(find $1 -name '*.c' -print)
-if [[ -n "$F" ]]; then $FMT1 $F; $FMT2 $F; fi
-F=$(find $1 -name '*.cpp' -print)
-if [[ -n "$F" ]]; then $FMT1 $F; $FMT3 $F; fi
+INDENTOPS="-gnu -l9999 -lc9999 -ncs -npcs -nprs"
 
-F=$(find $1 -name '*.[hx]' -print)
-if [[ -n "$F" ]]; then $FMT1 $F; $FMTH $F; fi
-F=$(find $1 -name '*.h.in' -print)
-if [[ -n "$F" ]]; then $FMT1 $F; $FMTH $F; fi
+function fmtc ()
+{
+  if [[ -z "$@" ]]; then return; fi
+  indent $INDENTOPS $@
+  funcnewlines $@
+  $UNC -c $HOME/.ecrustify.cfg --no-backup --replace -l C $@
+}
+
+function fmtcpp ()
+{
+  if [[ -z "$@" ]]; then return; fi
+  indent $INDENTOPS $@
+  funcnewlines $@
+  $UNC -c $HOME/.ecrustify.cfg --no-backup --replace -l CPP $@
+}
+
+function fmth ()
+{
+  if [[ -z "$@" ]]; then return; fi
+  indent $INDENTOPS $@
+  funcnewlines $@
+  $UNC -c $HOME/.ecrustify-headers.cfg --no-backup --replace -l C $@
+}
+
+function fmthpp ()
+{
+  if [[ -z "$@" ]]; then return; fi
+  indent $INDENTOPS $@
+  funcnewlines $@
+  $UNC -c $HOME/.ecrustify-headers.cfg --no-backup --replace -l CPP $@
+}
+
+fmtc `find $1 -name '*.c' -print`
+fmtcpp `find $1 -name '*.cpp' -print`
+fmth `find $1 -name '*.h' -print`
+fmthpp `find $1 -name '*.hpp' -print`
+fmth `find $1 -name '*.h.in' -print`
+fmthpp `find $1 -name '*.hpp.in' -print`
 
   echo "==================================================================="
   echo " DONE"
