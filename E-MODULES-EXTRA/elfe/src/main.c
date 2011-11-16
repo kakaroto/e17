707634b5f56a7aae9bec1a3ee34f9485895d79cc
captainigloo <captainigloo@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-11-16 22:31:43 +0000

    E-MODULES-EXTRA/elfe : Fix segfault due to bad argc/argv passed to elm_init
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@65314 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/elfe/src/main.c
+++ E-MODULES-EXTRA/elfe/src/main.c
@@ -138,7 +138,8 @@ _elfe_home_win_new(E_Zone *zone)
    char buf[PATH_MAX];
    const char *bgfile;
    const char *file;
-   const char *args[] = {"elfe"};
+   char **argv;
+   int argc;
    Evas_Object *rect;
    Evas_Coord w, h;
 
@@ -171,7 +172,8 @@ _elfe_home_win_new(E_Zone *zone)
 
 
    /* Specific ELM initialisation */
-   elm_init(1, args);
+   ecore_app_args_get(&argc, &argv);
+   elm_init(argc, argv);
    elm_need_efreet();
 
    hwin->layout = elm_layout_add(rect);
