1971b30af8b345d4d7b67c0ea7e5dfa67579d315
seoz <seoz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-12-01 05:38:35 +0000

    BINDINGS/cxx elementaryxx: Applied recent gen/gengrid/genlist api changes.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@65748 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/cxx/elementaryxx/src/Gen.cpp
+++ BINDINGS/cxx/elementaryxx/src/Gen.cpp
@@ -19,38 +19,38 @@ Gen::~Gen () {}
   
 void Gen::clear ()
 {
-  elm_gen_clear (o);
+  elm_genlist_clear (o);
 }
 
 void Gen::setAlwaysSelectMode (bool alwaysSelect)
 {
-  elm_gen_always_select_mode_set (o, alwaysSelect);
+  elm_genlist_always_select_mode_set (o, alwaysSelect);
 }
 
 bool Gen::getAlwaysSelectMode ()
 {
-  return elm_gen_always_select_mode_get (o);
+  return elm_genlist_always_select_mode_get (o);
 }
 
 void Gen::setNoSelectMode (bool noSelect)
 {
-  elm_gen_no_select_mode_set (o, noSelect);
+  elm_genlist_no_select_mode_set (o, noSelect);
 }
 
 bool Gen::getNoSelectMode ()
 {
-  return elm_gen_no_select_mode_get (o);
+  return elm_genlist_no_select_mode_get (o);
 }
 
 void Gen::setBounce (bool hBounce, bool vBounce)
 {
-  elm_gen_bounce_set (o, hBounce, vBounce);
+  elm_genlist_bounce_set (o, hBounce, vBounce);
 }
 
 void Gen::getBounce (bool &hBounceOut, bool &vBounceOut)
 {
   Eina_Bool h, v;
-  elm_gen_bounce_get (o, &h, &v);
+  elm_genlist_bounce_get (o, &h, &v);
   hBounceOut = h;
   vBounceOut = v;
 }
