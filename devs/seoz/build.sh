6315bf553fdb9f688701ee186599db5394fbdcb4
seoz <seoz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-12-09 04:42:54 +0000

    devs seoz: Updated build script.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@66055 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/seoz/build.sh
+++ devs/seoz/build.sh
@@ -4,7 +4,7 @@
 # build-essential automake libtool ccache zlib1g-dev libjpeg62-dev libfreetype6-dev libdbus-1-dev liblua5.1-0-dev g++ libxext-dev libxrender-dev libpng12-dev libxrandr-dev libfontconfig1-dev autopoint libxcomposite-dev libxcursor-dev libxdamage-dev libxdmcp-dev libxfixes-dev libxfont-dev  libxi-dev libxinerama-dev libxss-dev libxv-dev libtiff4-dev librsvg2-dev libfribidi-dev libcurl4-openssl-dev libexif-dev libiptcdata0-dev libxml2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libudev-dev libxcb-shape0-dev libsigc++-2.0-dev byacc libsqlite3-dev python-dev
 
 set -e
-#set -x
+set -x
 
 export PREFIX=/usr/local
 export CFLAGS="-O2 -march=native -ffast-math -g3 -W -Wall -Wextra" # -Wshadow"
@@ -22,27 +22,38 @@ export BUILD_BINDINGS=$BUILD_PYTHON_BINDINGS" "$BUILD_C_BINDINGS" "
 export BUILD_E_MODULES="E-MODULES-EXTRA/comp-scale E-MODULES-EXTRA/elfe E-MODULES-EXTRA/engage E-MODULES-EXTRA/everything-shotgun"
 export BUILD_EXAMPLE="EXAMPLES/elementary/calculator EXAMPLES/elementary/converter EXAMPLES/elementary/phonebook EXAMPLES/elementary/sticky-notes"
 export BUILD_ETC="enjoy editje PROTO/eyelight FORMATTING/ecrustify ephoto edje_viewer PROTO/emap PROTO/azy elmdentica enlil PROTO/emote emprint clouseau PROTO/enna-explorer enki excessive envision ensure"
-export BUILD=$BUILD_BASIC" "$BUILD_BINDINGS" "$BUILD_E_MODULES" "$BUILD_EXAMPLE" "$BUILD_ETC
 
 PWD=`pwd`
 LOG_WARN_FILE=$PWD"/warnings.txt"
+
 rm $LOG_WARN_FILE -f
 touch $LOG_WARN_FILE
 
-for I in $BUILD; do
-  pushd $I
-	echo " "
-	echo "============ "$I" ============"
-	echo "" >> $LOG_WARN_FILE
-	echo "["$I"]" >> $LOG_WARN_FILE
-	make clean distclean || true
-	./autogen.sh
-	make 2>> $LOG_WARN_FILE 
-	sudo make install
-	sudo ldconfig
-	#ctags -h ".h.x.cpp.c" -R
-  popd
-done
+function build()
+{
+	build_dir=$1
+	autogen_option="$2"
+	for I in $build_dir; do
+  	pushd $I
+		echo " "
+		echo "============ "$I" ============"
+		echo "" >> $LOG_WARN_FILE
+		echo "["$I"]" >> $LOG_WARN_FILE
+		make clean distclean || true
+		./autogen.sh "$autogen_option"
+		make 2>> $LOG_WARN_FILE 
+		sudo make install
+		sudo ldconfig
+
+		#ctags -h ".h.x.cpp.c" -R
+  	popd
+	done
+}
+
+build "$BUILD_BASIC"
+build "$BUILD_PYTHON_BINDINGS" "--prefix=/usr/local"
+build "$BUILD_C_BINDINGS"
+build "$BUILD_E_MODULES $BUILD_EXAMPLE $BUILD_ETC"
 
 #efenniht theme
 echo ""
