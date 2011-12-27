57da4d7cbb9ba5f3b62e42f00ccf8f365d9dc2db
andreas <andreas@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2011-12-27 17:53:29 +0000

    ups, forgot to commit a file - nobody noticed :-( - good choose to build now daily on launchpad...
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@66576 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ BINDINGS/cxx/elementaryxx/include/elementaryxx/GenListItem.h
@@ -0,0 +1,63 @@
+#ifndef ELMXX_GEN_LIST_ITEM_H
+#define ELMXX_GEN_LIST_ITEM_H
+
+/* EFL */
+#include <Elementary.h>
+
+/* EFLxx */
+#include <evasxx/Evasxx.h>
+#include "GenListColumnConstructor.h"
+#include "GenItem.h"
+
+namespace Elmxx {
+
+/* forward declarations */
+class Gen;
+class GenDataModel;
+
+class GenListItem : public GenItem
+{
+public:
+  friend class Gen;
+  friend class GenList;
+
+  virtual ~GenListItem ();
+
+  void clearSubItems ();
+  void setSelected (bool selected);
+  bool getSelected () const;
+  void setExpanded (bool expanded);
+  bool getExpanded () const;
+  void setDisabled (bool disabled);
+  bool getDisabled () const;
+  void setDisplayOnly (bool displayOnly);
+  bool getDisplayOnly () const;
+  void show ();
+  void bringIn ();
+  void showTop ();
+  void bringInTop ();
+  void showMiddle ();
+  void bringInMiddle ();
+  void update ();
+
+  const Evasxx::Object *getEvasObject ();
+
+  static GenListItem *wrap (Elm_Genlist_Item &item, GenDataModel &model);
+
+  static GenListItem *objectLink (const Elm_Genlist_Item *item);
+  
+  
+private:
+  GenListItem (Elm_Genlist_Item *item);
+
+  const void *getData ();
+  void setData (const void *data);
+  
+  Elm_Gen_Item *mItem;
+  GenDataModel *mDataModel;
+};
+
+} // end namespace Elmxx
+
+#endif // ELMXX_GEN_LIST_ITEM_H
+
