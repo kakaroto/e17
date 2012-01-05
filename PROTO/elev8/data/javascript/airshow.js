61665bb97287ab701f66f91dfa22865fffcde076
sanjeev <sanjeev@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-01-05 00:48:09 +0000

    This airshow is beginning to look good.
    Signed-off-by: Sanjeev BA <iamsanjeev@gmail.com>
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@66880 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/data/javascript/airshow.js
+++ PROTO/elev8/data/javascript/airshow.js
@@ -8,6 +8,60 @@ var FILL_X = { x : -1.0, y : 0.0 };
 var FILL_Y = { x : 0.0, y : -1.0 };
 
 var myplane=null;
+var num_planes = 4;
+
+function move_up()
+{
+	myplane.y-=10;
+	if(myplane.y<10)
+		myplane.y=500;
+}
+function move_down()
+{
+	myplane.y+=10;
+	if(myplane.y>590)
+		myplane.y=10;
+}
+function move_right()
+{
+	myplane.x+=10;
+	if(myplane.x>790)
+		myplane.x=10;
+}
+function move_left()
+{
+	myplane.x-=10;
+	if(myplane.x<10)
+		myplane.x=790;
+}
+
+function animator(arg, n, is_shadow) {
+	t = elm.loop_time();
+	fac = 3.0/num_planes;
+	r = 120;
+	zz = (((2 + Math.sin(t*6 + (Math.PI * (n * fac))))/3) * 64 ) * 2;
+	xx = (Math.cos(t * 4 + (Math.PI * (n * fac))) * r) * 2;
+	yy = (Math.sin(t * 6 + (Math.PI * (n * fac))) * r) * 2;
+
+	x = my_win.width / 2 + xx - (arg.width / 2);
+	y = my_win.height / 2 + yy - (arg.height / 2);
+	w = zz;
+	h = zz;
+
+	/* get pointer position */
+	lx = arg.pointer.x
+	ly = arg.pointer.y;
+	if (is_shadow) {
+		x -= ((lx - (x + w / 2)) / 4);
+		y -= ((ly - (y + h / 2)) / 4);
+	}
+
+	/* resize and move the object */
+	arg.x = x;
+	arg.y = y;
+	arg.width = w;
+	arg.height = h;
+}
 
 function init()
 {
@@ -19,34 +73,8 @@ function init()
 	my_win.elements.plane3.y = 300;
 	my_win.elements.plane4.x = 1;
 	my_win.elements.plane4.y = 450;
+	myplane.on_animate = null;
 }
-
-function move_up()
-{
-
-	myplane.y-=5;
-	if(myplane.y<10)
-		myplane.y=500;
-}
-function move_down()
-{
-	myplane.y+=5;
-	if(myplane.y>590)
-		myplane.y=10;
-}
-function move_right()
-{
-	myplane.x+=5;
-	if(myplane.x>790)
-		myplane.x=10;
-}
-function move_left()
-{
-	myplane.x-=5;
-	if(myplane.x<10)
-		myplane.x=790;
-}
-
 
 var my_win = new elm.window({
 		type : "main",
@@ -78,6 +106,9 @@ var my_win = new elm.window({
 					    init();
 				    }
 				},
+				on_animate : function(arg) {
+						animator(arg, 0, false);
+				},
             },
 			plane2 : {
 				type : "image",
@@ -94,6 +125,9 @@ var my_win = new elm.window({
 						init();
 				    }
 			    },
+				on_animate : function(arg) {
+						animator(arg, 1, false);
+				},
 		    },
 		    plane3 : {
 			    type : "image",
@@ -109,6 +143,9 @@ var my_win = new elm.window({
 					    myplane = my_win.elements.plane3;
 						init();
 				    }
+				},
+				on_animate : function(arg) {
+						animator(arg, 2, false);
 				},
 		    },
 			plane4 : {
@@ -127,6 +164,9 @@ var my_win = new elm.window({
 						init();
 				    }
 			    },
+				on_animate : function(arg) {
+						animator(arg, 3, false);
+				},
 		    },
 		},
 		on_keydown : function () {
