b83569ecf2b6df68757eed94479b9571c7e6805b
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-01-05 22:02:26 +0000

    welcome back to the infamous 23oz glass theme! some things are still not perfect yet, and there is no matching elm theme
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@66906 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ THEMES/23oz/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+edje_cc -id . -fd . default.edc -o 23oz.edj
+[[ $? != 0 ]] && exit 1
+case "$1" in
+    '-p')
+    edje_viewer 23oz.edj
+    ;;
+    '-i')
+    cp 23oz.edj ~/.e/e/themes/
+    ;;
+    '-r')
+    cp 23oz.edj ~/.e/e/themes/
+    enlightenment_remote -restart
+    ;;
+esac
