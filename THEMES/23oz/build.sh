19009c41ddd067ead7f09894340c09f942af93c7
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-01-06 02:20:13 +0000

    update build.sh
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@66910 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- THEMES/23oz/build.sh
+++ THEMES/23oz/build.sh
@@ -1,2 +1,15 @@
-#!/bin/sh
-edje_cc $@ -id . -fd . default.edc -o 23Oz_016_999_050_1.edj
+#!/bin/bash
+edje_cc -id . -fd . default.edc -o 23oz.edj
+[[ $? != 0 ]] && exit 1
+case "$1" in
+    '-p')
+    edje_viewer 23oz.edj
+    ;;
+    '-i')
+    cp 23oz.edj ~/.e/e/themes/
+    ;;
+    '-r')
+    cp 23oz.edj ~/.e/e/themes/
+    enlightenment_remote -restart
+    ;;
+esac
