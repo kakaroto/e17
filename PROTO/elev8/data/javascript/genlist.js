677316be85f9f52d25abd6df06e535f5792c2c59
sanjeev <sanjeev@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-01-06 07:23:18 +0000

    An improved version of genlist example.
    Signed-off-by: Sanjeev BA <iamsanjeev@gmail.com>
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@66929 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/data/javascript/genlist.js
+++ PROTO/elev8/data/javascript/genlist.js
@@ -7,89 +7,117 @@
 var EXPAND_BOTH = { x : 1.0, y : 1.0 };
 var FILL_BOTH = { x : -1.0, y : -1.0 };
 
-/*var button = {
-	type : "button",
-	label : "GenList Item",
-	//weight : { x : -1.0, y : -1.0 },
-	on_clicked : function(arg) {
-		print("clicked event on Button: 1 " + this.label);
-	},
+elm.datadir=elm.datadir + "/data/images/";
+
+var yellow = {
+	type : "icon",
+	prescale : 1,
+	image : elm.datadir + "yellow.png",
+};
+var orange = {
+	type : "icon",
+	prescale : 1,
+	image : elm.datadir + "orange.png",
+};
+var violet = {
+	type : "icon",
+	prescale : 1,
+	image : elm.datadir + "violet.png",
 };
 
-var text_get = function(arg) {
-	print("Calling text get function.");
-	return "Clone";
+function button_prepend() {
+    print("button_prepend");
+}
+function button_append() {
+    print("button_append");
+	for (var i = 0; i<10; i++)
+    	win.elements.box.elements.the_list.append(new ItemType1(i));
+}
+
+var ItemType1 = function(data)
+{
+	this.data = data;
+	this.type = "default";
 };
 
-var content_get = function(arg) {
-	print("Calling content get function.");
-	return button;
+ItemType1.prototype = {
+		on_selected : function() {
+			print("The member is " + this.type);
+		},
+		on_text : function(arg) {
+			print("Calling text get function." + this.data);
+			return this.data.toString();
+		},
+		on_content : function(arg) {
+			print("Calling content get function.");
+		},
+		on_state : function(arg) {
+			print("Calling state get function.");
+		},
 };
-
-var state_get = function(arg) {
-	print("Calling state get function.");
-};
-
-var delete_item = function(arg) {
-	print("Calling delete item function.");
-};*/
 
 var win = new elm.window({
-	type : "main",		
-	label : "Genlist Demo",
-	width : 480,
-	height : 800,
-	weight : EXPAND_BOTH,
-	align : FILL_BOTH,
+    type : "main",
+	label : "GenList Example",
+	width : 320,
+	height : 480,
 	elements : {
 		the_background : {
 			type : "background",
 			weight : EXPAND_BOTH,
-			align : FILL_BOTH,
-			resize : true,
+            align : FILL_BOTH,
+            resize : true,
 		},
-		the_box : {
-			type : "box",
-			weight : EXPAND_BOTH,
-			//resize : true,
-			elements : {
-			    scroll : {
-					type : "scroller",
-					weight : EXPAND_BOTH,
-					bounce : { x : false, y : true },
-					policy : { x : "off", y : "on" },
-					weight : EXPAND_BOTH,
-					resize : true,
-					content : {
-						type : "box",
-						weight : EXPAND_BOTH,
-						elements : {
-							the_list : {
-								type : "genlist",
-								weight : EXPAND_BOTH,
-							},
-						},
-						but_box : {
-							type : "box",
-							weight : EXPAND_BOTH,
-							horizontal : true,
-							resize : true,
-							elements : {
-								prepend : {
-									type : "button",
-									label : "Prepend",
-									weight : EXPAND_BOTH,
-								},
-								append : {
-									type : "button",
-									label : "Append",
-									weight : EXPAND_BOTH,
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	},
+        box : {
+            type : "box",
+            weight : EXPAND_BOTH,
+            align : FILL_BOTH,
+            resize : true,
+            elements : {
+                /*scroll : {
+                    type : "scroller",
+                    bounce : { x : false, y : true },
+                    policy : { x : "off", y : "on" },
+                    weight : EXPAND_BOTH,
+                    align : FILL_BOTH,
+                    resize : true,
+                    content : {
+                        type : "box",
+                        weight : EXPAND_BOTH,
+                        align : FILL_BOTH,
+                        elements : {*/
+                            the_list : {
+                                type : "genlist",
+                                weight : EXPAND_BOTH,
+                                align : FILL_BOTH,
+                            },
+                        /*},
+                    },
+                },*/
+                but_box : {
+                    type : "box",
+                    weight : EXPAND_BOTH,
+                    align : FILL_BOTH,
+                    resize : true,
+                    horizontal : true,
+                    elements : {
+                        prepend : {
+                            type : "button",
+                            label : "Prepend",
+                            weight : EXPAND_BOTH,
+                            icon : violet,
+                        },
+                        append : {
+                            type : "button",
+                            label : "Append",
+                            weight : EXPAND_BOTH,
+                            icon : violet,
+							on_clicked : button_append,
+                        },
+                    },
+                },
+            },
+        },
+    },
 });
+
