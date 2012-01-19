910de38b1ca943cb6a6efc1d1cbd3b56cffc9a2d
barbieri <barbieri@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-01-19 13:56:40 +0000

    python-ecore: fix test to have a single fd_handler per fd.
    newer code (likely epoll) spots that the fd is already being watched
    and fails, it was a stupid bug.
    
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@67342 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/python/python-ecore/tests/07-fd_handler.py
+++ BINDINGS/python/python-ecore/tests/07-fd_handler.py
@@ -4,21 +4,10 @@ import ecore
 import os
 
 
-def cb_read1(fd_handler, a, b):
+def cb_read(fd_handler, a, b):
     data = os.read(fd_handler.fd, 50)
-    print "ready for read1:", fd_handler, ", params: a=", a, ", b=", b
+    print "ready for read:", fd_handler, ", params: a=", a, ", b=", b
     return True
-
-
-def cb_read2(fd_handler, a, b):
-    print "ready for read2:", fd_handler, ", params: a=", a, ", b=", b
-    return True
-
-
-def cb_read3(fd_handler, a, b):
-    print "ready for read3:", fd_handler, ", params: a=", a, ", b=", b
-    return False
-
 
 def timer_write(wfd):
     print "write to fd:", wfd
@@ -26,29 +15,19 @@ def timer_write(wfd):
     return True
 
 rfd, wfd = os.pipe()
-fdh1 = ecore.fd_handler_add(rfd, ecore.ECORE_FD_READ, cb_read1, 123, b="xyz")
-fdh2 = ecore.FdHandler(rfd, ecore.ECORE_FD_READ, cb_read2, 456, b="www")
-fdh3 = ecore.FdHandler(rfd, ecore.ECORE_FD_READ, cb_read3, 789, b="stop next")
+fdh = ecore.fd_handler_add(rfd, ecore.ECORE_FD_READ, cb_read, 123, b="xyz")
 
 ecore.timer_add(0.2, timer_write, wfd)
 
-print "before: fdh1=", fdh1
-print "before: fdh2=", fdh2
-print "before: fdh3=", fdh3
+print "before: fdh=", fdh
 
 ecore.timer_add(1, ecore.main_loop_quit)
 ecore.main_loop_begin()
 print "main loop stopped"
 
-print "after: fdh1=", fdh1
-print "after: fdh2=", fdh2
-print "after: fdh3=", fdh3
+print "after: fdh=", fdh
 
-fdh1.delete()
-fdh2.delete()
-# fdh3 already deleted since it returned false
-del fdh1
-del fdh2
-del fdh3
+fdh.delete()
+del fdh
 
 ecore.shutdown()
