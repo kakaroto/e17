3ee679705de3e31d18a00670a7ed68c3d03ad98e
tasn <tasn@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-01-31 14:40:51 +0000

    Clouseau: Added a script wrapper launcher.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@67634 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ clouseau/src/scripts/gen_script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cat <<-ENDOFMESSAGE>clouseau
+#!/bin/sh
+if [ \$# -lt 1 ]
+then
+   echo "Usage: clouseau <executable> [executable parameters]"
+fi
+LD_PRELOAD="$1/libclouseau.so" \$*
+ENDOFMESSAGE
+
+chmod +x clouseau
