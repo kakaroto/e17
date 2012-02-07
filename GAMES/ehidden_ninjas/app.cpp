0d8340547a02e0c328db36d025ebbf69cf6c0581
hermet <hermet@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-02-07 15:19:27 +0000

    GAMES/ehidden_ninjas - registered name. ;p
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@67734 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ GAMES/ehidden_ninjas/app.cpp
@@ -0,0 +1,52 @@
+/*
+ * Enlightement Hidden Ninjas.
+ *
+ * Copyright 2012 Hermet (ChunEon Park)
+ *
+ * This application is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+#include <Eina.h>
+#include <Elementary.h>
+
+static Eina_Bool init_app()
+{
+   //Init window
+   Evas_Object *win = elm_win_add(NULL, "eHidden Ninjas", ELM_WIN_BASIC);
+   if (!win) return EINA_FALSE;
+   elm_win_title_set(win, "eHidden Ninjas");
+   evas_object_resize(win, 400, 400);
+   evas_object_show(win);
+
+   Evas *e = evas_object_evas_get(win);
+
+   //Set Background
+   Evas_Object *rect = evas_object_rectangle_add(e);
+   if (!rect) return EINA_FALSE;
+   evas_object_color_set(rect, 0, 0, 0, 255);
+   evas_object_resize(rect, 400, 400);
+   evas_object_show(rect);
+
+   return EINA_TRUE;
+}
+
+int main(int argc, char **argv)
+{
+   elm_init(argc, argv);
+   init_app();
+   elm_run();
+   elm_exit();
+   return 0;
+}
+
---
+++ GAMES/ehidden_ninjas/object.h
@@ -0,0 +1,15 @@
+
+#include <string>
+
+namespace eHiddenNinja
+{
+   class Object
+     {
+        virtual ~Object();
+
+      private:
+        std:: string name;
+        std:: string id;
+     };
+
+}
