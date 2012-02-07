54c6a45a7f9d8d0615be6125bc2fed6d28a47f29
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-02-07 01:21:15 +0000

    remove some nonsense in autogen logic as brought to my attention by Jérémy Zurcher <jeremy@asynk.ch>
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@67721 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/esskyuehl/autogen.sh
+++ PROTO/esskyuehl/autogen.sh
@@ -5,12 +5,10 @@ rm -rf "${bs_dir}"/autom4te.cache
 rm -f "${bs_dir}"/aclocal.m4 "${bs_dir}"/ltmain.sh
 
 echo 'Running autoreconf -if...'
+cd "${bs_dir}" &> /dev/null
 autoreconf -if || exit 1
 if test -z "$NOCONFIGURE" ; then
 	echo 'Configuring...'
-	cd "${bs_dir}" &> /dev/null
-	test "$?" = "0" || e=1
 	./configure $@
-	test "$cwd" != "$bs_dir" && cd "$cwd" &> /dev/null
-	test "$e" = "1" && exit 1
 fi
+test "$cwd" != "$bs_dir" && cd "$cwd" &> /dev/null
