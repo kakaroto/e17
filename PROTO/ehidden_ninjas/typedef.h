ae3aef4d807788868de1dd48cafe28762cfe681d
hermet <hermet@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-02-08 10:10:34 +0000

    ehidden_ninjas - moved from GAMES to PROTO
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@67753 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ PROTO/ehidden_ninjas/typedef.h
@@ -0,0 +1,21 @@
+#ifndef __TYPEDEF__H__
+#define __TYPEDEF__H__
+
+#define ID_BLOCK         1
+#define ID_BOMB          2
+
+
+typedef float VAR_TYPE;
+
+
+typename <T>
+struct Vector2
+{
+   T x;
+   T y;
+   vector2() x(0), y(0) {}
+}
+
+
+#endif
+
