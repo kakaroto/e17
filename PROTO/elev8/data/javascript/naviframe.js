61e08e0dfa74459ad1016384fbe1c0b3ec0f7c49
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-02-24 20:13:40 +0000

    elev8: Implement naviframe object.
    Included naviframe.js is adapted from pager.js.
    
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@68428 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ PROTO/elev8/data/javascript/naviframe.js
@@ -0,0 +1,128 @@
+#!/usr/local/bin/elev8
+
+elm.datadir=elm.datadir + "/data/images/";
+
+var green = {
+    type : "photo",
+    size : 80,
+	weight : EXPAND_BOTH,
+	align : FILL_BOTH,
+	image : elm.datadir + "green.png",
+	fill : true,
+};
+var yellow = {
+    type : "photo",
+    size : 80,
+	weight : EXPAND_BOTH,
+	align : FILL_BOTH,
+	image : elm.datadir + "yellow.png",
+	fill : true,
+};
+var orange = {
+    type : "photo",
+    size : 80,
+	weight : EXPAND_BOTH,
+	align : FILL_BOTH,
+	image : elm.datadir + "orange.png",
+	fill : true,
+};
+var maroon = {
+	type : "icon",
+	prescale : 1,
+	image : elm.datadir + "maroon.png",
+};
+var violet = {
+	type : "icon",
+	prescale : 1,
+	image : elm.datadir + "violet.png",
+};
+
+var EXPAND_BOTH = { x : 1.0, y : 1.0 };
+var FILL_BOTH = { x : -1.0, y : -1.0 };
+
+function button_pop() {
+    print("button_pop");
+    win.elements.box.elements.naviframe.pop();
+}
+function button_push() {
+    print("button_push");
+    var naviframe = win.elements.box.elements.naviframe;
+    
+    naviframe.push({
+        "Orange": orange,
+        "Yellow": yellow,
+        "Green": green
+    }[this.label] || orange, this.label);
+}
+
+var win = new elm.window({
+    type : "main",
+	label : "Naviframe Example",
+	width : 320,
+	height : 480,
+	elements : {
+		the_background : {
+			type : "background",
+			weight : EXPAND_BOTH,
+            align : FILL_BOTH,
+            resize : true,
+		},
+        box : {
+            type : "box",
+            weight : EXPAND_BOTH,
+            align : FILL_BOTH,
+            resize : true,
+            elements : {
+                naviframe : {
+                    type : "naviframe",
+                    weight : EXPAND_BOTH,
+                    align : FILL_BOTH,
+                    resize : true,
+                },
+                but_box : {
+                    type : "box",
+                    weight : EXPAND_BOTH,
+                    align : FILL_BOTH,
+                    resize : true,
+                    horizontal : true,
+                    elements : {
+                        pop : {
+                            type : "button",
+                            label : "Pop",
+                            weight : EXPAND_BOTH,
+                            on_clicked : button_pop,
+                            icon : maroon,
+                        },
+                    },
+                },
+                add_box : {
+                    type : "box",
+                    weight : EXPAND_BOTH,
+                    align : FILL_BOTH,
+                    resize : true,
+                    horizontal : true,
+                    elements : {
+                        push_orange : {
+                            type : "button",
+                            label : "Orange",
+                            weight : EXPAND_BOTH,
+                            on_clicked : button_push,
+                        },
+                        push_violet : {
+                            type : "button",
+                            label : "Yellow",
+                            weight : EXPAND_BOTH,
+                            on_clicked : button_push,
+                        },
+                        push_green : {
+                            type : "button",
+                            label : "Green",
+                            weight : EXPAND_BOTH,
+                            on_clicked : button_push,
+                        },
+                    },
+                },
+            },
+        },
+    },
+});
--- PROTO/elev8/src/modules/elm/elev8_elm.cc
+++ PROTO/elev8/src/modules/elm/elev8_elm.cc
@@ -5858,6 +5858,90 @@ CEvasObject::CPropHandler<CElmPager>::list[] = {
   { NULL, NULL, NULL },
 };
 
+class CElmNaviframe : public CEvasObject {
+protected:
+   CPropHandler<CElmNaviframe> prop_handler;
+
+public:
+   CElmNaviframe(CEvasObject *parent, Local<Object> obj) :
+       CEvasObject(),
+       prop_handler(property_list_base)
+     {
+        eo = elm_naviframe_add(parent->top_widget_get());
+        construct(eo, obj);
+        get_object()->Set(String::New("pop"), FunctionTemplate::New(pop)->GetFunction());
+        get_object()->Set(String::New("push"), FunctionTemplate::New(push)->GetFunction());
+     }
+
+   static Handle<Value> pop(const Arguments& args)
+     {
+        CEvasObject *self = eo_from_info(args.This());
+        CElmNaviframe *naviFrame = static_cast<CElmNaviframe *>(self);
+
+        if (elm_naviframe_top_item_get(naviFrame->get()))
+           elm_naviframe_item_pop(naviFrame->get());
+
+        return Undefined();
+     }
+
+   static Handle<Value> push(const Arguments& args)
+     {
+        CEvasObject *self = eo_from_info(args.This());
+        CElmNaviframe *naviFrame = static_cast<CElmNaviframe *>(self);
+        CEvasObject *prev_btn, *next_btn, *content;
+        bool has_prev_btn = false, has_next_btn = false;
+
+        if (!args[0]->IsObject())
+           return ThrowException(Exception::Error(String::New("Parameter 1 should be an object description")));
+
+        if (!args[1]->IsString())
+           return ThrowException(Exception::Error(String::New("Parameter 2 should be a string")));
+
+        if (args.Length() >= 3) {
+           if (!args[2]->IsObject())
+              return ThrowException(Exception::Error(String::New("Parameter 3 should either be undefined or an object description")));
+           has_prev_btn = true;
+        }
+          
+        if (args.Length() >= 4) {
+           if (!args[3]->IsObject())
+              return ThrowException(Exception::Error(String::New("Parameter 4 should either be undefined or an object description")));
+           has_next_btn = true;
+        }
+
+        content = realize_one(naviFrame, args[0]);
+        if (!content)
+           return ThrowException(Exception::Error(String::New("Could not create content from description")));
+
+        if (has_prev_btn)
+          {
+             prev_btn = realize_one(naviFrame, args[2]->ToObject());
+             if (!prev_btn)
+               return ThrowException(Exception::Error(String::New("Could not create back button from description")));
+          }
+        if (has_next_btn)
+          {
+             next_btn = realize_one(naviFrame, args[3]->ToObject());
+             if (!next_btn)
+               return ThrowException(Exception::Error(String::New("Could not create next button from description")));
+          }
+
+        String::Utf8Value titleParam(args[1]->ToString());
+        elm_naviframe_item_push(naviFrame->get(),
+                                *titleParam,
+                                has_prev_btn ? prev_btn->get() : 0,
+                                has_next_btn ? next_btn->get() : 0,
+                                content->get(),
+                                0);
+        return Undefined();
+     }
+};
+
+template<> CEvasObject::CPropHandler<CElmNaviframe>::property_list
+CEvasObject::CPropHandler<CElmNaviframe>::list[] = {
+  { NULL, NULL, NULL },
+};
+
 class CElmGrid : public CEvasObject {
 protected:
    CPropHandler<CElmGrid> prop_handler;
@@ -6241,6 +6325,8 @@ realize_one(CEvasObject *parent, Handle<Value> object_val)
      eo = new CElmNotify(parent,obj);
    else if (!strcmp(*str, "pager"))
      eo = new CElmPager(parent,obj);
+   else if (!strcmp(*str, "naviframe"))
+     eo = new CElmNaviframe(parent,obj);
    else if (!strcmp(*str, "grid"))
      eo = new CElmGrid(parent,obj);
 
