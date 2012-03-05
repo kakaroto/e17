51481beb7c9acf57b0e91623ead4b129ec647a56
tasn <tasn@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-03-05 20:04:43 +0000

    Ecrire: Fixed possible race.
    Po creation depends on having the pot file.
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@68734 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- ecrire/cmake/Modules/GettextTranslate.cmake
+++ ecrire/cmake/Modules/GettextTranslate.cmake
@@ -199,7 +199,7 @@ macro(GettextTranslate)
           ${PO_FILE_NAME} ${TEMPLATE_FILE_ABS} 
           -o ${PO_FILE_NAME}.new
         COMMAND mv ${PO_FILE_NAME}.new ${PO_FILE_NAME}
-        DEPENDS ${TEMPLATE_FILE_ABS}
+        DEPENDS ${MAKEVAR_DOMAIN}.pot-update
       )
 
     endif()
