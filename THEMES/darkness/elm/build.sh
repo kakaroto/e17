c85f9263c707e7789c4ac8d93e0cc3f31dd55467
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-03-07 20:32:21 +0000

    fix profiles for darkness finally
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@69027 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- THEMES/darkness/elm/build.sh
+++ THEMES/darkness/elm/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-edje_cc -id . -fd ../fonts  darkness-desktop.edc darkness-elm-desktop.edj
+edje_cc -id . -fd ../fonts  darkness-desktop.edc darkness-desktop.edj
 [[ $? != 0 ]] && exit 1
-edje_cc -id . -fd ../fonts  darkness.edc darkness-elm.edj
+edje_cc -id . -fd ../fonts  darkness.edc darkness.edj
 [[ $? != 0 ]] && exit 1
+cd config && ./build.sh $@
+cd ..
 case "$1" in
     '-p')
-    edje_viewer darkness-elm.edj
+    edje_viewer darkness.edj
     ;;
     '-i')
-    cp darkness-elm{,-desktop}.edj ~/.elementary/themes/
+    cp darkness{,-desktop}.edj ~/.elementary/themes/
     ;;
 esac
--- THEMES/darkness/elm/config/build.sh
+++ THEMES/darkness/elm/config/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-for x in darkness-{default,standard,illume}; do
+for x in darkness-{default,desktop,illume}; do
 	eet -e $x/base.cfg config $x/base.src 1
+	[[ $? != 0 ]] && exit 1
 done
 
-eet -i darkness-elm.cfg config profile.src 0
+eet -i darkness.cfg config profile.src 0
+[[ $? != 0 ]] && exit 1
+[[ "$1" != "-i" ]] && exit 0
+cp -r darkness-default darkness-illume darkness-desktop darkness.cfg ~/.elementary/config
+rm ~/.elementary/config/darkness-{default,illume,desktop}/base.src
