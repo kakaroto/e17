a7de7eb04e313a7b781b3ef0d937d03a5cfa08d8
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-03-15 19:26:01 +0000

    elev8: elm: cleanup CElmLayout
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@69394 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/modules/elm/CElmLayout.cc
+++ PROTO/elev8/src/modules/elm/CElmLayout.cc
@@ -1,86 +1,85 @@
 #include "CElmLayout.h"
 
-CElmLayout::CElmLayout(CEvasObject * parent, Local <Object> obj)
-  : CEvasObject()
-  , prop_handler(property_list_base)
+CElmLayout::CElmLayout(CEvasObject *parent, Local<Object> obj)
+   : CEvasObject()
+   , prop_handler(property_list_base)
 {
-    the_contents = Persistent <Object>::New(Object::New());
-    eo = elm_layout_add(parent->top_widget_get());
-    construct(eo, obj);
+   the_contents = Persistent <Object>::New(Object::New());
+   eo = elm_layout_add(parent->top_widget_get());
+   construct(eo, obj);
 }
 
-Handle <Value> CElmLayout::contents_get() const
+Handle<Value> CElmLayout::contents_get() const
 {
-    return the_contents;
+   HandleScope scope;
+
+   return scope.Close(the_contents);
 }
 
-void CElmLayout::contents_set(Handle <Value> val)
+void CElmLayout::contents_set(Handle<Value> val)
 {
-    if (val->IsObject()) {
-        Handle <Object> contents = val->ToObject();
-        Handle <Array> properties = contents->GetPropertyNames();
+   if (!val->IsObject())
+     return;
 
-        for (unsigned int i = 0; i <properties->Length(); i++) {
-            Handle <Value> element = properties->Get(Integer::New(i));
-
-            CEvasObject *child = make_or_get(this, contents->Get(element->ToString()));
-            if (!child)
-                continue;
-
-            String::Utf8Value elementName(element);
-            elm_object_part_content_set(eo, *elementName,
-                            child->get());
-
-            the_contents->Set(element, child->get_object());
-        }
-    }
+   Handle<Object> contents = val->ToObject();
+   Handle<Array> properties = contents->GetPropertyNames();
+   for (unsigned int i = 0; i <properties->Length(); i++)
+     {
+        Handle<Value> element = properties->Get(Integer::New(i));
+        CEvasObject *child = make_or_get(this, contents->Get(element->ToString()));
+        if (child)
+          {
+             elm_object_part_content_set(eo, *String::Utf8Value(element), child->get());
+             the_contents->Set(element, child->get_object());
+          }
+     }
 }
 
-Handle <Value> CElmLayout::file_get() const
+Handle<Value> CElmLayout::file_get() const
 {
-    // FIXME: implement
-    return Undefined();
+   // FIXME: implement
+   return Undefined();
 }
 
-void CElmLayout::file_set(Handle <Value> val)
+void CElmLayout::file_set(Handle<Value> val)
 {
-    if (val->IsObject()) {
-        Local <Object> obj = val->ToObject();
-        Local <Value> fileParam = obj->Get(String::New("name"));
-        Local <Value> groupParam = obj->Get(String::New("group"));
+   if (!val->IsObject())
+     return;
 
-        String::Utf8Value fileName(fileParam);
-        String::Utf8Value groupName(groupParam);
+   Local<Object> obj = val->ToObject();
+   Local<Value> fileParam = obj->Get(String::New("name"));
+   Local<Value> groupParam = obj->Get(String::New("group"));
 
-        elm_layout_file_set(eo, *fileName, *groupName);
-    }
+   elm_layout_file_set(eo,
+                       *String::Utf8Value(fileParam),
+                       *String::Utf8Value(groupParam));
 }
 
 Handle <Value> CElmLayout::theme_get() const
 {
-    // FIXME: implement
-    return Undefined();
+   // FIXME: implement
+   return Undefined();
 }
 
 void CElmLayout::theme_set(Handle <Value> val)
 {
-    if (val->IsObject()) {
-        Local <Object> obj = val->ToObject();
-        Local <Value> classParam = obj->Get(String::New("class"));
-        Local <Value> groupParam = obj->Get(String::New("group"));
-        Local <Value> styleParam = obj->Get(String::New("style"));
+   if (!val->IsObject())
+     return;
 
-        String::Utf8Value className(classParam);
-        String::Utf8Value groupName(groupParam);
-        String::Utf8Value styleName(styleParam);
+   Local<Object> obj = val->ToObject();
+   Local<Value> classParam = obj->Get(String::New("class"));
+   Local<Value> groupParam = obj->Get(String::New("group"));
+   Local<Value> styleParam = obj->Get(String::New("style"));
 
-        elm_layout_theme_set(eo, *className, *groupName, *styleName);
-    }
+   elm_layout_theme_set(eo,
+                        *String::Utf8Value(classParam),
+                        *String::Utf8Value(groupParam),
+                        *String::Utf8Value(styleParam));
 }
 
 PROPERTIES_OF(CElmLayout) = {
-    PROP_HANDLER(CElmLayout, file),
-    PROP_HANDLER(CElmLayout, theme),
-    PROP_HANDLER(CElmLayout, contents),
-    { NULL }
+   PROP_HANDLER(CElmLayout, file),
+   PROP_HANDLER(CElmLayout, theme),
+   PROP_HANDLER(CElmLayout, contents),
+   { NULL }
 };
--- PROTO/elev8/src/modules/elm/CElmLayout.h
+++ PROTO/elev8/src/modules/elm/CElmLayout.h
@@ -9,12 +9,15 @@ class CElmLayout : public CEvasObject {
 protected:
    Persistent<Object> the_contents;
    CPropHandler<CElmLayout> prop_handler;
-public:
+
    CElmLayout(CEvasObject *parent, Local<Object> obj);
+public:
    virtual Handle<Value> contents_get() const;
    virtual void contents_set(Handle<Value> val);
+
    virtual Handle<Value> file_get() const;
    virtual void file_set(Handle<Value> val);
+
    virtual Handle<Value> theme_get() const;
    virtual void theme_set(Handle<Value> val);
 };
