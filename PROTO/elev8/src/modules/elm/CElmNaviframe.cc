443dd9ef5262a38e76b823d2992db515e3dc3fd0
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-03-15 19:33:29 +0000

    elev8: elm: cleanup CElmNaviframe
    Patch by: Fabio Mello <fabio.mello@profusion.mobi>
    
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@69433 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/modules/elm/CElmNaviframe.cc
+++ PROTO/elev8/src/modules/elm/CElmNaviframe.cc
@@ -1,8 +1,8 @@
 #include "CElmNaviframe.h"
 
-CElmNaviframe::CElmNaviframe(CEvasObject *parent, Local<Object> obj) :
-   CEvasObject(),
-   prop_handler(property_list_base)
+CElmNaviframe::CElmNaviframe(CEvasObject *parent, Local<Object> obj)
+   : CEvasObject()
+   , prop_handler(property_list_base)
 {
    eo = elm_naviframe_add(parent->top_widget_get());
    construct(eo, obj);
@@ -12,8 +12,7 @@ CElmNaviframe::CElmNaviframe(CEvasObject *parent, Local<Object> obj) :
 
 Handle<Value> CElmNaviframe::pop(const Arguments& args)
 {
-   CEvasObject *self = eo_from_info(args.This());
-   CElmNaviframe *naviFrame = static_cast<CElmNaviframe *>(self);
+   CElmNaviframe *naviFrame = static_cast<CElmNaviframe *>(eo_from_info(args.This()));
 
    if (elm_naviframe_top_item_get(naviFrame->get()))
      elm_naviframe_item_pop(naviFrame->get());
@@ -23,10 +22,8 @@ Handle<Value> CElmNaviframe::pop(const Arguments& args)
 
 Handle<Value> CElmNaviframe::push(const Arguments& args)
 {
-   CEvasObject *self = eo_from_info(args.This());
-   CElmNaviframe *naviFrame = static_cast<CElmNaviframe *>(self);
+   CElmNaviframe *naviFrame = static_cast<CElmNaviframe *>(eo_from_info(args.This()));
    CEvasObject *prev_btn = NULL, *next_btn = NULL, *content;
-   bool has_prev_btn = false, has_next_btn = false;
 
    if (!args[0]->IsObject())
      return ThrowException(Exception::Error(String::New("Parameter 1 should be an object description or an elm.widget")));
@@ -35,39 +32,36 @@ Handle<Value> CElmNaviframe::push(const Arguments& args)
      return ThrowException(Exception::Error(String::New("Parameter 2 should be a string")));
 
    if (args.Length() >= 3) {
+
         if (!args[2]->IsObject())
           return ThrowException(Exception::Error(String::New("Parameter 3 should either be undefined or an object description")));
-        has_prev_btn = true;
+
+        prev_btn = make_or_get(naviFrame, args[2]->ToObject());
+
+        if (!prev_btn)
+          return ThrowException(Exception::Error(String::New("Could not create back button from description")));
    }
 
    if (args.Length() >= 4) {
+
         if (!args[3]->IsObject())
           return ThrowException(Exception::Error(String::New("Parameter 4 should either be undefined or an object description")));
-        has_next_btn = true;
+
+        next_btn = make_or_get(naviFrame, args[3]->ToObject());
+
+        if (!next_btn)
+          return ThrowException(Exception::Error(String::New("Could not create next button from description")));
    }
 
    content = make_or_get(naviFrame, args[0]);
    if (!content)
      return ThrowException(Exception::Error(String::New("Could not create content from description")));
 
-   if (has_prev_btn)
-     {
-        prev_btn = make_or_get(naviFrame, args[2]->ToObject());
-        if (!prev_btn)
-          return ThrowException(Exception::Error(String::New("Could not create back button from description")));
-     }
-   if (has_next_btn)
-     {
-        next_btn = make_or_get(naviFrame, args[3]->ToObject());
-        if (!next_btn)
-          return ThrowException(Exception::Error(String::New("Could not create next button from description")));
-     }
-
    String::Utf8Value titleParam(args[1]->ToString());
    elm_naviframe_item_push(naviFrame->get(),
                            *titleParam,
-                           has_prev_btn ? prev_btn->get() : 0,
-                           has_next_btn ? next_btn->get() : 0,
+                           prev_btn ? prev_btn->get() : 0,
+                           next_btn ? next_btn->get() : 0,
                            content->get(),
                            0);
    return Undefined();
