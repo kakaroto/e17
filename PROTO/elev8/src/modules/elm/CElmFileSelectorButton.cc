077be7c4590f650f5129695367687ee409e06ad6
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-03-15 19:30:00 +0000

    elev8: elm: cleanup CElmFileSelectorButton
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@69420 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/modules/elm/CElmFileSelectorButton.cc
+++ PROTO/elev8/src/modules/elm/CElmFileSelectorButton.cc
@@ -1,8 +1,8 @@
 #include "CElmFileSelectorButton.h"
 
-CElmFileSelectorButton::CElmFileSelectorButton(CEvasObject *parent, Local<Object> obj) :
-   CEvasObject(),
-   prop_handler(property_list_base)
+CElmFileSelectorButton::CElmFileSelectorButton(CEvasObject *parent, Local<Object> obj)
+   : CEvasObject()
+   , prop_handler(property_list_base)
 {
    eo = elm_fileselector_button_add (parent->top_widget_get());
    construct(eo, obj);
@@ -11,55 +11,51 @@ CElmFileSelectorButton::CElmFileSelectorButton(CEvasObject *parent, Local<Object
 Handle<Value> CElmFileSelectorButton::win_title_get() const
 {
    return String::New(elm_fileselector_button_window_title_get(eo));
-
 }
 
 void CElmFileSelectorButton::win_title_set(Handle<Value> val)
 {
    if (val->IsString() || val->IsNumber())
-     {
-        String::Utf8Value str(val);
-        elm_fileselector_button_window_title_set(eo, *str);
-     }
+     elm_fileselector_button_window_title_set(eo, *String::Utf8Value(val));
 }
 
 Handle<Value> CElmFileSelectorButton::path_get() const
 {
    return String::New(elm_fileselector_button_path_get(eo));
-
 }
 
 void CElmFileSelectorButton::path_set(Handle<Value> val)
 {
    if (val->IsString() || val->IsNumber())
-     {
-        String::Utf8Value str(val);
-        elm_fileselector_button_path_set(eo, *str);
-     }
+     elm_fileselector_button_path_set(eo, *String::Utf8Value(val));
 }
+
 void CElmFileSelectorButton::win_size_set(Handle<Value> val)
 {
+   HandleScope scope;
+
    if (!val->IsObject())
-     return ;
-   Evas_Coord width, height;
+     return;
+
    Local<Object> obj = val->ToObject();
    Local<Value> w = obj->Get(String::New("w"));
    Local<Value> h = obj->Get(String::New("h"));
-   if (!w->IsInt32() || !h->IsInt32())
-     return;
-   width = w->Int32Value();
-   height = h->Int32Value();
-   elm_fileselector_button_window_size_set (eo,  width, height);
+
+   if (w->IsInt32() && h->IsInt32())
+     elm_fileselector_button_window_size_set(eo, w->Int32Value(), h->Int32Value());
 }
 
 Handle<Value> CElmFileSelectorButton::win_size_get(void) const
 {
+   HandleScope scope;
    Evas_Coord w, h;
-   elm_fileselector_button_window_size_get  (eo,  &w, &h);
+
+   elm_fileselector_button_window_size_get(eo,  &w, &h);
    Local<Object> obj = Object::New();
    obj->Set(String::New("w"), Number::New(w));
    obj->Set(String::New("h"), Number::New(h));
-   return obj;
+
+   return scope.Close(obj);
 }
 
 Handle<Value> CElmFileSelectorButton::expandable_get() const
@@ -72,6 +68,7 @@ void CElmFileSelectorButton::expandable_set(Handle<Value> val)
    if (val->IsBoolean())
      elm_fileselector_button_expandable_set(eo, val->BooleanValue());
 }
+
 Handle<Value> CElmFileSelectorButton::folder_only_get() const
 {
    return Boolean::New(elm_fileselector_button_folder_only_get(eo));
@@ -82,6 +79,7 @@ void CElmFileSelectorButton::folder_only_set(Handle<Value> val)
    if (val->IsBoolean())
      elm_fileselector_button_folder_only_set(eo, val->BooleanValue());
 }
+
 Handle<Value> CElmFileSelectorButton::is_save_get() const
 {
    return Boolean::New(elm_fileselector_button_is_save_get (eo));
@@ -106,24 +104,25 @@ void CElmFileSelectorButton::inwin_mode_set(Handle<Value> val)
 
 void CElmFileSelectorButton::on_click(void *event_info)
 {
-   Handle<Object> obj = get_object();
    HandleScope handle_scope;
+   Handle<Object> obj = get_object();
    Handle<Value> val = on_clicked_val;
 
-   // also provide x and y positions where it was clicked
-   //
-   if (event_info!=NULL)
-     {
-        Handle<String> path = String::New((const char *)event_info);
-        Handle<Value> args[2] = { obj, path };
-        assert(val->IsFunction());
-        Handle<Function> fn(Function::Cast(*val));
-        fn->Call(obj, 2, args);
-     }
+   if (!event_info)
+     return;
+
+   // FIXME: Also provide click coordinates.
+   Handle<String> path = String::New((const char *)event_info);
+   Handle<Value> args[2] = { obj, path };
+   assert(val->IsFunction());
+   Handle<Function> fn(Function::Cast(*val));
+   fn->Call(obj, 2, args);
 }
+
 void CElmFileSelectorButton::on_clicked_set(Handle<Value> val)
 {
    on_clicked_val.Dispose();
+
    on_clicked_val = Persistent<Value>::New(val);
    if (val->IsFunction())
      evas_object_smart_callback_add(eo, "file,chosen", &eo_on_click, this);
@@ -137,12 +136,12 @@ Handle<Value> CElmFileSelectorButton::on_clicked_get(void) const
 }
 
 PROPERTIES_OF(CElmFileSelectorButton) = {
-     PROP_HANDLER(CElmFileSelectorButton, win_title),
-     PROP_HANDLER(CElmFileSelectorButton, win_size),
-     PROP_HANDLER(CElmFileSelectorButton, path),
-     PROP_HANDLER(CElmFileSelectorButton, expandable),
-     PROP_HANDLER(CElmFileSelectorButton, folder_only),
-     PROP_HANDLER(CElmFileSelectorButton, is_save),
-     PROP_HANDLER(CElmFileSelectorButton, inwin_mode),
-     { NULL }
+   PROP_HANDLER(CElmFileSelectorButton, win_title),
+   PROP_HANDLER(CElmFileSelectorButton, win_size),
+   PROP_HANDLER(CElmFileSelectorButton, path),
+   PROP_HANDLER(CElmFileSelectorButton, expandable),
+   PROP_HANDLER(CElmFileSelectorButton, folder_only),
+   PROP_HANDLER(CElmFileSelectorButton, is_save),
+   PROP_HANDLER(CElmFileSelectorButton, inwin_mode),
+   { NULL }
 };
--- PROTO/elev8/src/modules/elm/CElmFileSelectorButton.h
+++ PROTO/elev8/src/modules/elm/CElmFileSelectorButton.h
@@ -12,41 +12,31 @@ class CElmFileSelectorButton : public CEvasObject {
 protected:
    CPropHandler<CElmFileSelectorButton> prop_handler;
 
-public:
+   virtual void on_click(void *event_info);
    CElmFileSelectorButton(CEvasObject *parent, Local<Object> obj);
-
+public:
    virtual Handle<Value> win_title_get() const;
-
    virtual void win_title_set(Handle<Value> val);
 
    virtual Handle<Value> path_get() const;
-
    virtual void path_set(Handle<Value> val);
 
    virtual void win_size_set(Handle<Value> val);
-
    virtual Handle<Value> win_size_get(void) const;
 
    virtual Handle<Value> expandable_get() const;
-
    virtual void expandable_set(Handle<Value> val);
 
    virtual Handle<Value> folder_only_get() const;
-
    virtual void folder_only_set(Handle<Value> val);
 
    virtual Handle<Value> is_save_get() const;
-
    virtual void is_save_set(Handle<Value> val);
 
    virtual Handle<Value> inwin_mode_get() const;
-
    virtual void inwin_mode_set(Handle<Value> val);
 
-   virtual void on_click(void *event_info);
-
    virtual void on_clicked_set(Handle<Value> val);
-
    virtual Handle<Value> on_clicked_get(void) const;
 };
 #endif
