85573359e4468da2bb91e85e4b89f47b1d0844c3
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-03-15 19:33:08 +0000

    elev8: elm: cleanup CElmScroller
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@69431 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/modules/elm/CElmScroller.h
+++ PROTO/elev8/src/modules/elm/CElmScroller.h
@@ -9,14 +9,19 @@ class CElmScroller : public CEvasObject {
 private:
    CPropHandler<CElmScroller> prop_handler;
 
-public:
-   CElmScroller(CEvasObject *parent, Local<Object> obj);
-   virtual void bounce_set(Handle<Value> val);
-   virtual Handle<Value> bounce_get() const;
    static Elm_Scroller_Policy policy_from_string(Handle<Value> val);
    static Local<Value> string_from_policy(Elm_Scroller_Policy policy);
+
+   CElmScroller(CEvasObject *parent, Local<Object> obj);
+public:
+   virtual void bounce_set(Handle<Value> val);
+   virtual Handle<Value> bounce_get() const;
+
    virtual void policy_set(Handle<Value> val);
    virtual Handle<Value> policy_get() const;
+
+   virtual void content_set(Handle<Value> val);
+   virtual Handle<Value> content_get() const;
 };
 
 #endif // C_ELM_SCROLLER_H
