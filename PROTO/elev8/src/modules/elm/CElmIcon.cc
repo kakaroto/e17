cc1987ab032b33ef4331c91f53f684ec56d85b86
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-03-15 19:32:57 +0000

    elev8: CElmIcon: cleanup - formatting - do not set variables if not needed - reduce variable scope
    Patch by: Lucas De Marchi <lucas.demarchi@profusion.mobi>
    
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@69429 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/modules/elm/CElmIcon.cc
+++ PROTO/elev8/src/modules/elm/CElmIcon.cc
@@ -8,51 +8,52 @@ CElmIcon::CElmIcon(CEvasObject * parent, Local <Object> obj)
     construct(eo, obj);
 }
 
-void CElmIcon::resizable_up_set(Handle <Value> val)
+void CElmIcon::resizable_up_set(Handle<Value> val)
 {
-    Eina_Bool up, down;
-    if (val->IsBoolean()) {
-        elm_icon_resizable_get(eo, &up, &down);
+   if (val->IsBoolean())
+     {
+        Eina_Bool down;
+        elm_icon_resizable_get(eo, NULL, &down);
         elm_icon_resizable_set(eo, val->BooleanValue(), down);
-    }
+     }
 }
 
-Handle <Value> CElmIcon::resizable_up_get() const
+Handle<Value> CElmIcon::resizable_up_get() const
 {
-    Eina_Bool up, down;
-    elm_icon_resizable_get(eo, &up, &down);
-    return Boolean::New(up);
+   Eina_Bool up;
+   elm_icon_resizable_get(eo, &up, NULL);
+   return Boolean::New(up);
 }
 
-void CElmIcon::resizable_down_set(Handle <Value> val)
+void CElmIcon::resizable_down_set(Handle<Value> val)
 {
-    Eina_Bool up, down;
-    if (val->IsBoolean()) {
-        elm_icon_resizable_get(eo, &up, &down);
+   if (val->IsBoolean())
+     {
+        Eina_Bool up;
+        elm_icon_resizable_get(eo, &up, NULL);
         elm_icon_resizable_set(eo, up, val->BooleanValue());
-    }
+     }
 }
 
-Handle <Value> CElmIcon::resizable_down_get() const
+Handle<Value> CElmIcon::resizable_down_get() const
 {
-    Eina_Bool up, down;
-    elm_icon_resizable_get(eo, &up, &down);
-    return Boolean::New(down);
+   Eina_Bool down;
+   elm_icon_resizable_get(eo, NULL, &down);
+   return Boolean::New(down);
 }
 
-Handle <Value> CElmIcon::prescale_get() const
+Handle<Value> CElmIcon::prescale_get() const
 {
-    int prescale = elm_icon_prescale_get(eo);
-    return Integer::New(prescale);
+   return Integer::New(elm_icon_prescale_get(eo));
 }
 
-void CElmIcon::prescale_set(Handle <Value> val)
+void CElmIcon::prescale_set(Handle<Value> val)
 {
-    if (val->IsNumber())
-        elm_icon_prescale_set(eo, val->IntegerValue());
+   if (val->IsNumber())
+     elm_icon_prescale_set(eo, val->IntegerValue());
 }
 
-void CElmIcon::image_set(Handle <Value> val)
+void CElmIcon::image_set(Handle<Value> val)
 {
    if (!val->IsString())
      return;
@@ -64,25 +65,27 @@ void CElmIcon::image_set(Handle <Value> val)
         elm_icon_file_set(eo, *str, NULL);
         return;
      }
-   else if (elm_icon_standard_set(eo, *str))
+
+   if (elm_icon_standard_set(eo, *str))
      return;
 
    ELM_ERR("warning: can't read standard or file icon %s", *str);
 }
 
-Handle <Value> CElmIcon::image_get(void) const
+Handle<Value> CElmIcon::image_get(void) const
 {
-    const char *file = NULL, *group = NULL;
-    elm_icon_file_get(eo, &file, &group);
-    if (file)
-        return String::New(file);
-    else
-        return Null();
+   const char *file = NULL, *group = NULL;
+
+   elm_icon_file_get(eo, &file, &group);
+   if (file)
+     return String::New(file);
+
+   return Null();
 }
 
 PROPERTIES_OF(CElmIcon) = {
-     PROP_HANDLER(CElmIcon, resizable_up),
-     PROP_HANDLER(CElmIcon, resizable_down),
-     PROP_HANDLER(CElmIcon, prescale),
-     { NULL }
+   PROP_HANDLER(CElmIcon, resizable_up),
+   PROP_HANDLER(CElmIcon, resizable_down),
+   PROP_HANDLER(CElmIcon, prescale),
+   { NULL }
 };
