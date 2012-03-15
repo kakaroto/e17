f7aa84ffb5530335c961fc6b3d5a4356deea77d1
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-03-15 19:29:10 +0000

    elev8: elm: CElmBackground formatting
    Patch by: Fabio Mello <fabio.mello@profusion.mobi>
    
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@69414 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/modules/elm/CElmBackground.cc
+++ PROTO/elev8/src/modules/elm/CElmBackground.cc
@@ -1,79 +1,79 @@
 #include "CElmBackground.h"
 
-CElmBackground::CElmBackground(CEvasObject * parent, Local <Object> obj)
-    : CEvasObject()
-    , prop_handler(property_list_base)
+CElmBackground::CElmBackground(CEvasObject *parent, Local<Object> obj)
+   : CEvasObject()
+   , prop_handler(property_list_base)
 {
-    eo = elm_bg_add(parent->get());
-    construct(eo, obj);
+   eo = elm_bg_add(parent->get());
+   construct(eo, obj);
 }
 
-void CElmBackground::image_set(Handle <Value> val)
+void CElmBackground::image_set(Handle<Value> val)
 {
-    if (val->IsString()) {
-        String::Utf8Value str(val);
-        elm_bg_file_set(eo, *str, NULL);
-    }
+   if (val->IsString())
+      elm_bg_file_set(eo, *String::Utf8Value(val), NULL);
 }
 
-Handle <Value> CElmBackground::image_get(void) const
+Handle<Value> CElmBackground::image_get(void) const
 {
-    const char *file = NULL, *group = NULL;
-    elm_bg_file_get(eo, &file, &group);
-    if (file)
-        return String::New(file);
-    return Null();
+   const char *file = NULL;
+   elm_bg_file_get(eo, &file, NULL);
+
+   if (file)
+     return String::New(file);
+
+   return Null();
 }
 
-Handle <Value> CElmBackground::red_get() const
+Handle<Value> CElmBackground::red_get() const
 {
-    int r, g, b;
-    elm_bg_color_get(eo, &r, &g, &b);
-    return Number::New(r);
+   int r;
+   elm_bg_color_get(eo, &r, NULL, NULL);
+   return Number::New(r);
 }
 
-void CElmBackground::red_set(Handle <Value> val)
+void CElmBackground::red_set(Handle<Value> val)
 {
-    if (val->IsNumber()) {
-        int r, g, b;
-        elm_bg_color_get(eo, &r, &g, &b);
-        r = val->ToNumber()->Value();
-        elm_bg_color_set(eo, r, g, b);
-    }
+   if (!val->IsNumber())
+     return;
+
+   int g, b;
+   elm_bg_color_get(eo, NULL, &g, &b);
+   elm_bg_color_set(eo, val->ToNumber()->Value(), g, b);
 }
 
-Handle <Value> CElmBackground::green_get() const
+Handle<Value> CElmBackground::green_get() const
 {
-    int r, g, b;
-    elm_bg_color_get(eo, &r, &g, &b);
-    return Number::New(g);
+   int g;
+   elm_bg_color_get(eo, NULL, &g, NULL);
+   return Number::New(g);
 }
 
-void CElmBackground::green_set(Handle <Value> val)
+void CElmBackground::green_set(Handle<Value> val)
 {
-    if (val->IsNumber()) {
-        int r, g, b;
-        elm_bg_color_get(eo, &r, &g, &b);
-        g = val->ToNumber()->Value();
-        elm_bg_color_set(eo, r, g, b);
-    }
+   if (!val->IsNumber())
+     return;
+
+   int r, b;
+   elm_bg_color_get(eo, &r, NULL, &b);
+   elm_bg_color_set(eo, r, val->ToNumber()->Value(), b);
 }
 
-Handle <Value> CElmBackground::blue_get() const
+Handle<Value> CElmBackground::blue_get() const
 {
-    int r, g, b;
-    elm_bg_color_get(eo, &r, &g, &b);
-    return Number::New(b);
+   int b;
+   elm_bg_color_get(eo, NULL, NULL, &b);
+   return Number::New(b);
 }
 
-void CElmBackground::blue_set(Handle <Value> val)
+void CElmBackground::blue_set(Handle<Value> val)
 {
-    if (val->IsNumber()) {
-        int r, g, b;
-        elm_bg_color_get(eo, &r, &g, &b);
-        b = val->ToNumber()->Value();
-        elm_bg_color_set(eo, r, g, b);
-    }
+   if (!val->IsNumber())
+     return;
+
+   int r, g;
+   elm_bg_color_get(eo, &r, &g, NULL);
+   elm_bg_color_set(eo, r, g, val->ToNumber()->Value());
 }
 
 PROPERTIES_OF(CElmBackground) = {
--- PROTO/elev8/src/modules/elm/CElmBackground.h
+++ PROTO/elev8/src/modules/elm/CElmBackground.h
@@ -6,16 +6,22 @@
 
 class CElmBackground : public CEvasObject {
    FACTORY(CElmBackground)
+
 protected:
    CPropHandler<CElmBackground> prop_handler;
+
 public:
    explicit CElmBackground(CEvasObject *parent, Local<Object> obj);
+
    virtual void image_set(Handle<Value> val);
    virtual Handle<Value> image_get(void) const;
+
    virtual Handle<Value> red_get() const;
    virtual void red_set(Handle<Value> val);
+
    virtual Handle<Value> green_get() const;
    virtual void green_set(Handle<Value> val);
+
    virtual Handle<Value> blue_get() const;
    virtual void blue_set(Handle<Value> val);
 };
