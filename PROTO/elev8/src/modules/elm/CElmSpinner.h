be387b156bee3f28cf563fa79f599e6f250acb6a
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-03-15 19:28:25 +0000

    elev8: elm: cleanup CElmSpinner
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@69404 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/modules/elm/CElmSpinner.h
+++ PROTO/elev8/src/modules/elm/CElmSpinner.h
@@ -14,18 +14,22 @@ public:
 
    virtual Handle<Value> label_format_get() const;
    virtual void label_format_set(Handle<Value> val);
+
    virtual Handle<Value> step_get() const;
    virtual void step_set(Handle<Value> val);
+
    virtual Handle<Value> min_get() const;
    virtual void min_set(Handle<Value> value);
+
    virtual Handle<Value> max_get() const;
    virtual void max_set(Handle<Value> value);
-   virtual Handle<Value> style_get() const;
-   virtual void style_set(Handle<Value> val);
+
    virtual Handle<Value> editable_get() const;
    virtual void editable_set(Handle<Value> val);
+
    virtual Handle<Value> disabled_get() const;
    virtual void disabled_set(Handle<Value> val);
+
    virtual Handle<Value> special_value_get() const;
    virtual void special_value_set(Handle<Value> val);
 };
