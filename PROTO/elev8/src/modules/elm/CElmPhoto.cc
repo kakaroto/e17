73b53c4a34b0bf27bdff3890b0ca0d2176270429
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-03-15 19:33:36 +0000

    elev8: elm: cleanup CElmPhoto
    Patch by: Fabio Mello <fabio.mello@profusion.mobi>
    
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@69434 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/modules/elm/CElmPhoto.cc
+++ PROTO/elev8/src/modules/elm/CElmPhoto.cc
@@ -1,62 +1,59 @@
 #include "CElmPhoto.h"
 
 CElmPhoto::CElmPhoto(CEvasObject * parent, Local <Object> obj)
-    : CEvasObject()
-    , prop_handler(property_list_base)
+   : CEvasObject()
+   , prop_handler(property_list_base)
 {
-    eo = elm_photo_add(parent->top_widget_get());
-    construct(eo, obj);
+   eo = elm_photo_add(parent->top_widget_get());
+   construct(eo, obj);
 }
 
 Handle <Value> CElmPhoto::image_get() const
 {
-    //No getter available
-    return Undefined();
+   //No getter available
+   return Undefined();
 }
 
 void CElmPhoto::image_set(Handle <Value> val)
 {
-    if (val->IsString())
-        return;
+   if (val->IsString())
+     return;
 
-    String::Utf8Value str(val);
+   String::Utf8Value str(val);
 
-    if (0 > access(*str, R_OK))
-        ELM_ERR("warning: can't read image file %s", *str);
+   if (0 > access(*str, R_OK))
+     ELM_ERR("warning: can't read image file %s", *str);
 
-    Eina_Bool retval = elm_photo_file_set(eo, *str);
-    if (retval == EINA_FALSE)
-        ELM_ERR("Unable to set the image");
+   if (elm_photo_file_set(eo, *String::Utf8Value(val)))
+     ELM_ERR("Unable to set the image");
 }
 
 Handle <Value> CElmPhoto::size_get() const
 {
-    //No getter available
-    return Undefined();
+   //No getter available
+   return Undefined();
 }
 
 void CElmPhoto::size_set(Handle <Value> val)
 {
-    if (val->IsNumber()) {
-        int size = val->ToInt32()->Value();
-        elm_photo_size_set(eo, size);
-    }
+   if (val->IsNumber())
+     elm_photo_size_set(eo, val->ToInt32()->Value());
 }
 
 Handle <Value> CElmPhoto::fill_get() const
 {
-    //No getter available
-    return Undefined();
+   //No getter available
+   return Undefined();
 }
 
 void CElmPhoto::fill_set(Handle <Value> val)
 {
-    if (val->IsBoolean())
-        elm_photo_fill_inside_set(eo, val->BooleanValue());
+   if (val->IsBoolean())
+     elm_photo_fill_inside_set(eo, val->BooleanValue());
 }
 
 PROPERTIES_OF(CElmPhoto) = {
-    PROP_HANDLER(CElmPhoto, size),
-    PROP_HANDLER(CElmPhoto, fill),
-    { NULL }
+     PROP_HANDLER(CElmPhoto, size),
+     PROP_HANDLER(CElmPhoto, fill),
+     { NULL }
 };
