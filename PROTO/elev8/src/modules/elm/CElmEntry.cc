ba8976124be3e44c04ea97152a8fa5671e3f5be0
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-03-15 19:28:44 +0000

    elev8: elm: cleanup CElmEntry
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@69408 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/modules/elm/CElmEntry.cc
+++ PROTO/elev8/src/modules/elm/CElmEntry.cc
@@ -1,74 +1,73 @@
 #include "CElmEntry.h"
 
 CElmEntry::CElmEntry(CEvasObject * parent, Local <Object> obj)
-    : CEvasObject()
-    , prop_handler(property_list_base)
+   : CEvasObject()
+   , prop_handler(property_list_base)
 {
-    eo = elm_entry_add(parent->get());
-    construct(eo, obj);
+   eo = elm_entry_add(parent->get());
+   construct(eo, obj);
 }
 
 Handle <Value> CElmEntry::password_get() const
 {
-    return Boolean::New(elm_entry_password_get(eo));
+   return Boolean::New(elm_entry_password_get(eo));
 }
 
 void CElmEntry::password_set(Handle <Value> value)
 {
-    if (value->IsBoolean())
-        elm_entry_password_set(eo, value->BooleanValue());
+   if (value->IsBoolean())
+     elm_entry_password_set(eo, value->BooleanValue());
 }
 
 Handle <Value> CElmEntry::editable_get() const
 {
-    return Boolean::New(elm_entry_editable_get(eo));
+   return Boolean::New(elm_entry_editable_get(eo));
 }
 
 void CElmEntry::editable_set(Handle <Value> value)
 {
-    if (value->IsBoolean())
-        elm_entry_editable_set(eo, value->BooleanValue());
+   if (value->IsBoolean())
+     elm_entry_editable_set(eo, value->BooleanValue());
 }
 
 Handle <Value> CElmEntry::line_wrap_get() const
 {
-    return Integer::New(elm_entry_line_wrap_get(eo));
+   return Integer::New(elm_entry_line_wrap_get(eo));
 }
 
 void CElmEntry::line_wrap_set(Handle <Value> value)
 {
-    if (value->IsNumber())
-        elm_entry_line_wrap_set(eo, (Elm_Wrap_Type) value->Int32Value());
+   if (value->IsNumber())
+     elm_entry_line_wrap_set(eo, (Elm_Wrap_Type) value->Int32Value());
 }
 
 Handle <Value> CElmEntry::scrollable_get() const
 {
-    return Boolean::New(elm_entry_scrollable_get(eo));
+   return Boolean::New(elm_entry_scrollable_get(eo));
 }
 
 void CElmEntry::scrollable_set(Handle <Value> value)
 {
-    if (value->IsBoolean())
-        elm_entry_scrollable_set(eo, value->BooleanValue());
+   if (value->IsBoolean())
+     elm_entry_scrollable_set(eo, value->BooleanValue());
 }
 
 Handle <Value> CElmEntry::single_line_get() const
 {
-    return Integer::New(elm_entry_single_line_get(eo));
+   return Integer::New(elm_entry_single_line_get(eo));
 }
 
 void CElmEntry::single_line_set(Handle <Value> value)
 {
-    if (value->IsNumber())
-        elm_entry_single_line_set(eo, (Elm_Wrap_Type) value->Int32Value());
+   if (value->IsNumber())
+     elm_entry_single_line_set(eo, (Elm_Wrap_Type) value->Int32Value());
 }
 
 PROPERTIES_OF(CElmEntry) = {
-    PROP_HANDLER(CElmEntry, password),
-    PROP_HANDLER(CElmEntry, editable),
-    PROP_HANDLER(CElmEntry, line_wrap),
-    PROP_HANDLER(CElmEntry, scrollable),
-    PROP_HANDLER(CElmEntry, single_line),
-    { NULL }
+   PROP_HANDLER(CElmEntry, password),
+   PROP_HANDLER(CElmEntry, editable),
+   PROP_HANDLER(CElmEntry, line_wrap),
+   PROP_HANDLER(CElmEntry, scrollable),
+   PROP_HANDLER(CElmEntry, single_line),
+   { NULL }
 };
-
