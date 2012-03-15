4892830e47bd255514e7a7ef306508b55cbb51db
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-03-15 19:29:28 +0000

    elev8: elm: CElmCheck formatting
    Patch by: Fabio Mello <fabio.mello@profusion.mobi>
    
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@69417 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/modules/elm/CElmCheck.cc
+++ PROTO/elev8/src/modules/elm/CElmCheck.cc
@@ -1,40 +1,40 @@
 #include "CElmCheck.h"
 
-CElmCheck::CElmCheck(CEvasObject *parent, Local<Object> obj) :
-    CEvasObject(),
-    prop_handler(property_list_base)
+CElmCheck::CElmCheck(CEvasObject *parent, Local<Object> obj)
+   : CEvasObject()
+   , prop_handler(property_list_base)
 {
-    eo = elm_check_add(parent->get());
-    construct(eo, obj);
+   eo = elm_check_add(parent->get());
+   construct(eo, obj);
 }
 
 CElmCheck::~CElmCheck()
 {
-    the_icon.Dispose();
+   the_icon.Dispose();
 }
 
 void CElmCheck::state_set(Handle<Value> value)
 {
-    if (value->IsBoolean())
-        elm_check_state_set(eo, value->BooleanValue());
+   if (value->IsBoolean())
+     elm_check_state_set(eo, value->BooleanValue());
 }
 
 Handle<Value> CElmCheck::state_get() const
 {
-    return Boolean::New(elm_check_state_get(eo));
+   return Boolean::New(elm_check_state_get(eo));
 }
 
 Handle<Value> CElmCheck::icon_get() const
 {
-    return the_icon;
+   return the_icon;
 }
 
 void CElmCheck::icon_set(Handle<Value> value)
 {
-    the_icon.Dispose();
-    CEvasObject *icon = make_or_get(this, value);
-    elm_object_content_set(eo, icon->get());
-    the_icon = Persistent<Value>::New(icon->get_object());
+   the_icon.Dispose();
+   CEvasObject *icon = make_or_get(this, value);
+   elm_object_content_set(eo, icon->get());
+   the_icon = Persistent<Value>::New(icon->get_object());
 }
 
 PROPERTIES_OF(CElmCheck) = {
