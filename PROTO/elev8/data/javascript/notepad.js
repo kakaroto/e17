502eec5382f4119a75b0323325123a8962e9b419
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-04-11 20:46:43 +0000

    elev8: change notepad.js to new layout
    Patch by: Fabio Mello <fabio.mello@profusion.mobi>
    
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@70107 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/data/javascript/notepad.js
+++ PROTO/elev8/data/javascript/notepad.js
@@ -1,48 +1,45 @@
 #!/usr/local/bin/elev8
 
+var elm = require('elm');
+
 var EXPAND_BOTH = { x : 1.0, y : 1.0 };
 var FILL_BOTH = { x : -1.0, y : -1.0 };
 
 var entry_text =
             "Start typing...";
 
-var my_window = new elm.window({
-    type : "main",
-    label : "Entry",
+var my_window = elm.realise(elm.Window({
+    title : "Notepad",
     width : 320,
     height : 480,
     weight : EXPAND_BOTH,
     align : FILL_BOTH,
     elements : {
-        the_background : {
-            type : "background",
+        the_background : elm.Background ({
             weight : EXPAND_BOTH,
             align : FILL_BOTH,
             resize : true,
-        },
-        the_box1 : {
-            type : "box",
+        }),
+        the_box1 : elm.Box ({
             weight : EXPAND_BOTH,
             align : FILL_BOTH,
             resize : true,
             elements : {
-                the_entry : {
-                    type : "entry",
+                the_entry : elm.Entry ({
                     text : entry_text,
                     weight : EXPAND_BOTH,
                     align : FILL_BOTH,
                     line_wrap : 3,
                     editable : true,
-                },
-                icon_no_scale : {
-                    type : "button",
+                }),
+                icon_no_scale : elm.Button ({
                     label : "Clear",
                     weight : { x : -1.0, y : -1.0 },
-                    on_clicked : function(arg) {
+                    on_click : function(arg) {
                         my_window.elements.the_box1.elements.the_entry.text = "";
                     }
-                },
+                }),
             },
-        },
+        }),
     },
-});
+}));
