d56c5a039e6a21993d0cb549ac16bedfd0af44a2
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-05-04 22:26:24 +0000

    test_follow is cool now
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@70785 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/efx/src/bin/test_follow.c
+++ PROTO/efx/src/bin/test_follow.c
@@ -6,85 +6,143 @@
 #include <Ecore.h>
 #include <Ecore_Evas.h>
 
-static Eina_Bool _spin(void *data);
-static void _zoom(void *data __UNUSED__, Efx_Map_Data *e, Evas_Object *obj);
+#define RECT_SIZE 75
 
-static Evas_Object *r, *r2;
+static Eina_Bool _start2(void *r);
+static void _up(void *d __UNUSED__, Efx_Map_Data *e __UNUSED__, Evas_Object *obj);
 
-static void
-_zoom4(void *data __UNUSED__, Efx_Map_Data *e, Evas_Object *obj)
+static Evas_Object *
+rect_create(Evas *e)
 {
-   printf("starting zoom #4 from %g\n", e->zoom);
-   efx_zoom(obj, EFX_EFFECT_SPEED_SINUSOIDAL, e->zoom, 2.0, NULL, 3.0, _zoom, NULL);
-   _spin(obj);
-}
-
-static void
-_zoom3(void *data __UNUSED__, Efx_Map_Data *e, Evas_Object *obj)
-{
-   printf("starting zoom #3 from %g\n", e->zoom);
-   efx_zoom(obj, EFX_EFFECT_SPEED_ACCELERATE, e->zoom, 0.1, NULL, 3.0, _zoom4, NULL);
-}
-
-static void
-_zoom2(void *data __UNUSED__, Efx_Map_Data *e, Evas_Object *obj)
-{
-   printf("starting zoom #2 from %g\n", e->zoom);
-   efx_zoom(obj, EFX_EFFECT_SPEED_DECELERATE, e->zoom, 0.5, NULL, 3.0, _zoom3, NULL);
-}
-
-static void
-_zoom(void *data __UNUSED__, Efx_Map_Data *e, Evas_Object *obj)
-{
-   printf("starting zoom from %g\n", e ? e->zoom : 1.0);
-   efx_zoom(obj, EFX_EFFECT_SPEED_LINEAR, e ? e->zoom : 1.0, 1.5, NULL, 1.0, _zoom2, NULL);
+   Evas_Object *r;
+   r = evas_object_rectangle_add(e);
+   evas_object_resize(r, RECT_SIZE, RECT_SIZE);
+   evas_object_show(r);
+   return r;
 }
 
 static Eina_Bool
-_spin4(void *data)
+_vanish(void *t)
 {
-   printf("starting spin #4\n");
-   efx_spin_start(data, -200, NULL);
-   ecore_timer_add(5.0, _spin, data);
-   _zoom(NULL, NULL, data);
+   evas_object_del(t);
    return EINA_FALSE;
 }
 
 static Eina_Bool
-_spin3(void *data)
+_notice(void *r3)
 {
-   Evas_Coord px, py;
-   printf("starting spin #3\n");
-   evas_output_size_get(evas_object_evas_get(data), &px, &py);
-   efx_spin_start(data, 100, &(Evas_Point){px / 2, py / 2});
-   ecore_timer_add(5.0, _spin4, data);
+   Evas *e = evas_object_evas_get(r3);
+   Evas_Object *t, *r;
+   Evas_Coord x, y;
+   Eina_List *l;
+
+   l = efx_followers_get(r3);
+   r = l->data;
+   eina_list_free(l);
+
+   evas_object_geometry_get(r, &x, &y, NULL, NULL);
+
+   t = evas_object_textblock_add(e);
+   {
+      Evas_Textblock_Style *ts;
+      ts = evas_textblock_style_new();
+      evas_textblock_style_set(ts, "DEFAULT='font=Sans:style=Bold font_size=14 color=#000'"
+              "br='\n'"
+              "ps='ps'"
+              "tab='\t'");
+      evas_object_textblock_style_set(t, ts);
+   }
+   evas_object_textblock_text_markup_set(t, "Rotation center overridden by owner object");
+   evas_object_resize(t, 400, 20);
+   evas_object_move(t, x, y - 30 - 20);
+   evas_object_show(t);
+   efx_unfollow(r);
+   efx_follow(r, t);
+   efx_follow(r3, r);
+   ecore_timer_add(3.0, _vanish, t);
    return EINA_FALSE;
 }
 
-static Eina_Bool
-_spin2(void *data)
+static void
+_rotate4(void *d __UNUSED__, Efx_Map_Data *e __UNUSED__, Evas_Object *obj)
 {
-   efx_spin_stop(data);
-   printf("starting spin #2\n");
-   efx_spin_start(data, -5, NULL);
-   ecore_timer_add(5.0, _spin3, data);
-   return EINA_FALSE;
+   efx_rotate_reset(obj);
+   efx_rotate(obj, EFX_EFFECT_SPEED_DECELERATE, -360, NULL, 2.0, NULL, NULL);
+   ecore_timer_add(2.0, _notice, obj);
+   ecore_timer_add(5.0, _start2, obj);
+}
+
+static void
+_rotate3(void *d __UNUSED__, Efx_Map_Data *e __UNUSED__, Evas_Object *obj)
+{
+   efx_rotate(obj, EFX_EFFECT_SPEED_LINEAR, 360, NULL, 1.3, _rotate4, NULL);
+}
+
+static void
+_rotate2(void *d __UNUSED__, Efx_Map_Data *e __UNUSED__, Evas_Object *obj)
+{
+   efx_rotate(obj, EFX_EFFECT_SPEED_LINEAR, -360, NULL, 1.3, _rotate3, NULL);
+}
+
+static void
+_rotate(void *d __UNUSED__, Efx_Map_Data *e __UNUSED__, Evas_Object *obj)
+{
+   efx_rotate(obj, EFX_EFFECT_SPEED_ACCELERATE, 360, NULL, 2.0, _rotate2, NULL);
 }
 
 static Eina_Bool
-_spin(void *data)
+_start2(void *r)
 {
-   printf("starting spin\n");
-   efx_spin_start(data, 15, NULL);
-   ecore_timer_add(5.0, _spin2, data);
+   _rotate(NULL, NULL, r);
    return EINA_FALSE;
 }
 
-static Eina_Bool
-_start(void *data)
+static void
+_reset3(void *d __UNUSED__, Efx_Map_Data *e __UNUSED__, Evas_Object *obj)
 {
-   _zoom(NULL, NULL, data);
-   efx_follow(data, r2);
+   efx_move(obj, EFX_EFFECT_SPEED_LINEAR, &(Evas_Point){0, 400 - RECT_SIZE}, 1.0, _up, NULL);
+}
+
+static void
+_reset2(void *d __UNUSED__, Efx_Map_Data *e __UNUSED__, Evas_Object *obj)
+{
+   efx_rotate(obj, EFX_EFFECT_SPEED_LINEAR, 360, &(Evas_Point){200, 200}, 1.0, _reset3, NULL);
+}
+
+static void
+_reset(void *d __UNUSED__, Efx_Map_Data *e __UNUSED__, Evas_Object *obj)
+{
+   efx_move(obj, EFX_EFFECT_SPEED_LINEAR, &(Evas_Point){200, 400 - RECT_SIZE}, 1.0, _reset2, NULL);
+}
+
+static void
+_down2(void *d __UNUSED__, Efx_Map_Data *e __UNUSED__, Evas_Object *obj)
+{
+   efx_move(obj, EFX_EFFECT_SPEED_DECELERATE, &(Evas_Point){400, 400 - RECT_SIZE}, 2.0, _reset, NULL);
+}
+
+static void
+_up2(void *d __UNUSED__, Efx_Map_Data *e __UNUSED__, Evas_Object *obj)
+{
+   efx_move(obj, EFX_EFFECT_SPEED_LINEAR, &(Evas_Point){300, 0}, 1.3, _down2, NULL);
+}
+
+static void
+_down(void *d __UNUSED__, Efx_Map_Data *e __UNUSED__, Evas_Object *obj)
+{
+   efx_move(obj, EFX_EFFECT_SPEED_LINEAR, &(Evas_Point){200, 400 - RECT_SIZE}, 1.3, _up2, NULL);
+}
+
+static void
+_up(void *d __UNUSED__, Efx_Map_Data *e __UNUSED__, Evas_Object *obj)
+{
+   efx_move(obj, EFX_EFFECT_SPEED_ACCELERATE, &(Evas_Point){100, 0}, 2.0, _down, NULL);
+}
+
+static Eina_Bool
+_start(void *r)
+{
+   _up(NULL, NULL, r);
    return EINA_FALSE;
 }
 
@@ -99,31 +157,37 @@ main(void)
 {
    Ecore_Evas *ee;
    Evas *e;
+   Evas_Object *r, *r2, *r3;
 
    efx_init();
    ecore_evas_init();
    eina_log_domain_level_set("efx", EINA_LOG_LEVEL_DBG);
-   ee = ecore_evas_software_x11_new(NULL, 0, 0, 0, 450, 450);
+   ee = ecore_evas_software_x11_new(NULL, 0, 0, 0, 600, 400);
    ecore_evas_callback_delete_request_set(ee, _end);
+   ecore_evas_title_set(ee, "Follow");
    ecore_evas_show(ee);
    e = ecore_evas_get(ee);
    r = evas_object_rectangle_add(e);
-   evas_object_resize(r, 450, 450);
+   evas_object_resize(r, 600, 400);
    evas_object_show(r);
 
-   r = evas_object_rectangle_add(e);
+   r = rect_create(e);
    evas_object_color_set(r, 255, 0, 0, 255);
-   evas_object_resize(r, 150, 150);
-   evas_object_move(r, 25, 25);
-   evas_object_show(r);
+   evas_object_move(r, 0, 400 - RECT_SIZE);
 
-   r2 = evas_object_rectangle_add(e);
+   r2 = rect_create(e);
    evas_object_color_set(r2, 0, 0, 255, 255);
-   evas_object_resize(r2, 100, 100);
-   evas_object_move(r2, (450 - 10) - 100, (450 - 10) - 100);
-   evas_object_show(r2);
+   evas_object_move(r2, RECT_SIZE + 100, 400 - RECT_SIZE);
+
+   r3 = rect_create(e);
+   evas_object_color_set(r3, 0, 0, 0, 255);
+   evas_object_move(r3, 0, 0);
+
+   efx_follow(r, r2);
+   efx_follow(r3, r);
 
    ecore_timer_add(1.0, (Ecore_Task_Cb)_start, r);
+   ecore_timer_add(1.0, (Ecore_Task_Cb)_start2, r3);
    ecore_main_loop_begin();
    return 0;
 }
