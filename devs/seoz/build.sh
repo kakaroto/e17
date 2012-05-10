32d9f3dab1dee61d669d004dc0dea8628978403e
seoz <seoz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-05-10 02:13:04 +0000

    devs/seoz build.sh: Now my build script works with recent e. ccache was not the reason, it was MAKEFLAGS with 'make -j6'
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@70898 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/seoz/build.sh
+++ devs/seoz/build.sh
@@ -11,10 +11,9 @@
 set -e
 #set -x
 
-export PREFIX=/usr/local
 export CFLAGS="-O2 -march=native -ffast-math -g3 -W -Wall -Wextra" # -Wshadow"
-#export CC="ccache gcc"
-export MAKEFLAGS="make -j6"
+export CC="ccache gcc"
+#export MAKEFLAGS="make -j6"
 alias make='make -j6'
 
 export BUILD_E_DEPENDS="eeze"
