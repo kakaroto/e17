019142a0aae2ffbd05eb171b03add8dbdab195ee
tasn <tasn@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-05-14 14:38:59 +0000

    clouseau: Broke Obj_Information broke it to several structs this is done as preperation for transferring Obj_Information with eet desc.
    Signed-off-by: Aharon Hillel <a.hillel@partner.samsung.com>
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@71027 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- clouseau/src/lib/ui/obj_information.h
+++ clouseau/src/lib/ui/obj_information.h
@@ -2,69 +2,99 @@
 #define _OBJ_INFORMATION_H
 
 #include "../libclouseau.h"
+enum _en_obj_type
+{
+   CLOUSEAU_OBJ_TYPE_OTHER,
+   CLOUSEAU_OBJ_TYPE_ELM,
+   CLOUSEAU_OBJ_TYPE_TEXT,
+   CLOUSEAU_OBJ_TYPE_IMAGE,
+   CLOUSEAU_OBJ_TYPE_EDJE,
+   CLOUSEAU_OBJ_TYPE_TEXTBLOCK,
+};
+typedef enum _en_obj_type en_obj_type;
 
-typedef struct _Obj_Information Obj_Information;
+struct _st_evas_props
+{
+   const char *name;
+   const char *bt;
+   short layer;
+   Evas_Coord x, y, w, h;
+   double scale;
+   Evas_Coord min_w, min_h, max_w, max_h, req_w, req_h;
+   double align_x, align_y;
+   double weight_x, weight_y;
+   int r, g, b, a;
+   Eina_Bool pass_events;
+   Eina_Bool has_focus;
+   Eina_Bool is_clipper;
+   Eina_Bool is_visible;
+   Evas_Object_Pointer_Mode mode;
+};
+typedef struct _st_evas_props st_evas_props;
+
+struct _st_elm
+{
+   const char *type;
+   const char *style;
+   double scale;
+   Eina_Bool has_focus;
+   Eina_Bool is_disabled;
+   Eina_Bool is_mirrored;
+   Eina_Bool is_mirrored_automatic;
+};
+typedef struct _st_elm st_elm;
+
+struct _st_text
+{
+   const char *font;
+   int size;
+   const char *source;
+   const char *text;
+};
+typedef struct _st_text st_text;
+
+struct _st_image
+{
+   const char *file, *key;
+   void *source;
+   const char *load_err;
+};
+typedef struct _st_image st_image;
+
+struct _st_edje
+{
+   const char *file, *group;
+   const char *load_err;
+};
+typedef struct _st_edje st_edje;
+
+struct _st_textblock
+{
+   const char *style;
+};
+typedef struct _st_textblock st_textblock;
+
+union _un_extra_props
+{
+   st_elm elm;
+   st_text text;
+   st_image image;
+   st_edje edje;
+   st_textblock textblock;
+};
+typedef union _un_extra_props un_extra_props;
+
 struct _Obj_Information
 {
-   struct {
-        const char *name;
-        const char *bt;
-        short layer;
-        Evas_Coord x, y, w, h;
-        double scale;
-        Evas_Coord min_w, min_h, max_w, max_h, req_w, req_h;
-        double align_x, align_y;
-        double weight_x, weight_y;
-        int r, g, b, a;
-        Eina_Bool pass_events;
-        Eina_Bool has_focus;
-        Eina_Bool is_clipper;
-        Eina_Bool is_visible;
-        Evas_Object_Pointer_Mode mode;
-   } evas_props;
-
-   enum {
-        CLOUSEAU_OBJ_TYPE_OTHER,
-        CLOUSEAU_OBJ_TYPE_ELM,
-        CLOUSEAU_OBJ_TYPE_TEXT,
-        CLOUSEAU_OBJ_TYPE_IMAGE,
-        CLOUSEAU_OBJ_TYPE_EDJE,
-        CLOUSEAU_OBJ_TYPE_TEXTBLOCK,
-   } obj_type;
-
-   union {
-        struct {
-             const char *type;
-             const char *style;
-             double scale;
-             Eina_Bool has_focus;
-             Eina_Bool is_disabled;
-             Eina_Bool is_mirrored;
-             Eina_Bool is_mirrored_automatic;
-        } elm;
-        struct {
-             const char *font;
-             int size;
-             const char *source;
-             const char *text;
-        } text;
-        struct {
-             const char *file, *key;
-             void *source;
-             const char *load_err;
-        } image;
-        struct {
-             const char *file, *group;
-             const char *load_err;
-        } edje;
-        struct {
-             const char *style;
-        } textblock;
-   } extra_props;
+   st_evas_props evas_props;
+   en_obj_type obj_type;
+   un_extra_props extra_props;
 };
+typedef struct _Obj_Information Obj_Information;
+
+
 
 Evas_Object *clouseau_obj_information_list_add(Evas_Object *parent);
 void clouseau_obj_information_list_populate(Tree_Item *treeit);
 void clouseau_obj_information_list_clear();
-
 #endif
