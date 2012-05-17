48d0e854049157f1083e70ce27b1820140cd17fa
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-05-17 20:35:16 +0000

    remove edje ebuild...not sure why this was still here
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@71216 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- packaging/gentoo/media-libs/edje/files/50edje-gentoo.el
+++
@@ -1,5 +0,0 @@
-;;; edje site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(add-to-list 'auto-mode-alist '("\\.edc$" . edje-mode))
-(autoload 'edje-mode "edje-mode" "Edje source file syntax highlight and programming helpers")
