49f03768a4e258c4ce9278b2bd391e4a0d10b6db
seoz <seoz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-06-08 02:11:21 +0000

    devs/seoz build.sh: No c99. The build issue came from following line.    export MAKEFLAGS="make -j6" error:    http://slexy.org/view/s2oJkNemhV
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@71827 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/seoz/build.sh
+++ devs/seoz/build.sh
@@ -11,9 +11,8 @@
 set -e
 #set -x
 
-export CFLAGS="-O2 -march=native -ffast-math -g3 -W -Wall -Wextra -std=gnu99" # -Wshadow"
+export CFLAGS="-O2 -march=native -ffast-math -g3 -W -Wall -Wextra" # -Wshadow"
 export CC="ccache gcc"
-export MAKEFLAGS="make -j6"
 alias make='make -j6'
 
 export BUILD_E_DEPENDS="eeze"
