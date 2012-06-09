4af4e5bd5173f08310b8c5b6fcad27fbbf1d9820
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-06-09 10:03:19 +0000

    reset presentation after each run
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@71879 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/efx/src/bin/test_presentation.c
+++ PROTO/efx/src/bin/test_presentation.c
@@ -8,73 +8,23 @@
 
 static Evas *e;
 
-static Eina_Bool _start(Evas_Object *bg);
 static void _del(void *data __UNUSED__, Efx_Map_Data *emd __UNUSED__, Evas_Object *r);
-
-static Evas_Object *
-rect_create(void)
-{
-   Evas_Object *r;
-   r = evas_object_rectangle_add(e);
-   evas_object_resize(r, 72, 72);
-   evas_object_move(r, 25, 350);
-   evas_object_show(r);
-   return r;
-}
+static Evas_Object *_create(void);
+static Eina_Bool _start(Evas_Object *bg);
 
 static void
 _del(void *data __UNUSED__, Efx_Map_Data *emd __UNUSED__, Evas_Object *r)
 {
    evas_object_del(r);
-   ecore_timer_add(1.0, (Ecore_Task_Cb)_start, NULL);
+   r = _create();
+   ecore_timer_add(1.0, (Ecore_Task_Cb)_start, r);
 }
 
-static Eina_Bool
-_start(Evas_Object *bg)
+static Evas_Object *
+_create(void)
 {
-   Efx_Queue_Data *eqd;
-   eqd = efx_queue_append(bg, EFX_EFFECT_SPEED_SINUSOIDAL,
-     EFX_QUEUED_EFFECT(EFX_EFFECT_ROTATE(90, NULL)), 2.0, NULL, NULL);
-   efx_queue_effect_attach(eqd, EFX_EFFECT_SPEED_SINUSOIDAL,
-     EFX_QUEUED_EFFECT(EFX_EFFECT_PAN(100, -300)), 2.0, NULL, NULL);
-   eqd = efx_queue_append(bg, EFX_EFFECT_SPEED_DECELERATE,
-     EFX_QUEUED_EFFECT(EFX_EFFECT_ROTATE(-360, NULL)), 2.0, NULL, NULL);
-   efx_queue_effect_attach(eqd, EFX_EFFECT_SPEED_DECELERATE,
-     EFX_QUEUED_EFFECT(EFX_EFFECT_PAN(100, 100)), 2.0, NULL, NULL);
-   eqd = efx_queue_append(bg, EFX_EFFECT_SPEED_DECELERATE,
-     EFX_QUEUED_EFFECT(EFX_EFFECT_ROTATE(-90, NULL)), 2.0, NULL, NULL);
-   efx_queue_effect_attach(eqd, EFX_EFFECT_SPEED_DECELERATE,
-     EFX_QUEUED_EFFECT(EFX_EFFECT_PAN(200, 200)), 2.0, NULL, NULL);
-   efx_queue_effect_attach(eqd, EFX_EFFECT_SPEED_DECELERATE,
-     EFX_QUEUED_EFFECT(EFX_EFFECT_ZOOM(0, 1.0, NULL)), 2.0, NULL, NULL);
-   eqd = efx_queue_append(bg, EFX_EFFECT_SPEED_LINEAR,
-     EFX_QUEUED_EFFECT(EFX_EFFECT_PAN(-185, -200)), 2.0, NULL, NULL);
-   efx_queue_effect_attach(eqd, EFX_EFFECT_SPEED_LINEAR,
-     EFX_QUEUED_EFFECT(EFX_EFFECT_ZOOM(0, 0.5, NULL)), 2.0, NULL, NULL);
-   efx_queue_run(bg);
-   return EINA_FALSE;
-}
-
-static void
-_end(Ecore_Evas *ee __UNUSED__)
-{
-   ecore_main_loop_quit();
-}
-
-int
-main(void)
-{
-   Ecore_Evas *ee;
    Evas_Object *bg, *efl, *is, *cool, *img;
 
-   efx_init();
-   ecore_evas_init();
-   eina_log_domain_level_set("efx", EINA_LOG_LEVEL_DBG);
-   ee = ecore_evas_software_x11_new(NULL, 0, 0, 0, 450, 450);
-   ecore_evas_callback_delete_request_set(ee, _end);
-   ecore_evas_title_set(ee, "presentation");
-   ecore_evas_show(ee);
-   e = ecore_evas_get(ee);
    bg = evas_object_rectangle_add(e);
    evas_object_color_set(bg, 0, 0, 0, 255);
    evas_object_resize(bg, 4500, 4500);
@@ -116,7 +66,7 @@ main(void)
    if (evas_object_image_load_error_get(img))
      {
         fprintf(stderr, "Could not locate image!\n");
-        return -1;
+        exit(-1);
      }
    {
       int w, h;
@@ -135,6 +85,56 @@ main(void)
 
    efx_pan(bg, EFX_EFFECT_SPEED_DECELERATE, EFX_POINT(-215, 200), 0.0, NULL, NULL);
    efx_zoom(bg, EFX_EFFECT_SPEED_DECELERATE, 1.0, 2.0, EFX_POINT(300, 100), 0.0, NULL, NULL);
+   return bg;
+}
+
+static Eina_Bool
+_start(Evas_Object *bg)
+{
+   Efx_Queue_Data *eqd;
+   eqd = efx_queue_append(bg, EFX_EFFECT_SPEED_SINUSOIDAL,
+     EFX_QUEUED_EFFECT(EFX_EFFECT_ROTATE(90, NULL)), 2.0, NULL, NULL);
+   efx_queue_effect_attach(eqd, EFX_EFFECT_SPEED_SINUSOIDAL,
+     EFX_QUEUED_EFFECT(EFX_EFFECT_PAN(100, -300)), 2.0, NULL, NULL);
+   eqd = efx_queue_append(bg, EFX_EFFECT_SPEED_DECELERATE,
+     EFX_QUEUED_EFFECT(EFX_EFFECT_ROTATE(-360, NULL)), 2.0, NULL, NULL);
+   efx_queue_effect_attach(eqd, EFX_EFFECT_SPEED_DECELERATE,
+     EFX_QUEUED_EFFECT(EFX_EFFECT_PAN(100, 100)), 2.0, NULL, NULL);
+   eqd = efx_queue_append(bg, EFX_EFFECT_SPEED_DECELERATE,
+     EFX_QUEUED_EFFECT(EFX_EFFECT_ROTATE(-90, NULL)), 2.0, NULL, NULL);
+   efx_queue_effect_attach(eqd, EFX_EFFECT_SPEED_DECELERATE,
+     EFX_QUEUED_EFFECT(EFX_EFFECT_PAN(200, 200)), 2.0, NULL, NULL);
+   efx_queue_effect_attach(eqd, EFX_EFFECT_SPEED_DECELERATE,
+     EFX_QUEUED_EFFECT(EFX_EFFECT_ZOOM(0, 1.0, NULL)), 2.0, NULL, NULL);
+   eqd = efx_queue_append(bg, EFX_EFFECT_SPEED_LINEAR,
+     EFX_QUEUED_EFFECT(EFX_EFFECT_PAN(-185, -200)), 2.0, NULL, NULL);
+   efx_queue_effect_attach(eqd, EFX_EFFECT_SPEED_LINEAR,
+     EFX_QUEUED_EFFECT(EFX_EFFECT_ZOOM(0, 0.5, NULL)), 2.0, _del, NULL);
+   efx_queue_run(bg);
+   return EINA_FALSE;
+}
+
+static void
+_end(Ecore_Evas *ee __UNUSED__)
+{
+   ecore_main_loop_quit();
+}
+
+int
+main(void)
+{
+   Ecore_Evas *ee;
+   Evas_Object *bg;
+
+   efx_init();
+   ecore_evas_init();
+   eina_log_domain_level_set("efx", EINA_LOG_LEVEL_DBG);
+   ee = ecore_evas_software_x11_new(NULL, 0, 0, 0, 450, 450);
+   ecore_evas_callback_delete_request_set(ee, _end);
+   ecore_evas_title_set(ee, "presentation");
+   ecore_evas_show(ee);
+   e = ecore_evas_get(ee);
+   bg = _create();
 
    ecore_timer_add(1.0, (Ecore_Task_Cb)_start, bg);
    ecore_main_loop_begin();
