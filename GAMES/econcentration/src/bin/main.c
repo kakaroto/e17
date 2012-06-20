8e088583a517ae65b93b3881386cacf3caad7575
bdilly <bdilly@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-06-20 22:20:18 +0000

    Econcentration: Revert changes regarding mobile version
    Reverts commits 71946 and 72031.
    
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@72553 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- GAMES/econcentration/src/bin/main.c
+++ GAMES/econcentration/src/bin/main.c
@@ -23,10 +23,18 @@ typedef struct _Game
     int play_time, attempts, found, board_size, prev_board_size, total_size;
     Card cards[BOARD_SIZE_MAX];
     Card *first_card, *second_card;
-    Evas_Object *time_lb, *attempts_lb, *won_lb, *popup, *table;
+    Evas_Object *time_lb, *attempts_lb, *found_lb, *won_lb, *inwin, *table;
     Ecore_Timer *play_timer, *show_timer, *dl_pop;
     Eina_Bool running:1;
 } Game;
+
+static void
+_board_size_cb(void *data, Evas_Object *obj, void *event_info __UNUSED__)
+{
+    Game *game = data;
+    game->board_size = elm_spinner_value_get(obj) * 2 + 2;
+    game->total_size = game->board_size * game->board_size;
+}
 
 static Eina_Bool
 _play_timer_cb(void *data)
@@ -57,7 +65,7 @@ static void
 _won_ok(void *data, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
 {
     Game *game = data;
-    evas_object_hide(game->popup);
+    evas_object_hide(game->inwin);
 }
 
 static void
@@ -68,18 +76,12 @@ _player_win(Game *game)
     ecore_timer_del(game->play_timer);
     game->play_timer = NULL;
 
-    evas_object_show(game->popup);
-
-    if (game->play_time >= 0 && game->play_time <= 60)
-      snprintf(buf, sizeof(buf),
-               "Wow ! you're very fast.<br> I liked your ninja style.<br>");
-    else if (game->play_time > 60 && game->play_time <= 120)
-      snprintf(buf, sizeof(buf),
-               "Nice try, you won but try to be faster!<br>");
-    else
-      snprintf(buf, sizeof(buf),
-               "ZzzzZz. You won.<br>But you're slow like a turtle.<br>");
-
+    evas_object_show(game->inwin);
+    snprintf(buf, sizeof(buf),
+            "Congratulations, you solved it!<br>"
+            "You spent %i seconds and<br>"
+            "made %i attempts.",
+            game->play_time, game->attempts);
     elm_object_text_set(game->won_lb, buf);
 }
 
@@ -114,6 +116,8 @@ _fl_selected(void *data, Evas_Object *obj, void *event_info __UNUSED__)
 
     /* first and second cards match */
     game->found++;
+    snprintf(buf, sizeof(buf), "%i", game->found);
+    elm_object_text_set(game->found_lb, buf);
     if (game->found == (game->total_size / 2))
         _player_win(game);
     game->first_card = NULL;
@@ -151,7 +155,10 @@ static Eina_Bool
 _populate_table(void *data)
 {
     Game *game = data;
-    int values[] = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8};
+    int values[] = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8,
+        9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17,
+        18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 23, 24, 24, 25, 25, 26, 26,
+        27, 27, 28, 28, 29, 29, 30, 30, 31, 31};
     int i;
     char buf[1024];
     Evas_Object *ic;
@@ -166,10 +173,11 @@ _populate_table(void *data)
         game->cards[i].value = values[i];
 
         ic = elm_icon_add(game->cards[i].flip);
-        elm_object_scale_set(ic, 1);
+        elm_object_scale_set(ic, 0.5);
         snprintf(buf, sizeof(buf), "%s/images/icon_%02i.png",
                 PACKAGE_DATA_DIR, values[i]);
         elm_icon_file_set(ic, buf, NULL);
+        elm_icon_resizable_set(ic, 0, 0);
         evas_object_size_hint_weight_set(ic, EVAS_HINT_EXPAND,
                 EVAS_HINT_EXPAND);
         evas_object_size_hint_align_set(ic, 0.5, 0.5);
@@ -200,14 +208,9 @@ static void
 _prepopulate_table(Game *game)
 {
     Evas_Object *fl, *bt, *table;
-    char buf[1024];
-    Evas_Object *ic;
     int i;
 
     table = game->table;
-
-    snprintf(buf, sizeof(buf), "%s/images/cards.png",
-             PACKAGE_DATA_DIR);
 
     for (i = 0; i < game->total_size; i++)
     {
@@ -220,23 +223,12 @@ _prepopulate_table(Game *game)
         evas_object_show(fl);
         game->cards[i].flip = fl;
 
-
         bt = elm_button_add(table);
         evas_object_size_hint_align_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
         evas_object_size_hint_weight_set(bt, EVAS_HINT_EXPAND,
                 EVAS_HINT_EXPAND);
         elm_object_part_content_set(fl, "front", bt);
         evas_object_show(bt);
-
-
-        ic = elm_icon_add(bt);
-        elm_object_scale_set(ic, 1);
-        elm_icon_file_set(ic, buf, NULL);
-        evas_object_size_hint_weight_set(ic, EVAS_HINT_EXPAND,
-                                         EVAS_HINT_EXPAND);
-        evas_object_size_hint_align_set(ic, EVAS_HINT_FILL, EVAS_HINT_FILL);
-        elm_object_part_content_set(bt, NULL, ic);
-        evas_object_show(ic);
 
         evas_object_data_set(bt, "card", &(game->cards[i]));
         evas_object_smart_callback_add(bt, "clicked", _fl_selected, game);
@@ -257,6 +249,7 @@ _start_game_cb(void *data, Evas_Object *obj __UNUSED__, void *event_info __UNUSE
 
     elm_object_text_set(game->time_lb, "0 s");
     elm_object_text_set(game->attempts_lb, "0");
+    elm_object_text_set(game->found_lb, "0");
 
     if (game->play_timer)
         ecore_timer_del(game->play_timer);
@@ -292,16 +285,12 @@ _win_delete_request(void *data, Evas_Object *obj, void *event_info __UNUSED__)
 static Eina_Bool
 _win_new(Game *game)
 {
-    Evas_Object *win, *popup, *bg, *bx, *bxctl, *table, *bt, *sp, *fr, *lb,
-                *boxLabels;
+    Evas_Object *win, *inwin, *bg, *bx, *bxctl, *table, *bt, *sp, *fr, *lb;
 
     win = elm_win_add(NULL, PACKAGE_NAME, ELM_WIN_BASIC);
     if (!win) return EINA_FALSE;
     evas_object_smart_callback_add(win, "delete,request",
             _win_delete_request, game);
-
-    elm_win_fullscreen_set(win, EINA_TRUE);
-    evas_object_move(win, 0, 0);
 
     bg = elm_bg_add(win);
     elm_win_resize_object_add(win, bg);
@@ -309,9 +298,9 @@ _win_new(Game *game)
     evas_object_show(bg);
 
     bx = elm_box_add(win);
+    elm_box_horizontal_set(bx, EINA_TRUE);
     evas_object_size_hint_weight_set(bx, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
     elm_win_resize_object_add(win, bx);
-    elm_box_padding_set(bx, 10, 10);
     evas_object_show(bx);
 
     game->table = table = elm_table_add(win);
@@ -325,81 +314,106 @@ _win_new(Game *game)
     bxctl = elm_box_add(win);
     evas_object_size_hint_align_set(bxctl, EVAS_HINT_FILL, 0);
     evas_object_show(bxctl);
-    elm_box_pack_start(bx, bxctl);
-    elm_box_horizontal_set(bxctl, EINA_TRUE);
+    elm_box_pack_end(bx, bxctl);
 
     bt = elm_button_add(win);
-    elm_object_text_set(bt, "Start");
+    elm_object_text_set(bt, "New game");
     evas_object_smart_callback_add(bt, "clicked", _start_game_cb, game);
     evas_object_size_hint_align_set(bt, EVAS_HINT_FILL, 0);
-    evas_object_size_hint_min_set(bt, 180, 88);
     evas_object_show(bt);
     elm_box_pack_end(bxctl, bt);
 
-    boxLabels = elm_box_add(win);
-    evas_object_size_hint_weight_set(boxLabels, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-    evas_object_size_hint_align_set(boxLabels, EVAS_HINT_FILL, EVAS_HINT_FILL);
-    evas_object_show(boxLabels);
-    elm_box_pack_end(bxctl, boxLabels);
-    elm_box_horizontal_set(boxLabels, EINA_TRUE);
+    bt = elm_button_add(win);
+    elm_object_text_set(bt, "Quit");
+    evas_object_smart_callback_add(bt, "clicked", _win_delete_request, game);
+    evas_object_size_hint_align_set(bt, EVAS_HINT_FILL, 0);
+    evas_object_show(bt);
+    elm_box_pack_end(bxctl, bt);
 
-    game->attempts_lb = lb = elm_label_add(win);
-    elm_object_text_set(lb, "0");
-    evas_object_size_hint_weight_set(lb, 0.0, 0.0);
-    evas_object_size_hint_align_set(lb, 1, 0.5);
-    evas_object_color_set(lb, 255, 255, 255, 255);
-    elm_box_pack_end(boxLabels, lb);
-    evas_object_show(lb);
+    fr = elm_frame_add(win);
+    evas_object_size_hint_weight_set(fr, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    evas_object_size_hint_align_set(fr, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    elm_object_text_set(fr, "Board size:");
+    elm_box_pack_end(bxctl, fr);
+    evas_object_show(fr);
 
-    lb = elm_label_add(win);
-    elm_object_text_set(lb, " attempts in ");
-    evas_object_size_hint_weight_set(lb, 0.0, 0.0);
-    evas_object_size_hint_align_set(lb, 1, 0.5);
-    evas_object_color_set(lb, 102, 102, 102, 255);
-    elm_box_pack_end(boxLabels, lb);
-    evas_object_show(lb);
+    sp = elm_spinner_add(win);
+    elm_spinner_min_max_set(sp, 1, 3);
+    elm_spinner_label_format_set(sp, "Board size: %.0f");
+    elm_spinner_step_set(sp, 1.0);
+    elm_spinner_editable_set(sp, EINA_FALSE);
+    elm_spinner_special_value_add(sp, 1, "Small");
+    elm_spinner_special_value_add(sp, 2, "Normal");
+    elm_spinner_special_value_add(sp, 3, "Big");
+    evas_object_smart_callback_add(sp, "changed", _board_size_cb, game);
+    evas_object_size_hint_align_set(sp, EVAS_HINT_FILL, 0);
+    elm_object_content_set(fr, sp);
+    evas_object_show(sp);
 
+    fr = elm_frame_add(win);
+    evas_object_size_hint_weight_set(fr, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    evas_object_size_hint_align_set(fr, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    elm_object_text_set(fr, "Time:");
+    elm_box_pack_end(bxctl, fr);
+    evas_object_show(fr);
 
     game->time_lb = lb = elm_label_add(win);
     elm_object_text_set(lb, "0 s");
     evas_object_size_hint_weight_set(lb, 0.0, 0.0);
     evas_object_size_hint_align_set(lb, 1, 0.5);
-    evas_object_color_set(lb, 255, 255, 255, 255);
-    elm_box_pack_end(boxLabels, lb);
+    elm_object_content_set(fr, lb);
     evas_object_show(lb);
 
-    bt = elm_button_add(win);
-    elm_object_text_set(bt, "Quit");
-    evas_object_smart_callback_add(bt, "clicked", _win_delete_request, game);
-    evas_object_size_hint_align_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
-    evas_object_size_hint_min_set(bt, 180, 88);
-    evas_object_show(bt);
-    elm_box_pack_end(bxctl, bt);
+    fr = elm_frame_add(win);
+    evas_object_size_hint_weight_set(fr, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    evas_object_size_hint_align_set(fr, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    elm_object_text_set(fr, "Attempts:");
+    elm_box_pack_end(bxctl, fr);
+    evas_object_show(fr);
 
-    game->popup = popup = elm_popup_add(win);
-    elm_object_part_text_set(popup, "title,text", "You won!");
-
-    bx = elm_box_add(popup);
-    evas_object_show(bx);
-    evas_object_size_hint_align_set(bx, EVAS_HINT_FILL, EVAS_HINT_FILL);
-
-    game->won_lb = lb = elm_label_add(popup);
+    game->attempts_lb = lb = elm_label_add(win);
+    elm_object_text_set(lb, "0");
     evas_object_size_hint_weight_set(lb, 0.0, 0.0);
-    evas_object_size_hint_align_set(lb, 0.5, 0.5);
-    evas_object_color_set(lb, 255, 255, 255, 255);
+    evas_object_size_hint_align_set(lb, 1, 0.5);
+    elm_object_content_set(fr, lb);
+    evas_object_show(lb);
+
+    fr = elm_frame_add(win);
+    evas_object_size_hint_weight_set(fr, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    evas_object_size_hint_align_set(fr, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    elm_object_text_set(fr, "Found:");
+    elm_box_pack_end(bxctl, fr);
+    evas_object_show(fr);
+
+    game->found_lb = lb = elm_label_add(win);
+    elm_object_text_set(lb, "0");
+    evas_object_size_hint_weight_set(lb, 0.0, 0.0);
+    evas_object_size_hint_align_set(lb, 1, 0.5);
+    elm_object_content_set(fr, lb);
+    evas_object_show(lb);
+
+    game->inwin = inwin = elm_win_inwin_add(win);
+    elm_object_style_set(inwin, "minimal");
+
+    bx = elm_box_add(win);
+    evas_object_show(bx);
+    elm_win_inwin_content_set(inwin, bx);
+
+    game->won_lb = lb = elm_label_add(win);
+    evas_object_size_hint_weight_set(lb, 0.0, 0.0);
+    evas_object_size_hint_align_set(lb, 0, 0);
     evas_object_show(lb);
     elm_box_pack_end(bx, lb);
 
-    bt = elm_button_add(popup);
+    bt = elm_button_add(win);
     elm_object_text_set(bt, "Yay!");
     evas_object_smart_callback_add(bt, "clicked", _won_ok, game);
-    evas_object_size_hint_weight_set(fr, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-    evas_object_size_hint_align_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    evas_object_size_hint_align_set(bt, 0.5, 0.5);
     evas_object_show(bt);
     elm_box_pack_end(bx, bt);
 
-    elm_object_content_set(popup, bx);
-
+    evas_object_resize(win, 480, 320);
+    evas_object_size_hint_min_set(win, 480, 320);
     elm_win_title_set(win, "Econcentration");
     evas_object_show(win);
 
