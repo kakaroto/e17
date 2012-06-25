f7aa022b7fe006a500f605ea177af5f021d153ff
thanatermesis <thanatermesis@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-06-25 15:02:21 +0000

    improved renamer script of skel emodule
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@72813 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/skel/rename.sh
+++ E-MODULES-EXTRA/skel/rename.sh
@@ -2,6 +2,9 @@
 
 # short name
 MODULE=my_module
+# used in extra parts of the code
+# suggestion: just caseup the first char of your $MODULE
+MODULE_UP=My_module
 # visible name
 MODULE_NAME=MyModule
 #your name
@@ -10,11 +13,13 @@ NAME2=Janetzek
 #email
 EMAIL=hannes.janetzek@gmail.com
 
-find . -type f -exec sed -i 's/Christopher Michael/'$NAME1' '$NAME2'/g' {} \;
-find . -type f -exec sed -i 's/devilhorns@comcast.net/'$EMAIL'/g' {} \;
-find . -type f -exec sed -i 's/skel/'$MODULE'/g' {} \;
-find . -type f -exec sed -i 's/Skeleton/'$MODULE_NAME'/g' {} \;
+
+find . -type f ! -name rename.sh -exec sed -i 's/Christopher Michael/'$NAME1' '$NAME2'/g' {} \;
+find . -type f ! -name rename.sh -exec sed -i 's/devilhorns@comcast.net/'$EMAIL'/g' {} \;
+find . -type f ! -name rename.sh -exec sed -i 's/Skeleton/'$MODULE_NAME'/g' {} \;
+find . -type f ! -name rename.sh -exec sed -i 's/Skel/'$MODULE_UP'/g' {} \;
+find . -type f ! -name rename.sh -exec sed -i 's/skel/'$MODULE'/g' {} \;
 
 mv e-module-skel.edc e-module-$MODULE.edc
 mv e_modules-skel.spec.in e_modules-$MODULE.spec.in
-rm po/skel.pot
+rm -f po/skel.pot
