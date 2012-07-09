0457605409cf9a452c21df0556df852621882a6f
hermet <hermet@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-07-09 08:51:29 +0000

    devs/hermet - updated
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@73491 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/hermet/update.sh
+++ devs/hermet/update.sh
@@ -6,7 +6,7 @@ svn update
 
 CFLAGS="-02 -g -W -Wall -Wextra -Wundef -Wshadow"
 
-for e in eina eet evas_generic_loaders evas ecore eeze efreet embryo edje e_dbus eio emotion PROTO/epdf ethumb elementary e expedite clouseau terminology
+for e in eina eet evas_generic_loaders evas ecore eeze efreet embryo edje e_dbus eio emotion PROTO/epdf ethumb elementary e expedite clouseau terminology ephoto
 do 
 	if [ -e $e ] && [ -d $e ]
 	then 
