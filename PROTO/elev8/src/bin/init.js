c91e7e2ce1cabff2620415d9a14bca631b27a4fd
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-07-12 18:53:27 +0000

    elev8: Remove useless callable() and contains() functions from Object prototypes
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@73623 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/src/bin/init.js
+++ PROTO/elev8/src/bin/init.js
@@ -8,26 +8,4 @@
  * Elementary support should be enabled by default.
  */
 elm = require('elm');
-
-/*
- * Allows to test if an object is a function:
- *    x = function() { return 42; }
- *    print(x.callable() ? "x is a function!" : "wtf?");
- */
-Object.prototype.callable = function() {
-  return typeof(this) === 'function';
-}
-
-/*
- * Allows checking if an object is inside an array:
- *   arr = [101,202,303,404,505];
- *   print(arr.contains(404) ? "arr contains 404!" : "404 not found");
- */
-Array.prototype.contains = function(obj) {
-  for (var i = this.length; i--;)
-    if (this[i] == obj)
-      return true;
-  return false;
-}
-
 localStorage = new localStorage();
