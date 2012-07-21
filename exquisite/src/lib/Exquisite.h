a0b997750fa139819ea42b33948fce066c9c4b56
caro <caro@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-07-21 17:41:30 +0000

    some clean and fixed here and there
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@74279 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- exquisite/src/lib/Exquisite.h
+++ exquisite/src/lib/Exquisite.h
@@ -41,6 +41,36 @@ tools :
 #include <Evas.h>
 #include <Edje.h>
 
+#ifdef EAPI
+# undef EAPI
+#endif
+
+#ifdef _WIN32
+# ifdef EFL_EXQUISITE_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif /* ! DLL_EXPORT */
+# else
+#  define EAPI __declspec(dllimport)
+# endif /* ! EFL_EINA_BUILD */
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#  else
+#   define EAPI
+#  endif
+# else
+/**
+ * @def EAPI
+ * @brief Used to export functions(by changing visibility).
+ */
+#  define EAPI
+# endif
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
