67a6b9aeafbbacd77b0803dac82f148bb4dd1823
sanjeev <sanjeev@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-08-03 05:38:47 +0000

    Fix vertical box example. Make same as horizontal box.
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@74815 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/data/javascript/box_vert.js
+++ PROTO/elev8/data/javascript/box_vert.js
@@ -3,9 +3,11 @@ elm = require('elm');
 var EXPAND_BOTH = { x: 1.0, y: 1.0 };
 var FILL_BOTH = { x: -1.0, y: -1.0 };
 
-var w = elm.Window({
-    title: "Horizontal Box example",
-    width: 320,
+var count = 0;
+
+var win = elm.realise(elm.Window({
+    title: "Vertical Box example",
+    width: 240,
     height: 480,
     elements: {
         background: elm.Background({
@@ -17,6 +19,17 @@ var w = elm.Window({
             horizontal: false,
             resize: true,
             elements: {
+                button: elm.Button({
+                    label: 'Pack',
+                    align: FILL_BOTH,
+                    on_click: function() {
+                      win.elements.box.elements['logo_' + count++] = elm.Icon({
+                        image: elm.datadir + "data/images/logo_small.png",
+                        resizable_up: false,
+                        resizable_down: false,
+                      })
+                    }
+                }),
                 logo_top: elm.Icon({
                     image: elm.datadir + "data/images/logo_small.png",
                     resizable_up: false,
@@ -38,6 +51,4 @@ var w = elm.Window({
             }
         })
     }
-});
-
-var e = elm.realise(w);
+}));
