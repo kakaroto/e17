322df89d06f1577106bcb3cd647a58f7d8ec1e87
stefan <stefan@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-08-09 16:19:36 +0000

    e_dbus: Adjust documentating and fix white spaces.
    Connman is not only for embedded devices. Adjust the doc for this. Also fix whitespaces while being
    here.
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@75056 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- e_dbus/src/lib/dbus/E_DBus.h
+++ e_dbus/src/lib/dbus/E_DBus.h
@@ -65,15 +65,14 @@
  * communications with Bleutooth devices.
  * @li @ref EConnman_Group Implementation of the <a
  * href="http://connman.net/">connman</a> specifications, which
- * manages internet connections within embedded devices running the
- * Linux operating system.
+ * manages internet connections running the Linux operating system.
  * @li @ref EHal_Group Implementation of the <a
  * href="http://www.freedesktop.org/wiki/Software/hal">HAL</a>
  * specifications, which is a (software) layer between the hardware
  * devices of a computer and the softwares that run on that
  * computer (Hardware Abstraction Layer). HAL is deprecated, in favor
  * of DeviceKit.
- * @li @ref ENotify_Group To de described. 
+ * @li @ref ENotify_Group To de described.
  * @li @ref EOfono_Group Implementation of the <a
  * href="http://ofono.org/">ofono</a> specifications, which is an
  * interface for mobile telephony applications.
@@ -99,10 +98,10 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-   
+
 #define E_DBUS_VERSION_MAJOR 1
 #define E_DBUS_VERSION_MINOR 6
-   
+
    typedef struct _E_DBus_Version
      {
         int major;
@@ -110,9 +109,9 @@ extern "C" {
         int micro;
         int revision;
      } E_DBus_Version;
-   
+
    EAPI extern E_DBus_Version *e_dbus_version;
-   
+
    EAPI extern int E_DBUS_DOMAIN_GLOBAL;
    EAPI extern int E_DBUS_EVENT_SIGNAL;
 
@@ -140,12 +139,11 @@ extern "C" {
    typedef struct E_DBus_Callback E_DBus_Callback;
 
 
-   
 /**
  * @brief Initialize e_dbus
  */
 EAPI int e_dbus_init(void);
-   
+
 /**
  * Shutdown e_dbus.
  */
@@ -153,7 +151,6 @@ EAPI int e_dbus_shutdown(void);
 
 /* setting up the connection */
 
-   
 /**
  * Retrieve a connection to the bus and integrate it with the ecore main loop.
  * @param type the type of bus to connect to, e.g. DBUS_BUS_SYSTEM or DBUS_BUS_SESSION
@@ -162,14 +159,13 @@ EAPI E_DBus_Connection *e_dbus_bus_get(DBusBusType type);
 
    EAPI void e_dbus_connection_ref(E_DBus_Connection *conn);
 
-   
 /**
  * Integrate a DBus connection with the ecore main loop
  *
  * @param conn - a dbus connection
  */
 EAPI E_DBus_Connection *e_dbus_connection_setup(DBusConnection *conn);
-   
+
 /**
  * Close out a connection retrieved with e_dbus_bus_get()
  * @param conn the connection to close
@@ -182,14 +178,14 @@ EAPI void e_dbus_connection_close(E_DBus_Connection *conn);
    EAPI void e_dbus_interface_unref(E_DBus_Interface *iface);
    EAPI void e_dbus_object_interface_attach(E_DBus_Object *obj, E_DBus_Interface *iface);
    EAPI void e_dbus_object_interface_detach(E_DBus_Object *obj, E_DBus_Interface *iface);
-   
+
 /**
  * Add a method to an object
  *
  * @param iface the E_DBus_Interface to which this method belongs
  * @param member the name of the method
  * @param signature  an optional message signature. if provided, then messages
- *                   with invalid signatures will be automatically rejected 
+ *                   with invalid signatures will be automatically rejected
  *                   (an Error response will be sent) and introspection data
  *                   will be available.
  *
@@ -197,7 +193,6 @@ EAPI void e_dbus_connection_close(E_DBus_Connection *conn);
  */
 EAPI int e_dbus_interface_method_add(E_DBus_Interface *iface, const char *member, const char *signature, const char *reply_signature, E_DBus_Method_Cb func);
 
-   
 /**
  * Add a signal to an object
  *
@@ -209,7 +204,6 @@ EAPI int e_dbus_interface_method_add(E_DBus_Interface *iface, const char *member
  */
 EAPI int e_dbus_interface_signal_add(E_DBus_Interface *iface, const char *name, const char *signature);
 
-   
 /**
  * Add a dbus object.
  *
@@ -219,46 +213,45 @@ EAPI int e_dbus_interface_signal_add(E_DBus_Interface *iface, const char *name, 
  *             e_dbus_object_data_get())
  */
 EAPI E_DBus_Object *e_dbus_object_add(E_DBus_Connection *conn, const char *object_path, void *data);
-   
+
 /**
  * Free a dbus object
  *
  * @param obj the object to free
  */
 EAPI void e_dbus_object_free(E_DBus_Object *obj);
-   
+
 /**
  * @brief Fetch the data pointer for a dbus object
  * @param obj the dbus object
  */
 EAPI void *e_dbus_object_data_get(E_DBus_Object *obj);
-   
+
 /**
  * @brief Get the dbus connection of a dbus object
  * @param obj the dbus object
  */
 EAPI E_DBus_Connection *e_dbus_object_conn_get(E_DBus_Object *obj);
-   
+
 /**
  * @brief Get the path of a dbus object
  * @param obj the dbus object
  */
 EAPI const char *e_dbus_object_path_get(E_DBus_Object *obj);
-   
+
 /**
  * @brief Get the interfaces of a dbus object
  * @param obj the dbus object
  */
 EAPI const Eina_List *e_dbus_object_interfaces_get(E_DBus_Object *obj);
 
-   
 /**
  * @brief Sets the callback to fetch properties from an object
  * @param obj the object
  * @param func the callback
  */
 EAPI void e_dbus_object_property_get_cb_set(E_DBus_Object *obj, E_DBus_Object_Property_Get_Cb func);
-   
+
 /**
  * @brief Sets the callback to set properties on an object
  * @param obj the object
@@ -269,8 +262,6 @@ EAPI void e_dbus_object_property_set_cb_set(E_DBus_Object *obj, E_DBus_Object_Pr
 
 /* sending method calls */
 
-
-   
 /**
  * @brief Send a DBus message with callbacks
  * @param conn The DBus connection
@@ -287,7 +278,6 @@ EAPI DBusPendingCall *e_dbus_message_send(E_DBus_Connection *conn, DBusMessage *
 
 /* signal receiving */
 
-   
 /**
  * Add a signal handler
  *
@@ -300,7 +290,7 @@ EAPI DBusPendingCall *e_dbus_message_send(E_DBus_Connection *conn, DBusMessage *
  * @param data custom data to pass in to the callback
  */
 EAPI E_DBus_Signal_Handler *e_dbus_signal_handler_add(E_DBus_Connection *conn, const char *sender, const char *path, const char *interface, const char *member, E_DBus_Signal_Cb cb_signal, void *data);
-   
+
 /**
  * Delete a signal handler
  *
@@ -336,7 +326,7 @@ EAPI void e_dbus_signal_handler_del(E_DBus_Connection *conn, E_DBus_Signal_Handl
 						      const void *data);
 
 /* standard methods calls on objects */
-   
+
 /**
  * Calls the Introspect method on a given bus and object path.
  * @param conn The dbus connection to use
@@ -348,7 +338,7 @@ EAPI void e_dbus_signal_handler_del(E_DBus_Connection *conn, E_DBus_Signal_Handl
  */
 EAPI DBusPendingCall *e_dbus_introspect(E_DBus_Connection *conn, const char *bus,
        const char *object_path, E_DBus_Method_Return_Cb cb_return, const void *data);
-   
+
 /**
  * Ping the dbus peer
  *
@@ -361,7 +351,7 @@ EAPI DBusPendingCall *e_dbus_introspect(E_DBus_Connection *conn, const char *bus
 EAPI DBusPendingCall *e_dbus_peer_ping(E_DBus_Connection *conn, const char *destination,
 					  const char *path, E_DBus_Method_Return_Cb cb_return,
 					  const void *data);
-   
+
 /**
  * Get the UUID of the peer
  *
@@ -379,7 +369,7 @@ EAPI DBusPendingCall *e_dbus_peer_get_machine_id(E_DBus_Connection *conn,
 						   const char *path, const char *interface,
 						   E_DBus_Method_Return_Cb cb_return,
 						   const void *data);
-   
+
 /**
  * Get the value of a property on an object
  *
@@ -396,7 +386,7 @@ EAPI DBusPendingCall *e_dbus_properties_get(E_DBus_Connection *conn, const char 
 					       const char *property,
 					       E_DBus_Method_Return_Cb cb_return,
 					       const void *data);
-   
+
 /**
  * Set the value of a property on an object
  *
@@ -417,7 +407,6 @@ EAPI DBusPendingCall *e_dbus_properties_set(E_DBus_Connection *conn, const char 
 					       const void *data);
 
 
-   
 /**
  * @brief Create a callback structure
  * @param cb_func the callback function
@@ -425,7 +414,6 @@ EAPI DBusPendingCall *e_dbus_properties_set(E_DBus_Connection *conn, const char 
  */
 EAPI E_DBus_Callback *e_dbus_callback_new(E_DBus_Callback_Func cb_func, E_DBus_Unmarshal_Func unmarshal_func, E_DBus_Free_Func free_func, void *user_data);
 
-   
 /**
  * @brief Free a callback structure
  * @param callback the callback to free
