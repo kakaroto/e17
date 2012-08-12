a1e2354f7b6e2ace85bfdae09fc924cc1a4300a6
thanatermesis <thanatermesis@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-08-12 19:34:41 +0000

    do not modify files matched in a possible .git directory, otherwise can broke the repository, some people could run this script inside an own git repository and mess things
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@75173 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- E-MODULES-EXTRA/skel/rename.sh
+++ E-MODULES-EXTRA/skel/rename.sh
@@ -14,11 +14,11 @@ NAME2=Janetzek
 EMAIL=hannes.janetzek@gmail.com
 
 
-find . -type f ! -name rename.sh -exec sed -i 's/Christopher Michael/'$NAME1' '$NAME2'/g' {} \;
-find . -type f ! -name rename.sh -exec sed -i 's/devilhorns@comcast.net/'$EMAIL'/g' {} \;
-find . -type f ! -name rename.sh -exec sed -i 's/Skeleton/'$MODULE_NAME'/g' {} \;
-find . -type f ! -name rename.sh -exec sed -i 's/Skel/'$MODULE_UP'/g' {} \;
-find . -type f ! -name rename.sh -exec sed -i 's/skel/'$MODULE'/g' {} \;
+find . -name .git -prune -o -type f ! -name rename.sh -exec sed -i 's/Christopher Michael/'$NAME1' '$NAME2'/g' {} \;
+find . -name .git -prune -o -type f ! -name rename.sh -exec sed -i 's/devilhorns@comcast.net/'$EMAIL'/g' {} \;
+find . -name .git -prune -o -type f ! -name rename.sh -exec sed -i 's/Skeleton/'$MODULE_NAME'/g' {} \;
+find . -name .git -prune -o -type f ! -name rename.sh -exec sed -i 's/Skel/'$MODULE_UP'/g' {} \;
+find . -name .git -prune -o -type f ! -name rename.sh -exec sed -i 's/skel/'$MODULE'/g' {} \;
 
 mv e-module-skel.edc e-module-$MODULE.edc
 mv e_modules-skel.spec.in e_modules-$MODULE.spec.in
