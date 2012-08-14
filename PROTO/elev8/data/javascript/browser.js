bc449c11a7805bafd81d34a723bc063126d580fd
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-08-14 19:04:47 +0000

    elev8: Add a toolbar to the web browser example
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@75264 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/data/javascript/browser.js
+++ PROTO/elev8/data/javascript/browser.js
@@ -1,18 +1,18 @@
 var elm = require('elm');
 
-var EXPAND_BOTH = { x : 1.0, y : 1.0 };
-var FILL_BOTH = { x : -1.0, y : -1.0 };
+var EXPAND_BOTH = { x: 1.0, y: 1.0 };
+var FILL_BOTH = { x: -1.0, y: -1.0 };
 
-var URL = "http://www.enlightenment.org/";
+var HOME_URL = "http://www.enlightenment.org/";
 
 var window = elm.realise(elm.Window({
   width: 800,
   height: 600,
   elements: {
     background: elm.Background({
-      weight : EXPAND_BOTH,
-      align : FILL_BOTH,
-      resize : true,
+      weight: EXPAND_BOTH,
+      align: FILL_BOTH,
+      resize: true,
     }),
     vbox: elm.Box({
       weight: EXPAND_BOTH,
@@ -20,9 +20,71 @@ var window = elm.realise(elm.Window({
       resize: true,
       horizontal: false,
       elements: {
-        address: elm.Entry({
-          align: {x: -1, y: 0},
-          single_line: true,
+        toolbar: elm.Toolbar({
+          align: FILL_BOTH,
+          select_mode: 'none',
+          elements: [
+            {
+              icon: 'go-previous',
+              label: 'Back',
+              priority: 500,
+              on_select: function() {
+                window.elements.vbox.elements.webpage.elements.web.back();
+              }
+            },
+            {
+              icon: 'go-next',
+              label: 'Forward',
+              priority: 100,
+              on_select: function() {
+                window.elements.vbox.elements.webpage.elements.web.forward();
+              }
+            },
+            {
+              icon: 'process-stop',
+              label: 'Stop',
+              states: {
+                'page-finished-loading': {
+                  icon: 'reload',
+                  label: 'Reload',
+                  on_select: function() {
+                    window.elements.vbox.elements.webpage.elements.web.reload();
+                  }
+                },
+                'page-is-loading': {
+                  icon: 'process-stop',
+                  label: 'Stop',
+                  on_select: function() {
+                    window.elements.vbox.elements.webpage.elements.web.stop();
+                  }
+                }
+              },
+              priority: 200
+            },
+            { separator: true },
+            {
+              element: elm.Entry({
+                align: FILL_BOTH,
+                weight: EXPAND_BOTH,
+                single_line: true,
+                scrollable: true,
+                hint_min: {width: 250, height: 30},
+                on_activate: function(text) {
+                  window.elements.vbox.elements.webpage.elements.web.uri = text;
+                }
+              }),
+              priority: 400
+            },
+            { separator: true },
+            {
+              icon: 'go-home',
+              label: 'Home',
+              priority: 50,
+              on_select: function() {
+                window.elements.vbox.elements.webpage.elements.web.uri = HOME_URL;
+              }
+            }
+          ]
         }),
         webpage: elm.Box({
           weight: EXPAND_BOTH,
@@ -31,36 +93,39 @@ var window = elm.realise(elm.Window({
           horizontal: true,
           elements: {
             web: elm.Web({
-              weight : EXPAND_BOTH,
-              align : FILL_BOTH,
-              resize : true,
-              history_enabled : false,
-              uri : URL
+              weight: EXPAND_BOTH,
+              align: FILL_BOTH,
+              resize: true,
+              history_enabled: false,
+              uri: HOME_URL,
+              on_title_change: function(title) {
+                window.title = title;
+              },
+              on_load_progress: function(load) {
+                if (load == 1) {
+                  window.elements.vbox.elements.toolbar.item_state_set(window.elements.vbox.elements.toolbar.elements[2], 'page-finished-loading');
+                  window.elements.progress.visible = false;
+                } else {
+                  window.elements.vbox.elements.toolbar.item_state_set(window.elements.vbox.elements.toolbar.elements[2], 'page-is-loading');
+                  window.elements.progress.visible = true;
+                  window.elements.progress.content.value = load;
+                }
+              },
+              on_uri_change: function(uri) {
+                window.elements.vbox.elements.toolbar.elements[4].element.text = uri;
+              }
             })
           }
         })
       }
+    }),
+    progress: elm.Notify({
+        content: elm.ProgressBar({ value: 0.0 }),
+        visible: false,
+        orient: 'bottom-right',
+        hint_min: { width: 200, height: 20 },
+        align: FILL_BOTH,
+        weight: EXPAND_BOTH
     })
   }
 }));
-
-var address = window.elements.vbox.elements.address;
-
-var web = window.elements.vbox.elements.webpage.elements.web;
-
-web.on_load_progress = function(load)
-{
-  var p = (load == 1) ? "100.00%" : " " + (100*load).toFixed(2) + "%";
-
-  print("Loading " + this.uri + ": " + p);
-}
-
-web.on_title_change = function(title)
-{
-  window.title = title;
-}
-
-address.on_activate = function(text)
-{
-  web.uri = text;
-}
