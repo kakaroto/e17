0c0ae34c583a0adf3c4eacc96075f6967033c675
mello <mello@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-08-24 16:23:11 +0000

    elev8: Rewrite elev8_demo use fs.listFiles and include missing apps in Makefile
    Author:    Daniel Franzolin <daniel@profusion.mobi>
    
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@75673 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elev8/data/javascript/elev8_demo.js
+++ PROTO/elev8/data/javascript/elev8_demo.js
@@ -1,79 +1,43 @@
-#!/usr/local/bin/elev8
+var elm = require('elm');
+var fs = require('fs');
+require('http');
+
+environment['ELEV8_MODPATH'] = elm.datadir + 'data/javascript/';
 
 var EXPAND_BOTH = { x : 1.0, y : 1.0 };
 var FILL_BOTH = { x : -1.0, y : -1.0 };
-var item_count = 0;
-
-function createItem() {
-  var list = win.elements.box.elements.list;
-  list.elements[item_count] = {
-    data: examples[item_count],
-    class: default_class,
-    on_select: function(item) {
-      item_selected = item;
-      require("./" + item.data[0] + ".js");
-    }
-  };
-  item_count++;
-}
 
 var default_class = {
    text: function(part) {
-      return this.data[1];
+      return this.data;
    },
 };
 
-var examples = new Array(
-    ["actionslider","Action Slider Example"],
-    ["airshow","A simple game."],
-    ["anim","Animator"],
-    ["bg","Background Example"],
-    ["box_horiz","Box Horizontal Example"],
-    ["box","Box Example"],
-    ["box_vert","Box Vertical Example"],
-    ["bubble","Bubble Example"],
-    ["button","Button Example"],
-    ["calculator","Calculator Example"],
-    ["calendar","Calendar Example"],
-    ["check","Check Example"],
-    ["clock","Clock Example"],
-    ["colorselector","Color Selector Example"],
-    ["conform","Conform Example"],
-    ["datetime","Datetime Example"],
-    ["dayselector","Day Selector Example"],
-    ["dilbert","Dilbert Example"],
-    ["entry","Entry Example"],
-    ["fileselectorbutton","File Selector button Example"],
-    ["fileselectorentry","File Selector Entry Example"],
-    ["fileselector","File Selector Example"],
-    ["flip","Flip Example"],
-    ["fs","Fs Example"],
-    ["genlist","Genlist Example"],
-    ["grid","Grid Example"],
-    ["http_test","Simple HTTP Example"],
-    ["inwin","Inwin Example"],
-    ["label","Label Example"],
-    ["naviframe","Naviframe Example"],
-    ["notepad","Notepad Example"],
-    ["notify","Notify Example"],
-    ["packing","Packing Example"],
-    ["panes","Panes Example"],
-    ["photocam","PhotoCam Example"],
-    ["photo","Photo Example"],
-    ["popup","Popup Example"],
-    ["progressbar","Progress Bar Example"],
-    ["radio","Radio Example"],
-    ["segment","Segment Example"],
-    ["simple-button","Simple  Button Example"],
-    ["slider","Slider Example"],
-    ["spinner","Spinner Example"],
-    ["stock","Stock Example"],
-    ["storage","Storage Example"],
-    ["table","Table Example"],
-    ["thumb","Thumb Example"],
-    ["timer","Timer Example"],
-    ["toolbar","Toolbar Example"]
-    );
+function listFilesCb(files, isDone, isError) {
+  for (i = 0; i < (files.length-1); i++)
+    {
+       var appName = files[i].path.substring(path.length,
+                                              files[i].path.length -3);
+       var list = win.elements.box.elements.list;
+       list.elements[i] = {
+         data: appName,
+         class: default_class,
+         on_select: function(item) {
+             item_selected = item;
+             require(item.data + ".js");
+         }
+       };
+    }
+}
+
+var path = environment['ELEV8_MODPATH'];
+var patterns = ["*.js"];
+var ls = fs.listFiles(path, listFilesCb,
+                      {recursive: false,
+                       period: 2,
+                       batch_size: -1,
+                       allow_hidden: false,
+                       filters: patterns});
 
 var win = elm.realise(elm.Window({
     title : "Elev8 Demo",
@@ -99,6 +63,3 @@ var win = elm.realise(elm.Window({
         })
     },
 }));
-
-for (i = 0; i < (examples.length-1); i++)
-    createItem();
