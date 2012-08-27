79d08352ded05164a77477d96b30543812f52337
cedric <cedric@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-08-27 08:59:26 +0000

    www: I will get it right somehow !
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@75731 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- web/www/p.php
+++ web/www/p.php
@@ -23,6 +23,16 @@ if (is_file("p/$page/$lang-rss"))
    echo '<link rel="alternate" type="application/rss+xml" title="RSS" href="rss.php?p=$page&l=$lang">';
 
  ?>
+<script type="text/javascript">
+/* <![CDATA[ */
+    (function() {
+        var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+        t.parentNode.insertBefore(s, t);
+    })();
+/* ]]> */</script>
 </head>
 
 <body>
