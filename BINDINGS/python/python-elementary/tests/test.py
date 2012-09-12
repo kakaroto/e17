7aad569f51e9eb02ecd496d168f36775e645fa91
davemds <davemds@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-09-12 21:59:23 +0000

    Use the new external elm test in python bindings also
    git-svn-id: http://svn.enlightenment.org/svn/e/trunk@76565 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- BINDINGS/python/python-elementary/tests/test.py
+++ BINDINGS/python/python-elementary/tests/test.py
@@ -23,7 +23,8 @@ from test_datetime import datetime_clicked
 from test_dayselector import dayselector_clicked
 from test_diskselector import diskselector_clicked
 from test_entry import entry_clicked, entry_scrolled_clicked
-from test_external import edje_external_clicked
+from test_external import edje_external_button_clicked, edje_external_pbar_clicked, \
+    edje_external_scroller_clicked, edje_external_slider_clicked, edje_external_video_clicked
 from test_fileselector import fileselector_clicked, fileselector_button_clicked, fileselector_entry_clicked
 from test_flip import flip_clicked, flip_interactive_clicked
 from test_flipselector import flipselector_clicked
@@ -110,8 +111,12 @@ items = [("3D", [
             ("Entry Scrolled", entry_scrolled_clicked),
             ("MultiButtonEntry", multibuttonentry_clicked),
         ]),
-         ("External Widget", [
-            ("Edje External", edje_external_clicked),
+         ("Edje External", [
+            ("Ext Button", edje_external_button_clicked),
+            ("Ext ProgressBar", edje_external_pbar_clicked),
+            ("Ext Scroller", edje_external_scroller_clicked),
+            ("Ext Slider", edje_external_slider_clicked),
+            ("Ext Video", edje_external_video_clicked),
         ]),
          ("Geographic", [
             ("Map", map_clicked),
--- BINDINGS/python/python-elementary/tests/test_external.py
+++ BINDINGS/python/python-elementary/tests/test_external.py
@@ -7,21 +7,124 @@ import ecore
 import evas
 
 
-#----- Edje External -{{{-
-def edje_external_clicked(obj):
-    win = elementary.Window("edje-external", elementary.ELM_WIN_BASIC)
-    win.title_set("Edje External test")
+#----- External Button -{{{-
+def edje_external_button_clicked(obj):
+    win = elementary.Window("edje-external-button", elementary.ELM_WIN_BASIC)
+    win.title_set("Edje External Button")
     win.autodel_set(True)
     if obj is None:
         win.callback_delete_request_add(lambda o: elementary.exit())
 
     ly = elementary.Layout(win)
-    ly.file_set("external.edj", "external_test")
+    ly.file_set("test_external.edj", "external/button")
     ly.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
     win.resize_object_add(ly)
     ly.show()
 
-    win.resize(480, 600)
+    win.resize(320, 400)
+    win.show()
+# }}}
+
+#----- External Progress Bar -{{{-
+def animate(ly):
+    val = ly.edje_get().part_external_object_get("ext_pbar1").value
+    val += 0.0123
+
+    for part_name in ["ext_pbar1", "ext_pbar3", "ext_pbar4", "ext_pbar6"]:
+        ly.edje_get().part_external_object_get(part_name).value = val
+
+    if val < 1:
+        ecore.timer_add(0.1, animate, ly)
+    else:
+        for part_name in ["ext_pbar2", "ext_pbar5", "ext_pbar7"]:
+            ly.edje_get().part_external_object_get(part_name).pulse(False)
+        for part_name in ["ext_button1", "ext_button2", "ext_button3"]:
+            ly.edje_get().part_external_object_get(part_name).disabled = False
+
+    return False
+
+def cb_btn3_clicked(bt, ly):
+    ly.edje_get().part_external_object_get("ext_pbar1").value = 0.0
+
+    for part_name in ["ext_pbar2", "ext_pbar5", "ext_pbar7"]:
+        ly.edje_get().part_external_object_get(part_name).pulse(True)
+    for part_name in ["ext_button1", "ext_button2", "ext_button3"]:
+        ly.edje_get().part_external_object_get(part_name).disabled = True
+
+    ecore.timer_add(0.1, animate, ly)
+
+def edje_external_pbar_clicked(obj):
+    win = elementary.Window("edje-external-pbar", elementary.ELM_WIN_BASIC)
+    win.title_set("Edje External Progress Bar")
+    win.autodel_set(True)
+    if obj is None:
+        win.callback_delete_request_add(lambda o: elementary.exit())
+
+    ly = elementary.Layout(win)
+    ly.file_set("test_external.edj", "external/pbar")
+    ly.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    win.resize_object_add(ly)
+    ly.show()
+
+    bt = ly.edje_get().part_external_object_get("ext_button3")
+    bt.text = "...or Pyton"
+    bt.callback_clicked_add(cb_btn3_clicked, ly)
+
+    win.resize(320, 400)
+    win.show()
+# }}}
+
+#----- External Scroller -{{{-
+def edje_external_scroller_clicked(obj):
+    win = elementary.Window("edje-external-scroller", elementary.ELM_WIN_BASIC)
+    win.title_set("Edje External Scroller")
+    win.autodel_set(True)
+    if obj is None:
+        win.callback_delete_request_add(lambda o: elementary.exit())
+
+    ly = elementary.Layout(win)
+    ly.file_set("test_external.edj", "external/scroller")
+    ly.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    win.resize_object_add(ly)
+    ly.show()
+
+    win.resize(320, 400)
+    win.show()
+# }}}
+
+#----- External Slider -{{{-
+def edje_external_slider_clicked(obj):
+    win = elementary.Window("edje-external-slider", elementary.ELM_WIN_BASIC)
+    win.title_set("Edje External Slider")
+    win.autodel_set(True)
+    if obj is None:
+        win.callback_delete_request_add(lambda o: elementary.exit())
+
+    ly = elementary.Layout(win)
+    ly.file_set("test_external.edj", "external/slider")
+    ly.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    win.resize_object_add(ly)
+    ly.show()
+
+    win.resize(320, 400)
+    win.show()
+# }}}
+
+#----- External Video -{{{-
+def edje_external_video_clicked(obj):
+    win = elementary.Window("edje-external-video", elementary.ELM_WIN_BASIC)
+    win.title_set("Edje External Video")
+    win.autodel_set(True)
+    if obj is None:
+        win.callback_delete_request_add(lambda o: elementary.exit())
+
+    ly = elementary.Layout(win)
+    ly.file_set("test_external.edj", "external/video")
+    ly.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    win.resize_object_add(ly)
+    ly.show()
+
+    win.resize(320, 400)
     win.show()
 # }}}
 
